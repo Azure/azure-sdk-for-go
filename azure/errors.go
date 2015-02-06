8ddaf37322fb3ba98b3a7dc7178cd4b4db7b8c09
James Nugent <james@jen20.com>
2015-02-06 15:06:48 -0500

    Improve error message for certificate path

--- azure/errors.go
+++ azure/errors.go
@@ -3,7 +3,7 @@ package azure
 const (
 	publishSettingsConfigurationError = "PublishSettingsFilePath is set. Consequently ManagementCertificatePath and SubscriptionId must not be set."
 
-	managementCertificateConfigurationError = "ManagementCertificatePath is set. Consequently SubscriptionId must also be set, and PublishSettingsFilePath must not be set."
+	managementCertificateConfigurationError = "Both ManagementCertificatePath and SubscriptionId should be set, and PublishSettingsFilePath must not be set."
 
 	paramNotSpecifiedError = "Parameter %s is not specified."
 )

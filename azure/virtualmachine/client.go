5e68a511c58b3a742aef4d2fcc991affc8b7c276
James Nugent <james@jen20.com>
2015-02-11 15:17:38 -0500

    Move NewUUID closer to usage and don't export

--- azure/virtualmachine/client.go
+++ azure/virtualmachine/client.go
@@ -580,7 +580,7 @@ func (self *VmClient) getVHDMediaLink(dnsName, location string) (string, error) 
 
 	if storageService == nil {
 
-		uuid, err := azure.NewUUID()
+		uuid, err := azure.newUUID()
 		if err != nil {
 			return "", err
 		}
@@ -787,6 +787,21 @@ func (self *VmClient) isInstanceSizeAvailableInLocation(location *locationclient
 	}
 
 	return false, nil
+}
+
+func newUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+
+	//return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+	return fmt.Sprintf("%x", uuid[10:]), nil
 }
 
 //Region private methods ends

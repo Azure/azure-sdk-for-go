b522fdb8d248455962c8d18b3a7ac2d0c632ff29
James Nugent <james@jen20.com>
2015-02-11 16:19:31 -0500

    Remove dead code

--- azure/virtualmachine/client.go
+++ azure/virtualmachine/client.go
@@ -781,6 +781,5 @@ func newUUID() (string, error) {
 	// version 4 (pseudo-random); see section 4.1.3
 	uuid[6] = uuid[6]&^0xf0 | 0x40
 
-	//return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
 	return fmt.Sprintf("%x", uuid[10:]), nil
 }

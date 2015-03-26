83ea78c145698bb27f5acfe42c40ac248c060936
Paul Meyer <paul.meyer@microsoft.com>
2015-03-26 10:45:15 -0700

    Expose GetOperationStatus

--- management/operations.go
+++ management/operations.go
@@ -2,28 +2,36 @@ package management
 
 import (
 	"encoding/xml"
-	"errors"
 	"fmt"
 	"time"
 )
 
-//operation represents an in-flight operation. Use client.getOperationStatus()
-//to get the operation given the operation ID, or use
-//client.waitAsyncOperation() to block until the operation has completed.
-type operation struct {
-	XMLName        xml.Name `xml:"Operation"`
+//  GetOperationStatusResponse  represents an in-flight operation. Use client.GetOperationStatus()
+// to get the operation given the operation ID, or use
+// client.WaitAsyncOperation() to block until the operation has completed.
+// See https://msdn.microsoft.com/en-us/library/azure/ee460783.aspx
+type GetOperationStatusResponse struct {
+	XMLName        xml.Name `xml:"http://schemas.microsoft.com/windowsazure Operation"`
 	ID             string
-	Status         string
+	Status         OperationStatus
 	HttpStatusCode string
-	Error          AzureError
+	Error          *AzureError
 }
+
+type OperationStatus string
+
+const (
+	OperationStatusInProgress OperationStatus = "InProgress"
+	OperationStatusSucceeded  OperationStatus = "Succeeded"
+	OperationStatusFailed     OperationStatus = "Failed"
+)
 
 // OperationId is assigned by Azure API and can be used to look up the status of an operation
 type OperationId string
 
-//getOperationStatus gets an operation given the operation ID.
-func (client *Client) getOperationStatus(operationId OperationId) (operation, error) {
-	operation := operation{}
+// getOperationStatus gets an operation given the operation ID.
+func (client *Client) GetOperationStatus(operationId OperationId) (GetOperationStatusResponse, error) {
+	operation := GetOperationStatusResponse{}
 	if operationId == "" {
 		return operation, fmt.Errorf(errParamNotSpecified, "operationId")
 	}
@@ -42,30 +50,30 @@ func (client *Client) getOperationStatus(operationId OperationId) (operation, er
 	return operation, nil
 }
 
-//waitAsyncOperation blocks until the operation with the given operationId is
-//no longer in the InProgress state. If the operation was successful, nothing is
-//returned, otherwise an error is returned.
-func (client *Client) WaitAsyncOperation(operationId OperationId) error {
-	if operationId == "" {
-		return fmt.Errorf(errParamNotSpecified, "operationId")
-	}
+const pollInterval = 30 * time.Second
 
-	status := "InProgress"
-	operation := operation{}
-	err := errors.New("")
-	for status == "InProgress" {
-		time.Sleep(2000 * time.Millisecond)
-		operation, err = client.getOperationStatus(operationId)
+// WaitAsyncOperation blocks until the operation with the given operationId is
+// no longer in the InProgress state. If the operation was successful, nothing is
+// returned, otherwise an error is returned.
+func (client *Client) WaitAsyncOperation(operationId OperationId) error {
+	for {
+		operation, err := client.GetOperationStatus(operationId)
 		if err != nil {
-			return err
+			return fmt.Errorf("Failed to get operation status '%s': %v", operationId, err)
 		}
 
-		status = operation.Status
+		switch operation.Status {
+		case OperationStatusSucceeded:
+			return nil
+		case OperationStatusFailed:
+			if operation.Error != nil {
+				return operation.Error
+			}
+			return fmt.Errorf("Azure operation %s failed", operationId)
+		case OperationStatusInProgress:
+			time.Sleep(pollInterval)
+		default:
+			return fmt.Errorf("Unknown operation status: %s", operation.Status)
+		}
 	}
-
-	if status == "Failed" {
-		return fmt.Errorf("Azure operation %s failed. Code: %s, Message: %s", operationId, operation.Error.Code, operation.Error.Message)
-	}
-
-	return nil
 }

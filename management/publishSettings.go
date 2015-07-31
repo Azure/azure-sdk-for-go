5cbf49f8a14c4ee5c0f79cfd4e749820dc75b713
Clint Shryock <clint@ctshryock.com>
2015-07-31 16:28:52 -0500

    refactoring

--- management/publishSettings.go
+++ management/publishSettings.go
@@ -26,6 +26,36 @@ func ClientFromPublishSettingsDataWithConfig(settingsData []byte, subscriptionID
 		return client, err
 	}
 
+	return clientFromPublishData(publishData, subscriptionID, config)
+}
+
+// ClientFromPublishSettingsFile reads a publish settings file downloaded from https://manage.windowsazure.com/publishsettings.
+// If subscriptionID is left empty, the first subscription in the file is used.
+func ClientFromPublishSettingsFile(filePath, subscriptionID string) (client Client, err error) {
+	return ClientFromPublishSettingsFileWithConfig(filePath, subscriptionID, DefaultConfig())
+}
+
+// ClientFromPublishSettingsFileWithConfig reads a publish settings file downloaded from https://manage.windowsazure.com/publishsettings.
+// If subscriptionID is left empty, the first subscription in the file is used.
+func ClientFromPublishSettingsFileWithConfig(filePath, subscriptionID string, config ClientConfig) (client Client, err error) {
+	if filePath == "" {
+		return client, fmt.Errorf(errParamNotSpecified, "filePath")
+	}
+
+	publishSettingsContent, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return client, err
+	}
+
+	publishData := publishData{}
+	if err = xml.Unmarshal(publishSettingsContent, &publishData); err != nil {
+		return client, err
+	}
+
+	return clientFromPublishData(publishData, subscriptionID, config)
+}
+
+func clientFromPublishData(publishData publishData, subscriptionID string, config ClientConfig) (client Client, err error) {
 	for _, profile := range publishData.PublishProfiles {
 		for _, sub := range profile.Subscriptions {
 			if sub.ID == subscriptionID || subscriptionID == "" {
@@ -53,58 +83,6 @@ func ClientFromPublishSettingsDataWithConfig(settingsData []byte, subscriptionID
 	}
 
 	return client, fmt.Errorf("could not find subscription '%s' in settings provided", subscriptionID)
-}
-
-// ClientFromPublishSettingsFile reads a publish settings file downloaded from https://manage.windowsazure.com/publishsettings.
-// If subscriptionID is left empty, the first subscription in the file is used.
-func ClientFromPublishSettingsFile(filePath, subscriptionID string) (client Client, err error) {
-	return ClientFromPublishSettingsFileWithConfig(filePath, subscriptionID, DefaultConfig())
-}
-
-// ClientFromPublishSettingsFileWithConfig reads a publish settings file downloaded from https://manage.windowsazure.com/publishsettings.
-// If subscriptionID is left empty, the first subscription in the file is used.
-func ClientFromPublishSettingsFileWithConfig(filePath, subscriptionID string, config ClientConfig) (client Client, err error) {
-	if filePath == "" {
-		return client, fmt.Errorf(errParamNotSpecified, "filePath")
-	}
-
-	publishSettingsContent, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return client, err
-	}
-
-	publishData := publishData{}
-	if err = xml.Unmarshal(publishSettingsContent, &publishData); err != nil {
-		return client, err
-	}
-
-	for _, profile := range publishData.PublishProfiles {
-		for _, sub := range profile.Subscriptions {
-			if sub.ID == subscriptionID || subscriptionID == "" {
-				base64Cert := sub.ManagementCertificate
-				if base64Cert == "" {
-					base64Cert = profile.ManagementCertificate
-				}
-
-				pfxData, err := base64.StdEncoding.DecodeString(base64Cert)
-				if err != nil {
-					return client, err
-				}
-
-				pems, err := pkcs12.ConvertToPEM(pfxData, nil)
-
-				cert := []byte{}
-				for _, b := range pems {
-					cert = append(cert, pem.EncodeToMemory(b)...)
-				}
-
-				config.ManagementURL = sub.ServiceManagementURL
-				return makeClient(sub.ID, cert, config)
-			}
-		}
-	}
-
-	return client, fmt.Errorf("could not find subscription '%s' in '%s'", subscriptionID, filePath)
 }
 
 type publishSettings struct {

b31761ed351c7483b4acc83a7db7345d3c368fd5
Clint Shryock <clint@ctshryock.com>
2015-07-31 14:52:42 -0500

    Add support for creating Clients from a previously read settings file

--- management/publishSettings.go
+++ management/publishSettings.go
@@ -10,6 +10,51 @@ import (
 	"github.com/Azure/go-pkcs12"
 )
 
+// ClientFromPublishSettingsData unmarshalls the contents of a publish settings file
+// from https://manage.windowsazure.com/publishsettings.
+// If subscriptionID is left empty, the first subscription in the file is used.
+func ClientFromPublishSettingsData(settingsData []byte, subscriptionID string) (client Client, err error) {
+	return ClientFromPublishSettingsDataWithConfig(settingsData, subscriptionID, DefaultConfig())
+}
+
+// ClientFromPublishSettingsDataWithConfig unmarshalls the contents of a publish settings file
+// from https://manage.windowsazure.com/publishsettings.
+// If subscriptionID is left empty, the first subscription in the string is used.
+func ClientFromPublishSettingsDataWithConfig(settingsData []byte, subscriptionID string, config ClientConfig) (client Client, err error) {
+	publishData := publishData{}
+	if err = xml.Unmarshal(settingsData, &publishData); err != nil {
+		return client, err
+	}
+
+	for _, profile := range publishData.PublishProfiles {
+		for _, sub := range profile.Subscriptions {
+			if sub.ID == subscriptionID || subscriptionID == "" {
+				base64Cert := sub.ManagementCertificate
+				if base64Cert == "" {
+					base64Cert = profile.ManagementCertificate
+				}
+
+				pfxData, err := base64.StdEncoding.DecodeString(base64Cert)
+				if err != nil {
+					return client, err
+				}
+
+				pems, err := pkcs12.ConvertToPEM(pfxData, nil)
+
+				cert := []byte{}
+				for _, b := range pems {
+					cert = append(cert, pem.EncodeToMemory(b)...)
+				}
+
+				config.ManagementURL = sub.ServiceManagementURL
+				return makeClient(sub.ID, cert, config)
+			}
+		}
+	}
+
+	return client, fmt.Errorf("could not find subscription '%s' in settings provided", subscriptionID)
+}
+
 // ClientFromPublishSettingsFile reads a publish settings file downloaded from https://manage.windowsazure.com/publishsettings.
 // If subscriptionID is left empty, the first subscription in the file is used.
 func ClientFromPublishSettingsFile(filePath, subscriptionID string) (client Client, err error) {

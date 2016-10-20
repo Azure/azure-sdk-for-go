d2fbe2f3d885e1c039bde8ef00ef777ec7860856
Ken Faulkner <ken.faulkner@gmail.com>
2016-10-20 18:11:38 +1100

    test fix

--- storage/blob_test.go
+++ storage/blob_test.go
@@ -689,8 +689,7 @@ func (s *StorageBlobSuite) TestAcquireLeaseWithNoProposedLeaseID(c *chk.C) {
 	c.Assert(cli.putSingleBlockBlob(cnt, blob, []byte{}), chk.IsNil)
 
 	_, err := cli.AcquireLease(cnt, blob, 30, "")
-	c.Assert(err, chk.NotNil)
-
+	c.Assert(err, chk.IsNil)
 }
 
 func (s *StorageBlobSuite) TestAcquireLeaseWithProposedLeaseID(c *chk.C) {

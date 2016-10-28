7287de88ecfb31bcbfc86cbba17a0d0fc2f9e6bb
Ken Faulkner <ken.faulkner@gmail.com>
2016-10-28 12:37:10 +1100

    debugging tests working fine locally

--- storage/blob.go
+++ storage/blob.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -521,6 +522,8 @@ func (b BlobStorageClient) SetContainerPermissions(container string, containerPe
 
 	// generate the XML for the SharedAccessSignature if required.
 	accessPolicyXML, err := b.generateAccessPolicy(containerPermissions.AccessPolicy)
+	log.Printf("AccessPolicyXML is %s", accessPolicyXML)
+
 	if err != nil {
 		return err
 	}

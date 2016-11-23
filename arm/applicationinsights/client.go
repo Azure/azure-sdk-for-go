41f3faca46341abc31b1c7252744c2964c05585b
tombuildsstuff <git@ibuildstuff.co.uk>
2016-11-23 18:38:29 +0000

    Linting

--- arm/applicationinsights/client.go
+++ arm/applicationinsights/client.go
@@ -1,9 +1,8 @@
 // Package applicationinsights implements the Azure ARM Applicationinsights
 // service API version 2014-04-01.
-// 
+//
 // Azure Application Insights client
 package applicationinsights
-
 
 // Copyright (c) Microsoft and contributors.  All rights reserved.
 //
@@ -24,37 +23,36 @@ package applicationinsights
 // regenerated.
 
 import (
-    "github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest"
 )
 
 const (
-    // APIVersion is the version of the Applicationinsights
-    APIVersion = "2014-04-01"
+	// APIVersion is the version of the Applicationinsights
+	APIVersion = "2014-04-01"
 
-    // DefaultBaseURI is the default URI used for the service Applicationinsights
-    DefaultBaseURI = "https://management.azure.com"
+	// DefaultBaseURI is the default URI used for the service Applicationinsights
+	DefaultBaseURI = "https://management.azure.com"
 )
 
 // ManagementClient is the base client for Applicationinsights.
 type ManagementClient struct {
-    autorest.Client
-    BaseURI string
-    APIVersion string
-    SubscriptionID string
+	autorest.Client
+	BaseURI        string
+	APIVersion     string
+	SubscriptionID string
 }
 
 // New creates an instance of the ManagementClient client.
-func New(subscriptionID string)ManagementClient {
-    return NewWithBaseURI(DefaultBaseURI, subscriptionID)
+func New(subscriptionID string) ManagementClient {
+	return NewWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
 // NewWithBaseURI creates an instance of the ManagementClient client.
 func NewWithBaseURI(baseURI string, subscriptionID string) ManagementClient {
-   return ManagementClient{
-        Client: autorest.NewClientWithUserAgent(UserAgent()),
-        BaseURI: baseURI,
-        APIVersion: APIVersion,
-        SubscriptionID: subscriptionID,
-    }
+	return ManagementClient{
+		Client:         autorest.NewClientWithUserAgent(UserAgent()),
+		BaseURI:        baseURI,
+		APIVersion:     APIVersion,
+		SubscriptionID: subscriptionID,
+	}
 }
-
--- arm/applicationinsights/version.go
+++ arm/applicationinsights/version.go
@@ -19,25 +19,25 @@ package applicationinsights
 // regenerated.
 
 import (
-    "fmt"
+	"fmt"
 )
 
 const (
-    major = "6"
-    minor = "0"
-    patch = "0"
-    // Always begin a "tag" with a dash (as per http://semver.org)
-    tag   = "-beta"
-    semVerFormat = "%s.%s.%s%s"
-    userAgentFormat = "Azure-SDK-for-Go/%s arm-%s/%s"
+	major = "6"
+	minor = "0"
+	patch = "0"
+	// Always begin a "tag" with a dash (as per http://semver.org)
+	tag             = "-beta"
+	semVerFormat    = "%s.%s.%s%s"
+	userAgentFormat = "Azure-SDK-for-Go/%s arm-%s/%s"
 )
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-    return fmt.Sprintf(userAgentFormat, Version(), "applicationinsights", "2014-04-01")
+	return fmt.Sprintf(userAgentFormat, Version(), "applicationinsights", "2014-04-01")
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
 func Version() string {
-    return fmt.Sprintf(semVerFormat, major, minor, patch, tag)
+	return fmt.Sprintf(semVerFormat, major, minor, patch, tag)
 }

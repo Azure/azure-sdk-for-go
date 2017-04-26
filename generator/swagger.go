d9be12f7ad88dc04ec7e651e96731baef4fb32cf
Martin Strobel <marstr@microsoft.com>
2017-04-26 13:31:48 -0700

    Stashing some work

--- generator/swagger.go
+++ generator/swagger.go
@@ -1,20 +1,8 @@
 package main
 
-// Swagger represents top-level swagger contents needed for generator serialization.
+import "github.com/marstr/swagger"
+
 type Swagger struct {
-	SwaggerVersion string    `json:"swagger"`
-	Info           AzureInfo `json:"info"`
-	Documents      []string  `json:"documents"`
-}
-
-// AzureInfo holds the contents of the metadata surrounding
-type AzureInfo struct {
-	Title    string                 `json:"title"`
-	Version  string                 `json:"version"`
-	Settings CodeGenerationSettings `json:"x-ms-code-generation-settings"`
-}
-
-// CodeGenerationSettings handles the Microsoft Swagger extension "x-ms-code-generation-settings"
-type CodeGenerationSettings struct {
-	Name string `json:"name"`
+	swagger.Swagger
+	Path string
 }

87ba7b0544ac3a20b0d2e2fd5fb9b8673c9c5611
Joel Hendrix <jhendrix@microsoft.com>
2017-05-10 14:59:39 -0700

    Bump patch version to v10.0.2

--- arm/operationalinsights/version.go
+++ arm/operationalinsights/version.go
@@ -20,10 +20,10 @@ package operationalinsights
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "Azure-SDK-For-Go/v10.0.1-beta arm-operationalinsights/2015-11-01-preview"
+	return "Azure-SDK-For-Go/v10.0.2-beta arm-operationalinsights/2015-11-01-preview"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
 func Version() string {
-	return "v10.0.1-beta"
+	return "v10.0.2-beta"
 }

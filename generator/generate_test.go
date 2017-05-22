8859f83015a173f02f7b307d41fcb3e3bd5a3f4f
Martin Strobel <marstr@microsoft.com>
2017-05-22 12:55:43 -0700

    Cleaning up generator tests.

--- generator/generate_test.go
+++ generator/generate_test.go
@@ -1,34 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
 	"testing"
 )
 
-func TestGetNamespace_Standard(t *testing.T) {
+func TestGetNamespace(t *testing.T) {
 	basePath := filepath.Join("dir1", "dir2")
-	swaggerPath := filepath.Join(basePath, "plane-package", "2016-02-17", "swagger", "example.json")
-	result, err := getNamespace(swaggerPath, basePath)
-	if err != nil {
-		t.Error(err)
+
+	testCases := []struct {
+		given    string
+		expected string
+	}{
+		{
+			filepath.Join(basePath, "plane-package", "2016-02-17", "swagger", "example.json"),
+			"plane/package/2016-02-17/example",
+		},
+		{
+			filepath.Join(basePath, "plane-split-name", "dir3", "dir4", "2016-02-09-preview", "swagger", "example.json"),
+			"plane/split-name/dir3/dir4/2016-02-09-preview/example",
+		},
+		{
+			filepath.Join(basePath, "myService/2015-06-01/swagger/example.json"),
+			"services/myService/2015-06-01/example",
+		},
 	}
-	expected := "github.com/Azure/azure-sdk-for-go/plane/package"
-	if result != expected {
-		t.Logf("got:\n%s\nwant:\n%s", result, expected)
-		t.Fail()
+
+	for _, tc := range testCases {
+		t.Run(tc.given, func(t *testing.T) {
+			result, err := getNamespace(Swagger{
+				Path: tc.given,
+			})
+			if err != nil {
+				t.Error(err)
+			}
+
+			if result != tc.expected {
+				t.Logf("got:\n%s\nwant:\n%s", result, tc.expected)
+				t.Fail()
+			}
+		})
 	}
 }
 
-func TestGetNamespace_Nested(t *testing.T) {
-	basePath := filepath.Join("dir1", "dir2")
-	swaggerPath := filepath.Join(basePath, "plane-split-name", "dir3", "dir4", "2016-02-09-preview", "swagger", "example.json")
-	result, err := getNamespace(swaggerPath, basePath)
-	if err != nil {
-		t.Error(err)
+func TestMain(m *testing.M) {
+	exitStatus := m.Run()
+	if noClone == false {
+		if err := os.RemoveAll(localAzureRestAPISpecsPath); err != nil {
+			fmt.Fprintln(os.Stderr, "Unable to delete folder: ", localAzureRestAPISpecsPath)
+		}
 	}
-	expected := "github.com/Azure/azure-sdk-for-go/plane/split-name/dir3/dir4"
-	if result != expected {
-		t.Logf("got:\n%s\nwant:\n%s", result, expected)
-		t.Fail()
-	}
+	os.Exit(exitStatus)
 }

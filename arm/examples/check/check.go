4d3824bd627f4fd053cf1b5977b86dbe99a26448
Christoph Schittko <cschittk@microsoft.com>
2017-05-30 20:47:18 -0500

    Updated to new async function

--- arm/examples/check/check.go
+++ arm/examples/check/check.go
@@ -9,7 +9,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/arm/examples/helpers"
 	"github.com/Azure/azure-sdk-for-go/arm/storage"
 	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/azure"	
+	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/Azure/go-autorest/autorest/to"
 )
 

aa755cfc1b34b0bddc409b7d751f7f136652b32e
mcardosos <mcardoso@microsoft.com>
2017-06-07 14:15:34 -0700

    Added kubernetes build to CI

---
+++ break.sh
@@ -0,0 +1,26 @@
+# This should only run on cronjobs
+if [ "cron" != $TRAVIS_EVENT_TYPE ]; then
+   exit 0
+fi
+
+# get kubernetes
+go get k8s.io/kubernetes
+cd $GOPATH/src/k8s.io/kubernetes/vendor
+
+#replace vendored SDK in kubernetes with the latest version
+files=()
+
+while IFS= read -d $'\0' -r files
+do files+=("$file")
+done < <(find github.com/Azure/azure-sdk-for-go '*.go' -print0)
+
+for i in "${files[@]}"
+do
+    rm $i
+    cp $GOPATH/src/$i $(dirname $i)
+done
+
+# try to build
+cd $GOPATH/src/k8s.io/kubernetes
+make
+exit $?

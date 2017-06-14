7acdf6c13fa74fe8f66f3857b984c87b75792e50
Martin Strobel <marstr@microsoft.com>
2017-06-14 15:54:34 -0700

    Beginning to hack up the shape of a profile.

---
+++ arm/profile/example/network/applicationGateway/models.go
@@ -0,0 +1,9 @@
+package applicationGateway
+
+import (
+	origin "github.com/Azure/azure-sdk-for-go/arm/network/2017-03-01/applicationGateway"
+)
+
+type Model origin.Model
+
+type BackendHealthType origin.BackendHealthType
---
+++ arm/profile/example/network/network/clients.go
@@ -0,0 +1,9 @@
+package network
+
+import (
+	origin "github.com/Azure/azure-sdk-for-go/arm/network/2016-03-30/network"
+)
+
+type VirtualNetworkGatewayConnectionsClient origin.VirtualNetworkGatewayConnectionsClient
+
+type ExpressRouteCircuitAuthorizationsClient origin.ExpressRouteCircuitAuthorizationsClient

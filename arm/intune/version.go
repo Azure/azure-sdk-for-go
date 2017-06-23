669733bd8e989b4b93c72c37f051e93b5f7e0efc
mcardosos <mcardoso@microsoft.com>
2017-06-23 13:57:52 -0700

    Updated SDK version

--- arm/intune/version.go
+++ arm/intune/version.go
@@ -20,10 +20,10 @@ package intune
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "Azure-SDK-For-Go/v10.0.2-beta arm-intune/2015-01-14-preview"
+	return "Azure-SDK-For-Go/v10.1.0-beta arm-intune/2015-01-14-preview"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
 func Version() string {
-	return "v10.0.2-beta"
+	return "v10.1.0-beta"
 }

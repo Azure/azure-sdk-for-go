9c6caf0a9eec64a6a940defbcc865ab89c92a11c
Martin Strobel <marstr@microsoft.com>
2017-10-05 14:15:23 -0700

    Updating `build` commands in CI to be less chatty.
    The way we specified `go build` enumerated each package to be compiled.
    This created a lot of noise because folders that didn't contain any Go
    source themselves, but had children that did caused an error message.
    This way, we'll still get all of the protection of making sure that our
    entire repo builds, without the noise.
    
    Also, a couple of formatting changes in profileBuilder.

--- tools/profileBuilder/latest.go
+++ tools/profileBuilder/latest.go
@@ -1,5 +1,3 @@
-package main
-
 // Copyright 2017 Microsoft Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +11,8 @@ package main
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+package main
 
 import (
 	"fmt"

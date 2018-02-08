3debc48ea67436a8a4bec4bc34aeb3bb6bd49c28
Martin Strobel <marstr@microsoft.com>
2018-02-07 18:49:09 -0800

    Adding headers

--- tools/profileBuilder/model/list.go
+++ tools/profileBuilder/model/list.go
@@ -1,6 +1,6 @@
 // +build go1.9
 
-// Copyright 2017 Microsoft Corporation and contributors
+// Copyright 2018 Microsoft Corporation and contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

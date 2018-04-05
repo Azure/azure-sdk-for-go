f35347a7c7b3291f03bfd9a2b21bd967e081c19b
Mariana Cardoso <mcardoso@microsoft.com>
2018-04-04 18:40:19 -0700

    Moves CI to its own script

---
+++ ci.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+REALEXITSTATUS=0
+if [[ $MODE == 'default' ]]; then
+    bash rungas.sh
+    REALEXITSTATUS=$(($REALEXITSTATUS+$?))
+
+    grep -L -r --include *.go --exclude-dir vendor -P "Copyright (\d{4}|\(c\)) Microsoft" ./ | tee /dev/stderr | test -z "$(< /dev/stdin)"
+    REALEXITSTATUS=$(($REALEXITSTATUS+$?))
+
+    test -z "$(go build $(go list ./... | grep -v vendor) | tee /dev/stderr)"
+    REALEXITSTATUS=$(($REALEXITSTATUS+$?))
+    
+    test -z "$(go fmt $(go list ./... | grep -v vendor) | tee /dev/stderr)"
+    REALEXITSTATUS=$(($REALEXITSTATUS+$?))
+
+    go vet $(go list ./... | grep -v vendor)
+    REALEXITSTATUS=$(($REALEXITSTATUS+$?))
+    
+    go test $(sh ./findTestedPackages.sh)
+    REALEXITSTATUS=$(($REALEXITSTATUS+$?))
+fi
+
+if [[ $MODE == 'breaking' ]]; then
+    go run ./tools/apidiff/main.go packages ./services FETCH_HEAD~1 FETCH_HEAD --copyrepo --breakingchanges
+    REALEXITSTATUS=$(($REALEXITSTATUS+$?))
+fi
+exit $REALEXITSTATUS

52a82b92ca3f44382f503ead7261c4da762aa82e
Joel Hendrix <jhendrix@microsoft.com>
2018-04-11 09:30:08 -0700

    update profiles

--- profiles/latest/cognitiveservices/contentmoderator/models.go
+++ profiles/latest/cognitiveservices/contentmoderator/models.go
@@ -156,6 +156,7 @@ type RefreshIndexAdvancedInfoItem = original.RefreshIndexAdvancedInfoItem
 type Review = original.Review
 type Screen = original.Screen
 type SetObject = original.SetObject
+type SSN = original.SSN
 type Status = original.Status
 type String = original.String
 type Tag = original.Tag

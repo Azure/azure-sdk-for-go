4fc622071fa316f9d126203a3a2c7223c6d0ee1f
David Justice <david@devigned.com>
2018-12-21 15:57:42 -0800

    added dup detection example

--- package_examples_test.go
+++ package_examples_test.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/Azure/azure-amqp-common-go/uuid"
+
 	"github.com/Azure/azure-service-bus-go"
 )
 
@@ -103,8 +105,8 @@ func Example_auto_forward() {
 		fmt.Println(err)
 		return
 	}
-	defer func(){
-		_ := sourceQueue.Close(ctx)
+	defer func() {
+		_ = sourceQueue.Close(ctx)
 	}()
 
 	if err := sourceQueue.Send(ctx, servicebus.NewMessageFromString("forward me to target!")); err != nil {
@@ -117,8 +119,8 @@ func Example_auto_forward() {
 		fmt.Println(err)
 		return
 	}
-	defer func(){
-		_ := targetQueue.Close(ctx)
+	defer func() {
+		_ = targetQueue.Close(ctx)
 	}()
 
 	if err := targetQueue.ReceiveOne(ctx, MessagePrinter{}); err != nil {
@@ -158,8 +160,8 @@ func Example_deadletter_queues() {
 		fmt.Println(err)
 		return
 	}
-	defer func(){
-		_ := q.Close(ctx)
+	defer func() {
+		_ = q.Close(ctx)
 	}()
 
 	if err := q.Send(ctx, servicebus.NewMessageFromString("foo")); err != nil {
@@ -186,10 +188,9 @@ func Example_deadletter_queues() {
 		fmt.Println(err)
 		return
 	}
-	defer func(){
-		_ := qdl.Close(ctx)
+	defer func() {
+		_ = qdl.Close(ctx)
 	}()
-
 
 	// Output:
 	// count: 1
@@ -235,7 +236,7 @@ func Example_defer_messages() {
 		return
 	}
 	defer func() {
-		_ := q.Close(ctx)
+		_ = q.Close(ctx)
 	}()
 
 	type recipeStep struct {
@@ -275,9 +276,9 @@ func Example_defer_messages() {
 			}
 
 			msg := &servicebus.Message{
-				Data: j,
+				Data:        j,
 				ContentType: "application/json",
-				Label: "RecipeStep",
+				Label:       "RecipeStep",
 			}
 
 			// we shuffle the message order to introduce a random delay before each of the messages is sent to
@@ -328,6 +329,73 @@ func Example_defer_messages() {
 	// step: 3, Prepare
 	// step: 4, Cook
 	// step: 5, Eat
+}
+
+func Example_duplicate_message_detection() {
+	ctx, cancel := context.WithTimeout(context.Background(), 40*time.Second)
+	defer cancel()
+
+	connStr := os.Getenv("SERVICEBUS_CONNECTION_STRING")
+	if connStr == "" {
+		fmt.Println("FATAL: expected environment variable SERVICEBUS_CONNECTION_STRING not set")
+		return
+	}
+
+	// Create a client to communicate with a Service Bus Namespace.
+	ns, err := servicebus.NewNamespace(servicebus.NamespaceWithConnectionString(connStr))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	window := 30 * time.Second
+	qm := ns.NewQueueManager()
+	qe, err := ensureQueue(ctx, qm, "DuplicateDetectionExample", servicebus.QueueEntityWithDuplicateDetection(&window))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	q, err := ns.NewQueue(qe.Name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer func() {
+		_ = q.Close(ctx)
+	}()
+
+	guid, err := uuid.NewV4()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	msg := servicebus.NewMessageFromString("foo")
+	msg.ID = guid.String()
+
+	// send the message twice with the same ID
+	for i := 0; i < 2; i++ {
+		if err := q.Send(ctx, msg); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	// there should be only 1 message received from the queue
+	go func() {
+		if err := q.Receive(ctx, MessagePrinter{}); err != nil {
+			if err.Error() != "context canceled" {
+				fmt.Println(err)
+				return
+			}
+		}
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	// Output:
+	// foo
 }
 
 func Example_priority_subscriptions() {

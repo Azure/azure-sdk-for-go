bb4703b0edc0d70a5b96e9892b5ecf8e32d3fc40
arcturusZhang <ufo54153@gmail.com>
2019-09-03 13:03:49 +0800

    Fix bad package name

--- services/preview/storagecache/mgmt/2019-08-01-preview/storagecache/storagecacheapi/interfaces.go
+++ services/preview/storagecache/mgmt/2019-08-01-preview/storagecache/storagecacheapi/interfaces.go
@@ -18,54 +18,50 @@ package storagecacheapi
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-	"context"
-	"github.com/Azure/azure-sdk-for-go/services/preview/storagecache/mgmt/2019-08-01-preview/storagecache"
+    "context"
+    "github.com/Azure/azure-sdk-for-go/services/preview/storagecache/mgmt/2019-08-01-preview/storagecache"
 )
 
-// OperationsClientAPI contains the set of methods on the OperationsClient type.
-type OperationsClientAPI interface {
-	List(ctx context.Context) (result storagecache.APIOperationListResultPage, err error)
-}
+        // OperationsClientAPI contains the set of methods on the OperationsClient type.
+        type OperationsClientAPI interface {
+            List(ctx context.Context) (result storagecache.APIOperationListResultPage, err error)
+        }
 
-var _ OperationsClientAPI = (*storagecache.OperationsClient)(nil)
+        var _ OperationsClientAPI = (*storagecache.OperationsClient)(nil)
+        // SkusClientAPI contains the set of methods on the SkusClient type.
+        type SkusClientAPI interface {
+            List(ctx context.Context) (result storagecache.ResourceSkusResultPage, err error)
+        }
 
-// SkusClientAPI contains the set of methods on the SkusClient type.
-type SkusClientAPI interface {
-	List(ctx context.Context) (result storagecache.ResourceSkusResultPage, err error)
-}
+        var _ SkusClientAPI = (*storagecache.SkusClient)(nil)
+        // UsageModelsClientAPI contains the set of methods on the UsageModelsClient type.
+        type UsageModelsClientAPI interface {
+            List(ctx context.Context) (result storagecache.UsageModelsResultPage, err error)
+        }
 
-var _ SkusClientAPI = (*storagecache.SkusClient)(nil)
+        var _ UsageModelsClientAPI = (*storagecache.UsageModelsClient)(nil)
+        // CachesClientAPI contains the set of methods on the CachesClient type.
+        type CachesClientAPI interface {
+            Create(ctx context.Context, resourceGroupName string, cacheName string, cache *storagecache.Cache) (result storagecache.CachesCreateFuture, err error)
+            Delete(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.CachesDeleteFuture, err error)
+            Flush(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.CachesFlushFuture, err error)
+            Get(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.Cache, err error)
+            List(ctx context.Context) (result storagecache.CachesListResultPage, err error)
+            ListByResourceGroup(ctx context.Context, resourceGroupName string) (result storagecache.CachesListResultPage, err error)
+            Start(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.CachesStartFuture, err error)
+            Stop(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.CachesStopFuture, err error)
+            Update(ctx context.Context, resourceGroupName string, cacheName string, cache *storagecache.Cache) (result storagecache.Cache, err error)
+            UpgradeFirmware(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.CachesUpgradeFirmwareFuture, err error)
+        }
 
-// UsageModelsClientAPI contains the set of methods on the UsageModelsClient type.
-type UsageModelsClientAPI interface {
-	List(ctx context.Context) (result storagecache.UsageModelsResultPage, err error)
-}
+        var _ CachesClientAPI = (*storagecache.CachesClient)(nil)
+        // StorageTargetsClientAPI contains the set of methods on the StorageTargetsClient type.
+        type StorageTargetsClientAPI interface {
+            Create(ctx context.Context, resourceGroupName string, cacheName string, storageTargetName string, storagetarget *storagecache.StorageTarget) (result storagecache.StorageTargetsCreateFuture, err error)
+            Delete(ctx context.Context, resourceGroupName string, cacheName string, storageTargetName string) (result storagecache.StorageTargetsDeleteFuture, err error)
+            Get(ctx context.Context, resourceGroupName string, cacheName string, storageTargetName string) (result storagecache.StorageTarget, err error)
+            ListByCache(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.StorageTargetsResultPage, err error)
+            Update(ctx context.Context, resourceGroupName string, cacheName string, storageTargetName string, storagetarget *storagecache.StorageTarget) (result storagecache.StorageTarget, err error)
+        }
 
-var _ UsageModelsClientAPI = (*storagecache.UsageModelsClient)(nil)
-
-// CachesClientAPI contains the set of methods on the CachesClient type.
-type CachesClientAPI interface {
-	Create(ctx context.Context, resourceGroupName string, cacheName string, cache *storagecache.Cache) (result storagecache.CachesCreateFuture, err error)
-	Delete(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.CachesDeleteFuture, err error)
-	Flush(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.CachesFlushFuture, err error)
-	Get(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.Cache, err error)
-	List(ctx context.Context) (result storagecache.CachesListResultPage, err error)
-	ListByResourceGroup(ctx context.Context, resourceGroupName string) (result storagecache.CachesListResultPage, err error)
-	Start(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.CachesStartFuture, err error)
-	Stop(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.CachesStopFuture, err error)
-	Update(ctx context.Context, resourceGroupName string, cacheName string, cache *storagecache.Cache) (result storagecache.Cache, err error)
-	UpgradeFirmware(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.CachesUpgradeFirmwareFuture, err error)
-}
-
-var _ CachesClientAPI = (*storagecache.CachesClient)(nil)
-
-// StorageTargetsClientAPI contains the set of methods on the StorageTargetsClient type.
-type StorageTargetsClientAPI interface {
-	Create(ctx context.Context, resourceGroupName string, cacheName string, storageTargetName string, storagetarget *storagecache.StorageTarget) (result storagecache.StorageTargetsCreateFuture, err error)
-	Delete(ctx context.Context, resourceGroupName string, cacheName string, storageTargetName string) (result storagecache.StorageTargetsDeleteFuture, err error)
-	Get(ctx context.Context, resourceGroupName string, cacheName string, storageTargetName string) (result storagecache.StorageTarget, err error)
-	ListByCache(ctx context.Context, resourceGroupName string, cacheName string) (result storagecache.StorageTargetsResultPage, err error)
-	Update(ctx context.Context, resourceGroupName string, cacheName string, storageTargetName string, storagetarget *storagecache.StorageTarget) (result storagecache.StorageTarget, err error)
-}
-
-var _ StorageTargetsClientAPI = (*storagecache.StorageTargetsClient)(nil)
+        var _ StorageTargetsClientAPI = (*storagecache.StorageTargetsClient)(nil)

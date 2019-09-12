f5d2fee3769d74fd9d48678cf4d22175cb22fd34
Jake Bjorke <jrbjorke@gmail.com>
2019-09-11 19:36:35 -0500

    Batch disposition calls return nil rather than 0 value

--- batch_disposition_test.go
+++ batch_disposition_test.go
@@ -46,7 +46,7 @@ func TestBatchDispositionUnsupportedStatus(t *testing.T) {
 		receivingEntity: newReceivingEntity(newEntity("foo", "bar", nil)),
 	}
 	err := subscription.SendBatchDisposition(context.Background(), bdi)
-	be := err.(BatchDispositionError)
+	be := err.(*BatchDispositionError)
 	assert.NotNil(t, be, fmt.Sprintf("Wrong error type %T", err))
 	assert.EqualErrorf(t, err, fmt.Sprintf("Operation failed, %d error(s) reported.", len(be.Errors)), err.Error())
 
@@ -54,4 +54,18 @@ func TestBatchDispositionUnsupportedStatus(t *testing.T) {
 		assert.NotNil(t, innerErr.UnWrap(), "Unwrapped error is nil")
 		assert.EqualErrorf(t, innerErr, "unsupported bulk disposition status \"suspended\"", innerErr.Error())
 	}
+}
+
+func TestBatchDispositiondoUpdateNoError(t *testing.T) {
+	//want done to return immediately on a call to Done
+	//so no error can be generated, we can do that by
+	//placing an empty slice for the lock tokens
+	bdi := BatchDispositionIterator{
+		LockTokenIDs: []*uuid.UUID{},
+	}
+
+	//using nil for the entity connector as it will never be called
+	result := bdi.doUpdate(context.Background(), nil)
+
+	assert.Nil(t, result, fmt.Sprintf("Expected a nil error to be returned got %v", result))
 }

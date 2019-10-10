c6ed4fdba38b9a3412bbc4712a6b1db0c8d9645d
arcturusZhang <ufo54153@gmail.com>
2019-10-10 13:05:41 +0800

    Add test for list all stage folders

---
+++ tools/versioner/cmd/root_test.go
@@ -0,0 +1,52 @@
+// Copyright 2018 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func Test_findAllSubDirectories(t *testing.T) {
+	expected := []string{
+		"../../testdata/scenarioa/foo/stage",
+		"../../testdata/scenariob/foo/stage",
+		"../../testdata/scenarioc/foo/stage",
+		"../../testdata/scenariod/foo/stage",
+		"../../testdata/scenarioe/foo/stage",
+		"../../testdata/scenariof/foo/stage",
+	}
+	root, err := filepath.Abs("../../testdata")
+	if err != nil {
+		t.Fatalf("error when get absolute path of root: %+v", err)
+	}
+	stages, err := findAllSubDirectories(root, "stage")
+	if err != nil {
+		t.Fatalf("error when listing all stage folders: %+v", err)
+	}
+	if len(stages) != len(expected) {
+		t.Fatalf("expected %d stages folders, but got %d", len(expected), len(stages))
+	}
+	for i, stage := range stages {
+		e, err := filepath.Abs(expected[i])
+		if err != nil {
+			t.Fatalf("error when parsing expected results '%s'(%d)", expected[i], i)
+		}
+		if !strings.EqualFold(stage, e) {
+			t.Fatalf("expected folder '%s', but got '%s'", e, stage)
+		}
+	}
+}

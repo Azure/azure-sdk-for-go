6e9757480cd5f246163bccab2a48ae95fc7476f3
arcturusZhang <ufo54153@gmail.com>
2019-10-11 17:23:23 +0800

    Add a new test case for more realistic

---
+++ tools/testdata/scenariog/foo/mgmt/2019-10-11/foo/stage/version.go
@@ -0,0 +1,25 @@
+// Copyright 2018 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package foo
+
+// UserAgent returns the UserAgent string to use when sending http.Requests.
+func UserAgent() string {
+	return "Azure-SDK-For-Go/1.0.0 foo/2019-10-11"
+}
+
+// Version returns the semantic version (see http://semver.org) of the client.
+func Version() string {
+	return "1.0.0"
+}

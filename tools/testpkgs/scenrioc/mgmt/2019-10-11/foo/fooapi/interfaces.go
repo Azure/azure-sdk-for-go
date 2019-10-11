a16d5fa1d365d39b18cd8feb798fa3eedd04f35d
arcturusZhang <ufo54153@gmail.com>
2019-10-11 16:02:43 +0800

    Some comments changes to silence linter

--- tools/testpkgs/scenrioc/mgmt/2019-10-11/foo/fooapi/interfaces.go
+++ tools/testpkgs/scenrioc/mgmt/2019-10-11/foo/fooapi/interfaces.go
@@ -5,7 +5,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/tools/testpkgs/scenrioa/foo"
 )
 
-// GatewaysClientAPI
+// GatewaysClientAPI ...
 type GatewaysClientAPI interface {
 	CreateOrUpdate(ctx context.Context, resourceGroupName string, gatewayName string, parameters foo.Gateway) (result foo.Gateway, err error)
 }

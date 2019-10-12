11fcb0a2f7373e95ffede23ef363a8e245222867
arcturusZhang <ufo54153@gmail.com>
2019-10-12 10:13:15 +0800

    Fix errors in the new test case

--- tools/versioner/cmd/root_test.go
+++ tools/versioner/cmd/root_test.go
@@ -28,6 +28,7 @@ func Test_findAllSubDirectories(t *testing.T) {
 		"../../testdata/scenariod/foo/stage",
 		"../../testdata/scenarioe/foo/stage",
 		"../../testdata/scenariof/foo/stage",
+		"../../testdata/scenariog/foo/mgmt/2019-10-11/foo/stage",
 	}
 	root, err := filepath.Abs("../../testdata")
 	if err != nil {

09ae410a0fbe969d09ea9a37c5af7c0b2effa169
Joel Hendrix <jhendrix@microsoft.com>
2019-11-22 15:07:01 -0800

    add request body rewind and close coverage

--- sdk/azcore/policy_retry_test.go
+++ sdk/azcore/policy_retry_test.go
@@ -6,7 +6,9 @@ package azcore
 import (
 	"context"
 	"errors"
+	"io"
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 
@@ -21,12 +23,20 @@ func TestRetryPolicySuccess(t *testing.T) {
 	srv.SetResponse(mock.WithStatusCode(http.StatusOK))
 	pl := NewPipeline(srv, NewRetryPolicy(RetryOptions{}))
 	req := pl.NewRequest(http.MethodGet, srv.URL())
+	body := newRewindTrackingBody("stuff")
+	req.SetBody(body)
 	resp, err := req.Do(context.Background())
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 	if resp.StatusCode != http.StatusOK {
 		t.Fatalf("unexpected status code: %d", resp.StatusCode)
+	}
+	if body.rcount > 0 {
+		t.Fatalf("unexpected rewind count: %d", body.rcount)
+	}
+	if !body.closed {
+		t.Fatal("request body wasn't closed")
 	}
 }
 
@@ -36,6 +46,8 @@ func TestRetryPolicyFailOnStatusCode(t *testing.T) {
 	srv.SetResponse(mock.WithStatusCode(http.StatusInternalServerError))
 	pl := NewPipeline(srv, NewRetryPolicy(RetryOptions{RetryDelay: retryDelay}))
 	req := pl.NewRequest(http.MethodGet, srv.URL())
+	body := newRewindTrackingBody("stuff")
+	req.SetBody(body)
 	resp, err := req.Do(context.Background())
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
@@ -45,6 +57,12 @@ func TestRetryPolicyFailOnStatusCode(t *testing.T) {
 	}
 	if r := srv.Requests(); r != defaultMaxRetries {
 		t.Fatalf("wrong retry count, got %d expected %d", r, defaultMaxRetries)
+	}
+	if body.rcount != defaultMaxRetries-1 {
+		t.Fatalf("unexpected rewind count: %d", body.rcount)
+	}
+	if !body.closed {
+		t.Fatal("request body wasn't closed")
 	}
 }
 
@@ -56,6 +74,8 @@ func TestRetryPolicySuccessWithRetry(t *testing.T) {
 	srv.AppendResponse()
 	pl := NewPipeline(srv, NewRetryPolicy(RetryOptions{RetryDelay: retryDelay}))
 	req := pl.NewRequest(http.MethodGet, srv.URL())
+	body := newRewindTrackingBody("stuff")
+	req.SetBody(body)
 	resp, err := req.Do(context.Background())
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
@@ -66,6 +86,12 @@ func TestRetryPolicySuccessWithRetry(t *testing.T) {
 	if r := srv.Requests(); r != 3 {
 		t.Fatalf("wrong retry count, got %d expected %d", r, 3)
 	}
+	if body.rcount != 2 {
+		t.Fatalf("unexpected rewind count: %d", body.rcount)
+	}
+	if !body.closed {
+		t.Fatal("request body wasn't closed")
+	}
 }
 
 func TestRetryPolicyFailOnError(t *testing.T) {
@@ -75,6 +101,8 @@ func TestRetryPolicyFailOnError(t *testing.T) {
 	srv.SetError(fakeErr)
 	pl := NewPipeline(srv, NewRetryPolicy(RetryOptions{RetryDelay: retryDelay}))
 	req := pl.NewRequest(http.MethodPost, srv.URL())
+	body := newRewindTrackingBody("stuff")
+	req.SetBody(body)
 	resp, err := req.Do(context.Background())
 	if !errors.Is(err, fakeErr) {
 		t.Fatalf("unexpected error: %v", err)
@@ -84,6 +112,12 @@ func TestRetryPolicyFailOnError(t *testing.T) {
 	}
 	if r := srv.Requests(); r != defaultMaxRetries {
 		t.Fatalf("wrong retry count, got %d expected %d", r, defaultMaxRetries)
+	}
+	if body.rcount != defaultMaxRetries-1 {
+		t.Fatalf("unexpected rewind count: %d", body.rcount)
+	}
+	if !body.closed {
+		t.Fatal("request body wasn't closed")
 	}
 }
 
@@ -96,6 +130,8 @@ func TestRetryPolicySuccessWithRetryComplex(t *testing.T) {
 	srv.AppendResponse(mock.WithStatusCode(http.StatusAccepted))
 	pl := NewPipeline(srv, NewRetryPolicy(RetryOptions{RetryDelay: retryDelay}))
 	req := pl.NewRequest(http.MethodGet, srv.URL())
+	body := newRewindTrackingBody("stuff")
+	req.SetBody(body)
 	resp, err := req.Do(context.Background())
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
@@ -106,6 +142,12 @@ func TestRetryPolicySuccessWithRetryComplex(t *testing.T) {
 	if r := srv.Requests(); r != defaultMaxRetries {
 		t.Fatalf("wrong retry count, got %d expected %d", r, 3)
 	}
+	if body.rcount != defaultMaxRetries-1 {
+		t.Fatalf("unexpected rewind count: %d", body.rcount)
+	}
+	if !body.closed {
+		t.Fatal("request body wasn't closed")
+	}
 }
 
 func TestRetryPolicyRequestTimedOut(t *testing.T) {
@@ -114,7 +156,9 @@ func TestRetryPolicyRequestTimedOut(t *testing.T) {
 	srv.SetError(errors.New("bogus error"))
 	pl := NewPipeline(srv, NewRetryPolicy(RetryOptions{}))
 	req := pl.NewRequest(http.MethodPost, srv.URL())
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	body := newRewindTrackingBody("stuff")
+	req.SetBody(body)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 	resp, err := req.Do(ctx)
 	if !errors.Is(err, context.DeadlineExceeded) {
@@ -122,6 +166,12 @@ func TestRetryPolicyRequestTimedOut(t *testing.T) {
 	}
 	if resp != nil {
 		t.Fatal("unexpected response")
+	}
+	if body.rcount > 0 {
+		t.Fatalf("unexpected rewind count: %d", body.rcount)
+	}
+	if !body.closed {
+		t.Fatal("request body wasn't closed")
 	}
 }
 
@@ -146,4 +196,38 @@ func TestRetryPolicyFixedDelaySuccessWithRetry(t *testing.T) {
 	if r := srv.Requests(); r != 3 {
 		t.Fatalf("wrong retry count, got %d expected %d", r, 3)
 	}
+}
+
+func newRewindTrackingBody(s string) *rewindTrackingBody {
+	// there are two rewinds that happen before rewinding for a retry
+	// 1. to get the body's size in SetBody()
+	// 2. the first call to Do() in the retry policy
+	// to offset this we init rcount with -2 so rcount is only > 0 on a rewind due to a retry
+	return &rewindTrackingBody{
+		body:   strings.NewReader(s),
+		rcount: -2,
+	}
+}
+
+// used to track the number of times a request body has been rewound
+type rewindTrackingBody struct {
+	body   *strings.Reader
+	closed bool // indicates if the body was closed
+	rcount int  // number of times a rewind happened
+}
+
+func (r *rewindTrackingBody) Close() error {
+	r.closed = true
+	return nil
+}
+
+func (r *rewindTrackingBody) Read(b []byte) (int, error) {
+	return r.body.Read(b)
+}
+
+func (r *rewindTrackingBody) Seek(offset int64, whence int) (int64, error) {
+	if offset == 0 && whence == io.SeekStart {
+		r.rcount++
+	}
+	return r.body.Seek(offset, whence)
 }

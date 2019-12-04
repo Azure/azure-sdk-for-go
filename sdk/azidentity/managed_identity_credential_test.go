441b7c020b5bf7f9fa18e908f1339777e800ca46
catalinaperalta <catalina.peralta@microsoft.com>
2019-12-04 14:49:12 -0800

    Track2 identitytests (#6493)
    * Added some tests for credential types and added specific option types for credentials
    
    * Fixed managed identity credential options, added custom managed identity pipeline and retry policy. 
    
    * Cleaned up default azure credential, added custom retry policy for managed identity, added credential type field to CredentialUnavailable Error, switched over cases where we should use errors.As() instead of errors.Is()
    
    * Cleaned up some comments and extra/un-used code
    
    * Changed GetToken to receive options by val
    
    * Added checks for nil response returned from credential requests and removed unnecessary test for this situation
    
    * Added TokenRequestOptions to the AuthenticationPolicyOptions, removed scopes since it is a field in TokenRequestOptions. Added AuthenticationPolicy implementations in ClientCertificateCredential and UsernamePasswordCredential. Fixed newBearerTokenPolicy calls for all credentials
    
    * Removed fields from ManagedIdentityOptions, assign retry defaults in newDefaultMSIPipeline
    
    * Update default azure credential logic and fixed chain token credential error handling. Removed message field from ChainedCredentialError
    
    * Added test for authentication request creation
    
    * Added tests for GetToken in ClientSecretCredential
    
    * Added initial testing for client certificate credential and renamed client secret credential tests
    
    * Fixed AuthenticationFailedError Error() method
    
    * Fixed ChainedTokenCredential GetToken test
    
    * Adding test for GetToken CredentialUnavailableError in ChainedTokenCredential
    
    * Renamed tests
    
    * Improved setDefaultValues() in azidentity.go, changed to use consts in tests
    
    * Fixed query param parsing and removed extra spaces, added additional query param checks in client certificate credential
    
    * Added minor fixes removed space and changed error embedding in test string
    
    * Fixed error in scope check for ClientSecretCredential
    
    * Add certificate for tests
    
    * Added check for MSI_ENDPOINT and removed Println statements in ManagedIdentityCredential tests
    
    * Fixed merge conflicts with track2 branch

--- sdk/azidentity/managed_identity_credential_test.go
+++ sdk/azidentity/managed_identity_credential_test.go
@@ -2,32 +2,20 @@ package azidentity
 
 import (
 	"context"
-	"errors"
-	"fmt"
+	"os"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 )
 
-func Test_GetToken_CloudShell(t *testing.T) {
-	managedClient := NewManagedIdentityCredential("", newDefaultManagedIdentityOptions())
-	managedAT, err := managedClient.GetToken(context.Background(), azcore.TokenRequestOptions{Scopes: []string{"https://storage.azure.com"}})
-	if err != nil {
-		fmt.Println("Error: ", err)
-	} else {
-		fmt.Println(managedAT)
+func TestManagedIdentityCredential_GetTokenInCloudShell(t *testing.T) {
+	msiEndpoint := os.Getenv("MSI_ENDPOINT")
+	if len(msiEndpoint) == 0 {
+		t.Skip()
 	}
-}
-
-func Test_GetToken_NilScopes(t *testing.T) {
-	managedClient := NewManagedIdentityCredential("", newDefaultManagedIdentityOptions())
-	_, err := managedClient.GetToken(context.Background(), azcore.TokenRequestOptions{})
+	msiCred := NewManagedIdentityCredential("", newDefaultManagedIdentityOptions())
+	_, err := msiCred.GetToken(context.Background(), azcore.TokenRequestOptions{Scopes: []string{scope}})
 	if err != nil {
-		var authFailed *AuthenticationFailedError
-		if !errors.As(err, &authFailed) {
-			t.Fatalf("Expected an AuthenticationFailedError, but instead got: %T", err)
-		}
-	} else {
-		t.Fatalf("Expected an error but did not receive one.")
+		t.Fatalf("Received an error when attempting to retrieve a token")
 	}
 }

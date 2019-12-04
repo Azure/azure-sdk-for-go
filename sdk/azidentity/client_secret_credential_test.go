91328b9008c453f517e25cdceb7d0c0260cbed30
catalinaperalta <catalina.peralta@microsoft.com>
2019-12-04 14:49:12 -0800

    Track2 identitytests (#6493)
    * Added some tests for credential types and added specific option types for credentials
    
    * Fixed managed identity credential options, added custom managed identity pipeline and retry policy. 
    
    * Cleaned up default azure credential, added custom retry policy for managed identity, added credential type field to CredentialUnavailable Error, switched over cases where we should use errors.As() instead of errors.Is()
    
    * Cleaned up some comments and extra/un-used code
    
    * Changed GetToken to receive options by val
    
    * Added checks for nil response returned from credential requests and removed unnecessary test for this situation
    
    * Added TokenRequestOptions to the AuthenticationPolicyOptions, removed scopes since it is a field in TokenRequestOptions. Added AuthenticationPolicy implementations in ClientCertificateCredential and UsernamePasswordCredential. Fixed newBearerTokenPolicy calls for all credentials
    
    * Removed fields from ManagedIdentityOptions, assign retry defaults in newDefaultMSIPipeline
    
    * Update default azure credential logic and fixed chain token credential error handling. Removed message field from ChainedCredentialError
    
    * Added test for authentication request creation
    
    * Added tests for GetToken in ClientSecretCredential
    
    * Added initial testing for client certificate credential and renamed client secret credential tests
    
    * Fixed AuthenticationFailedError Error() method
    
    * Fixed ChainedTokenCredential GetToken test
    
    * Adding test for GetToken CredentialUnavailableError in ChainedTokenCredential
    
    * Renamed tests
    
    * Improved setDefaultValues() in azidentity.go, changed to use consts in tests
    
    * Fixed query param parsing and removed extra spaces, added additional query param checks in client certificate credential
    
    * Added minor fixes removed space and changed error embedding in test string
    
    * Fixed error in scope check for ClientSecretCredential
    
    * Add certificate for tests
    
    * Added check for MSI_ENDPOINT and removed Println statements in ManagedIdentityCredential tests
    
    * Fixed merge conflicts with track2 branch

---
+++ sdk/azidentity/client_secret_credential_test.go
@@ -0,0 +1,82 @@
+package azidentity
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/mock"
+)
+
+const (
+	tenantID                 = "expected_tenant"
+	clientID                 = "expected_client"
+	secret                   = "secret"
+	wrongSecret              = "wrong_secret"
+	tokenValue               = "new_token"
+	tokenExpiresIn           = "3600"
+	scope                    = "http://storage.azure.com/.default"
+	defaultTestAuthorityHost = "login.microsoftonline.com"
+)
+
+func TestClientSecretCredential_CreateAuthRequestSuccess(t *testing.T) {
+	cred := NewClientSecretCredential(tenantID, clientID, secret, nil)
+	req, err := cred.client.createClientSecretAuthRequest(cred.tenantID, cred.clientID, cred.clientSecret, []string{scope})
+	if err != nil {
+		t.Fatalf("Unexpectedly received an error: %v", err)
+	}
+	if req.Request.Header.Get(azcore.HeaderContentType) != azcore.HeaderURLEncoded {
+		t.Fatalf("Unexpected value for Content-Type header")
+	}
+	body, err := ioutil.ReadAll(req.Request.Body)
+	if err != nil {
+		t.Fatalf("Unable to read request body")
+	}
+	bodyStr := string(body)
+	reqQueryParams, err := url.ParseQuery(bodyStr)
+	if err != nil {
+		t.Fatalf("Unable to parse query params in request")
+	}
+	if reqQueryParams[qpClientID][0] != clientID {
+		t.Fatalf("Unexpected client ID in the client_id header")
+	}
+	if reqQueryParams[qpClientSecret][0] != secret {
+		t.Fatalf("Unexpected secret in the client_secret header")
+	}
+	if reqQueryParams[qpScope][0] != scope {
+		t.Fatalf("Unexpected scope in scope header")
+	}
+	if req.Request.URL.Host != defaultTestAuthorityHost {
+		t.Fatalf("Unexpected default authority host")
+	}
+	if req.Request.URL.Scheme != "https" {
+		t.Fatalf("Wrong request scheme")
+	}
+}
+
+func TestClientSecretCredential_GetTokenSuccess(t *testing.T) {
+	srv, close := mock.NewServer()
+	defer close()
+	srv.AppendResponse(mock.WithBody([]byte(`{"access_token": "ey0....", "expires_in": 3600}`)))
+	srvURL := srv.URL()
+	cred := NewClientSecretCredential(tenantID, clientID, secret, &TokenCredentialOptions{HTTPClient: srv, AuthorityHost: &srvURL})
+	_, err := cred.GetToken(context.Background(), azcore.TokenRequestOptions{Scopes: []string{scope}})
+	if err != nil {
+		t.Fatalf("Expected an empty error but received: %s", err.Error())
+	}
+}
+
+func TestClientSecretCredential_GetTokenInvalidCredentials(t *testing.T) {
+	srv, close := mock.NewServer()
+	defer close()
+	srv.SetResponse(mock.WithStatusCode(http.StatusUnauthorized))
+	srvURL := srv.URL()
+	cred := NewClientSecretCredential(tenantID, clientID, wrongSecret, &TokenCredentialOptions{HTTPClient: srv, AuthorityHost: &srvURL})
+	_, err := cred.GetToken(context.Background(), azcore.TokenRequestOptions{Scopes: []string{scope}})
+	if err == nil {
+		t.Fatalf("Expected an error but did not receive one.")
+	}
+}

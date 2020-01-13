b70fcc839e1f7307112a9808d2773131cd43fb2e
SDK Automation <sdkautomation@microsoft.com>
2020-01-13 01:56:24 +0000

    Generated from f3ea51f95e067d56ed04c07a984ce34af5963d92
    change verbosity of privateLinkServiceConnectionState

--- services/search/mgmt/2015-08-19/search/adminkeys.go
+++ services/search/mgmt/2015-08-19/search/adminkeys.go
@@ -36,7 +36,8 @@ func NewAdminKeysClient(subscriptionID string) AdminKeysClient {
 	return NewAdminKeysClientWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
-// NewAdminKeysClientWithBaseURI creates an instance of the AdminKeysClient client.
+// NewAdminKeysClientWithBaseURI creates an instance of the AdminKeysClient client using a custom endpoint.  Use this
+// when interacting with an Azure cloud that uses a non-standard base URI (sovereign clouds, Azure stack).
 func NewAdminKeysClientWithBaseURI(baseURI string, subscriptionID string) AdminKeysClient {
 	return AdminKeysClient{NewWithBaseURI(baseURI, subscriptionID)}
 }

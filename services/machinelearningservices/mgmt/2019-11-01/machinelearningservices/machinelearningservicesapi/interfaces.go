baef0f0f6a2ff88fe28fb27c702530d092632500
SDK Automation <sdkautomation@microsoft.com>
2020-01-14 06:52:39 +0000

    Generated from 465c1c1a3d0c5505620750d02f639bdd31a4223f
    made encryption.status as required property

--- services/machinelearningservices/mgmt/2019-11-01/machinelearningservices/machinelearningservicesapi/interfaces.go
+++ services/machinelearningservices/mgmt/2019-11-01/machinelearningservices/machinelearningservicesapi/interfaces.go
@@ -26,6 +26,7 @@ import (
 // BaseClientAPI contains the set of methods on the BaseClient type.
 type BaseClientAPI interface {
 	ListSkus(ctx context.Context) (result machinelearningservices.SkuListResultPage, err error)
+	ListSkusComplete(ctx context.Context) (result machinelearningservices.SkuListResultIterator, err error)
 }
 
 var _ BaseClientAPI = (*machinelearningservices.BaseClient)(nil)
@@ -43,7 +44,9 @@ type WorkspacesClientAPI interface {
 	Delete(ctx context.Context, resourceGroupName string, workspaceName string) (result autorest.Response, err error)
 	Get(ctx context.Context, resourceGroupName string, workspaceName string) (result machinelearningservices.Workspace, err error)
 	ListByResourceGroup(ctx context.Context, resourceGroupName string, skiptoken string) (result machinelearningservices.WorkspaceListResultPage, err error)
+	ListByResourceGroupComplete(ctx context.Context, resourceGroupName string, skiptoken string) (result machinelearningservices.WorkspaceListResultIterator, err error)
 	ListBySubscription(ctx context.Context, skiptoken string) (result machinelearningservices.WorkspaceListResultPage, err error)
+	ListBySubscriptionComplete(ctx context.Context, skiptoken string) (result machinelearningservices.WorkspaceListResultIterator, err error)
 	ListKeys(ctx context.Context, resourceGroupName string, workspaceName string) (result machinelearningservices.ListWorkspaceKeysResult, err error)
 	ResyncKeys(ctx context.Context, resourceGroupName string, workspaceName string) (result autorest.Response, err error)
 	Update(ctx context.Context, resourceGroupName string, workspaceName string, parameters machinelearningservices.WorkspaceUpdateParameters) (result machinelearningservices.Workspace, err error)
@@ -54,6 +57,7 @@ var _ WorkspacesClientAPI = (*machinelearningservices.WorkspacesClient)(nil)
 // WorkspaceFeaturesClientAPI contains the set of methods on the WorkspaceFeaturesClient type.
 type WorkspaceFeaturesClientAPI interface {
 	List(ctx context.Context, resourceGroupName string, workspaceName string) (result machinelearningservices.ListAmlUserFeatureResultPage, err error)
+	ListComplete(ctx context.Context, resourceGroupName string, workspaceName string) (result machinelearningservices.ListAmlUserFeatureResultIterator, err error)
 }
 
 var _ WorkspaceFeaturesClientAPI = (*machinelearningservices.WorkspaceFeaturesClient)(nil)
@@ -61,6 +65,7 @@ var _ WorkspaceFeaturesClientAPI = (*machinelearningservices.WorkspaceFeaturesCl
 // UsagesClientAPI contains the set of methods on the UsagesClient type.
 type UsagesClientAPI interface {
 	List(ctx context.Context, location string) (result machinelearningservices.ListUsagesResultPage, err error)
+	ListComplete(ctx context.Context, location string) (result machinelearningservices.ListUsagesResultIterator, err error)
 }
 
 var _ UsagesClientAPI = (*machinelearningservices.UsagesClient)(nil)
@@ -75,6 +80,7 @@ var _ VirtualMachineSizesClientAPI = (*machinelearningservices.VirtualMachineSiz
 // QuotasClientAPI contains the set of methods on the QuotasClient type.
 type QuotasClientAPI interface {
 	List(ctx context.Context, location string) (result machinelearningservices.ListWorkspaceQuotasPage, err error)
+	ListComplete(ctx context.Context, location string) (result machinelearningservices.ListWorkspaceQuotasIterator, err error)
 	Update(ctx context.Context, location string, parameters machinelearningservices.QuotaUpdateParameters) (result machinelearningservices.UpdateWorkspaceQuotasResult, err error)
 }
 
@@ -86,6 +92,7 @@ type MachineLearningComputeClientAPI interface {
 	Delete(ctx context.Context, resourceGroupName string, workspaceName string, computeName string, underlyingResourceAction machinelearningservices.UnderlyingResourceAction) (result machinelearningservices.MachineLearningComputeDeleteFuture, err error)
 	Get(ctx context.Context, resourceGroupName string, workspaceName string, computeName string) (result machinelearningservices.ComputeResource, err error)
 	ListByWorkspace(ctx context.Context, resourceGroupName string, workspaceName string, skiptoken string) (result machinelearningservices.PaginatedComputeResourcesListPage, err error)
+	ListByWorkspaceComplete(ctx context.Context, resourceGroupName string, workspaceName string, skiptoken string) (result machinelearningservices.PaginatedComputeResourcesListIterator, err error)
 	ListKeys(ctx context.Context, resourceGroupName string, workspaceName string, computeName string) (result machinelearningservices.ComputeSecretsModel, err error)
 	ListNodes(ctx context.Context, resourceGroupName string, workspaceName string, computeName string) (result machinelearningservices.AmlComputeNodesInformation, err error)
 	Update(ctx context.Context, resourceGroupName string, workspaceName string, computeName string, parameters machinelearningservices.ClusterUpdateParameters) (result machinelearningservices.MachineLearningComputeUpdateFuture, err error)

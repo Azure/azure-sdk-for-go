3347de7d29b5bb15f3bdd8c1c6fde6b7a2a8a48e
SDK Automation <sdkautomation@microsoft.com>
2020-01-22 08:23:16 +0000

    Generated from 80838d0f484db324b45fa1f27b00e9aef3ef9e20
    Revert changes "fix api-version from method property to client property" for resources.

--- services/resources/mgmt/2016-07-01/resources/resourcesapi/interfaces.go
+++ services/resources/mgmt/2016-07-01/resources/resourcesapi/interfaces.go
@@ -33,6 +33,7 @@ type DeploymentsClientAPI interface {
 	ExportTemplate(ctx context.Context, resourceGroupName string, deploymentName string) (result resources.DeploymentExportResult, err error)
 	Get(ctx context.Context, resourceGroupName string, deploymentName string) (result resources.DeploymentExtended, err error)
 	List(ctx context.Context, resourceGroupName string, filter string, top *int32) (result resources.DeploymentListResultPage, err error)
+	ListComplete(ctx context.Context, resourceGroupName string, filter string, top *int32) (result resources.DeploymentListResultIterator, err error)
 	Validate(ctx context.Context, resourceGroupName string, deploymentName string, parameters resources.Deployment) (result resources.DeploymentValidateResult, err error)
 }
 
@@ -42,6 +43,7 @@ var _ DeploymentsClientAPI = (*resources.DeploymentsClient)(nil)
 type ProvidersClientAPI interface {
 	Get(ctx context.Context, resourceProviderNamespace string, expand string) (result resources.Provider, err error)
 	List(ctx context.Context, top *int32, expand string) (result resources.ProviderListResultPage, err error)
+	ListComplete(ctx context.Context, top *int32, expand string) (result resources.ProviderListResultIterator, err error)
 	Register(ctx context.Context, resourceProviderNamespace string) (result resources.Provider, err error)
 	Unregister(ctx context.Context, resourceProviderNamespace string) (result resources.Provider, err error)
 }
@@ -56,7 +58,9 @@ type GroupsClientAPI interface {
 	ExportTemplate(ctx context.Context, resourceGroupName string, parameters resources.ExportTemplateRequest) (result resources.GroupExportResult, err error)
 	Get(ctx context.Context, resourceGroupName string) (result resources.Group, err error)
 	List(ctx context.Context, filter string, top *int32) (result resources.GroupListResultPage, err error)
+	ListComplete(ctx context.Context, filter string, top *int32) (result resources.GroupListResultIterator, err error)
 	ListResources(ctx context.Context, resourceGroupName string, filter string, expand string, top *int32) (result resources.ListResultPage, err error)
+	ListResourcesComplete(ctx context.Context, resourceGroupName string, filter string, expand string, top *int32) (result resources.ListResultIterator, err error)
 	Patch(ctx context.Context, resourceGroupName string, parameters resources.Group) (result resources.Group, err error)
 }
 
@@ -69,6 +73,7 @@ type ClientAPI interface {
 	Delete(ctx context.Context, resourceGroupName string, resourceProviderNamespace string, parentResourcePath string, resourceType string, resourceName string, APIVersion string) (result autorest.Response, err error)
 	Get(ctx context.Context, resourceGroupName string, resourceProviderNamespace string, parentResourcePath string, resourceType string, resourceName string, APIVersion string) (result resources.GenericResource, err error)
 	List(ctx context.Context, filter string, expand string, top *int32) (result resources.ListResultPage, err error)
+	ListComplete(ctx context.Context, filter string, expand string, top *int32) (result resources.ListResultIterator, err error)
 	MoveResources(ctx context.Context, sourceResourceGroupName string, parameters resources.MoveInfo) (result resources.MoveResourcesFuture, err error)
 	Update(ctx context.Context, resourceGroupName string, resourceProviderNamespace string, parentResourcePath string, resourceType string, resourceName string, APIVersion string, parameters resources.GenericResource) (result resources.UpdateFuture, err error)
 }
@@ -82,6 +87,7 @@ type TagsClientAPI interface {
 	Delete(ctx context.Context, tagName string) (result autorest.Response, err error)
 	DeleteValue(ctx context.Context, tagName string, tagValue string) (result autorest.Response, err error)
 	List(ctx context.Context) (result resources.TagsListResultPage, err error)
+	ListComplete(ctx context.Context) (result resources.TagsListResultIterator, err error)
 }
 
 var _ TagsClientAPI = (*resources.TagsClient)(nil)
@@ -90,6 +96,7 @@ var _ TagsClientAPI = (*resources.TagsClient)(nil)
 type DeploymentOperationsClientAPI interface {
 	Get(ctx context.Context, resourceGroupName string, deploymentName string, operationID string) (result resources.DeploymentOperation, err error)
 	List(ctx context.Context, resourceGroupName string, deploymentName string, top *int32) (result resources.DeploymentOperationsListResultPage, err error)
+	ListComplete(ctx context.Context, resourceGroupName string, deploymentName string, top *int32) (result resources.DeploymentOperationsListResultIterator, err error)
 }
 
 var _ DeploymentOperationsClientAPI = (*resources.DeploymentOperationsClient)(nil)

60001495aabce4027812735b59060895ec2f7cbc
SDK Automation <sdkautomation@microsoft.com>
2020-01-29 20:44:04 +0000

    Generated from 3692c0dfdf033256237ab43721f93d3442c0063c
    fix code style issues

--- services/resources/mgmt/2019-06-01/subscriptions/subscriptionsapi/interfaces.go
+++ services/resources/mgmt/2019-06-01/subscriptions/subscriptionsapi/interfaces.go
@@ -25,6 +25,7 @@ import (
 // OperationsClientAPI contains the set of methods on the OperationsClient type.
 type OperationsClientAPI interface {
 	List(ctx context.Context) (result subscriptions.OperationListResultPage, err error)
+	ListComplete(ctx context.Context) (result subscriptions.OperationListResultIterator, err error)
 }
 
 var _ OperationsClientAPI = (*subscriptions.OperationsClient)(nil)
@@ -33,6 +34,7 @@ var _ OperationsClientAPI = (*subscriptions.OperationsClient)(nil)
 type ClientAPI interface {
 	Get(ctx context.Context, subscriptionID string) (result subscriptions.Subscription, err error)
 	List(ctx context.Context) (result subscriptions.ListResultPage, err error)
+	ListComplete(ctx context.Context) (result subscriptions.ListResultIterator, err error)
 	ListLocations(ctx context.Context, subscriptionID string) (result subscriptions.LocationListResult, err error)
 }
 
@@ -41,6 +43,7 @@ var _ ClientAPI = (*subscriptions.Client)(nil)
 // TenantsClientAPI contains the set of methods on the TenantsClient type.
 type TenantsClientAPI interface {
 	List(ctx context.Context) (result subscriptions.TenantListResultPage, err error)
+	ListComplete(ctx context.Context) (result subscriptions.TenantListResultIterator, err error)
 }
 
 var _ TenantsClientAPI = (*subscriptions.TenantsClient)(nil)

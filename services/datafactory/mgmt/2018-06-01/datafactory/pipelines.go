d5129d531344e71b7cad2042f01f43c28635fc08
SDK Automation <sdkautomation@microsoft.com>
2020-02-21 09:52:56 +0000

    Generated from cce4898730132b8905bcef0bf5230dc55aeb23ad
    Revert "Salesforce add apiVersion and set securityToken optional"
    
    This reverts commit 233cd93f43a9e4e995834b608560316bbab54fbf.

--- services/datafactory/mgmt/2018-06-01/datafactory/pipelines.go
+++ services/datafactory/mgmt/2018-06-01/datafactory/pipelines.go
@@ -135,8 +135,7 @@ func (client PipelinesClient) CreateOrUpdatePreparer(ctx context.Context, resour
 // CreateOrUpdateSender sends the CreateOrUpdate request. The method will close the
 // http.Response Body if it receives an error.
 func (client PipelinesClient) CreateOrUpdateSender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-	return autorest.SendWithSender(client, req, sd...)
+	return client.Send(req, azure.DoRetryWithRegistration(client.Client))
 }
 
 // CreateOrUpdateResponder handles the response to the CreateOrUpdate request. The method always
@@ -257,8 +256,7 @@ func (client PipelinesClient) CreateRunPreparer(ctx context.Context, resourceGro
 // CreateRunSender sends the CreateRun request. The method will close the
 // http.Response Body if it receives an error.
 func (client PipelinesClient) CreateRunSender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-	return autorest.SendWithSender(client, req, sd...)
+	return client.Send(req, azure.DoRetryWithRegistration(client.Client))
 }
 
 // CreateRunResponder handles the response to the CreateRun request. The method always
@@ -352,8 +350,7 @@ func (client PipelinesClient) DeletePreparer(ctx context.Context, resourceGroupN
 // DeleteSender sends the Delete request. The method will close the
 // http.Response Body if it receives an error.
 func (client PipelinesClient) DeleteSender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-	return autorest.SendWithSender(client, req, sd...)
+	return client.Send(req, azure.DoRetryWithRegistration(client.Client))
 }
 
 // DeleteResponder handles the response to the Delete request. The method always
@@ -452,8 +449,7 @@ func (client PipelinesClient) GetPreparer(ctx context.Context, resourceGroupName
 // GetSender sends the Get request. The method will close the
 // http.Response Body if it receives an error.
 func (client PipelinesClient) GetSender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-	return autorest.SendWithSender(client, req, sd...)
+	return client.Send(req, azure.DoRetryWithRegistration(client.Client))
 }
 
 // GetResponder handles the response to the Get request. The method always
@@ -542,8 +538,7 @@ func (client PipelinesClient) ListByFactoryPreparer(ctx context.Context, resourc
 // ListByFactorySender sends the ListByFactory request. The method will close the
 // http.Response Body if it receives an error.
 func (client PipelinesClient) ListByFactorySender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-	return autorest.SendWithSender(client, req, sd...)
+	return client.Send(req, azure.DoRetryWithRegistration(client.Client))
 }
 
 // ListByFactoryResponder handles the response to the ListByFactory request. The method always

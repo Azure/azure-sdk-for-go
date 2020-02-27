8a0988c78ed214774c6a9fefc05c2b20f27ed80c
SDK Automation <sdkautomation@microsoft.com>
2020-02-27 07:47:34 +0000

    Generated from efbcb1f41f9db4d2e38193966776fab66cb7e301
    Merge branch 'master' into operationsApis

--- services/sql/mgmt/2014-04-01/sql/databaseconnectionpolicies.go
+++ services/sql/mgmt/2014-04-01/sql/databaseconnectionpolicies.go
@@ -114,8 +114,7 @@ func (client DatabaseConnectionPoliciesClient) CreateOrUpdatePreparer(ctx contex
 // CreateOrUpdateSender sends the CreateOrUpdate request. The method will close the
 // http.Response Body if it receives an error.
 func (client DatabaseConnectionPoliciesClient) CreateOrUpdateSender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-	return autorest.SendWithSender(client, req, sd...)
+	return client.Send(req, azure.DoRetryWithRegistration(client.Client))
 }
 
 // CreateOrUpdateResponder handles the response to the CreateOrUpdate request. The method always
@@ -196,8 +195,7 @@ func (client DatabaseConnectionPoliciesClient) GetPreparer(ctx context.Context, 
 // GetSender sends the Get request. The method will close the
 // http.Response Body if it receives an error.
 func (client DatabaseConnectionPoliciesClient) GetSender(req *http.Request) (*http.Response, error) {
-	sd := autorest.GetSendDecorators(req.Context(), azure.DoRetryWithRegistration(client.Client))
-	return autorest.SendWithSender(client, req, sd...)
+	return client.Send(req, azure.DoRetryWithRegistration(client.Client))
 }
 
 // GetResponder handles the response to the Get request. The method always

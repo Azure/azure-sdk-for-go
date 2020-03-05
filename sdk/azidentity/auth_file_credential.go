7009f95c0f81b8466ef28da317c8b82afedaa846
Luyunmt <2534523917@qq.com>
2020-03-05 09:21:46 +0800

    update comment and code according to conversation

--- sdk/azidentity/auth_file_credential.go
+++ sdk/azidentity/auth_file_credential.go
@@ -26,30 +26,26 @@ type authFileAccesstoken struct {
 	ManagementEndpointUrl          string `json:"managementEndpointUrl"`
 }
 
-// Enables authentication to Azure Active Directory using configuration information stored Azure SDK Auth File.
+// AuthFileCredential enables authentication to Azure Active Directory using configuration information stored Azure SDK Auth File.
 type AuthFileCredential struct {
 	filePath        string
 	credential      azcore.TokenCredential
 	authfileoptions *TokenCredentialOptions
 }
 
-// Creates an instance of the SdkAuthFileCredential class based on information in given SDK Auth file.
+// NewAuthFileCredential creates an instance of the AuthFileCredential class based on information in given SDK Auth file.
 func NewAuthFileCredential(filePath string, options *TokenCredentialOptions) (*AuthFileCredential, error) {
-	if filePath == "" {
-		return nil, fmt.Errorf("The parameter 'filePath' must set a value")
-	}
-
 	return &AuthFileCredential{filePath: filePath, authfileoptions: options}, nil
 }
 
-// Obtains a token from the Azure Active Directory service, using the specified client detailed specified in the SDK Auth file.
+// GetToken Obtains a token from the Azure Active Directory service, using the specified client detailed specified in the SDK Auth file.
 func (c *AuthFileCredential) GetToken(ctx context.Context, opts azcore.TokenRequestOptions) (*azcore.AccessToken, error) {
-	_, err := c.ensureCredential()
+	cred, err := c.ensureCredential()
 	if err != nil {
 		return nil, &AuthenticationFailedError{msg: "Error parsing SDK Auth File", inner: err}
 	}
 
-	return c.credential.GetToken(ctx, opts)
+	return cred.GetToken(ctx, opts)
 }
 
 // AuthenticationPolicy implements the azcore.Credential interface on AuthFileCredential.
@@ -57,16 +53,17 @@ func (c *AuthFileCredential) AuthenticationPolicy(options azcore.AuthenticationP
 	return newBearerTokenPolicy(c, options)
 }
 
-// Ensures that credential information is loaded from the SDK Auth file. This method should be called to initialize.
+// ensureCredential ensures that credential information is loaded from the SDK Auth file.
 func (c *AuthFileCredential) ensureCredential() (azcore.TokenCredential, error) {
 	if c.credential != nil {
 		return c.credential, nil
 	}
 
-	authData, err := c.parseCredentialsFile(c.filePath)
+	authData, err := ioutil.ReadFile(c.filePath)
 	if err != nil {
 		return nil, err
 	}
+
 
 	c.credential, err = c.buildCredentialForCredentialsFile(authData)
 	if err != nil {
@@ -76,17 +73,7 @@ func (c *AuthFileCredential) ensureCredential() (azcore.TokenCredential, error) 
 	return c.credential, nil
 }
 
-// Parse credential file from local file path to byte.
-func (c *AuthFileCredential) parseCredentialsFile(filePath string) ([]byte, error) {
-	authData, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	return authData, nil
-}
-
-// Use the SDK auth file info to build a ClientSecretCredential
+// buildCredentialForCredentialsFile use the SDK auth file info to build a ClientSecretCredential
 func (c *AuthFileCredential) buildCredentialForCredentialsFile(authData []byte) (*ClientSecretCredential, error) {
 	token := &authFileAccesstoken{}
 
@@ -99,10 +86,6 @@ func (c *AuthFileCredential) buildCredentialForCredentialsFile(authData []byte) 
 	clientSecret := token.ClientSecret
 	tenantId := token.TenantId
 	activeDirectoryEndpointUrl := token.ActiveDirectoryEndpointUrl
-
-	if clientId == "" || clientSecret == "" || tenantId == "" || activeDirectoryEndpointUrl == "" {
-		return nil, fmt.Errorf("Malformed Azure SDK Auth file. The file should contain 'clientId', 'clientSecret', 'tenantId' and 'activeDirectoryEndpointUrl' values.")
-	}
 
 	// Parse string activeDirectoryEndpointUrl to a Url.
 	AuthorityHost, err := url.Parse(activeDirectoryEndpointUrl)

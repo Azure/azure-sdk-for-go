8a6eaf3562f4b59e7685243c1c275cec8d5b67e5
SDK Automation <sdkautomation@microsoft.com>
2020-03-23 03:47:17 +0000

    Generated from 5e379c095815ecaaff3aaea4efb4446b77bd8707
    update cli readme per change in clicommon

--- services/preview/monitor/mgmt/2019-03-01/insights/vminsights.go
+++ services/preview/monitor/mgmt/2019-03-01/insights/vminsights.go
@@ -18,99 +18,99 @@ package insights
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-	"context"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/azure"
-	"github.com/Azure/go-autorest/tracing"
-	"net/http"
+    "github.com/Azure/go-autorest/autorest"
+    "github.com/Azure/go-autorest/autorest/azure"
+    "net/http"
+    "context"
+    "github.com/Azure/go-autorest/tracing"
 )
 
 // VMInsightsClient is the monitor Management Client
 type VMInsightsClient struct {
-	BaseClient
+    BaseClient
 }
-
 // NewVMInsightsClient creates an instance of the VMInsightsClient client.
 func NewVMInsightsClient(subscriptionID string) VMInsightsClient {
-	return NewVMInsightsClientWithBaseURI(DefaultBaseURI, subscriptionID)
+    return NewVMInsightsClientWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
 // NewVMInsightsClientWithBaseURI creates an instance of the VMInsightsClient client using a custom endpoint.  Use this
 // when interacting with an Azure cloud that uses a non-standard base URI (sovereign clouds, Azure stack).
-func NewVMInsightsClientWithBaseURI(baseURI string, subscriptionID string) VMInsightsClient {
-	return VMInsightsClient{NewWithBaseURI(baseURI, subscriptionID)}
-}
+    func NewVMInsightsClientWithBaseURI(baseURI string, subscriptionID string) VMInsightsClient {
+        return VMInsightsClient{ NewWithBaseURI(baseURI, subscriptionID)}
+    }
 
 // GetOnboardingStatus retrieves the VM Insights onboarding status for the specified resource or resource scope.
-// Parameters:
-// resourceURI - the fully qualified Azure Resource manager identifier of the resource, or scope, whose status
-// to retrieve.
+    // Parameters:
+        // resourceURI - the fully qualified Azure Resource manager identifier of the resource, or scope, whose status
+        // to retrieve.
 func (client VMInsightsClient) GetOnboardingStatus(ctx context.Context, resourceURI string) (result VMInsightsOnboardingStatus, err error) {
-	if tracing.IsEnabled() {
-		ctx = tracing.StartSpan(ctx, fqdn+"/VMInsightsClient.GetOnboardingStatus")
-		defer func() {
-			sc := -1
-			if result.Response.Response != nil {
-				sc = result.Response.Response.StatusCode
-			}
-			tracing.EndSpan(ctx, sc, err)
-		}()
-	}
-	req, err := client.GetOnboardingStatusPreparer(ctx, resourceURI)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "insights.VMInsightsClient", "GetOnboardingStatus", nil, "Failure preparing request")
-		return
-	}
+    if tracing.IsEnabled() {
+        ctx = tracing.StartSpan(ctx, fqdn + "/VMInsightsClient.GetOnboardingStatus")
+        defer func() {
+            sc := -1
+            if result.Response.Response != nil {
+                sc = result.Response.Response.StatusCode
+            }
+            tracing.EndSpan(ctx, sc, err)
+        }()
+    }
+        req, err := client.GetOnboardingStatusPreparer(ctx, resourceURI)
+    if err != nil {
+    err = autorest.NewErrorWithError(err, "insights.VMInsightsClient", "GetOnboardingStatus", nil , "Failure preparing request")
+    return
+    }
 
-	resp, err := client.GetOnboardingStatusSender(req)
-	if err != nil {
-		result.Response = autorest.Response{Response: resp}
-		err = autorest.NewErrorWithError(err, "insights.VMInsightsClient", "GetOnboardingStatus", resp, "Failure sending request")
-		return
-	}
+            resp, err := client.GetOnboardingStatusSender(req)
+            if err != nil {
+            result.Response = autorest.Response{Response: resp}
+            err = autorest.NewErrorWithError(err, "insights.VMInsightsClient", "GetOnboardingStatus", resp, "Failure sending request")
+            return
+            }
 
-	result, err = client.GetOnboardingStatusResponder(resp)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "insights.VMInsightsClient", "GetOnboardingStatus", resp, "Failure responding to request")
-	}
+            result, err = client.GetOnboardingStatusResponder(resp)
+            if err != nil {
+            err = autorest.NewErrorWithError(err, "insights.VMInsightsClient", "GetOnboardingStatus", resp, "Failure responding to request")
+            }
 
-	return
-}
+    return
+    }
 
-// GetOnboardingStatusPreparer prepares the GetOnboardingStatus request.
-func (client VMInsightsClient) GetOnboardingStatusPreparer(ctx context.Context, resourceURI string) (*http.Request, error) {
-	pathParameters := map[string]interface{}{
-		"resourceUri": resourceURI,
-	}
+    // GetOnboardingStatusPreparer prepares the GetOnboardingStatus request.
+    func (client VMInsightsClient) GetOnboardingStatusPreparer(ctx context.Context, resourceURI string) (*http.Request, error) {
+            pathParameters := map[string]interface{} {
+            "resourceUri": resourceURI,
+            }
 
-	const APIVersion = "2018-11-27-preview"
-	queryParameters := map[string]interface{}{
-		"api-version": APIVersion,
-	}
+                        const APIVersion = "2018-11-27-preview"
+        queryParameters := map[string]interface{} {
+        "api-version": APIVersion,
+        }
 
-	preparer := autorest.CreatePreparer(
-		autorest.AsGet(),
-		autorest.WithBaseURL(client.BaseURI),
-		autorest.WithPathParameters("/{resourceUri}/providers/Microsoft.Insights/vmInsightsOnboardingStatuses/default", pathParameters),
-		autorest.WithQueryParameters(queryParameters))
-	return preparer.Prepare((&http.Request{}).WithContext(ctx))
-}
+        preparer := autorest.CreatePreparer(
+    autorest.AsGet(),
+    autorest.WithBaseURL(client.BaseURI),
+    autorest.WithPathParameters("/{resourceUri}/providers/Microsoft.Insights/vmInsightsOnboardingStatuses/default",pathParameters),
+    autorest.WithQueryParameters(queryParameters))
+    return preparer.Prepare((&http.Request{}).WithContext(ctx))
+    }
 
-// GetOnboardingStatusSender sends the GetOnboardingStatus request. The method will close the
-// http.Response Body if it receives an error.
-func (client VMInsightsClient) GetOnboardingStatusSender(req *http.Request) (*http.Response, error) {
-	return client.Send(req, autorest.DoRetryForStatusCodes(client.RetryAttempts, client.RetryDuration, autorest.StatusCodesForRetry...))
-}
+    // GetOnboardingStatusSender sends the GetOnboardingStatus request. The method will close the
+    // http.Response Body if it receives an error.
+    func (client VMInsightsClient) GetOnboardingStatusSender(req *http.Request) (*http.Response, error) {
+            return client.Send(req, autorest.DoRetryForStatusCodes(client.RetryAttempts, client.RetryDuration, autorest.StatusCodesForRetry...))
+            }
 
 // GetOnboardingStatusResponder handles the response to the GetOnboardingStatus request. The method always
 // closes the http.Response Body.
 func (client VMInsightsClient) GetOnboardingStatusResponder(resp *http.Response) (result VMInsightsOnboardingStatus, err error) {
-	err = autorest.Respond(
-		resp,
-		client.ByInspecting(),
-		azure.WithErrorUnlessStatusCode(http.StatusOK),
-		autorest.ByUnmarshallingJSON(&result),
-		autorest.ByClosing())
-	result.Response = autorest.Response{Response: resp}
-	return
-}
+    err = autorest.Respond(
+    resp,
+    client.ByInspecting(),
+    azure.WithErrorUnlessStatusCode(http.StatusOK),
+    autorest.ByUnmarshallingJSON(&result),
+    autorest.ByClosing())
+    result.Response = autorest.Response{Response: resp}
+        return
+    }
+

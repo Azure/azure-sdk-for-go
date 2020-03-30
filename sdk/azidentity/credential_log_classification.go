84d5bfa84dee4aa3ca11ee509f5f4a964a219c8f
Catalina Peralta <caperal@microsoft.com>
2020-03-30 09:50:17 -0700

    Adding credential log classifications

---
+++ sdk/azidentity/credential_log_classification.go
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azidentity
+
+import (
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+)
+
+const (
+	CredentialClassification        azcore.LogClassification = "credential"
+	CredentialClassificationVerbose azcore.LogClassification = "credentialVerbose"
+)

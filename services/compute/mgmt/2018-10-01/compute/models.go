97572f8f47ace437bddbad09917694c618689431
SDK Automation <sdkautomation@microsoft.com>
2020-04-09 02:05:16 +0000

    Generated from 869affe0a30e518d4bdafe1299419efa3f442ad2
    Update grace period description to include maximum allowed grace period value

--- services/compute/mgmt/2018-10-01/compute/models.go
+++ services/compute/mgmt/2018-10-01/compute/models.go
@@ -1249,7 +1249,7 @@ type AutomaticOSUpgradeProperties struct {
 type AutomaticRepairsPolicy struct {
 	// Enabled - Specifies whether automatic repairs should be enabled on the virtual machine scale set. The default value is false.
 	Enabled *bool `json:"enabled,omitempty"`
-	// GracePeriod - The amount of time for which automatic repairs are suspended due to a state change on VM. The grace time starts after the state change has completed. This helps avoid premature or accidental repairs. The time duration should be specified in ISO 8601 format. The minimum allowed grace period is 30 minutes (PT30M), which is also the default value.
+	// GracePeriod - The amount of time for which automatic repairs are suspended due to a state change on VM. The grace time starts after the state change has completed. This helps avoid premature or accidental repairs. The time duration should be specified in ISO 8601 format. The minimum allowed grace period is 30 minutes (PT30M), which is also the default value. The maximum allowed grace period is 90 minutes (PT90M).
 	GracePeriod *string `json:"gracePeriod,omitempty"`
 }
 

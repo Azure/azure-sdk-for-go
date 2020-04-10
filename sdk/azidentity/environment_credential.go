3d6c6a8ea25049b153bf2c85edb8cc02e39ec8b4
Catalina Peralta <caperal@microsoft.com>
2020-04-10 15:56:04 -0700

    Adding logging for NewEnvironmentCredential() and fixing logging in Certificate and secret creds

--- sdk/azidentity/environment_credential.go
+++ sdk/azidentity/environment_credential.go
@@ -4,7 +4,10 @@
 package azidentity
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 )
 
 // NewEnvironmentCredential creates an instance of the EnvironmentCredential type and reads client secret details from environment variables.
@@ -12,19 +15,29 @@ import (
 // options: The options used to configure the management of the requests sent to the Azure Active Directory service.
 func NewEnvironmentCredential(options *TokenCredentialOptions) (*ClientSecretCredential, error) {
 	tenantID := os.Getenv("AZURE_TENANT_ID")
+	log := azcore.Log()
 	if tenantID == "" {
-		return nil, &CredentialUnavailableError{CredentialType: "Environment Credential", Message: "Missing environment variable AZURE_TENANT_ID"}
+		err := &CredentialUnavailableError{CredentialType: "Environment Credential", Message: "Missing environment variable AZURE_TENANT_ID"}
+		msg := fmt.Sprintf("Azure Identity => ERROR in NewEnvironmentCredential() call: %s", err.Error())
+		log.Write(azcore.LogError, msg)
+		return nil, err
 	}
 
 	clientID := os.Getenv("AZURE_CLIENT_ID")
 	if clientID == "" {
-		return nil, &CredentialUnavailableError{CredentialType: "Environment Credential", Message: "Missing environment variable AZURE_CLIENT_ID"}
+		err := &CredentialUnavailableError{CredentialType: "Environment Credential", Message: "Missing environment variable AZURE_CLIENT_ID"}
+		msg := fmt.Sprintf("Azure Identity => ERROR in NewEnvironmentCredential() call: %s", err.Error())
+		log.Write(azcore.LogError, msg)
+		return nil, err
 	}
 
 	clientSecret := os.Getenv("AZURE_CLIENT_SECRET")
 	if clientSecret == "" {
-		return nil, &CredentialUnavailableError{CredentialType: "Environment Credential", Message: "Missing environment variable AZURE_CLIENT_SECRET"}
+		err := &CredentialUnavailableError{CredentialType: "Environment Credential", Message: "Missing environment variable AZURE_CLIENT_SECRET"}
+		msg := fmt.Sprintf("Azure Identity => ERROR in NewEnvironmentCredential() call: %s", err.Error())
+		log.Write(azcore.LogError, msg)
+		return nil, err
 	}
-
+	log.Write(LogCredential, "Azure Identity => NewEnvironmentCredential() invoking ClientSecretCredential")
 	return NewClientSecretCredential(tenantID, clientID, clientSecret, options)
 }

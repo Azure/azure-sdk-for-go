e8fecfa7ac77817d6268c864aa4e19e73acb1d97
SDK Automation <sdkautomation@microsoft.com>
2020-06-03 00:01:54 +0000

    Generated from 1b645cbcbc97df8dec3a84aa1fb9c5e6e34ccbd9
    Update the code in order to unify the naming rules.

--- services/preview/hdinsight/2018-11-01-preview/hdinsight/hdinsightapi/interfaces.go
+++ services/preview/hdinsight/2018-11-01-preview/hdinsight/hdinsightapi/interfaces.go
@@ -25,15 +25,31 @@ import (
 
 // JobClientAPI contains the set of methods on the JobClient type.
 type JobClientAPI interface {
+	DeleteSparkBatch(ctx context.Context, batchID int32, requestedBy string) (result hdinsight.SparkJobDeletedResult, err error)
+	DeleteSparkSessionJob(ctx context.Context, sessionID int32, requestedBy string) (result hdinsight.SparkJobDeletedResult, err error)
+	DeleteSparkStatementJob(ctx context.Context, sessionID int32, statementID int32, requestedBy string) (result hdinsight.SparkStatementCancellationResult, err error)
 	Get(ctx context.Context, jobID string) (result hdinsight.JobDetailRootJSONObject, err error)
 	GetAppState(ctx context.Context, appID string) (result hdinsight.AppState, err error)
+	GetSparkBatchJob(ctx context.Context, batchID int32) (result hdinsight.SparkBatchJob, err error)
+	GetSparkBatchLog(ctx context.Context, batchID int32, from *int32, size *int32) (result hdinsight.SparkJobLog, err error)
+	GetSparkBatchState(ctx context.Context, batchID int32) (result hdinsight.SparkJobState, err error)
+	GetSparkSessionJob(ctx context.Context, sessionID int32) (result hdinsight.SparkSessionJob, err error)
+	GetSparkSessionLog(ctx context.Context, sessionID int32, from *int32, size *int32) (result hdinsight.SparkJobLog, err error)
+	GetSparkSessionState(ctx context.Context, sessionID int32) (result hdinsight.SparkJobState, err error)
+	GetSparkStatementJob(ctx context.Context, sessionID int32, statementID int32) (result hdinsight.SparkStatement, err error)
 	Kill(ctx context.Context, jobID string) (result hdinsight.JobDetailRootJSONObject, err error)
 	List(ctx context.Context) (result hdinsight.ListJobListJSONObject, err error)
 	ListAfterJobID(ctx context.Context, jobid string, numrecords *int32) (result hdinsight.ListJobListJSONObject, err error)
+	ListSparkBatchJob(ctx context.Context, from *int32, size *int32) (result hdinsight.SparkBatchJobCollection, err error)
+	ListSparkSessionJob(ctx context.Context, from *int32, size *int32) (result hdinsight.SparkSessionCollection, err error)
+	ListSparkStatementJob(ctx context.Context, sessionID int32) (result hdinsight.SparkStatementCollection, err error)
 	SubmitHiveJob(ctx context.Context, content io.ReadCloser) (result hdinsight.JobSubmissionJSONResponse, err error)
 	SubmitMapReduceJob(ctx context.Context, content io.ReadCloser) (result hdinsight.JobSubmissionJSONResponse, err error)
 	SubmitMapReduceStreamingJob(ctx context.Context, content io.ReadCloser) (result hdinsight.JobSubmissionJSONResponse, err error)
 	SubmitPigJob(ctx context.Context, content io.ReadCloser) (result hdinsight.JobSubmissionJSONResponse, err error)
+	SubmitSparkBatchJob(ctx context.Context, sparkBatchJobRequest hdinsight.SparkBatchJobRequest, requestedBy string) (result hdinsight.SparkBatchJob, err error)
+	SubmitSparkSessionJob(ctx context.Context, sparkSessionJobRequest hdinsight.SparkSessionJobRequest, requestedBy string) (result hdinsight.SparkSessionJob, err error)
+	SubmitSparkStatementJob(ctx context.Context, sessionID int32, sparkStatementRequest hdinsight.SparkStatementRequest, requestedBy string) (result hdinsight.SparkStatement, err error)
 	SubmitSqoopJob(ctx context.Context, content io.ReadCloser) (result hdinsight.JobSubmissionJSONResponse, err error)
 }
 

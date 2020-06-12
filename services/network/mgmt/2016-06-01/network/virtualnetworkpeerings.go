4e1afc0c0edc49f0bbefab90d5af7d90f27ab66c
SDK Automation <sdkautomation@microsoft.com>
2020-06-12 06:45:23 +0000

    Generated from cee292ec6bdb6666f99e7829f7048b4d0bb9bda8
    fix network python sdk

--- services/network/mgmt/2016-06-01/network/virtualnetworkpeerings.go
+++ services/network/mgmt/2016-06-01/network/virtualnetworkpeerings.go
@@ -115,7 +115,6 @@ func (client VirtualNetworkPeeringsClient) CreateOrUpdateSender(req *http.Reques
 func (client VirtualNetworkPeeringsClient) CreateOrUpdateResponder(resp *http.Response) (result VirtualNetworkPeering, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK, http.StatusCreated),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -193,7 +192,6 @@ func (client VirtualNetworkPeeringsClient) DeleteSender(req *http.Request) (futu
 func (client VirtualNetworkPeeringsClient) DeleteResponder(resp *http.Response) (result autorest.Response, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK, http.StatusAccepted, http.StatusNoContent),
 		autorest.ByClosing())
 	result.Response = resp
@@ -270,7 +268,6 @@ func (client VirtualNetworkPeeringsClient) GetSender(req *http.Request) (*http.R
 func (client VirtualNetworkPeeringsClient) GetResponder(resp *http.Response) (result VirtualNetworkPeering, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -347,7 +344,6 @@ func (client VirtualNetworkPeeringsClient) ListSender(req *http.Request) (*http.
 func (client VirtualNetworkPeeringsClient) ListResponder(resp *http.Response) (result VirtualNetworkPeeringListResult, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())

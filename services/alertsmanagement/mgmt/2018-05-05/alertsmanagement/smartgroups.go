e0ea00880ef7da72284cf530bc045236af921869
SDK Automation <sdkautomation@microsoft.com>
2020-06-15 09:11:49 +0000

    Generated from 1a427dcf2521c6b838a0bbb547f47bac3f1f6f94
    unhidden status

--- services/alertsmanagement/mgmt/2018-05-05/alertsmanagement/smartgroups.go
+++ services/alertsmanagement/mgmt/2018-05-05/alertsmanagement/smartgroups.go
@@ -109,7 +109,6 @@ func (client SmartGroupsClient) ChangeStateSender(req *http.Request) (*http.Resp
 func (client SmartGroupsClient) ChangeStateResponder(resp *http.Response) (result SmartGroup, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -229,7 +228,6 @@ func (client SmartGroupsClient) GetAllSender(req *http.Request) (*http.Response,
 func (client SmartGroupsClient) GetAllResponder(resp *http.Response) (result SmartGroupsList, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -340,7 +338,6 @@ func (client SmartGroupsClient) GetByIDSender(req *http.Request) (*http.Response
 func (client SmartGroupsClient) GetByIDResponder(resp *http.Response) (result SmartGroup, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -414,7 +411,6 @@ func (client SmartGroupsClient) GetHistorySender(req *http.Request) (*http.Respo
 func (client SmartGroupsClient) GetHistoryResponder(resp *http.Response) (result SmartGroupModification, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())

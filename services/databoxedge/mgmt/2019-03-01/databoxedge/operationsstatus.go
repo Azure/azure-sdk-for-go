02c6e88ae8c79be7d20fe2d5fe26bbf851399db9
SDK Automation <sdkautomation@microsoft.com>
2020-06-15 12:28:38 +0000

    Generated from 018162a4fdd3f0db191dbb826c6a7a757ed31f02
    add readme.trenton.md for RP Databoxedge

--- services/databoxedge/mgmt/2019-03-01/databoxedge/operationsstatus.go
+++ services/databoxedge/mgmt/2019-03-01/databoxedge/operationsstatus.go
@@ -112,7 +112,6 @@ func (client OperationsStatusClient) GetSender(req *http.Request) (*http.Respons
 func (client OperationsStatusClient) GetResponder(resp *http.Response) (result Job, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())

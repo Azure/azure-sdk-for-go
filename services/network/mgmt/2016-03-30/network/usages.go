769b6a25d16d8c2793bfc1d3f48f4341b3f6ad51
SDK Automation <sdkautomation@microsoft.com>
2020-06-18 13:52:21 +0000

    Generated from d7968ec7ebcea57cd339ab7383ced0c1f09a2840
    removed format for RoundTripTime

--- services/network/mgmt/2016-03-30/network/usages.go
+++ services/network/mgmt/2016-03-30/network/usages.go
@@ -114,7 +114,6 @@ func (client UsagesClient) ListSender(req *http.Request) (*http.Response, error)
 func (client UsagesClient) ListResponder(resp *http.Response) (result UsagesListResult, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())

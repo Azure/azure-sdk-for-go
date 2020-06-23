2581d0098b59b80e303f0e079b22b54b5ed19f33
Joel Hendrix <jhendrix@microsoft.com>
2020-06-23 08:52:14 -0700

    Go mod tidy hack for internal an to modules

--- sdk/internal/go.mod
+++ sdk/internal/go.mod
@@ -1,3 +1,5 @@
 module github.com/Azure/azure-sdk-for-go/sdk/internal
 
 go 1.13
+
+require github.com/Azure/azure-sdk-for-go v43.3.0+incompatible
---
+++ sdk/internal/go_mod_tidy_hack.go
@@ -0,0 +1,10 @@
+// +build modhack
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package internal
+
+// Necessary for safely adding multi-module repo.
+// See: https://github.com/golang/go/wiki/Modules#is-it-possible-to-add-a-module-to-a-multi-module-repository
+import _ "github.com/Azure/azure-sdk-for-go"

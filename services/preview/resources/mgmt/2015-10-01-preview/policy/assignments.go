ae10082f6d884808fc10092cc9460571f0b8aa5b
SDK Automation <sdkautomation@microsoft.com>
2020-07-02 14:06:15 +0000

    Generated from eb15028b83aacec7e74babd82f2566324275916f
    Template Specs: Dropping "Model" suffix from TemplateSpec and TemplateSpecVersion

--- services/preview/resources/mgmt/2015-10-01-preview/policy/assignments.go
+++ services/preview/resources/mgmt/2015-10-01-preview/policy/assignments.go
@@ -114,7 +114,6 @@ func (client AssignmentsClient) CreateSender(req *http.Request) (*http.Response,
 func (client AssignmentsClient) CreateResponder(resp *http.Response) (result Assignment, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK, http.StatusCreated),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -196,7 +195,6 @@ func (client AssignmentsClient) CreateByIDSender(req *http.Request) (*http.Respo
 func (client AssignmentsClient) CreateByIDResponder(resp *http.Response) (result Assignment, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK, http.StatusCreated),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -271,7 +269,6 @@ func (client AssignmentsClient) DeleteSender(req *http.Request) (*http.Response,
 func (client AssignmentsClient) DeleteResponder(resp *http.Response) (result Assignment, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -348,7 +345,6 @@ func (client AssignmentsClient) DeleteByIDSender(req *http.Request) (*http.Respo
 func (client AssignmentsClient) DeleteByIDResponder(resp *http.Response) (result Assignment, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -423,7 +419,6 @@ func (client AssignmentsClient) GetSender(req *http.Request) (*http.Response, er
 func (client AssignmentsClient) GetResponder(resp *http.Response) (result Assignment, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -500,7 +495,6 @@ func (client AssignmentsClient) GetByIDSender(req *http.Request) (*http.Response
 func (client AssignmentsClient) GetByIDResponder(resp *http.Response) (result Assignment, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -577,7 +571,6 @@ func (client AssignmentsClient) ListSender(req *http.Request) (*http.Response, e
 func (client AssignmentsClient) ListResponder(resp *http.Response) (result AssignmentListResult, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -709,7 +702,6 @@ func (client AssignmentsClient) ListForResourceSender(req *http.Request) (*http.
 func (client AssignmentsClient) ListForResourceResponder(resp *http.Response) (result AssignmentListResult, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -833,7 +825,6 @@ func (client AssignmentsClient) ListForResourceGroupSender(req *http.Request) (*
 func (client AssignmentsClient) ListForResourceGroupResponder(resp *http.Response) (result AssignmentListResult, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())

1fc9c47490589064068ad4f15fd259011482b201
Joel Hendrix <jhendrix@microsoft.com>
2020-07-09 16:24:24 -0700

    Add HTTPResponse interface (#11428)
    * Add HTTPResponse interface
    
    To be used by callers to access the raw HTTP response from an error in
    the event of an API call failure.
    Updated sdk/internal dependency to latest version.
    
    * rename package alias

--- sdk/azcore/error.go
+++ sdk/azcore/error.go
@@ -7,7 +7,9 @@ package azcore
 
 import (
 	"errors"
-	"fmt"
+	"net/http"
+
+	sdkruntime "github.com/Azure/azure-sdk-for-go/sdk/internal/runtime"
 )
 
 var (
@@ -15,39 +17,19 @@ var (
 	ErrNoMorePolicies = errors.New("no more policies")
 )
 
-// TODO: capture frame info for marshal, unmarshal, and parsing errors
-// built in frame in xerror?  %w
-type frameInfo struct {
-	file string
-	line int
+var (
+	// StackFrameCount contains the number of stack frames to include when a trace is being collected.
+	StackFrameCount = 32
+)
+
+// HTTPResponse provides access to an HTTP response when available.
+// Errors returned from failed API calls will implement this interface.
+// Use errors.As() to access this interface in the error chain.
+// If there was no HTTP response then this interface will be omitted
+// from any error in the chain.
+type HTTPResponse interface {
+	RawResponse() *http.Response
 }
 
-func (f frameInfo) String() string {
-	if f.zero() {
-		return ""
-	}
-	return fmt.Sprintf("file: %s, line: %d", f.file, f.line)
-}
-
-func (f frameInfo) zero() bool {
-	return f.file == "" && f.line == 0
-}
-
-// RequestError is returned when the service returns an unsuccessful resopnse code (4xx, 5xx).
-type RequestError struct {
-	msg  string
-	resp *Response
-}
-
-func newRequestError(message string, response *Response) error {
-	return RequestError{msg: message, resp: response}
-}
-
-func (re RequestError) Error() string {
-	return re.msg
-}
-
-// Response returns the underlying response.
-func (re RequestError) Response() *Response {
-	return re.resp
-}
+// ensure our internal ResponseError type implements HTTPResponse
+var _ HTTPResponse = (*sdkruntime.ResponseError)(nil)
--- sdk/azcore/go.mod
+++ sdk/azcore/go.mod
@@ -1,5 +1,5 @@
 module github.com/Azure/azure-sdk-for-go/sdk/azcore
 
-require github.com/Azure/azure-sdk-for-go/sdk/internal v0.1.1
+require github.com/Azure/azure-sdk-for-go/sdk/internal v0.2.0
 
 go 1.13

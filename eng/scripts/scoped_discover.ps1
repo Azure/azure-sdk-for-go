b94025ee84d16dcfa2caea62c309b0e2be4c191d
Joel Hendrix <jhendrix@microsoft.com>
2020-07-20 15:37:06 -0700

    Build in module-aware mode
    Get the list of module directories.  For each directory, do
    build/vet/test.

--- eng/scripts/scoped_discover.ps1
+++ eng/scripts/scoped_discover.ps1
@@ -10,5 +10,10 @@ else {
 }
 
 $path = Resolve-Path -Path $targetDir
+$modDirs = [System.Collections.ArrayList]@()
 
-return "$path/..."
+# find each module directory under $path
+Get-Childitem -recurse -path $path -filter go.mod | foreach-object { $modDirs += $_.Directory }
+
+# return the list of module directories
+return $modDirs

20e6fe3ce2fe6850cfa0d010b069cee8debf46a2
Joel Hendrix <jhendrix@microsoft.com>
2020-07-21 08:03:03 -0700

    add some logging

--- eng/scripts/scoped_discover.ps1
+++ eng/scripts/scoped_discover.ps1
@@ -13,7 +13,11 @@ $path = Resolve-Path -Path $targetDir
 $modDirs = [System.Collections.ArrayList]@()
 
 # find each module directory under $path
-Get-Childitem -recurse -path $path -filter go.mod | foreach-object { $modDirs += $_.Directory }
+Get-Childitem -recurse -path $path -filter go.mod | foreach-object {
+  $cdir = $_.Directory
+  Write-Host "Adding $cdir to list of module paths"
+  $modDirs += $cdir
+}
 
 # return the list of module directories
 return $modDirs

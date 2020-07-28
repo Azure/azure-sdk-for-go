0318b944604bdb2dd820d2e304f450c3b87f55a6
SDK Automation <sdkautomation@microsoft.com>
2020-07-28 06:58:37 +0000

    Generated from e63a15f4648deddf32ddf62d53c5b6eefc250ca1
    Fixing errors in model

--- services/databricks/mgmt/2018-04-01/databricks/databricksapi/interfaces.go
+++ services/databricks/mgmt/2018-04-01/databricks/databricksapi/interfaces.go
@@ -36,6 +36,17 @@ type WorkspacesClientAPI interface {
 
 var _ WorkspacesClientAPI = (*databricks.WorkspacesClient)(nil)
 
+// VNetPeeringClientAPI contains the set of methods on the VNetPeeringClient type.
+type VNetPeeringClientAPI interface {
+	CreateOrUpdate(ctx context.Context, virtualNetworkPeeringParameters databricks.VirtualNetworkPeering, resourceGroupName string, workspaceName string, peeringName string) (result databricks.VNetPeeringCreateOrUpdateFuture, err error)
+	Delete(ctx context.Context, resourceGroupName string, workspaceName string, peeringName string) (result databricks.VNetPeeringDeleteFuture, err error)
+	Get(ctx context.Context, resourceGroupName string, workspaceName string, peeringName string) (result databricks.VirtualNetworkPeering, err error)
+	ListByWorkspace(ctx context.Context, resourceGroupName string, workspaceName string) (result databricks.VirtualNetworkPeeringListPage, err error)
+	ListByWorkspaceComplete(ctx context.Context, resourceGroupName string, workspaceName string) (result databricks.VirtualNetworkPeeringListIterator, err error)
+}
+
+var _ VNetPeeringClientAPI = (*databricks.VNetPeeringClient)(nil)
+
 // OperationsClientAPI contains the set of methods on the OperationsClient type.
 type OperationsClientAPI interface {
 	List(ctx context.Context) (result databricks.OperationListResultPage, err error)

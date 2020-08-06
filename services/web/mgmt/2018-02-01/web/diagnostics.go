8afe99399f6741471021b99a333ee4313fc12598
SDK Automation <sdkautomation@microsoft.com>
2020-08-06 04:55:35 +0000

    Generated from e5ce9c76c6fe095af71dc4fbd6a47c35c419c437
    Add 2020-06-01 for web

--- services/web/mgmt/2018-02-01/web/diagnostics.go
+++ services/web/mgmt/2018-02-01/web/diagnostics.go
@@ -138,7 +138,6 @@ func (client DiagnosticsClient) ExecuteSiteAnalysisSender(req *http.Request) (*h
 func (client DiagnosticsClient) ExecuteSiteAnalysisResponder(resp *http.Response) (result DiagnosticAnalysis, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -243,7 +242,6 @@ func (client DiagnosticsClient) ExecuteSiteAnalysisSlotSender(req *http.Request)
 func (client DiagnosticsClient) ExecuteSiteAnalysisSlotResponder(resp *http.Response) (result DiagnosticAnalysis, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -346,7 +344,6 @@ func (client DiagnosticsClient) ExecuteSiteDetectorSender(req *http.Request) (*h
 func (client DiagnosticsClient) ExecuteSiteDetectorResponder(resp *http.Response) (result DiagnosticDetectorResponse, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -451,7 +448,6 @@ func (client DiagnosticsClient) ExecuteSiteDetectorSlotSender(req *http.Request)
 func (client DiagnosticsClient) ExecuteSiteDetectorSlotResponder(resp *http.Response) (result DiagnosticDetectorResponse, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -552,7 +548,6 @@ func (client DiagnosticsClient) GetHostingEnvironmentDetectorResponseSender(req 
 func (client DiagnosticsClient) GetHostingEnvironmentDetectorResponseResponder(resp *http.Response) (result DetectorResponse, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -640,7 +635,6 @@ func (client DiagnosticsClient) GetSiteAnalysisSender(req *http.Request) (*http.
 func (client DiagnosticsClient) GetSiteAnalysisResponder(resp *http.Response) (result DiagnosticAnalysis, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -730,7 +724,6 @@ func (client DiagnosticsClient) GetSiteAnalysisSlotSender(req *http.Request) (*h
 func (client DiagnosticsClient) GetSiteAnalysisSlotResponder(resp *http.Response) (result DiagnosticAnalysis, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -781,6 +774,9 @@ func (client DiagnosticsClient) GetSiteDetector(ctx context.Context, resourceGro
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "GetSiteDetector", resp, "Failure responding to request")
 	}
+	if result.ddc.hasNextLink() && result.ddc.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -819,7 +815,6 @@ func (client DiagnosticsClient) GetSiteDetectorSender(req *http.Request) (*http.
 func (client DiagnosticsClient) GetSiteDetectorResponder(resp *http.Response) (result DiagnosticDetectorCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -957,7 +952,6 @@ func (client DiagnosticsClient) GetSiteDetectorResponseSender(req *http.Request)
 func (client DiagnosticsClient) GetSiteDetectorResponseResponder(resp *http.Response) (result DetectorResponse, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -1060,7 +1054,6 @@ func (client DiagnosticsClient) GetSiteDetectorResponseSlotSender(req *http.Requ
 func (client DiagnosticsClient) GetSiteDetectorResponseSlotResponder(resp *http.Response) (result DetectorResponse, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -1112,6 +1105,9 @@ func (client DiagnosticsClient) GetSiteDetectorSlot(ctx context.Context, resourc
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "GetSiteDetectorSlot", resp, "Failure responding to request")
 	}
+	if result.ddc.hasNextLink() && result.ddc.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -1151,7 +1147,6 @@ func (client DiagnosticsClient) GetSiteDetectorSlotSender(req *http.Request) (*h
 func (client DiagnosticsClient) GetSiteDetectorSlotResponder(resp *http.Response) (result DiagnosticDetectorCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -1274,7 +1269,6 @@ func (client DiagnosticsClient) GetSiteDiagnosticCategorySender(req *http.Reques
 func (client DiagnosticsClient) GetSiteDiagnosticCategoryResponder(resp *http.Response) (result DiagnosticCategory, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -1362,7 +1356,6 @@ func (client DiagnosticsClient) GetSiteDiagnosticCategorySlotSender(req *http.Re
 func (client DiagnosticsClient) GetSiteDiagnosticCategorySlotResponder(resp *http.Response) (result DiagnosticCategory, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -1411,6 +1404,9 @@ func (client DiagnosticsClient) ListHostingEnvironmentDetectorResponses(ctx cont
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "ListHostingEnvironmentDetectorResponses", resp, "Failure responding to request")
 	}
+	if result.drc.hasNextLink() && result.drc.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -1447,7 +1443,6 @@ func (client DiagnosticsClient) ListHostingEnvironmentDetectorResponsesSender(re
 func (client DiagnosticsClient) ListHostingEnvironmentDetectorResponsesResponder(resp *http.Response) (result DetectorResponseCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -1534,6 +1529,9 @@ func (client DiagnosticsClient) ListSiteAnalyses(ctx context.Context, resourceGr
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "ListSiteAnalyses", resp, "Failure responding to request")
 	}
+	if result.dac.hasNextLink() && result.dac.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -1571,7 +1569,6 @@ func (client DiagnosticsClient) ListSiteAnalysesSender(req *http.Request) (*http
 func (client DiagnosticsClient) ListSiteAnalysesResponder(resp *http.Response) (result DiagnosticAnalysisCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -1659,6 +1656,9 @@ func (client DiagnosticsClient) ListSiteAnalysesSlot(ctx context.Context, resour
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "ListSiteAnalysesSlot", resp, "Failure responding to request")
 	}
+	if result.dac.hasNextLink() && result.dac.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -1697,7 +1697,6 @@ func (client DiagnosticsClient) ListSiteAnalysesSlotSender(req *http.Request) (*
 func (client DiagnosticsClient) ListSiteAnalysesSlotResponder(resp *http.Response) (result DiagnosticAnalysisCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -1783,6 +1782,9 @@ func (client DiagnosticsClient) ListSiteDetectorResponses(ctx context.Context, r
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "ListSiteDetectorResponses", resp, "Failure responding to request")
 	}
+	if result.drc.hasNextLink() && result.drc.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -1819,7 +1821,6 @@ func (client DiagnosticsClient) ListSiteDetectorResponsesSender(req *http.Reques
 func (client DiagnosticsClient) ListSiteDetectorResponsesResponder(resp *http.Response) (result DetectorResponseCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -1906,6 +1907,9 @@ func (client DiagnosticsClient) ListSiteDetectorResponsesSlot(ctx context.Contex
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "ListSiteDetectorResponsesSlot", resp, "Failure responding to request")
 	}
+	if result.drc.hasNextLink() && result.drc.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -1943,7 +1947,6 @@ func (client DiagnosticsClient) ListSiteDetectorResponsesSlotSender(req *http.Re
 func (client DiagnosticsClient) ListSiteDetectorResponsesSlotResponder(resp *http.Response) (result DetectorResponseCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -2030,6 +2033,9 @@ func (client DiagnosticsClient) ListSiteDetectors(ctx context.Context, resourceG
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "ListSiteDetectors", resp, "Failure responding to request")
 	}
+	if result.ddc.hasNextLink() && result.ddc.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -2067,7 +2073,6 @@ func (client DiagnosticsClient) ListSiteDetectorsSender(req *http.Request) (*htt
 func (client DiagnosticsClient) ListSiteDetectorsResponder(resp *http.Response) (result DiagnosticDetectorCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -2155,6 +2160,9 @@ func (client DiagnosticsClient) ListSiteDetectorsSlot(ctx context.Context, resou
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "ListSiteDetectorsSlot", resp, "Failure responding to request")
 	}
+	if result.ddc.hasNextLink() && result.ddc.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -2193,7 +2201,6 @@ func (client DiagnosticsClient) ListSiteDetectorsSlotSender(req *http.Request) (
 func (client DiagnosticsClient) ListSiteDetectorsSlotResponder(resp *http.Response) (result DiagnosticDetectorCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -2279,6 +2286,9 @@ func (client DiagnosticsClient) ListSiteDiagnosticCategories(ctx context.Context
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "ListSiteDiagnosticCategories", resp, "Failure responding to request")
 	}
+	if result.dcc.hasNextLink() && result.dcc.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -2315,7 +2325,6 @@ func (client DiagnosticsClient) ListSiteDiagnosticCategoriesSender(req *http.Req
 func (client DiagnosticsClient) ListSiteDiagnosticCategoriesResponder(resp *http.Response) (result DiagnosticCategoryCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())
@@ -2402,6 +2411,9 @@ func (client DiagnosticsClient) ListSiteDiagnosticCategoriesSlot(ctx context.Con
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "web.DiagnosticsClient", "ListSiteDiagnosticCategoriesSlot", resp, "Failure responding to request")
 	}
+	if result.dcc.hasNextLink() && result.dcc.IsEmpty() {
+		err = result.NextWithContext(ctx)
+	}
 
 	return
 }
@@ -2439,7 +2451,6 @@ func (client DiagnosticsClient) ListSiteDiagnosticCategoriesSlotSender(req *http
 func (client DiagnosticsClient) ListSiteDiagnosticCategoriesSlotResponder(resp *http.Response) (result DiagnosticCategoryCollection, err error) {
 	err = autorest.Respond(
 		resp,
-		client.ByInspecting(),
 		azure.WithErrorUnlessStatusCode(http.StatusOK),
 		autorest.ByUnmarshallingJSON(&result),
 		autorest.ByClosing())

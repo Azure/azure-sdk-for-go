7676f031354c1a0a28d455c67c09cbbc62d3efe9
SDK Automation <sdkautomation@microsoft.com>
2020-08-07 07:14:57 +0000

    Generated from 96c0e4788da79889df9bd5534c6b77c845b2c7fc
    slove LRO for IR

--- services/preview/synapse/mgmt/2019-06-01-preview/synapse/models.go
+++ services/preview/synapse/mgmt/2019-06-01-preview/synapse/models.go
@@ -2137,6 +2137,58 @@ func (irr *IntegrationRuntimeResource) UnmarshalJSON(body []byte) error {
 	return nil
 }
 
+// IntegrationRuntimesCreateFuture an abstraction for monitoring and retrieving the results of a long-running
+// operation.
+type IntegrationRuntimesCreateFuture struct {
+	azure.Future
+}
+
+// Result returns the result of the asynchronous operation.
+// If the operation has not completed it will return an error.
+func (future *IntegrationRuntimesCreateFuture) Result(client IntegrationRuntimesClient) (irr IntegrationRuntimeResource, err error) {
+	var done bool
+	done, err = future.DoneWithContext(context.Background(), client)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "synapse.IntegrationRuntimesCreateFuture", "Result", future.Response(), "Polling failure")
+		return
+	}
+	if !done {
+		err = azure.NewAsyncOpIncompleteError("synapse.IntegrationRuntimesCreateFuture")
+		return
+	}
+	sender := autorest.DecorateSender(client, autorest.DoRetryForStatusCodes(client.RetryAttempts, client.RetryDuration, autorest.StatusCodesForRetry...))
+	if irr.Response.Response, err = future.GetResult(sender); err == nil && irr.Response.Response.StatusCode != http.StatusNoContent {
+		irr, err = client.CreateResponder(irr.Response.Response)
+		if err != nil {
+			err = autorest.NewErrorWithError(err, "synapse.IntegrationRuntimesCreateFuture", "Result", irr.Response.Response, "Failure responding to request")
+		}
+	}
+	return
+}
+
+// IntegrationRuntimesDeleteFuture an abstraction for monitoring and retrieving the results of a long-running
+// operation.
+type IntegrationRuntimesDeleteFuture struct {
+	azure.Future
+}
+
+// Result returns the result of the asynchronous operation.
+// If the operation has not completed it will return an error.
+func (future *IntegrationRuntimesDeleteFuture) Result(client IntegrationRuntimesClient) (ar autorest.Response, err error) {
+	var done bool
+	done, err = future.DoneWithContext(context.Background(), client)
+	if err != nil {
+		err = autorest.NewErrorWithError(err, "synapse.IntegrationRuntimesDeleteFuture", "Result", future.Response(), "Polling failure")
+		return
+	}
+	if !done {
+		err = azure.NewAsyncOpIncompleteError("synapse.IntegrationRuntimesDeleteFuture")
+		return
+	}
+	ar.Response = future.Response()
+	return
+}
+
 // IntegrationRuntimeSsisCatalogInfo catalog information for managed dedicated integration runtime.
 type IntegrationRuntimeSsisCatalogInfo struct {
 	// AdditionalProperties - Unmatched properties from the message are deserialized this collection

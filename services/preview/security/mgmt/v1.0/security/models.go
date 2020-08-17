30b448b79e69e5493df7ad7ca932a744811f76b0
SDK Automation <sdkautomation@microsoft.com>
2020-08-17 17:07:31 +0000

    Generated from 432f6fb786883a359d426791598f1e41463f55f1
    Added weight property for secureScores type

--- services/preview/security/mgmt/v1.0/security/models.go
+++ services/preview/security/mgmt/v1.0/security/models.go
@@ -8412,6 +8412,8 @@ type SecureScoreItemProperties struct {
 	DisplayName *string `json:"displayName,omitempty"`
 	// ScoreDetails - READ-ONLY; score object
 	*ScoreDetails `json:"score,omitempty"`
+	// Weight - READ-ONLY; The weight for calculation of an aggregated score for several scopes
+	Weight *int64 `json:"weight,omitempty"`
 }
 
 // MarshalJSON is the custom marshaler for SecureScoreItemProperties.
@@ -8446,6 +8448,15 @@ func (ssip *SecureScoreItemProperties) UnmarshalJSON(body []byte) error {
 					return err
 				}
 				ssip.ScoreDetails = &scoreDetails
+			}
+		case "weight":
+			if v != nil {
+				var weight int64
+				err = json.Unmarshal(*v, &weight)
+				if err != nil {
+					return err
+				}
+				ssip.Weight = &weight
 			}
 		}
 	}

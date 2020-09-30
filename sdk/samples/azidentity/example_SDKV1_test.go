b4ec4be104027815e94fb72ca5bf017c6caf3e14
Catalina Peralta <catalinaperaltah@hotmail.com>
2020-09-30 12:03:34 -0700

    Updating comments

--- sdk/samples/azidentity/example_SDKV1_test.go
+++ sdk/samples/azidentity/example_SDKV1_test.go
@@ -73,7 +73,7 @@ func ExampleGroupsClientWithDefaultAzureCredential() {
 // SDK V1 authorizer.
 // NewAzureIdentityCredentialAdapter can take any credential type defined in azidentity and convert it to an authorizer that is compatible with the Azure SDK for Go
 // V1 implementation. For a list of the credentials that azidentity includes, please see: https://github.com/Azure/azure-sdk-for-go/tree/master/sdk/azidentity.
-// NewClientSecretCredential can be used in place of any authorizer that supports client credentials.
+// NewClientSecretCredential can be used in place of auth.NewClientCredentialsConfig().
 func ExampleGroupsClientWithClientSecretCredential() {
 	groupsClient := resources.NewGroupsClient(subscriptionID)
 	// instantiate a new ClientSecretCredential as specified in the documentation
@@ -111,7 +111,7 @@ func ExampleGroupsClientWithClientSecretCredential() {
 // SDK V1 authorizer.
 // NewAzureIdentityCredentialAdapter can take any credential type defined in azidentity and convert it to an authorizer that is compatible with the Azure SDK for Go
 // V1 implementation. For a list of the credentials that azidentity includes, please see: https://github.com/Azure/azure-sdk-for-go/tree/master/sdk/azidentity.
-// NewEnvironmentCredential can be used in place of NewAuthorizerFromEnvironment. An important distinction is that NewEnvironmentCredential does not include Managed
+// NewEnvironmentCredential can be used in place of auth.NewAuthorizerFromEnvironment(). An important distinction is that NewEnvironmentCredential does not include Managed
 // Identity credential, for a credential that also checks the environment for Managed Identity credential use the NewDefaultAzureCredentialAdapter. Alternatively,
 // create a custom credential chain with NewChainedTokenCredential and add all desired token credentials to try into the chain.
 func ExampleGroupsClientWithEnvironmentCredential() {

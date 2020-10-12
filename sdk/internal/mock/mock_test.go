431eb28940a085e68f319719c895e174912d1389
Catalina Peralta <catalinaperaltah@hotmail.com>
2020-10-12 10:08:50 -0700

    Add test for TLSHTTP2Server

--- sdk/internal/mock/mock_test.go
+++ sdk/internal/mock/mock_test.go
@@ -262,6 +262,40 @@ func TestComplexResponseTLS(t *testing.T) {
 	}
 }
 
+func TestComplexResponseTLSHTTP2(t *testing.T) {
+	srv, close := NewTLSHTTP2Server()
+	defer close()
+	const body = "this is the response body"
+	srv.AppendResponse(
+		WithStatusCode(http.StatusOK),
+		WithBody([]byte(body)),
+		WithHeader("some", "value"),
+		WithSlowResponse(2*time.Second),
+	)
+	req, err := http.NewRequest(http.MethodGet, srv.URL(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := srv.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("unexpected status code %d", resp.StatusCode)
+	}
+	if h := resp.Header.Get("some"); h != "value" {
+		t.Fatalf("unexpected header value %s", h)
+	}
+	r, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp.Body.Close()
+	if string(r) != body {
+		t.Fatalf("unexpected response body %s", string(r))
+	}
+}
+
 func TestBodyReadError(t *testing.T) {
 	srv, close := NewServer()
 	defer close()

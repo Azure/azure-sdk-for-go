c42e4e5a90cad716d0db85234fbdfbac08cfb7b6
Dapeng Zhang <dapzhang@microsoft.com>
2020-10-12 16:55:16 +0800

    Move hybridcompute/2019-12-12 out of preview since it should be stable

--- services/hybridcompute/mgmt/2019-12-12/hybridcompute/hybridcomputeapi/interfaces.go
+++ services/hybridcompute/mgmt/2019-12-12/hybridcompute/hybridcomputeapi/interfaces.go
@@ -19,7 +19,7 @@ package hybridcomputeapi
 
 import (
 	"context"
-	"github.com/Azure/azure-sdk-for-go/services/preview/hybridcompute/mgmt/2019-12-12/hybridcompute"
+	"github.com/Azure/azure-sdk-for-go/services/hybridcompute/mgmt/2019-12-12/hybridcompute"
 	"github.com/Azure/go-autorest/autorest"
 )
 

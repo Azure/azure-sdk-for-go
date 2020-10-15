af7751811be4a73dc25f40b0e530fbbe98e14687
Joel Hendrix <jhendrix@microsoft.com>
2020-10-15 08:02:25 -0700

    Added docs and updated dependencies (#12778)

---
+++ sdk/armcore/doc.go
@@ -0,0 +1,12 @@
+// +build go1.13
+
+// Copyright 2017 Microsoft Corporation. All rights reserved.
+// Use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
+
+// Package armcore implements various helpers for Azure Resource Manager APIs.
+//
+// You do not need to explicitly import this module.  It will automatically be included as a dependency for all ARM modules.
+//
+// The contents of this module are used by generated code.  It is not intended for external use.
+package armcore
--- sdk/armcore/go.mod
+++ sdk/armcore/go.mod
@@ -3,6 +3,6 @@ module github.com/Azure/azure-sdk-for-go/sdk/armcore
 go 1.14
 
 require (
-	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.12.0
-	github.com/Azure/azure-sdk-for-go/sdk/internal v0.3.0
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.13.0
+	github.com/Azure/azure-sdk-for-go/sdk/internal v0.5.0
 )

e9751ee9d9a4b8fb2bf1fbbeba6a3ed2183d3fa1
Catalina Peralta <catalinaperaltah@hotmail.com>
2020-10-21 15:39:19 -0700

    Adding manual-tests directory for managed identity e2e tests

---
+++ sdk/samples/azidentity/manual-tests/webjobs/go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.15
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.13.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v0.4.3
+)
---
+++ sdk/samples/azidentity/manual-tests/webjobs/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+)
+
+func main() {
+	cred, err := azidentity.NewManagedIdentityCredential("", nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Calling GetToken()...")
+	tk, err := cred.GetToken(context.Background(), azcore.TokenRequestOptions{Scopes: []string{"https://vault.azure.net"}})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(fmt.Sprintf("Success! Token received %v", tk))
+}

fb5bb7c0f09e73b7163db43af83c02650dff526c
Joel Hendrix <jhendrix@microsoft.com>
2020-11-04 14:30:44 -0800

    Add Connection type
    The Connection type will be used for managing connections to ARM.
    Updated documentation with examples.

--- sdk/armcore/doc.go
+++ sdk/armcore/doc.go
@@ -4,9 +4,27 @@
 // Use of this source code is governed by an MIT
 // license that can be found in the LICENSE file.
 
-// Package armcore implements various helpers for Azure Resource Manager APIs.
+// Package armcore provides connections and utilities for Go SDK ARM client modules.
 //
-// You do not need to explicitly import this module.  It will automatically be included as a dependency for all ARM modules.
+// All Azure Resource Manager clients require a Connection, which is simply a
+// combination of the desired ARM endpoint and a pipeline for handling HTTP requests
+// and responses.
 //
-// The contents of this module are used by generated code.  It is not intended for external use.
+// To access the Azure public cloud, use the NewDefaultConnection() constructor with
+// the required token credential.  Module azidentity provides several methods for
+// obtaining token credentials.
+//
+//		cred, _ := azidentity.NewDefaultAzureCredential(nil)
+//		con := armcore.NewDefaultConnection(cred, nil)
+//
+// When accessing clouds other than the Azure public cloud, use the NewConnection()
+// constructor with the required ARM endpoint and token credential.  The most common
+// case is connecting to an Azure sovereign cloud or Azure Stack instance.
+//
+// NewDefaultConnection() and NewConnection() are configured with the same pipeline
+// thus have the same pipeline configuration options.  Use the NewConnectionWithPipeline()
+// constructor to create a connection that uses a custom azcore.Pipeline.  Note that
+// any custom pipeline will require at minimum an authentication policy obtained from
+// a token credential in order to authenticate with ARM.  See the implementation of
+// NewConnection() for how to obtain a credential's authentication policy.
 package armcore

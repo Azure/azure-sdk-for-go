21502ac17d892d46389a1743a02d34da26c4ee74
Joel Hendrix <jhendrix@microsoft.com>
2020-11-10 14:27:45 -0800

    Fix deadlock in bearer token refresh
    If GetToken() fails the renewing flag wasn't reset, leading to a
    deadlock.  Ensure the flag is reset before exiting.

--- sdk/azidentity/bearer_token_policy.go
+++ sdk/azidentity/bearer_token_policy.go
@@ -87,18 +87,24 @@ func (b *bearerTokenPolicy) Do(req *azcore.Request) (*azcore.Response, error) {
 	if getToken {
 		// this go routine has been elected to refresh the token
 		tk, err := b.creds.GetToken(req.Context(), b.options)
+		// update shared state
+		b.cond.L.Lock()
+		// didn't use defer here in order to explicitly control when unlocking happens
+		unlock := func() {
+			// signal any waiters that the token has been refreshed
+			b.cond.Broadcast()
+			b.cond.L.Unlock()
+		}
+		// to avoid a deadlock if GetToken() fails we MUST reset b.renewing to false before returning
+		b.renewing = false
 		if err != nil {
+			unlock()
 			return nil, err
 		}
 		header = bearerTokenPrefix + tk.Token
-		// update shared state
-		b.cond.L.Lock()
-		b.renewing = false
 		b.header = header
 		b.expiresOn = tk.ExpiresOn
-		// signal any waiters that the token has been refreshed
-		b.cond.Broadcast()
-		b.cond.L.Unlock()
+		unlock()
 	}
 	req.Request.Header.Set(azcore.HeaderXmsDate, time.Now().UTC().Format(http.TimeFormat))
 	req.Request.Header.Set(azcore.HeaderAuthorization, header)

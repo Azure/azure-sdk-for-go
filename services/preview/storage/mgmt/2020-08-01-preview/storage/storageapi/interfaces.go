a75421d9cf9647d4a6e49d9c9f275421aa6149aa
SDK Automation <sdkautomation@microsoft.com>
2020-11-10 09:59:09 +0000

    Generated from 3a3936bb8a4768f77047ee6ac8b8e778329bcd29

--- services/preview/storage/mgmt/2020-08-01-preview/storage/storageapi/interfaces.go
+++ services/preview/storage/mgmt/2020-08-01-preview/storage/storageapi/interfaces.go
@@ -171,9 +171,9 @@ var _ FileServicesClientAPI = (*storage.FileServicesClient)(nil)
 
 // FileSharesClientAPI contains the set of methods on the FileSharesClient type.
 type FileSharesClientAPI interface {
-	Create(ctx context.Context, resourceGroupName string, accountName string, shareName string, fileShare storage.FileShare) (result storage.FileShare, err error)
-	Delete(ctx context.Context, resourceGroupName string, accountName string, shareName string) (result autorest.Response, err error)
-	Get(ctx context.Context, resourceGroupName string, accountName string, shareName string, expand storage.GetShareExpand) (result storage.FileShare, err error)
+	Create(ctx context.Context, resourceGroupName string, accountName string, shareName string, fileShare storage.FileShare, expand storage.PutSharesExpand) (result storage.FileShare, err error)
+	Delete(ctx context.Context, resourceGroupName string, accountName string, shareName string, xMsSnapshot string) (result autorest.Response, err error)
+	Get(ctx context.Context, resourceGroupName string, accountName string, shareName string, expand storage.GetShareExpand, xMsSnapshot string) (result storage.FileShare, err error)
 	List(ctx context.Context, resourceGroupName string, accountName string, maxpagesize string, filter string, expand storage.ListSharesExpand) (result storage.FileShareItemsPage, err error)
 	ListComplete(ctx context.Context, resourceGroupName string, accountName string, maxpagesize string, filter string, expand storage.ListSharesExpand) (result storage.FileShareItemsIterator, err error)
 	Restore(ctx context.Context, resourceGroupName string, accountName string, shareName string, deletedShare storage.DeletedShare) (result autorest.Response, err error)

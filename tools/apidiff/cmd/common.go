a5b7546554c9ac0210acb70ed07648adf82ab150
Dapeng Zhang <dapzhang@microsoft.com>
2020-11-27 16:31:05 +0800

    more refactor

--- tools/apidiff/cmd/common.go
+++ tools/apidiff/cmd/common.go
@@ -23,9 +23,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/tools/apidiff/ioext"
 	"github.com/Azure/azure-sdk-for-go/tools/apidiff/repo"
 	"github.com/Azure/azure-sdk-for-go/tools/apidiff/report"
+	"github.com/Azure/azure-sdk-for-go/tools/internal/ioext"
 )
 
 func printf(format string, a ...interface{}) {

bd4ffd2d26fe742926e313ef17662d2336d7ae4c
SDKAuto <sdkautomation@microsoft.com>
2020-12-14 04:34:12 +0000

    CodeGen from PR 11330 in Azure/azure-rest-api-specs add scheduler track2 config (#11330)

--- services/preview/scheduler/mgmt/2014-08-01-preview/scheduler/jobs.go
+++ services/preview/scheduler/mgmt/2014-08-01-preview/scheduler/jobs.go
@@ -74,6 +74,7 @@ func (client JobsClient) CreateOrUpdate(ctx context.Context, resourceGroupName s
 	result, err = client.CreateOrUpdateResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "scheduler.JobsClient", "CreateOrUpdate", resp, "Failure responding to request")
+		return
 	}
 
 	return
@@ -156,6 +157,7 @@ func (client JobsClient) Delete(ctx context.Context, resourceGroupName string, j
 	result, err = client.DeleteResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "scheduler.JobsClient", "Delete", resp, "Failure responding to request")
+		return
 	}
 
 	return
@@ -232,6 +234,7 @@ func (client JobsClient) Get(ctx context.Context, resourceGroupName string, jobC
 	result, err = client.GetResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "scheduler.JobsClient", "Get", resp, "Failure responding to request")
+		return
 	}
 
 	return
@@ -312,9 +315,11 @@ func (client JobsClient) List(ctx context.Context, resourceGroupName string, job
 	result.jlr, err = client.ListResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "scheduler.JobsClient", "List", resp, "Failure responding to request")
+		return
 	}
 	if result.jlr.hasNextLink() && result.jlr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -441,9 +446,11 @@ func (client JobsClient) ListJobHistory(ctx context.Context, resourceGroupName s
 	result.jhlr, err = client.ListJobHistoryResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "scheduler.JobsClient", "ListJobHistory", resp, "Failure responding to request")
+		return
 	}
 	if result.jhlr.hasNextLink() && result.jhlr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -568,6 +575,7 @@ func (client JobsClient) Patch(ctx context.Context, resourceGroupName string, jo
 	result, err = client.PatchResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "scheduler.JobsClient", "Patch", resp, "Failure responding to request")
+		return
 	}
 
 	return
@@ -650,6 +658,7 @@ func (client JobsClient) Run(ctx context.Context, resourceGroupName string, jobC
 	result, err = client.RunResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "scheduler.JobsClient", "Run", resp, "Failure responding to request")
+		return
 	}
 
 	return

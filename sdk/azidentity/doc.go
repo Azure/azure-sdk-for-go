22542fbe9164ce984f37a627ba2b6af46ffb05bb
Catalina Peralta <catalinaperaltah@hotmail.com>
2020-12-14 11:43:10 -0800

    Adding doc.go

---
+++ sdk/azidentity/doc.go
@@ -0,0 +1,88 @@
+// +build go1.13
+
+// Copyright 2017 Microsoft Corporation. All rights reserved.
+// Use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
+
+/*
+Package azidentity implements a set of credential types for use with
+Azure SDK clients that support Azure Active Directory (AAD) token authentication.
+
+By default, the recommendation is that users call NewDefaultAzureCredential() which will
+provide a default ChainedTokenCredential configuration conformed of:
+	- EnvironmentCredential
+	- ManagedIdentityCredential
+	- AzureCLICredential
+Configuration options can be used to exclude any of the previous credentials from the
+DefaultAzureCredential implementation.
+
+Example call to NewDefaultAzureCredential():
+	cred, err := NewDefaultAzureCredential(nil) // pass in nil to get default behavior for this credential
+	if err != nil {
+		// process error
+	}
+	client := armresources.NewResourcesClient(armcore.NewDefaultConnection(cred, nil), "<subscription ID>")
+
+Example call to NewDefaultAzureCredential() with options set:
+	// these options will make sure the AzureCLICredential will not be added to the credential chain
+	opts := DefaultAzureCredentialOptions{ExcludeAzureCLICredential: true}
+	cred, err := NewDefaultAzureCredential(&opts)
+	if err != nil {
+		// process error
+	}
+	client := armresources.NewResourcesClient(armcore.NewDefaultConnection(cred, nil), "<subscription ID>")
+
+The following credential types are included in this module:
+	- AuthorizationCodeCredential
+	- AzureCLICredential
+	- ChainedTokenCredential
+	- ClientCertificateCredential
+	- ClientSecretCredential
+	- DeviceCodeCredential
+	- EnvironmentCredential
+	- InteractiveBrowserCredential
+	- ManagedIdentityCredential
+	- UsernamePasswordCredential
+
+Additional configuration of each credential can be done through each credential's
+options type. These options can also be used to modify the default pipeline for each
+credential.
+
+Example pattern for modifying credential options:
+	opts := DefaultClientCertificateCredentialOptions()
+	opts.Password = "<certificate password>"
+	cred, err := azidentity.NewClientCertificateCredential("<tenant ID>", "<client ID>", "<certificate path>", &opts)
+	if err != nil {
+		// process error
+	}
+
+CREDENTIAL AUTHORITY HOSTS
+
+The default authority host for all credentials, except for ManagedIdentityCredential, is the
+AzurePublicCloud host. This value can be changed through the credential options or by specifying
+a different value in an environment variable called AZURE_AUTHORITY_HOST.
+
+Example of setting an alternate Azure authority host through code:
+	opts := DefaultClientSecretCredentialOptions()
+	opts.AuthorityHost = azidentity.AzureChina
+	cred, err := azidentity.NewClientSecretCredential("<tenant ID>", "<client ID>", "<client secret>", &opts)
+	if err != nil {
+		// process error
+	}
+	client := armresources.NewResourcesClient(armcore.NewDefaultConnection(cred, nil), "<subscription ID>")
+
+Example of setting an alternate authority host value in the AZURE_AUTHORITY_HOST environment variable (in Powershell):
+	$env:AZURE_AUTHORITY_HOST="https://contoso.com/auth/"
+
+
+ERROR HANDLING
+
+The credential types in azidentity will return one of the following error types, unless there was some other unexpected failure:
+	- CredentialUnavailableError: 	This error signals that an essential component for using the credential is missing or that
+									the credential is being instantiated in an environment that is incompatible with its
+									functionality. These will generally be returned at credential creation after calling
+									the constructor.
+	- AuthenticationFailedError:	This error typically signals that a request has been made to the service and that
+									authentication failed at the service level.
+*/
+package azidentity

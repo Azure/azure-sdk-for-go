83c3c050e18824e11acd3c1c0d865199ad53454a
SDKAuto <sdkautomation@microsoft.com>
2020-12-15 06:22:24 +0000

    CodeGen from PR 11743 in Azure/azure-rest-api-specs Servicebus : Added APIs to 2018-01-01-preview API version (#11743)
    * Added APIs to 2018-01-01-preview API version
    
    * added missing examples
    
    * added missign examples
    
    * update - removed unref examples
    
    * update
    
    * prettier check
    
    * update ptettier check all
    
    * Avocado fix
    
    * fix for ApiDocPreview
    
    * prettier check fix
    
    * Revert "prettier check fix"
    
    This reverts commit fcde91b2109071df3ed3ae8ef0fea5b474f5fb50.
    
    * Revert "fix for ApiDocPreview"
    
    This reverts commit 9789255f28386e5e5d48b95377d59aac19f69dd0.
    
    * schema fix
    
    * prettier fix
    
    * reverting all new changes
    
    * fix
    
    * update azureresourceschema

--- services/servicebus/mgmt/2017-04-01/servicebus/migrationconfigs.go
+++ services/servicebus/mgmt/2017-04-01/servicebus/migrationconfigs.go
@@ -86,6 +86,7 @@ func (client MigrationConfigsClient) CompleteMigration(ctx context.Context, reso
 	result, err = client.CompleteMigrationResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "servicebus.MigrationConfigsClient", "CompleteMigration", resp, "Failure responding to request")
+		return
 	}
 
 	return
@@ -266,6 +267,7 @@ func (client MigrationConfigsClient) Delete(ctx context.Context, resourceGroupNa
 	result, err = client.DeleteResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "servicebus.MigrationConfigsClient", "Delete", resp, "Failure responding to request")
+		return
 	}
 
 	return
@@ -351,6 +353,7 @@ func (client MigrationConfigsClient) Get(ctx context.Context, resourceGroupName 
 	result, err = client.GetResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "servicebus.MigrationConfigsClient", "Get", resp, "Failure responding to request")
+		return
 	}
 
 	return
@@ -438,9 +441,11 @@ func (client MigrationConfigsClient) List(ctx context.Context, resourceGroupName
 	result.mclr, err = client.ListResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "servicebus.MigrationConfigsClient", "List", resp, "Failure responding to request")
+		return
 	}
 	if result.mclr.hasNextLink() && result.mclr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -563,6 +568,7 @@ func (client MigrationConfigsClient) Revert(ctx context.Context, resourceGroupNa
 	result, err = client.RevertResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "servicebus.MigrationConfigsClient", "Revert", resp, "Failure responding to request")
+		return
 	}
 
 	return

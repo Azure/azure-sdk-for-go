683a474761b4c1414ea901e216d54bf6d259ab4f
Catalina Peralta <catalinaperaltah@hotmail.com>
2020-12-15 07:48:26 -0800

    adding test to check for properly incorporated certificate chain in the JWT

--- sdk/azidentity/client_certificate_credential_test.go
+++ sdk/azidentity/client_certificate_credential_test.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
@@ -63,6 +64,78 @@ func TestClientCertificateCredential_CreateAuthRequestSuccess(t *testing.T) {
 	}
 	if reqQueryParams[qpClientAssertionType][0] != clientAssertionType {
 		t.Fatalf("Wrong client assertion type assigned to request")
+	}
+	if reqQueryParams[qpScope][0] != scope {
+		t.Fatalf("Unexpected scope in scope header")
+	}
+	if len(reqQueryParams[qpClientAssertion][0]) == 0 {
+		t.Fatalf("Client assertion is not present on the request")
+	}
+	if req.Request.URL.Host != defaultTestAuthorityHost {
+		t.Fatalf("Unexpected default authority host")
+	}
+	if req.Request.URL.Scheme != "https" {
+		t.Fatalf("Wrong request scheme")
+	}
+}
+
+func TestClientCertificateCredential_CreateAuthRequestSuccess_withCertificateChain(t *testing.T) {
+	opts := DefaultClientCertificateCredentialOptions()
+	opts.SendCertificateChain = true
+	cred, err := NewClientCertificateCredential(tenantID, clientID, certificatePath, &opts)
+	if err != nil {
+		t.Fatalf("Failed to instantiate credential")
+	}
+	req, err := cred.client.createClientCertificateAuthRequest(context.Background(), cred.tenantID, cred.clientID, cred.cert, cred.sendCertificateChain, []string{scope})
+	if err != nil {
+		t.Fatalf("Unexpectedly received an error: %v", err)
+	}
+	if req.Request.Header.Get(azcore.HeaderContentType) != azcore.HeaderURLEncoded {
+		t.Fatalf("Unexpected value for Content-Type header")
+	}
+	if len(cred.cert.publicCertificates) != 1 {
+		t.Fatalf("Wrong number of public certificates. Expected: %v, Received: %v", 1, len(cred.cert.publicCertificates))
+	}
+	body, err := ioutil.ReadAll(req.Request.Body)
+	if err != nil {
+		t.Fatalf("Unable to read request body")
+	}
+	bodyStr := string(body)
+	reqQueryParams, err := url.ParseQuery(bodyStr)
+	if err != nil {
+		t.Fatalf("Unable to parse query params in request")
+	}
+	if reqQueryParams[qpClientID][0] != clientID {
+		t.Fatalf("Unexpected client ID in the client_id header")
+	}
+	if reqQueryParams[qpGrantType][0] != "client_credentials" {
+		t.Fatalf("Wrong grant type in request body")
+	}
+	if reqQueryParams[qpClientAssertionType][0] != clientAssertionType {
+		t.Fatalf("Wrong client assertion type assigned to request")
+	}
+	// create a client assertion for comparison with the one in the request
+	certData, err := ioutil.ReadFile(certificatePath)
+	if err != nil {
+		t.Fatalf("Failed to read certificate: %v", err)
+	}
+	cert, err := extractFromPEMFile(certData, "", true)
+	if err != nil {
+		t.Fatalf("Failed extract data from PEM file: %v", err)
+	}
+	assertion, err := createClientAssertionJWT(clientID, azcore.JoinPaths(AzurePublicCloud, tenantID, tokenEndpoint), cert, true)
+	if err != nil {
+		t.Fatalf("Failed to create client assertion: %v", err)
+	}
+	// Get the index that separates the header of the JWT from the payload and signature.
+	// NOTE: the payload and signature cannot be used for comparison since they incorporate
+	// random numbers or unique IDs when being generated.
+	i := strings.Index(assertion, ".")
+	if i == -1 {
+		t.Fatalf("malformed JWT")
+	}
+	if !strings.HasPrefix(reqQueryParams[qpClientAssertion][0], assertion[:i]) {
+		t.Fatalf("Client assertion failed. Expected: %v, Received: %v", assertion, reqQueryParams[qpClientAssertion][0])
 	}
 	if reqQueryParams[qpScope][0] != scope {
 		t.Fatalf("Unexpected scope in scope header")

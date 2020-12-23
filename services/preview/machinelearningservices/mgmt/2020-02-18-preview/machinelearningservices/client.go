5444de05e9fdda5e4273a6c66941b33c0549bcf2
SDKAuto <sdkautomation@microsoft.com>
2020-12-23 06:03:35 +0000

    CodeGen from PR 12183 in Azure/azure-rest-api-specs add machinglearningservices track2 config (#12183)

--- services/preview/machinelearningservices/mgmt/2020-02-18-preview/machinelearningservices/client.go
+++ services/preview/machinelearningservices/mgmt/2020-02-18-preview/machinelearningservices/client.go
@@ -89,6 +89,7 @@ func (client BaseClient) ListSkus(ctx context.Context) (result SkuListResultPage
 	}
 	if result.slr.hasNextLink() && result.slr.IsEmpty() {
 		err = result.NextWithContext(ctx)
+		return
 	}
 
 	return
@@ -148,7 +149,6 @@ func (client BaseClient) listSkusNextResults(ctx context.Context, lastResults Sk
 	result, err = client.ListSkusResponder(resp)
 	if err != nil {
 		err = autorest.NewErrorWithError(err, "machinelearningservices.BaseClient", "listSkusNextResults", resp, "Failure responding to next results request")
-		return
 	}
 	return
 }

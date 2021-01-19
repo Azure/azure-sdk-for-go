7356a7cec74d2bdef421e3c39feb6c7b15840240
Catalina Peralta <catalinaperaltah@hotmail.com>
2021-01-19 15:50:54 -0800

    capitalize sentence

--- sdk/azidentity/managed_identity_credential.go
+++ sdk/azidentity/managed_identity_credential.go
@@ -75,7 +75,7 @@ func (c *ManagedIdentityCredential) GetToken(ctx context.Context, opts azcore.To
 }
 
 // AuthenticationPolicy implements the azcore.Credential interface on ManagedIdentityCredential.
-// NOTE: the TokenRequestOptions included in AuthenticationPolicyOptions must be a slice of resources in this case and not scopes.
+// NOTE: The TokenRequestOptions included in AuthenticationPolicyOptions must be a slice of resources in this case and not scopes.
 func (c *ManagedIdentityCredential) AuthenticationPolicy(options azcore.AuthenticationPolicyOptions) azcore.Policy {
 	// The following code will remove the /.default suffix from any scopes passed into the method since ManagedIdentityCredentials expect a resource string instead of a scope string
 	for i := range options.Options.Scopes {

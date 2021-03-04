73da33658f3469d8e58bc0b0440e5cf7e11783c5
Stéphane Erbrech <stephane.erbrech@gmail.com>
2021-03-05 10:07:13 +1100

    fix lock renewal test to take into account amqp flow (#213)
    * prove faster lock proof
    
    amqp now takes message dispostion into account to adjust the flow
    this means that no credits are provided to the sender
    until a disposition is sent for a given message.
    
    previous test handled messages concurrently without completing them
    and this won't work anymore, unless the prefetch is set higher.
    
    * fix gofmt
    
    * add MIT header
    
    * bump go-amqp and switch to t.Logf()
    
    Co-authored-by: Joel Hendrix <jhendrix@microsoft.com>

--- lockrenewal_test.go
+++ lockrenewal_test.go
@@ -1,5 +1,27 @@
 package servicebus
 
+//	MIT License
+//
+//	Copyright (c) Microsoft Corporation. All rights reserved.
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE
+
 import (
 	"context"
 	"math/rand"
@@ -12,7 +34,7 @@ import (
 )
 
 func (suite *serviceBusSuite) TestQueueSendReceiveWithLock() {
-	tests := map[string]func(context.Context, *testing.T, *Queue){
+	tests := map[string]func(context.Context, *testing.T, *Queue, int){
 		"SimpleSendReceiveWithLock": testQueueSendAndReceiveWithRenewLock,
 	}
 
@@ -22,13 +44,14 @@ func (suite *serviceBusSuite) TestQueueSendReceiveWithLock() {
 			queueName := suite.randEntityName()
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second*300)
 			defer cancel()
-			lockDuration := 30 * time.Second
+			lockDuration := 10 * time.Second
 
 			cleanup := makeQueue(ctx, t, ns, queueName, QueueEntityWithLockDuration(&lockDuration))
-			q, err := ns.NewQueue(queueName)
+			numMessages := rand.Intn(40) + 10
+			q, err := ns.NewQueue(queueName, QueueWithPrefetchCount(uint32(numMessages)))
 			suite.NoError(err)
 			defer cleanup()
-			testFunc(ctx, t, q)
+			testFunc(ctx, t, q, numMessages)
 			suite.NoError(q.Close(ctx))
 			if !t.Failed() {
 				// If there are message on the queue this would mean that a lock wasn't held and the message was requeued.
@@ -39,17 +62,18 @@ func (suite *serviceBusSuite) TestQueueSendReceiveWithLock() {
 	}
 }
 
-func testQueueSendAndReceiveWithRenewLock(ctx context.Context, t *testing.T, queue *Queue) {
+func testQueueSendAndReceiveWithRenewLock(ctx context.Context, t *testing.T, queue *Queue, numMessages int) {
 	ttl := 5 * time.Minute
-	numMessages := rand.Intn(40) + 20
 	activeMessages := make([]*Message, 0, numMessages)
 	expected := make(map[string]int, numMessages)
 	seen := make(map[string]int, numMessages)
 	errs := make(chan error, 1)
 
-	renewEvery := time.Second * 20
-	processingTime := time.Second * 100
-
+	renewEvery := time.Second * 6
+	// all are held in memory. we wait a bit longer than the lock expiry set to 10s
+	processingTime := 15 * time.Second
+	t.Logf("processing time : %s", processingTime)
+	t.Logf("processing time : %s \n", processingTime)
 	t.Logf("Sending/receiving %d messages", numMessages)
 
 	// Receiving Loop
@@ -59,7 +83,7 @@ func testQueueSendAndReceiveWithRenewLock(ctx context.Context, t *testing.T, que
 		errs <- queue.Receive(inner, HandlerFunc(func(ctx context.Context, msg *Message) error {
 			numSeen++
 			seen[string(msg.Data)]++
-
+			t.Logf("handling message %d - %s \n", numSeen, msg.LockToken)
 			activeMessages = append(activeMessages, msg)
 			if numSeen >= numMessages {
 				cancel()
@@ -81,6 +105,7 @@ func testQueueSendAndReceiveWithRenewLock(ctx context.Context, t *testing.T, que
 			if err != nil && runRenewal {
 				t.Error(err)
 			}
+			t.Logf("renewed locks successfuly for %d messages\n", len(activeMessages))
 		}
 	}()
 
@@ -100,6 +125,7 @@ func testQueueSendAndReceiveWithRenewLock(ctx context.Context, t *testing.T, que
 
 	// Then finally accept all the messages we're holding locks on
 	for _, msg := range activeMessages {
+		t.Logf("completing %d messages", len(activeMessages))
 		assert.NoError(t, msg.Complete(ctx))
 	}
 

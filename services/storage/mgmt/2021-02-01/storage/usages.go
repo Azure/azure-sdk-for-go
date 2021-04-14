6a3f7aa71b1c6b501ed124d8622070966143ed5b
yupwei68 <yupwei@microsoft.com>
2021-04-14 06:05:07 +0000

    update

--- services/storage/mgmt/2021-02-01/storage/usages.go
+++ services/storage/mgmt/2021-02-01/storage/usages.go
@@ -7,106 +7,106 @@ package storage
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-	"context"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/azure"
-	"github.com/Azure/go-autorest/autorest/validation"
-	"github.com/Azure/go-autorest/tracing"
-	"net/http"
+    "github.com/Azure/go-autorest/autorest"
+    "github.com/Azure/go-autorest/autorest/azure"
+    "net/http"
+    "context"
+    "github.com/Azure/go-autorest/tracing"
+    "github.com/Azure/go-autorest/autorest/validation"
 )
 
 // UsagesClient is the the Azure Storage Management API.
 type UsagesClient struct {
-	BaseClient
+    BaseClient
 }
-
 // NewUsagesClient creates an instance of the UsagesClient client.
 func NewUsagesClient(subscriptionID string) UsagesClient {
-	return NewUsagesClientWithBaseURI(DefaultBaseURI, subscriptionID)
+    return NewUsagesClientWithBaseURI(DefaultBaseURI, subscriptionID)
 }
 
 // NewUsagesClientWithBaseURI creates an instance of the UsagesClient client using a custom endpoint.  Use this when
 // interacting with an Azure cloud that uses a non-standard base URI (sovereign clouds, Azure stack).
-func NewUsagesClientWithBaseURI(baseURI string, subscriptionID string) UsagesClient {
-	return UsagesClient{NewWithBaseURI(baseURI, subscriptionID)}
-}
+    func NewUsagesClientWithBaseURI(baseURI string, subscriptionID string) UsagesClient {
+        return UsagesClient{ NewWithBaseURI(baseURI, subscriptionID)}
+    }
 
 // ListByLocation gets the current usage count and the limit for the resources of the location under the subscription.
-// Parameters:
-// location - the location of the Azure Storage resource.
+    // Parameters:
+        // location - the location of the Azure Storage resource.
 func (client UsagesClient) ListByLocation(ctx context.Context, location string) (result UsageListResult, err error) {
-	if tracing.IsEnabled() {
-		ctx = tracing.StartSpan(ctx, fqdn+"/UsagesClient.ListByLocation")
-		defer func() {
-			sc := -1
-			if result.Response.Response != nil {
-				sc = result.Response.Response.StatusCode
-			}
-			tracing.EndSpan(ctx, sc, err)
-		}()
-	}
-	if err := validation.Validate([]validation.Validation{
-		{TargetValue: client.SubscriptionID,
-			Constraints: []validation.Constraint{{Target: "client.SubscriptionID", Name: validation.MinLength, Rule: 1, Chain: nil}}}}); err != nil {
-		return result, validation.NewError("storage.UsagesClient", "ListByLocation", err.Error())
-	}
+    if tracing.IsEnabled() {
+        ctx = tracing.StartSpan(ctx, fqdn + "/UsagesClient.ListByLocation")
+        defer func() {
+            sc := -1
+        if result.Response.Response != nil {
+        sc = result.Response.Response.StatusCode
+        }
+            tracing.EndSpan(ctx, sc, err)
+        }()
+    }
+        if err := validation.Validate([]validation.Validation{
+        { TargetValue: client.SubscriptionID,
+         Constraints: []validation.Constraint{	{Target: "client.SubscriptionID", Name: validation.MinLength, Rule: 1, Chain: nil }}}}); err != nil {
+        return result, validation.NewError("storage.UsagesClient", "ListByLocation", err.Error())
+        }
 
-	req, err := client.ListByLocationPreparer(ctx, location)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "storage.UsagesClient", "ListByLocation", nil, "Failure preparing request")
-		return
-	}
+        req, err := client.ListByLocationPreparer(ctx, location)
+    if err != nil {
+    err = autorest.NewErrorWithError(err, "storage.UsagesClient", "ListByLocation", nil , "Failure preparing request")
+    return
+    }
 
-	resp, err := client.ListByLocationSender(req)
-	if err != nil {
-		result.Response = autorest.Response{Response: resp}
-		err = autorest.NewErrorWithError(err, "storage.UsagesClient", "ListByLocation", resp, "Failure sending request")
-		return
-	}
+        resp, err := client.ListByLocationSender(req)
+        if err != nil {
+        result.Response = autorest.Response{Response: resp}
+        err = autorest.NewErrorWithError(err, "storage.UsagesClient", "ListByLocation", resp, "Failure sending request")
+        return
+        }
 
-	result, err = client.ListByLocationResponder(resp)
-	if err != nil {
-		err = autorest.NewErrorWithError(err, "storage.UsagesClient", "ListByLocation", resp, "Failure responding to request")
-		return
-	}
+        result, err = client.ListByLocationResponder(resp)
+        if err != nil {
+        err = autorest.NewErrorWithError(err, "storage.UsagesClient", "ListByLocation", resp, "Failure responding to request")
+        return
+        }
 
-	return
+    return
 }
 
-// ListByLocationPreparer prepares the ListByLocation request.
-func (client UsagesClient) ListByLocationPreparer(ctx context.Context, location string) (*http.Request, error) {
-	pathParameters := map[string]interface{}{
-		"location":       autorest.Encode("path", location),
-		"subscriptionId": autorest.Encode("path", client.SubscriptionID),
-	}
+    // ListByLocationPreparer prepares the ListByLocation request.
+    func (client UsagesClient) ListByLocationPreparer(ctx context.Context, location string) (*http.Request, error) {
+        pathParameters := map[string]interface{} {
+        "location": autorest.Encode("path",location),
+        "subscriptionId": autorest.Encode("path",client.SubscriptionID),
+        }
 
-	const APIVersion = "2021-02-01"
-	queryParameters := map[string]interface{}{
-		"api-version": APIVersion,
-	}
+            const APIVersion = "2021-02-01"
+    queryParameters := map[string]interface{} {
+    "api-version": APIVersion,
+    }
 
-	preparer := autorest.CreatePreparer(
-		autorest.AsGet(),
-		autorest.WithBaseURL(client.BaseURI),
-		autorest.WithPathParameters("/subscriptions/{subscriptionId}/providers/Microsoft.Storage/locations/{location}/usages", pathParameters),
-		autorest.WithQueryParameters(queryParameters))
-	return preparer.Prepare((&http.Request{}).WithContext(ctx))
-}
+    preparer := autorest.CreatePreparer(
+autorest.AsGet(),
+autorest.WithBaseURL(client.BaseURI),
+autorest.WithPathParameters("/subscriptions/{subscriptionId}/providers/Microsoft.Storage/locations/{location}/usages",pathParameters),
+autorest.WithQueryParameters(queryParameters))
+    return preparer.Prepare((&http.Request{}).WithContext(ctx))
+    }
 
-// ListByLocationSender sends the ListByLocation request. The method will close the
-// http.Response Body if it receives an error.
-func (client UsagesClient) ListByLocationSender(req *http.Request) (*http.Response, error) {
-	return client.Send(req, azure.DoRetryWithRegistration(client.Client))
-}
+    // ListByLocationSender sends the ListByLocation request. The method will close the
+    // http.Response Body if it receives an error.
+    func (client UsagesClient) ListByLocationSender(req *http.Request) (*http.Response, error) {
+                return client.Send(req, azure.DoRetryWithRegistration(client.Client))
+                }
 
-// ListByLocationResponder handles the response to the ListByLocation request. The method always
-// closes the http.Response Body.
-func (client UsagesClient) ListByLocationResponder(resp *http.Response) (result UsageListResult, err error) {
-	err = autorest.Respond(
-		resp,
-		azure.WithErrorUnlessStatusCode(http.StatusOK),
-		autorest.ByUnmarshallingJSON(&result),
-		autorest.ByClosing())
-	result.Response = autorest.Response{Response: resp}
-	return
-}
+    // ListByLocationResponder handles the response to the ListByLocation request. The method always
+    // closes the http.Response Body.
+    func (client UsagesClient) ListByLocationResponder(resp *http.Response) (result UsageListResult, err error) {
+            err = autorest.Respond(
+            resp,
+            azure.WithErrorUnlessStatusCode(http.StatusOK),
+            autorest.ByUnmarshallingJSON(&result),
+            autorest.ByClosing())
+            result.Response = autorest.Response{Response: resp}
+            return
+    }
+

d7021048c51ab08565bae776d949bfdb9f59b7c3
ArcturusZhang <dapzhang@microsoft.com>
2021-04-20 17:15:05 +0800

    gofmt

--- tools/generator/pipeline/model.go
+++ tools/generator/pipeline/model.go
@@ -85,8 +85,8 @@ type PackageResult struct {
 
 // Changelog ...
 type Changelog struct {
-	Content             *string  `json:"content,omitempty"`
-	HasBreakingChange   *bool    `json:"hasBreakingChange,omitempty"`
+	Content             *string   `json:"content,omitempty"`
+	HasBreakingChange   *bool     `json:"hasBreakingChange,omitempty"`
 	BreakingChangeItems *[]string `json:"breakingChangeItems,omitempty"`
 }
 

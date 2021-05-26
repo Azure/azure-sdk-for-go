e1d8e7c67532089ba2125224cb9c7eeaa24cd83b
Arcturus <dapzhang@microsoft.com>
2021-05-26 11:44:07 +0800

    Compute track 2 module release (#14688)
    * compute track 2 module release
    
    * regenerate

---
+++ sdk/compute/armcompute/example_virtualmachineextensions_test.go
@@ -0,0 +1,78 @@
+// +build go1.13
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package armcompute_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/armcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/compute/armcompute"
+	"github.com/Azure/azure-sdk-for-go/sdk/to"
+)
+
+func ExampleVirtualMachineExtensionsClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armcompute.NewVirtualMachineExtensionsClient(armcore.NewDefaultConnection(cred, nil), "<subscription ID>")
+	poller, err := client.BeginCreateOrUpdate(
+		context.Background(),
+		"<resource group name>",
+		"<VM name>",
+		"<VM extension name>",
+		armcompute.VirtualMachineExtension{
+			Resource: armcompute.Resource{
+				Location: to.StringPtr("<Azure location>"),
+			},
+			Properties: &armcompute.VirtualMachineExtensionProperties{
+				AutoUpgradeMinorVersion: to.BoolPtr(true),
+				ProtectedSettings: &map[string]interface{}{
+					"AADClientSecret": "<client secret>",
+					"Passphrase":      "yourPassPhrase",
+				},
+				Publisher: to.StringPtr("Microsoft.Azure.Security"),
+				Settings: &map[string]interface{}{
+					"AADClientID":               "<client ID>",
+					"EncryptionOperation":       "EnableEncryption",
+					"KeyEncryptionAlgorithm":    "RSA-OAEP",
+					"KeyEncryptionKeyAlgorithm": "<key ID>",
+					"KeyVaultURL":               fmt.Sprintf("https://%s.%s/", "<vault name>", "<keyvault DNS suffix>"),
+					"SequenceVersion":           "<UUID string>",
+					"VolumeType":                "ALL",
+				},
+				Type:               to.StringPtr("AzureDiskEncryptionForLinux"),
+				TypeHandlerVersion: to.StringPtr("0.1"),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("failed to obtain a response: %v", err)
+	}
+	resp, err := poller.PollUntilDone(context.Background(), 30*time.Second)
+	if err != nil {
+		log.Fatalf("failed to obtain a response: %v", err)
+	}
+	log.Printf("VM extension ID: %v", *resp.VirtualMachineExtension.ID)
+}
+
+func ExampleVirtualMachineExtensionsClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armcompute.NewVirtualMachineExtensionsClient(armcore.NewDefaultConnection(cred, nil), "<subscription ID>")
+	resp, err := client.Get(context.Background(), "<resource group name>", "<VM name>", "<VM extension name>", nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a response: %v", err)
+	}
+	log.Printf("VM extension ID: %s", *resp.VirtualMachineExtension.ID)
+}
---
+++ sdk/compute/armcompute/go.mod
@@ -0,0 +1,10 @@
+module github.com/Azure/azure-sdk-for-go/sdk/compute/armcompute
+
+go 1.13
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/armcore v0.7.1
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.16.1
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v0.9.1
+	github.com/Azure/azure-sdk-for-go/sdk/to v0.1.4
+)

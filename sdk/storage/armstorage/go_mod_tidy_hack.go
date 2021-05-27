0388ad8a016b417755720fc10ff9f3068ead3be5
ArcturusZhang <dapzhang@microsoft.com>
2021-05-28 00:04:05 +0800

    fix package name

--- sdk/storage/armstorage/go_mod_tidy_hack.go
+++ sdk/storage/armstorage/go_mod_tidy_hack.go
@@ -6,7 +6,7 @@
 // This file, and the github.com/Azure/azure-sdk-for-go import, won't actually become part of
 // the resultant binary.
 
-package armcompute
+package armstorage
 
 // Necessary for safely adding multi-module repo. See: https://github.com/golang/go/wiki/Modules#is-it-possible-to-add-a-module-to-a-multi-module-repository
 import _ "github.com/Azure/azure-sdk-for-go"

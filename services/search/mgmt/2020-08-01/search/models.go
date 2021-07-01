f682dda254badcd4fed415a1d75b577702c69822
SDKAuto <sdkautomation@microsoft.com>
2021-07-01 14:25:18 +0000

    CodeGen from PR 14903 in Azure/azure-rest-api-specs [Azure Cognitive Search] Fix typos and add some descriptins (#14903)
    * Fix typos and add some descriptins
    
    * more description changes
    
    Co-authored-by: Arvind Krishnaa Jagannathan <arjagann@microsoft.com>

--- services/search/mgmt/2020-08-01/search/models.go
+++ services/search/mgmt/2020-08-01/search/models.go
@@ -614,8 +614,8 @@ func (plrr PrivateLinkResourcesResult) MarshalJSON() ([]byte, error) {
 	return json.Marshal(objectMap)
 }
 
-// ProxyResource the resource model definition for an Azure Resource Manager proxy resource. It will have
-// everything other than required location and tags
+// ProxyResource the resource model definition for a Azure Resource Manager proxy resource. It will not
+// have tags and a location
 type ProxyResource struct {
 	// ID - READ-ONLY; Fully qualified resource ID for the resource. Ex - /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}
 	ID *string `json:"id,omitempty"`
@@ -1502,6 +1502,7 @@ type Sku struct {
 }
 
 // TrackedResource the resource model definition for an Azure Resource Manager tracked top level resource
+// which has 'tags' and a 'location'
 type TrackedResource struct {
 	// Tags - Resource tags.
 	Tags map[string]*string `json:"tags"`

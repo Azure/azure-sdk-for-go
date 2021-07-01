62d090df0fbd1c7c9b90d5e8b0e69a493a4fd40a
seankane-msft <seankane@microsoft.com>
2021-07-01 17:32:04 -0400

    removing extra line

--- sdk/tables/aztable/errors.go
+++ sdk/tables/aztable/errors.go
@@ -6,7 +6,6 @@ package aztable
 import "errors"
 
 func checkEntityForPkRk(entity *map[string]interface{}, err error) error {
-
 	if _, ok := (*entity)["PartitionKey"]; !ok {
 		return errors.New("Entity must have a PartitionKey and RowKey")
 	}

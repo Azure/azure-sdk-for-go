48fce6edcb2f2ecb61b842e72db1baf1f06088a3
seankane-msft <seankane@microsoft.com>
2021-07-02 10:42:11 -0400

    moving error to constants file, renaming constants file

--- sdk/tables/aztable/errors.go
+++ sdk/tables/aztable/errors.go
@@ -3,15 +3,13 @@
 
 package aztable
 
-import "errors"
-
 func checkEntityForPkRk(entity *map[string]interface{}, err error) error {
-	if _, ok := (*entity)["PartitionKey"]; !ok {
-		return errors.New("Entity must have a PartitionKey and RowKey")
+	if _, ok := (*entity)[partitionKey]; !ok {
+		return partitionKeyRowKeyError
 	}
 
-	if _, ok := (*entity)["RowKey"]; !ok {
-		return errors.New("Entity must have a PartitionKey and RowKey")
+	if _, ok := (*entity)[rowKey]; !ok {
+		return partitionKeyRowKeyError
 	}
 
 	return err

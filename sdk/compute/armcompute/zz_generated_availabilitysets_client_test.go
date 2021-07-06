9ba06125dfba24f77c2fab5e154ce9f6f42842dc
ArcturusZhang <dapzhang@microsoft.com>
2021-07-06 15:22:15 +0800

    finish mock test case

--- sdk/compute/armcompute/zz_generated_availabilitysets_client_test.go
+++ sdk/compute/armcompute/zz_generated_availabilitysets_client_test.go
@@ -3,12 +3,28 @@
 package armcompute
 
 import (
+	"context"
+	"net/http"
 	"testing"
+	"time"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/armcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/internal/recording"
+	"github.com/Azure/azure-sdk-for-go/sdk/to"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+)
+
+const (
+	mockSubscriptionID = "00000000-0000-0000-0000-000000000000"
+	location           = "westus"
+	groupNamePrefix    = "rgMock"
+	setNamePrefix      = "setMock"
+)
+
+var (
+	ctx = context.Background()
 )
 
 type availabilitySetMockTest struct {
@@ -18,21 +34,46 @@ type availabilitySetMockTest struct {
 
 // Hookup to the testing framework
 func TestAvailabilitySetClient(t *testing.T) {
-	mockTest := availabilitySetMockTest{mode: recording.Record}
+	mockTest := availabilitySetMockTest{mode: recording.Playback}
 	suite.Run(t, &mockTest)
 }
 
-func (s *availabilitySetMockTest) TestCreateTable() {
-	assert := assert.New(s.T())
-	context := getTestContext(s.T().Name())
+func (s *availabilitySetMockTest) TestAvailabilitySetCRUD() {
+	assertion := assert.New(s.T())
+	state := getTestState(s.T().Name())
 	// generate a random recorded value for our table name.
-	tableName, err := context.recording.GenerateAlphaNumericID(tableNamePrefix, 20, true)
+	groupName, _ := state.recording.GenerateAlphaNumericID(groupNamePrefix, 10, true)
+	accountName, _ := state.recording.GenerateAlphaNumericID(setNamePrefix, 10, true)
 
-	resp, err := context.client.Create(ctx, tableName)
-	defer context.client.Delete(ctx, tableName)
+	// test create
+	parameters := AvailabilitySet{
+		Resource: Resource{
+			Location: to.StringPtr(location),
+		},
+	}
+	createResp, err := state.client.CreateOrUpdate(ctx, groupName, accountName, parameters, nil)
+	assertion.NoError(err)
+	assertion.NotNil(createResp.AvailabilitySet)
+	assertion.Equal(*createResp.AvailabilitySet.Name, accountName)
 
-	assert.Nil(err)
-	assert.Equal(*resp.TableResponse.TableName, tableName)
+	// test get
+	getResp, err := state.client.Get(ctx, groupName, accountName, nil)
+	assertion.NoError(err)
+	assertion.NotNil(getResp.AvailabilitySet)
+	assertion.Equal(*getResp.AvailabilitySet.Name, accountName)
+	//assertion.Equal(*getResp.AvailabilitySet.Location, location) // mock server is making this up, therefore this check cannot pass
+
+	// test delete
+	deleteResp, err := state.client.Delete(ctx, groupName, accountName, nil)
+	assertion.NoError(err)
+	assertion.NotNil(deleteResp)
+
+	// test get (and get an error)
+	getFailResp, err := state.client.Get(ctx, groupName, accountName, nil)
+	assertion.Error(err)
+	rawResponse := err.(azcore.HTTPResponse).RawResponse()
+	assertion.Equal(http.StatusNotFound, rawResponse.StatusCode)
+	assertion.Nil(getFailResp.AvailabilitySet)
 }
 
 func (s *availabilitySetMockTest) BeforeTest(suite string, test string) {
@@ -47,39 +88,33 @@ func (s *availabilitySetMockTest) AfterTest(suite string, test string) {
 
 type testState struct {
 	recording *recording.Recording
-	client    *TableServiceClient
+	client    *AvailabilitySetsClient
 	context   *recording.TestContext
 }
 
 // a map to store our created test contexts
-var clientsMap map[string]*testState = make(map[string]*testState)
+var clientsMap = make(map[string]*testState)
 
 // recordedTestSetup is called before each test execution by the test suite's BeforeTest method
 func recordedTestSetup(t *testing.T, testName string, mode recording.RecordMode) {
-	var accountName string
-	var suffix string
-	var cred *SharedKeyCredential
-	var secret string
-	var uri string
-	assert := assert.New(t)
+	assertion := assert.New(t)
 
 	// init the test framework
-	context := recording.NewTestContext(func(msg string) { assert.FailNow(msg) }, func(msg string) { t.Log(msg) }, func() string { return testName })
+	testContext := recording.NewTestContext(func(msg string) { assertion.FailNow(msg) }, func(msg string) { t.Log(msg) }, func() string { return testName })
 	//mode should be recording.Playback. This will automatically record if no test recording is available and playback if it is.
-	recording, err := recording.NewRecording(context, mode)
-	assert.Nil(err)
-	accountName, err := recording.GetEnvVar(storageAccountNameEnvVar, testframework.Default)
-	suffix := recording.GetOptionalEnvVar(storageEndpointSuffixEnvVar, DefaultStorageSuffix, testframework.Default)
-	secret, err := recording.GetEnvVar(storageAccountKeyEnvVar, testframework.Secret_Base64String)
-	cred, _ := NewSharedKeyCredential(accountName, secret)
-	uri := storageURI(accountName, suffix)
+	record, err := recording.NewRecording(testContext, mode)
+	assertion.Nil(err)
 	// Set our client's HTTPClient to our recording instance.
 	// Optionally, we can also configure MaxRetries to -1 to avoid the default retry behavior.
-	client, err := NewTableServiceClient(uri, cred, &TableClientOptions{HTTPClient: recording, Retry: azcore.RetryOptions{MaxRetries: -1}})
-	assert.Nil(err)
+	conn := armcore.NewConnection("https://localhost:8441/", &mockTokenCred{}, &armcore.ConnectionOptions{
+		HTTPClient: record,
+		Retry: azcore.RetryOptions{MaxRetries: -1},
+	})
+	client := NewAvailabilitySetsClient(conn, mockSubscriptionID)
+	assertion.Nil(err)
 
 	// either return your client instance, or store it somewhere that your test can use it for test execution.
-	clientsMap[testName] = &testState{client: client, recording: recording, context: &context}
+	clientsMap[testName] = &testState{client: client, recording: record, context: &testContext}
 }
 
 func getTestState(key string) *testState {
@@ -92,4 +127,20 @@ func recordedTestTeardown(key string) {
 	if ok && !(*context.context).IsFailed() {
 		context.recording.Stop()
 	}
+}
+
+// The below part might need to migrate to a more common place to avoid to be duplicated everywhere
+type mockTokenCred struct{}
+
+func (mockTokenCred) AuthenticationPolicy(azcore.AuthenticationPolicyOptions) azcore.Policy {
+	return azcore.PolicyFunc(func(req *azcore.Request) (*azcore.Response, error) {
+		return req.Next()
+	})
+}
+
+func (mockTokenCred) GetToken(context.Context, azcore.TokenRequestOptions) (*azcore.AccessToken, error) {
+	return &azcore.AccessToken{
+		Token:     "abc123",
+		ExpiresOn: time.Now().Add(1 * time.Hour),
+	}, nil
 }

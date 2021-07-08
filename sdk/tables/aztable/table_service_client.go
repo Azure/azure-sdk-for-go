83cf0f6df50e3a33c094da079205bb8de8d65036
seankane-msft <seankane@microsoft.com>
2021-07-08 11:52:26 -0400

    feedback from catalina

--- sdk/tables/aztable/table_service_client.go
+++ sdk/tables/aztable/table_service_client.go
@@ -64,7 +64,7 @@ func (t *TableServiceClient) Delete(ctx context.Context, name string) (TableDele
 //
 // Query returns a Pager, which allows iteration through each page of results. Example:
 //
-// pager := client.Query(&QueryOptions{})
+// pager := client.Query(nil)
 // for pager.NextPage(ctx) {
 //     resp = pager.PageResponse()
 //     fmt.sprintf("The page contains %i results", len(resp.TableQueryResponse.Value))

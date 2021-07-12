0ddeb8e45ca0b027023a4341868d30f64d89cb14
Matias Quaranta <ealsur@hotmail.com>
2021-07-12 16:32:08 -0700

    Initial folder structure

---
+++ sdk/cosmos/azcosmos/cosmos_client_options.go
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azcosmos
+
+import "github.com/Azure/azure-sdk-for-go/sdk/azcore"
+
+// A CosmosClient is used to interact with the Azure Cosmos DB database service.
+type CosmosClientOptions struct {
+	// HTTPClient sets the transport for making HTTP requests.
+	HTTPClient azcore.Transport
+	// Retry configures the built-in retry policy behavior.
+	Retry azcore.RetryOptions
+	// Telemetry configures the built-in telemetry policy behavior.
+	Telemetry azcore.TelemetryOptions
+}

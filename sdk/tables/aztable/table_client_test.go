98586c2c7c770d59ed57c2d76cfc6f7e148d3059
seankane-msft <seankane@microsoft.com>
2021-07-14 16:50:01 -0400

    fixing batch mixed test

--- sdk/tables/aztable/table_client_test.go
+++ sdk/tables/aztable/table_client_test.go
@@ -329,86 +329,103 @@ func (s *tableClientLiveTests) TestBatchAdd() {
 	assert.Equal(count, 10)
 }
 
-// func (s *tableClientLiveTests) TestBatchMixed() {
-// 	assert := assert.New(s.T())
-// 	require := require.New(s.T())
-// 	context := getTestContext(s.T().Name())
-// 	client, delete := s.init(true)
-// 	defer delete()
+func (s *tableClientLiveTests) TestBatchMixed() {
+	assert := assert.New(s.T())
+	require := require.New(s.T())
+	context := getTestContext(s.T().Name())
+	client, delete := s.init(true)
+	defer delete()
 
-// 	entitiesToCreate := createComplexMapEntities(context, 5, "partition")
-// 	batch := make([]TableTransactionAction, 3)
+	entitiesToCreate := createComplexEntities(5, "partition")
+	batch := make([]TableTransactionAction, 3)
 
-// 	// Add the first 3 entities.
-// 	for i := range batch {
-// 		batch[i] = TableTransactionAction{ActionType: Add, Entity: (*entitiesToCreate)[i]}
-// 	}
+	for i := range batch {
+		marshalled, err := json.Marshal((*entitiesToCreate)[i])
+		require.Nil(err)
+		batch[i] = TableTransactionAction{
+			ActionType: Add,
+			Entity:     marshalled,
+		}
+	}
 
-// 	resp, err := client.submitTransactionInternal(ctx, &batch, context.recording.UUID(), context.recording.UUID(), nil)
-// 	require.Nil(err)
-// 	for i := 0; i < len(*resp.TransactionResponses); i++ {
-// 		r := (*resp.TransactionResponses)[i]
-// 		assert.Equal(http.StatusNoContent, r.StatusCode)
-// 	}
+	resp, err := client.submitTransactionInternal(ctx, &batch, context.recording.UUID(), context.recording.UUID(), nil)
+	require.Nil(err)
+	for i := 0; i < len(*resp.TransactionResponses); i++ {
+		r := (*resp.TransactionResponses)[i]
+		assert.Equal(http.StatusNoContent, r.StatusCode)
+	}
 
-// 	var qResp TableEntityQueryResponseResponse
-// 	filter := "RowKey eq '1'"
-// 	pager := client.Query(QueryOptions{Filter: &filter})
-// 	for pager.NextPage(ctx) {
-// 		qResp = pager.PageResponse()
-// 	}
-// 	preMerge := qResp.TableEntityQueryResponse.Value[0]
+	var qResp TableEntityQueryByteResponseResponse
+	filter := "RowKey eq '1'"
+	query := &QueryOptions{Filter: &filter}
+	pager := client.Query(query)
+	for pager.NextPage(ctx) {
+		qResp = pager.PageResponse()
+	}
+	preMerge := qResp.TableEntityQueryResponse.Value[0]
+	var unMarshalledPreMerge map[string]interface{}
+	err = json.Unmarshal(preMerge, &unMarshalledPreMerge)
+	require.Nil(err)
 
-// 	// create a new batch slice.
-// 	batch = make([]TableTransactionAction, 5)
+	// create a new batch slice.
+	batch = make([]TableTransactionAction, 5)
 
-// 	// create a merge action for the first added entity
-// 	mergeProp := "MergeProperty"
-// 	val := "foo"
-// 	var mergeProperty = map[string]interface{}{
-// 		partitionKey: (*entitiesToCreate)[0][partitionKey],
-// 		rowKey:       (*entitiesToCreate)[0][rowKey],
-// 		mergeProp:    val,
-// 	}
-// 	batch[0] = TableTransactionAction{ActionType: UpdateMerge, Entity: mergeProperty, ETag: (*resp.TransactionResponses)[0].Header.Get(etag)}
+	// create a merge action for the first added entity
+	mergeProp := "MergeProperty"
+	val := "foo"
+	var mergeEntity = map[string]interface{}{
+		partitionKey: (*entitiesToCreate)[0].PartitionKey,
+		rowKey:       (*entitiesToCreate)[0].RowKey,
+		mergeProp:    val,
+	}
+	marshalledMergeEntity, err := json.Marshal(mergeEntity)
+	require.Nil(err)
+	batch[0] = TableTransactionAction{ActionType: UpdateMerge, Entity: marshalledMergeEntity, ETag: (*resp.TransactionResponses)[0].Header.Get(etag)}
 
-// 	// create a delete action for the second added entity
-// 	batch[1] = TableTransactionAction{ActionType: Delete, Entity: (*entitiesToCreate)[1]}
+	// create a delete action for the second added entity
+	marshalledSecondEntity, err := json.Marshal((*entitiesToCreate)[1])
+	require.Nil(err)
+	batch[1] = TableTransactionAction{ActionType: Delete, Entity: marshalledSecondEntity}
 
-// 	// create an upsert action to replace the third added entity with a new value
-// 	replaceProp := "ReplaceProperty"
-// 	var replaceProperties = map[string]interface{}{
-// 		partitionKey: (*entitiesToCreate)[2][partitionKey],
-// 		rowKey:       (*entitiesToCreate)[2][rowKey],
-// 		replaceProp:  val,
-// 	}
-// 	batch[2] = TableTransactionAction{ActionType: UpsertReplace, Entity: replaceProperties}
+	// create an upsert action to replace the third added entity with a new value
+	replaceProp := "ReplaceProperty"
+	var replaceProperties = map[string]interface{}{
+		partitionKey: (*entitiesToCreate)[2].PartitionKey,
+		rowKey:       (*entitiesToCreate)[2].RowKey,
+		replaceProp:  val,
+	}
+	marshalledThirdEntity, err := json.Marshal(replaceProperties)
+	require.Nil(err)
+	batch[2] = TableTransactionAction{ActionType: UpsertReplace, Entity: marshalledThirdEntity}
 
-// 	// Add the remaining 2 entities.
-// 	batch[3] = TableTransactionAction{ActionType: Add, Entity: (*entitiesToCreate)[3]}
-// 	batch[4] = TableTransactionAction{ActionType: Add, Entity: (*entitiesToCreate)[4]}
+	// Add the remaining 2 entities.
+	marshalled4thEntity, err := json.Marshal((*entitiesToCreate)[3])
+	marshalled5thEntity, err := json.Marshal((*entitiesToCreate)[4])
+	batch[3] = TableTransactionAction{ActionType: Add, Entity: marshalled4thEntity}
+	batch[4] = TableTransactionAction{ActionType: Add, Entity: marshalled5thEntity}
 
-// 	//batch = batch[1:]
+	resp, err = client.submitTransactionInternal(ctx, &batch, context.recording.UUID(), context.recording.UUID(), nil)
+	require.Nil(err)
 
-// 	resp, err = client.submitTransactionInternal(ctx, &batch, context.recording.UUID(), context.recording.UUID(), nil)
-// 	require.Nil(err)
+	for i := 0; i < len(*resp.TransactionResponses); i++ {
+		r := (*resp.TransactionResponses)[i]
+		assert.Equal(http.StatusNoContent, r.StatusCode)
 
-// 	for i := 0; i < len(*resp.TransactionResponses); i++ {
-// 		r := (*resp.TransactionResponses)[i]
-// 		assert.Equal(http.StatusNoContent, r.StatusCode)
+	}
 
-// 	}
+	pager = client.Query(query)
+	for pager.NextPage(ctx) {
+		qResp = pager.PageResponse()
+	}
+	postMerge := qResp.TableEntityQueryResponse.Value[0]
+	var unMarshaledPostMerge map[string]interface{}
+	err = json.Unmarshal(postMerge, &unMarshaledPostMerge)
+	require.Nil(err)
 
-// 	pager = client.Query(QueryOptions{Filter: &filter})
-// 	for pager.NextPage(ctx) {
-// 		qResp = pager.PageResponse()
-// 	}
-// 	postMerge := qResp.TableEntityQueryResponse.Value[0]
-
-// 	// The merged entity has all its properties + the merged property
-// 	assert.Equalf(len(preMerge)+1, len(postMerge), "postMerge should have one more property than preMerge")
-// 	assert.Equalf(postMerge[mergeProp], val, "%s property should equal %s", mergeProp, val)
-// }
+	// The merged entity has all its properties + the merged property
+	assert.Equalf(len(unMarshalledPreMerge)+1, len(unMarshaledPostMerge), "postMerge should have one more property than preMerge")
+	assert.Equalf(unMarshaledPostMerge[mergeProp], val, "%s property should equal %s", mergeProp, val)
+}
 
 // func (s *tableClientLiveTests) TestBatchError() {
 // 	assert := assert.New(s.T())

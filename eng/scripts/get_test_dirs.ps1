9179328db662396674b62f138d519500caaee517
seankane-msft <seankane@microsoft.com>
2021-07-16 16:15:28 -0400

    only finds tests with a Test method

--- eng/scripts/get_test_dirs.ps1
+++ eng/scripts/get_test_dirs.ps1
@@ -7,9 +7,15 @@ $testDirs = [Collections.Generic.List[String]]@()
 # find each directory under $serviceDir that contains Go test files
 Get-Childitem -recurse -path $serviceDir -filter *_test.go | foreach-object {
   $cdir = $_.Directory
-  if (!$testDirs.Contains($cdir)) {
-    Write-Host "Adding $cdir to list of test directories"
-    $testDirs.Add($cdir)
+  Write-Host $_
+  $tests = Select-String -Path $_ 'Test' -AllMatches
+
+  Write-Host $tests.Count
+  if ($tests.Count -eq 0) {
+    if (!$testDirs.Contains($cdir)) {
+      Write-Host "Adding $cdir to list of test directories"
+      $testDirs.Add($cdir)
+    }
   }
 }
 

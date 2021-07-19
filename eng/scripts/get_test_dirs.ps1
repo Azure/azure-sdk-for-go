f1b009e1089f82bdb326290c28244265780fd6f5
seankane-msft <seankane@microsoft.com>
2021-07-19 17:50:30 -0400

    bad conditional

--- eng/scripts/get_test_dirs.ps1
+++ eng/scripts/get_test_dirs.ps1
@@ -7,11 +7,9 @@ $testDirs = [Collections.Generic.List[String]]@()
 # find each directory under $serviceDir that contains Go test files
 Get-Childitem -recurse -path $serviceDir -filter *_test.go | foreach-object {
   $cdir = $_.Directory
-  Write-Host $_
   $tests = Select-String -Path $_ 'Test' -AllMatches
 
-  Write-Host $tests.Count
-  if ($tests.Count -eq 0) {
+  if ($tests.Count -gt 0) {
     if (!$testDirs.Contains($cdir)) {
       Write-Host "Adding $cdir to list of test directories"
       $testDirs.Add($cdir)

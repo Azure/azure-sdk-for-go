7230f4167ca4dafdb7ee4f812c9660813b897e3e
Chris Hamons <chris.hamons@microsoft.com>
2021-07-21 13:21:10 -0500

    [build.ps1] Add support for converting _meta.json to autorest.md during build

---
+++ eng/scripts/meta_generation.ps1
@@ -0,0 +1,27 @@
+function ShouldGenerate-AutorestConfig {
+    Param(
+        [string] $autorestPath
+    )
+    $metaPath = $autorestPath.Replace("autorest.md", "_meta.json")
+    return !(Test-Path $autorestPath -PathType Leaf) && (Test-Path $metaPath -PathType Leaf)
+}
+
+function Generate-AutorestConfig {
+    Param(
+        [string] $autorestPath
+    )
+    $metaPath = $autorestPath.Replace("autorest.md", "_meta.json")
+    $meta = Get-Content $metaPath | ConvertFrom-Json
+    $readme = "https://github.com/Azure/azure-rest-api-specs/blob/" + $meta.commit + $meta.readme.Split("/azure-rest-api-specs")[1];
+
+    $contents = @'
+### AutoRest Configuration
+> see https://aka.ms/autorest
+``` yaml
+tag: {0}
+require:
+- {1}
+```
+'@;
+    $contents -f $meta.tag, $readme | Out-File -FilePath $autorestPath
+}

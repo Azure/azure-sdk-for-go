aadf942e618cae5f9ca6c0891ece7245f5d1b89a
seankane-msft <seankane@microsoft.com>
2021-07-22 11:01:58 -0400

    putting coverage in a script

---
+++ eng/scripts/create_coverage.ps1
@@ -0,0 +1,17 @@
+#Requires -Version 7.0
+
+$coverageFiles = [Collections.Generic.List[String]]@()
+Get-Childitem -recurse -path $(SCOPE) -filter coverage.txt | foreach-object {
+  $covFile = $_.FullName
+  Write-Host "Adding $covFile to the list of code coverage files"
+  $coverageFiles.Add($covFile)
+}
+gocovmerge $coverageFiles > mergedCoverage.txt
+gocov convert ./mergedCoverage.txt > ./coverage.json
+
+# gocov converts rely on standard input
+Get-Content ./coverage.json | gocov-xml > ./coverage.xml
+Get-Content ./coverage.json | gocov-html > ./coverage.html
+
+# use internal tool to fail if coverage is too low
+go run ../tools/internal/coverage/main.go

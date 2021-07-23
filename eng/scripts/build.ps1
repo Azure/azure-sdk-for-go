fcf7db43b82095301dc94f1a3c796b9beb142665
Chris Hamons <chris.hamons@microsoft.com>
2021-07-23 13:16:30 -0500

    [build.ps1] Use finally to always return to current directory

--- eng/scripts/build.ps1
+++ eng/scripts/build.ps1
@@ -22,46 +22,49 @@ if (![string]::IsNullOrWhiteSpace($filter)) {
     $keys = $keys.Where( { $_ -match $filter })
 }
 
-$keys | ForEach-Object { $sdks[$_] } | ForEach-Object {
-    Push-Location $_.path
+try {
+    $keys | ForEach-Object { $sdks[$_] } | ForEach-Object {
+        Push-Location $_.path
 
-    if ($clean) {
-        Write-Host "##[command]Executing go clean -v ./... in " $_.path
-        go clean -v ./...
-    }
-
-    if ($generate) {
-        Write-Host "##[command]Executing autorest.go in " $_.path
-        $autorestPath = $_.path + "/" + $config
-
-        if (ShouldGenerate-AutorestConfig $autorestPath) {
-            Generate-AutorestConfig $autorestPath
-            $removeAutorestFile = $true
+        if ($clean) {
+            Write-Host "##[command]Executing go clean -v ./... in " $_.path
+            go clean -v ./...
         }
 
-        $autorestVersion = "@autorest/go@4.0.0-preview.23"
-        if ($outputFolder -eq '') {
-            $outputFolder = $_.path
+        if ($generate) {
+            Write-Host "##[command]Executing autorest.go in " $_.path
+            $autorestPath = $_.path + "/" + $config
+
+            if (ShouldGenerate-AutorestConfig $autorestPath) {
+                Generate-AutorestConfig $autorestPath
+                $removeAutorestFile = $true
+            }
+
+            $autorestVersion = "@autorest/go@4.0.0-preview.23"
+            if ($outputFolder -eq '') {
+                $outputFolder = $_.path
+            }
+            autorest --use=$autorestVersion --go --track2 --go-sdk-folder=$root --output-folder=$outputFolder --file-prefix="zz_generated_" --clear-output-folder=false $autorestPath
+            if ($removeAutorestFile) {
+                Remove-Item $autorestPath
+            }
         }
-        autorest --use=$autorestVersion --go --track2 --go-sdk-folder=$root --output-folder=$outputFolder --file-prefix="zz_generated_" --clear-output-folder=false $autorestPath
-        if ($removeAutorestFile) {
-            Remove-Item $autorestPath
+
+        if (!$skipBuild) {
+            Write-Host "##[command]Executing go build -v ./... in " $_.path
+            go build -x -v ./...
+            Write-Host "##[command]Build Complete!"
+
         }
+
+        if ($vet) {
+            Write-Host "##[command]Executing go vet ./... in " $_.path
+            go vet ./...
+        }
+
+        Pop-Location
     }
-
-    if (!$skipBuild) {
-        Write-Host "##[command]Executing go build -v ./... in " $_.path
-        go build -x -v ./...
-        Write-Host "##[command]Build Complete!"
-
-    }
-
-    if ($vet) {
-        Write-Host "##[command]Executing go vet ./... in " $_.path
-        go vet ./...
-    }
-
-    Pop-Location
 }
-
-Set-Location $startingDirectory
+finally {
+    Set-Location $startingDirectory
+}

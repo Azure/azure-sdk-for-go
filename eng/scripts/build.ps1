8cfd3cca2298c09389471b01981336c529a670f3
Chris Hamons <chris.hamons@microsoft.com>
2021-07-27 08:19:53 -0500

    Many build.ps1 improvements (#15135)
    - Fixes https://github.com/Azure/azure-sdk-for-go/issues/15128
    - Fixes https://github.com/Azure/azure-sdk-for-go/issues/15127
    - Part of https://github.com/Azure/azure-sdk-for-go/issues/15102

--- eng/scripts/build.ps1
+++ eng/scripts/build.ps1
@@ -1,22 +1,58 @@
 #Requires -Version 7.0
-param($filter, [switch]$clean, [switch]$vet, [switch]$generate, [switch]$skipBuild)
+param([string]$filter, [switch]$clean, [switch]$vet, [switch]$generate, [switch]$skipBuild, [string]$config = "autorest.md", [string]$outputFolder)
 
 . $PSScriptRoot/meta_generation.ps1
+. $PSScriptRoot/get_module_dirs.ps1
+
+
+function Process-Sdk ($path) {
+    if ($clean) {
+        Write-Host "##[command]Executing go clean -v ./... in " $path
+        go clean -v ./...
+    }
+
+    if ($generate) {
+        Write-Host "##[command]Executing autorest.go in " $path
+        $autorestPath = $path + "/" + $config
+
+        if (ShouldGenerate-AutorestConfig $autorestPath) {
+            Generate-AutorestConfig $autorestPath
+            $removeAutorestFile = $true
+        }
+
+        $autorestVersion = "@autorest/go@4.0.0-preview.23"
+        if ($outputFolder -eq '') {
+            $outputFolder = $path
+        }
+        autorest --use=$autorestVersion --go --track2 --go-sdk-folder=$root --output-folder=$outputFolder --file-prefix="zz_generated_" --clear-output-folder=false $autorestPath
+        if ($removeAutorestFile) {
+            Remove-Item $autorestPath
+        }
+    }
+
+    if (!$skipBuild) {
+        Write-Host "##[command]Executing go build -x -v ./... in " $path
+        go build -x -v ./...
+        Write-Host "##[command]Build Complete!"
+
+    }
+
+    if ($vet) {
+        Write-Host "##[command]Executing go vet ./... in " $path
+        go vet ./...
+    }
+
+}
 
 $startingDirectory = Get-Location
 $root = Resolve-Path ($PSScriptRoot + "/../..")
 Set-Location $root
 $sdks = @{};
 
-foreach ($sdk in (./eng/scripts/get_module_dirs.ps1 -serviceDir 'sdk/...')) {
+foreach ($sdk in (Get-ModuleDirs 'sdk/...')) {
     $name = $sdk | split-path -leaf
     $sdks[$name] = @{
-        'path'      = $sdk;
-        'clean'     = $clean;
-        'vet'       = $vet;
-        'generate'  = $generate;
-        'skipBuild' = $skipBuild;
-        'root'      = $root;
+        'path' = $sdk;
     }
 }
 
@@ -26,42 +62,13 @@ if (![string]::IsNullOrWhiteSpace($filter)) {
     $keys = $keys.Where( { $_ -match $filter })
 }
 
-$keys | ForEach-Object { $sdks[$_] } | ForEach-Object {
-    Push-Location $_.path
-
-    if ($_.clean) {
-        Write-Host "##[command]Executing go clean -v ./... in " $_.path
-        go clean -v ./...
+try {
+    $keys | ForEach-Object { $sdks[$_] } | ForEach-Object {
+        Push-Location $_.path
+        Process-Sdk $_.path
+        Pop-Location
     }
-
-    if ($_.generate) {
-        Write-Host "##[command]Executing autorest.go in " $_.path
-        $autorestPath = $_.path + "/autorest.md"
-
-        if (ShouldGenerate-AutorestConfig $autorestPath) {
-            Generate-AutorestConfig $autorestPath
-            $removeAutorestFile = $true
-        }
-
-        $autorestVersion = "@autorest/go@4.0.0-preview.23"
-        $outputFolder = $_.path
-        $root = $_.root
-        autorest --use=$autorestVersion --go --track2 --go-sdk-folder=$root --output-folder=$outputFolder --file-prefix="zz_generated_" --clear-output-folder=false $autorestPath
-        if ($removeAutorestFile) {
-            Remove-Item $autorestPath
-        }
-    }
-    if (!$_.skipBuild) {
-        Write-Host "##[command]Executing go build -v ./... in " $_.path
-        go build -x -v ./...
-        Write-Host "##[command]Build Complete!"
-
-    }
-    if ($_.vet) {
-        Write-Host "##[command]Executing go vet ./... in " $_.path
-        go vet ./...
-    }
-    Pop-Location
 }
-
-Set-Location $startingDirectory
+finally {
+    Set-Location $startingDirectory
+}

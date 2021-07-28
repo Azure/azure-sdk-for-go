181f75b884b7ca2bd37ab0bdbc695518806929cc
SDKAuto <sdkautomation@microsoft.com>
2021-07-28 02:36:30 +0000

    CodeGen from PR 15322 in Azure/azure-rest-api-specs adding the changes again; trying to fix prettier errors (#15322)
    * adding the changes again; trying to fix prettier errors
    
    * fixing model validation errors
    
    * prettier error fix
    
    * fixed the last prettier errors:
    
    * reverted to original package-lock.json

--- services/changeanalysis/mgmt/2021-04-01/changeanalysis/changes.go
+++ services/changeanalysis/mgmt/2021-04-01/changeanalysis/changes.go
@@ -56,8 +56,7 @@ func (client ChangesClient) ListChangesByResourceGroup(ctx context.Context, reso
 			Constraints: []validation.Constraint{{Target: "client.SubscriptionID", Name: validation.MinLength, Rule: 1, Chain: nil}}},
 		{TargetValue: resourceGroupName,
 			Constraints: []validation.Constraint{{Target: "resourceGroupName", Name: validation.MaxLength, Rule: 90, Chain: nil},
-				{Target: "resourceGroupName", Name: validation.MinLength, Rule: 1, Chain: nil},
-				{Target: "resourceGroupName", Name: validation.Pattern, Rule: `^[-\w\._\(\)]+$`, Chain: nil}}}}); err != nil {
+				{Target: "resourceGroupName", Name: validation.MinLength, Rule: 1, Chain: nil}}}}); err != nil {
 		return result, validation.NewError("changeanalysis.ChangesClient", "ListChangesByResourceGroup", err.Error())
 	}
 

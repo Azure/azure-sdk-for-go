2f2e9905e789d6e88646ba850ae1dd2e176a1459
seankane-msft <seankane@microsoft.com>
2021-07-28 17:35:27 -0400

    adding a second test for proxy and transport

--- sdk/tables/aztable/proxy_test.go
+++ sdk/tables/aztable/proxy_test.go
@@ -12,7 +12,7 @@ import (
 
 var AADAuthenticationScope = "https://storage.azure.com/.default"
 
-func Test_TestProxy(t *testing.T) {
+func Test_TestProxyPolicy(t *testing.T) {
 	require := require.New(t)
 	err := recording.StartRecording(t)
 	require.NoError(err)
@@ -20,12 +20,37 @@ func Test_TestProxy(t *testing.T) {
 
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	require.NoError(err)
-	// testProxyTransport := recording.TestProxyTransport{}
 	recordingPolicy := recording.NewRecordingPolicy(nil)
 	options := TableClientOptions{
 		Scopes:         []string{AADAuthenticationScope},
 		PerCallOptions: []azcore.Policy{recordingPolicy},
-		// HTTPClient: testProxyTransport,
+	}
+	client, err := NewTableClient("testproxy", "https://seankaneprim.table.core.windows.net", cred, &options)
+	require.NoError(err)
+
+	fmt.Println("CALLING CREATE")
+	_, err = client.Create(ctx)
+	fmt.Println("Create err: ", err.Error())
+	require.NoError(err)
+
+	fmt.Println("CALLING DELETE")
+	_, err = client.Delete(ctx)
+	fmt.Println("Delete err: ", err)
+	require.NoError(err)
+}
+
+func Test_TestProxyTransport(t *testing.T) {
+	require := require.New(t)
+	err := recording.StartRecording(t)
+	require.NoError(err)
+	defer recording.StopRecording(t)
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	require.NoError(err)
+	testProxyTransport := recording.TestProxyTransport{}
+	options := TableClientOptions{
+		Scopes:         []string{AADAuthenticationScope},
+		HTTPClient: testProxyTransport,
 	}
 	client, err := NewTableClient("testproxy", "https://seankaneprim.table.core.windows.net", cred, &options)
 	require.NoError(err)

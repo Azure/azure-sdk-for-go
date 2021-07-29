d605899ccccea478c80aec7987f0918c112291c0
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2021-07-29 11:27:06 -0400

    [azcore] Remove logger from azcore (#15160)
    * initial work, issues with using internal package
    
    * fixing up errors in logger file
    
    * adding logger test file
    
    * removed an example
    
    * feedback and removing circular dependency
    
    * moving logclassification to be in logger, type alias in azcore
    
    * update internal version
    
    * fixes in azcore
    
    * fixes to azcore, works now
    
    * linter false positive
    
    * catalina's comments
    
    * more feedback
    
    * update version
    
    * Update sdk/azcore/version.go
    
    Co-authored-by: catalinaperalta <catalinaperaltah@hotmail.com>
    
    Co-authored-by: catalinaperalta <catalinaperaltah@hotmail.com>

--- sdk/azcore/log_test.go
+++ sdk/azcore/log_test.go
@@ -9,21 +9,23 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/logger"
 )
 
 func TestLoggingDefault(t *testing.T) {
 	// ensure logging with nil listener doesn't fail
-	Log().SetListener(nil)
-	Log().Write(LogRequest, "this should work just fine")
+	SetListener(nil)
+	logger.Log().Write(logger.LogRequest, "this should work just fine")
 
 	log := map[LogClassification]string{}
-	Log().SetListener(func(cls LogClassification, msg string) {
+	SetListener(func(cls LogClassification, msg string) {
 		log[cls] = msg
 	})
 	const req = "this is a request"
-	Log().Write(LogRequest, req)
+	logger.Log().Write(logger.LogRequest, req)
 	const resp = "this is a response: %d"
-	Log().Writef(LogResponse, resp, http.StatusOK)
+	logger.Log().Writef(logger.LogResponse, resp, http.StatusOK)
 	if l := len(log); l != 2 {
 		t.Fatalf("unexpected log entry count: %d", l)
 	}
@@ -37,17 +39,17 @@ func TestLoggingDefault(t *testing.T) {
 
 func TestLoggingClassification(t *testing.T) {
 	log := map[LogClassification]string{}
-	Log().SetListener(func(cls LogClassification, msg string) {
+	SetListener(func(cls LogClassification, msg string) {
 		log[cls] = msg
 	})
-	Log().SetClassifications(LogRequest)
-	defer Log().resetClassifications()
-	Log().Write(LogResponse, "this shouldn't be in the log")
+	SetClassifications(LogRequest)
+	defer resetClassifications()
+	logger.Log().Write(logger.LogResponse, "this shouldn't be in the log")
 	if s, ok := log[LogResponse]; ok {
 		t.Fatalf("unexpected log entry %s", s)
 	}
 	const req = "this is a request"
-	Log().Write(LogRequest, req)
+	logger.Log().Write(logger.LogRequest, req)
 	if log[LogRequest] != req {
 		t.Fatalf("unexpected log entry: %s", log[LogRequest])
 	}

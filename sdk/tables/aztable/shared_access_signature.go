a0b98507faa92a25e9ad57d90216ce9fa2585269
seankane-msft <seankane@microsoft.com>
2021-08-03 18:13:45 -0400

    working sas credential for account signature

--- sdk/tables/aztable/shared_access_signature.go
+++ sdk/tables/aztable/shared_access_signature.go
@@ -4,8 +4,10 @@
 package aztable
 
 import (
+	"fmt"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 )
@@ -53,3 +55,240 @@ func (a *AzureSasCredential) AuthenticationPolicy(azcore.AuthenticationPolicyOpt
 		return req.Next()
 	})
 }
+
+type SharedAccessSignature struct {
+	accountName string
+	accountKey  string
+}
+
+func NewSharedAccessSignature(accountName, accountKey string) (*SharedAccessSignature, error) {
+	return &SharedAccessSignature{
+		accountName: accountName,
+		accountKey:  accountKey,
+	}, nil
+}
+
+type ResourceType struct {
+	Service   bool
+	Object    bool
+	Container bool
+}
+
+func (r ResourceType) String() string {
+	s := ""
+	if r.Service {
+		s += "s"
+	}
+	if r.Container {
+		s += "c"
+	}
+	if r.Object {
+		s += "o"
+	}
+	return s
+}
+
+type AccountSasPermissions struct {
+	Read    bool
+	Write   bool
+	Delete  bool
+	List    bool
+	Add     bool
+	Create  bool
+	Update  bool
+	Process bool
+}
+
+// TODO: Build bitmap
+func (a *AccountSasPermissions) String() string {
+	var ret string
+	if a.Read {
+		ret += "r"
+	}
+	if a.Write {
+		ret += "w"
+	}
+	if a.Delete {
+		ret += "d"
+	}
+	if a.List {
+		ret += "l"
+	}
+	if a.Add {
+		ret += "a"
+	}
+	if a.Create {
+		ret += "c"
+	}
+	if a.Update {
+		ret += "u"
+	}
+	if a.Process {
+		ret += "p"
+	}
+	return ret
+}
+
+type SasProtocol string
+
+const (
+	SasProtocolHttps = "https"
+	SasProtocolHttp  = "http"
+)
+
+type AccountSignatureProperties struct {
+	// Required: Specifies the services as a bitmap accessible with the account SAS. Default is Tables (t)
+	Services string
+	// Required: Specifies the resource types that are accessible with the account SAS. Options are Contain
+	ResourceTypes ResourceType
+	// Required: The permissions associated with the shared access signature, the user is restricted to operations
+	// allowed by the permissions
+	Permissions AccountSasPermissions
+	// Required: The time the shared access signature becomes invalid
+	Expiry *time.Time
+	// The time when the shared access signature becomes valid
+	Start *time.Time
+	// Specifies an IP address or range of IP addresses from which to accept requests
+	IpAddress string
+	// Specifies the protocol permitted for a request made.
+	Protocol SasProtocol
+}
+
+var X_MS_VERSION = "2020-08-04"
+
+// GenerateAccountSignature creates a signature that delegates service-level operations.
+func GenerateAccountSignature(cred SharedKeyCredential, properties AccountSignatureProperties) (string, error) {
+	sas := newSharedAccessSignature(cred, X_MS_VERSION)
+	return sas.generateAccount("t", properties)
+}
+
+type TableSignatureProperties struct {
+	TableName string
+}
+
+func GenerateTableSignature(cred SharedKeyCredential, properties TableSignatureProperties) (string, error) {
+	sas := newSharedAccessSignature(cred, X_MS_VERSION)
+	return sas.generateTable(properties)
+}
+
+type sharedAccessSignature struct {
+	cred     SharedKeyCredential
+	version  string
+	queryMap map[string]string
+}
+
+func newSharedAccessSignature(cred SharedKeyCredential, version string) *sharedAccessSignature {
+	return &sharedAccessSignature{
+		cred:     cred,
+		version:  X_MS_VERSION,
+		queryMap: make(map[string]string),
+	}
+}
+
+func (s *sharedAccessSignature) generateAccount(service string, properties AccountSignatureProperties) (string, error) {
+	s.addBase(properties.Permissions, properties.Expiry, properties.Start, properties.IpAddress, properties.Protocol, s.version)
+	s.addAccount("t", properties.ResourceTypes)
+	err := s.addAccountSignature()
+	if err != nil {
+		return "", err
+	}
+	return s.getToken()
+}
+
+func (s *sharedAccessSignature) generateTable(properties TableSignatureProperties) (string, error) {
+	return "", nil
+}
+
+func toUtcDatetime(t *time.Time) string {
+	return t.Format(utcLayout) + "Z"
+}
+
+func (s *sharedAccessSignature) addBase(permissions AccountSasPermissions, expiry *time.Time, start *time.Time, ip string, protocol SasProtocol, version string) {
+	s.addQuery(queryStringSignedExpiry, toUtcDatetime(expiry))
+	s.addQuery(queryStringSignedStart, toUtcDatetime(start))
+	s.addQuery(queryStringSignedPermission, permissions.String())
+	s.addQuery(queryStringSignedIp, ip)
+	s.addQuery(querySignedProtocol, string(protocol))
+	s.addQuery(querySignedVersion, version)
+}
+
+func (s *sharedAccessSignature) addAccount(services string, resources ResourceType) {
+	s.addQuery(querySignedServices, services)
+	s.addQuery(querySignedResourceTypes, resources.String())
+}
+
+func (s *sharedAccessSignature) addAccountSignature() error {
+	// Get string to sign
+	signedString := s.buildStringToSign()
+
+	signed, err := s.cred.ComputeHMACSHA256(signedString)
+	if err != nil {
+		return err
+	}
+	s.addQuery(queryStringSignedSignature, signed)
+	return nil
+}
+
+func (s *sharedAccessSignature) buildStringToSign() string {
+	var ret string
+	ret += s.cred.accountName + "\n"
+	ret += s.getValueToAppend(queryStringSignedPermission)
+	ret += s.getValueToAppend(querySignedServices)
+	ret += s.getValueToAppend(querySignedResourceTypes)
+	ret += s.getValueToAppend(queryStringSignedStart)
+	ret += s.getValueToAppend(queryStringSignedExpiry)
+	ret += s.getValueToAppend(queryStringSignedIp)
+	ret += s.getValueToAppend(querySignedProtocol)
+	ret += s.getValueToAppend(querySignedVersion)
+	fmt.Println(ret)
+	return ret
+}
+
+func (s *sharedAccessSignature) getValueToAppend(queryString string) string {
+	val, ok := s.queryMap[queryString]
+	if !ok {
+		return "" + "\n"
+	}
+	return val + "\n"
+}
+
+func (s *sharedAccessSignature) addQuery(name, val string) {
+	if val != "" {
+		s.queryMap[name] = val
+	}
+}
+
+func (s *sharedAccessSignature) getToken() (string, error) {
+	fmt.Println(s.queryMap)
+	pairsList := make([]string, 0)
+	for k, v := range s.queryMap {
+		if v != "" {
+			pairsList = append(pairsList, fmt.Sprintf("%v=%v", k, v))
+		}
+	}
+	return "?" + strings.Join(pairsList, "&"), nil
+}
+
+const (
+	queryStringSignedSignature    = "sig"
+	queryStringSignedPermission   = "sp"
+	queryStringSignedStart        = "st"
+	queryStringSignedExpiry       = "se"
+	queryStringSignedResource     = "sr"
+	queryStringSignedIdentifier   = "si"
+	queryStringSignedIp           = "sip"
+	querySignedProtocol           = "spr"
+	querySignedVersion            = "sv"
+	querySignedCacheControl       = "rscc"
+	querySignedContentDisposition = "rscd"
+	querySignedContentEncoding    = "rsce"
+	querySignedContentLanguage    = "rscl"
+	querySignedContentType        = "rsct"
+	querySignedStartPk            = "spk"
+	querySignedStartRk            = "srk"
+	querySignedEndPk              = "epk"
+	querySignedEndRk              = "erk"
+	querySignedResourceTypes      = "srt"
+	querySignedServices           = "ss"
+	querySignedTableName          = "tn"
+)
--- sdk/tables/aztable/shared_access_signature_test.go
+++ sdk/tables/aztable/shared_access_signature_test.go
@@ -5,7 +5,10 @@ package aztable
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -20,5 +23,50 @@ func TestSasCreateTable(t *testing.T) {
 	require.NoError(t, err)
 
 	_, err = client.Create(context.Background())
+	require.NoError(t, err)
+}
+
+func TestCreateSAS(t *testing.T) {
+	accountName := os.Getenv("TABLES_PRIMARY_ACCOUNT_NAME")
+	accountKey := os.Getenv("TABLES_PRIMARY_STORAGE_ACCOUNT_KEY")
+	cred, err := NewSharedKeyCredential(accountName, accountKey)
+	require.NoError(t, err)
+
+	start := time.Date(2021, time.August, 3, 18, 0, 6, 0, time.UTC)
+	end := time.Date(2021, time.August, 4, 2, 0, 6, 0, time.UTC)
+
+	properties := AccountSignatureProperties{
+		ResourceTypes: ResourceType{
+			Service:   true,
+			Container: true,
+			Object:    true,
+		},
+		Permissions: AccountSasPermissions{
+			Read:   true,
+			Write:  true,
+			Delete: true,
+			List:   true,
+			Add:    true,
+			Create: true,
+			Update: true,
+		},
+		Start:    &start,
+		Expiry:   &end,
+		Protocol: SasProtocolHttps,
+	}
+
+	key, err := GenerateAccountSignature(*cred, properties)
+	require.NoError(t, err)
+	fmt.Println(key)
+
+	sascred, err := NewAzureSasCredential(key)
+	require.NoError(t, err)
+
+	client, err := NewTableClient("sastable", fmt.Sprintf("https://%v.table.core.windows.net", accountName), sascred, nil)
+	require.NoError(t, err)
+	_, err = client.Create(context.Background())
+	require.NoError(t, err)
+
+	_, err = client.Delete(context.Background())
 	require.NoError(t, err)
 }

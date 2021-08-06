8b5faab3db59773e401d8d50bd4323f26c761822
Matias Quaranta <ealsur@hotmail.com>
2021-08-06 13:31:15 -0700

    container

---
+++ sdk/cosmos/azcosmos/cosmos_container_properties_test.go
@@ -0,0 +1,91 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azcosmos
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+)
+
+func TestContainerPropertiesSerialization(t *testing.T) {
+	nowAsUnix := time.Now().Unix()
+
+	now := UnixTime{
+		Time: time.Unix(nowAsUnix, 0),
+	}
+
+	properties := &CosmosContainerProperties{
+		Id:           "someId",
+		ETag:         "someEtag",
+		SelfLink:     "someSelfLink",
+		ResourceId:   "someResourceId",
+		LastModified: &now,
+		PartitionKeyDefinition: &PartitionKeyDefinition{
+			Paths:   []string{"somePath"},
+			Version: PartitionKeyDefinitionVersion2,
+		},
+	}
+
+	jsonString, err := json.Marshal(properties)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	otherProperties := &CosmosContainerProperties{}
+	err = json.Unmarshal(jsonString, otherProperties)
+	if err != nil {
+		t.Fatal(err, string(jsonString))
+	}
+
+	if properties.Id != otherProperties.Id {
+		t.Errorf("Expected otherProperties.Id to be %s, but got %s", properties.Id, otherProperties.Id)
+	}
+
+	if properties.ETag != otherProperties.ETag {
+		t.Errorf("Expected otherProperties.ETag to be %s, but got %s", properties.ETag, otherProperties.ETag)
+	}
+
+	if properties.SelfLink != otherProperties.SelfLink {
+		t.Errorf("Expected otherProperties.SelfLink to be %s, but got %s", properties.SelfLink, otherProperties.SelfLink)
+	}
+
+	if properties.ResourceId != otherProperties.ResourceId {
+		t.Errorf("Expected otherProperties.ResourceId to be %s, but got %s", properties.ResourceId, otherProperties.ResourceId)
+	}
+
+	if properties.LastModified.Time != otherProperties.LastModified.Time {
+		t.Errorf("Expected otherProperties.LastModified.Time to be %s, but got %s", properties.LastModified.Time.UTC(), otherProperties.LastModified.Time.UTC())
+	}
+
+	if otherProperties.PartitionKeyDefinition == nil {
+		t.Errorf("Expected otherProperties.PartitionKeyDefinition to be not nil, but got nil")
+	}
+
+	if properties.PartitionKeyDefinition.Paths[0] != otherProperties.PartitionKeyDefinition.Paths[0] {
+		t.Errorf("Expected otherProperties.PartitionKeyDefinition.Paths[0] to be %s, but got %s", properties.PartitionKeyDefinition.Paths[0], otherProperties.PartitionKeyDefinition.Paths[0])
+	}
+
+	if properties.PartitionKeyDefinition.Version != otherProperties.PartitionKeyDefinition.Version {
+		t.Errorf("Expected otherProperties.PartitionKeyDefinition.Version to be %d, but got %d", properties.PartitionKeyDefinition.Version, otherProperties.PartitionKeyDefinition.Version)
+	}
+}
+
+func TestContainerPropertiesSerializationWithTTL(t *testing.T) {
+	jsonString := []byte(`{"defaultTtl": 10, "analyticalStorageTtl": 20}`)
+
+	properties := &CosmosContainerProperties{}
+	err := json.Unmarshal(jsonString, properties)
+	if err != nil {
+		t.Fatal(err, string(jsonString))
+	}
+
+	if properties.DefaultTimeToLive != 10 {
+		t.Errorf("Expected properties.DefaultTimeToLive to be %d, but got %d", 10, properties.DefaultTimeToLive)
+	}
+
+	if properties.AnalyticalStoreTimeToLiveInSeconds != 20 {
+		t.Errorf("Expected properties.AnalyticalStoreTimeToLiveInSeconds to be %d, but got %d", 20, properties.AnalyticalStoreTimeToLiveInSeconds)
+	}
+}
---
+++ sdk/cosmos/azcosmos/cosmos_container_response_test.go
@@ -0,0 +1,112 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azcosmos
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/mock"
+)
+
+func TestContainerResponseParsing(t *testing.T) {
+	nowAsUnix := time.Now().Unix()
+
+	now := UnixTime{
+		Time: time.Unix(nowAsUnix, 0),
+	}
+
+	properties := &CosmosContainerProperties{
+		Id:           "someId",
+		ETag:         "someEtag",
+		SelfLink:     "someSelfLink",
+		ResourceId:   "someResourceId",
+		LastModified: &now,
+		PartitionKeyDefinition: &PartitionKeyDefinition{
+			Paths:   []string{"somePath"},
+			Version: PartitionKeyDefinitionVersion2,
+		},
+	}
+
+	jsonString, err := json.Marshal(properties)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	srv, close := mock.NewTLSServer()
+	defer close()
+	srv.SetResponse(
+		mock.WithBody(jsonString),
+		mock.WithHeader(cosmosHeaderEtag, "someEtag"),
+		mock.WithHeader(cosmosHeaderActivityId, "someActivityId"),
+		mock.WithHeader(cosmosHeaderRequestCharge, "13.42"))
+
+	req, err := azcore.NewRequest(context.Background(), http.MethodGet, srv.URL())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pl := azcore.NewPipeline(srv)
+	resp, _ := pl.Do(req)
+	parsedResponse, err := newCosmosContainerResponse(resp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if parsedResponse.RawResponse == nil {
+		t.Fatal("parsedResponse.RawResponse is nil")
+	}
+
+	if parsedResponse.ContainerProperties == nil {
+		t.Fatal("parsedResponse.ContainerProperties is nil")
+	}
+
+	if properties.Id != parsedResponse.ContainerProperties.Id {
+		t.Errorf("Expected Id to be %s, but got %s", properties.Id, parsedResponse.ContainerProperties.Id)
+	}
+
+	if properties.ETag != parsedResponse.ContainerProperties.ETag {
+		t.Errorf("Expected ETag to be %s, but got %s", properties.ETag, parsedResponse.ContainerProperties.ETag)
+	}
+
+	if properties.SelfLink != parsedResponse.ContainerProperties.SelfLink {
+		t.Errorf("Expected SelfLink to be %s, but got %s", properties.SelfLink, parsedResponse.ContainerProperties.SelfLink)
+	}
+
+	if properties.ResourceId != parsedResponse.ContainerProperties.ResourceId {
+		t.Errorf("Expected ResourceId to be %s, but got %s", properties.ResourceId, parsedResponse.ContainerProperties.ResourceId)
+	}
+
+	if properties.LastModified.Time != parsedResponse.ContainerProperties.LastModified.Time {
+		t.Errorf("Expected LastModified.Time to be %s, but got %s", properties.LastModified.Time.UTC(), parsedResponse.ContainerProperties.LastModified.Time.UTC())
+	}
+
+	if parsedResponse.ContainerProperties.PartitionKeyDefinition == nil {
+		t.Errorf("Expected PartitionKeyDefinition to be not nil, but got nil")
+	}
+
+	if properties.PartitionKeyDefinition.Paths[0] != parsedResponse.ContainerProperties.PartitionKeyDefinition.Paths[0] {
+		t.Errorf("Expected PartitionKeyDefinition.Paths[0] to be %s, but got %s", properties.PartitionKeyDefinition.Paths[0], parsedResponse.ContainerProperties.PartitionKeyDefinition.Paths[0])
+	}
+
+	if properties.PartitionKeyDefinition.Version != parsedResponse.ContainerProperties.PartitionKeyDefinition.Version {
+		t.Errorf("Expected PartitionKeyDefinition.Version to be %d, but got %d", properties.PartitionKeyDefinition.Version, parsedResponse.ContainerProperties.PartitionKeyDefinition.Version)
+	}
+
+	if parsedResponse.ActivityId() != "someActivityId" {
+		t.Errorf("Expected ActivityId to be %s, but got %s", "someActivityId", parsedResponse.ActivityId())
+	}
+
+	if parsedResponse.RequestCharge() != 13.42 {
+		t.Errorf("Expected RequestCharge to be %f, but got %f", 13.42, parsedResponse.RequestCharge())
+	}
+
+	if parsedResponse.ETag() != "someEtag" {
+		t.Errorf("Expected ETag to be %s, but got %s", "someEtag", parsedResponse.ActivityId())
+	}
+}

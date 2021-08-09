8f0a6eecc4956c645e7482585d31f77ebf42ee7e
Matias Quaranta <ealsur@hotmail.com>
2021-08-09 15:05:21 -0700

    indexing policy

--- sdk/cosmos/azcosmos/cosmos_container_properties.go
+++ sdk/cosmos/azcosmos/cosmos_container_properties.go
@@ -5,15 +5,15 @@ package azcosmos
 
 // CosmosContainerProperties represents the properties of a container.
 type CosmosContainerProperties struct {
-	// Id contains the unique id of the container
+	// Id contains the unique id of the container.
 	Id string `json:"id"`
-	// ETag contains the entity etag of the container
+	// ETag contains the entity etag of the container.
 	ETag string `json:"_etag,omitempty"`
-	// SelfLink contains the self-link of the container
+	// SelfLink contains the self-link of the container.
 	SelfLink string `json:"_self,omitempty"`
-	// ResourceId contains the resource id of the container
+	// ResourceId contains the resource id of the container.
 	ResourceId string `json:"_rid,omitempty"`
-	// LastModified contains the last modified time of the container
+	// LastModified contains the last modified time of the container.
 	LastModified *UnixTime `json:"_ts,omitempty"`
 	// DefaultTimeToLive contains the default time to live in seconds for items in the container.
 	// For more information see https://docs.microsoft.com/azure/cosmos-db/time-to-live#time-to-live-configurations
@@ -21,8 +21,10 @@ type CosmosContainerProperties struct {
 	// AnalyticalStoreTimeToLiveInSeconds contains the default time to live in seconds for analytical store in the container.
 	// For more information see https://docs.microsoft.com/azure/cosmos-db/analytical-store-introduction#analytical-ttl
 	AnalyticalStoreTimeToLiveInSeconds *int `json:"analyticalStorageTtl,omitempty"`
-	// PartitionKeyDefinition contains the partition key definition of the container
+	// PartitionKeyDefinition contains the partition key definition of the container.
 	PartitionKeyDefinition *PartitionKeyDefinition `json:"partitionKey,omitempty"`
+	// IndexingPolicy contains the indexing definition of the container.
+	IndexingPolicy *IndexingPolicy `json:"indexingPolicy,omitempty"`
 	// Container represented by these properties
 	Container *CosmosContainer `json:"-"`
 }

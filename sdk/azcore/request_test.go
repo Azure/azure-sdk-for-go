8a7a752b1cc3039caa2952ffab6054c6662371f7
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2021-08-13 13:42:55 -0400

    [azcore] JoinPaths will preserve query parameters (#15258)
    * adding join paths changes
    
    * fixing test
    
    * small fix and adding test scenario
    
    * jeffs feedback from another PR
    
    * forgot to remove the func
    
    * cleaner return

--- sdk/azcore/request_test.go
+++ sdk/azcore/request_test.go
@@ -510,11 +510,16 @@ func TestNewRequestFail(t *testing.T) {
 }
 
 func TestJoinPaths(t *testing.T) {
-	if path := JoinPaths(); path != "" {
+	if path := JoinPaths(""); path != "" {
 		t.Fatalf("unexpected path %s", path)
 	}
-	const expected = "http://test.contoso.com/path/one/path/two/path/three/path/four/"
+	expected := "http://test.contoso.com/path/one/path/two/path/three/path/four/"
 	if path := JoinPaths("http://test.contoso.com/", "/path/one", "path/two", "/path/three/", "path/four/"); path != expected {
+		t.Fatalf("got %s, expected %s", path, expected)
+	}
+
+	expected = "http://test.contoso.com/path/one/path/two/?qp1=abc&qp2=def"
+	if path := JoinPaths("http://test.contoso.com/?qp1=abc&qp2=def", "/path/one", "path/two"); path != expected {
 		t.Fatalf("got %s, expected %s", path, expected)
 	}
 }

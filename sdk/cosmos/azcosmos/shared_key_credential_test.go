7d869374ce7784e061c833340343ca02081f8082
Matias Quaranta <ealsur@hotmail.com>
2021-08-13 15:53:49 -0700

    removing emulator

--- sdk/cosmos/azcosmos/shared_key_credential_test.go
+++ sdk/cosmos/azcosmos/shared_key_credential_test.go
@@ -24,6 +24,7 @@ func getTestSharedKeyCredentialInfo() (endpoint string, key string) {
 
 func Test_buildCanonicalizedAuthHeader(t *testing.T) {
 	_, key := getTestSharedKeyCredentialInfo()
+
 	cred, err := NewSharedKeyCredential(key)
 
 	assert.NoError(t, err)

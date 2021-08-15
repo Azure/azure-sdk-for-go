5a531414693aefbcb1000e49b7041ddb08318b6c
tadelesh <tadelesh.shi@live.cn>
2021-08-15 20:09:14 +0800

    fix: ln pwsh.exe to pwsh to run in linux

--- initScriptTrack2.sh
+++ initScriptTrack2.sh
@@ -8,6 +8,7 @@ fi
 PATH=$PATH:$GOPATH/bin
 export GO111MODULE=on
 cd tools/generator && go build && cp generator $GOPATH/bin && cd ../..
+ln -s /usr/bin/pwsh $GOPATH/bin/pwsh.exe
 cat > $2 << EOF
 {
   "envs": {

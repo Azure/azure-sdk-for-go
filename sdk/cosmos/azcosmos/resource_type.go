2a124bb29912b34162ae88135b5a3e7eb2bc5602
Matias Quaranta <ealsur@hotmail.com>
2021-08-17 10:42:38 -0700

    adding resourcetype

---
+++ sdk/cosmos/azcosmos/resource_type.go
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azcosmos
+
+// resourceType defines supported values for resources.
+type resourceType int
+
+const (
+	resourceTypeDatabase   resourceType = 0
+	resourceTypeCollection resourceType = 1
+)

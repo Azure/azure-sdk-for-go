44580babf04236c21bf4e391cdbeeb2d8a041f7b
Matias Quaranta <ealsur@hotmail.com>
2021-08-17 10:42:47 -0700

    adding path map

--- sdk/cosmos/azcosmos/cosmos_paths.go
+++ sdk/cosmos/azcosmos/cosmos_paths.go
@@ -3,7 +3,23 @@
 
 package azcosmos
 
+import (
+	"errors"
+	"fmt"
+)
+
 const (
 	pathSegmentDatabase   string = "dbs"
 	pathSegmentCollection string = "colls"
 )
+
+func getResourcePath(resourceType resourceType) (string, error) {
+	switch resourceType {
+	case resourceTypeDatabase:
+		return pathSegmentDatabase, nil
+	case resourceTypeCollection:
+		return pathSegmentCollection, nil
+	default:
+		return "", errors.New(fmt.Sprintf("%v is not a valid resource type", resourceType))
+	}
+}

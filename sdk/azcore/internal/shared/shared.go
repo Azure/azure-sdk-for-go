5ad5060ac65d78755f0d02c5e6de4f9f7382da9b
Joel Hendrix <jhendrix@microsoft.com>
2021-08-23 16:10:37 -0700

    move constants to make version info easier to find

--- sdk/azcore/internal/shared/shared.go
+++ sdk/azcore/internal/shared/shared.go
@@ -17,33 +17,6 @@ import (
 	"time"
 )
 
-const (
-	ContentTypeAppJSON = "application/json"
-	ContentTypeAppXML  = "application/xml"
-)
-
-const (
-	HeaderAzureAsync        = "Azure-AsyncOperation"
-	HeaderContentLength     = "Content-Length"
-	HeaderContentType       = "Content-Type"
-	HeaderLocation          = "Location"
-	HeaderOperationLocation = "Operation-Location"
-	HeaderRetryAfter        = "Retry-After"
-	HeaderUserAgent         = "User-Agent"
-)
-
-const (
-	DefaultMaxRetries = 3
-)
-
-const (
-	// Module is the name of the calling module used in telemetry data.
-	Module = "azcore"
-
-	// Version is the semantic version (see http://semver.org) of this module.
-	Version = "v0.19.0"
-)
-
 // CtxWithHTTPHeaderKey is used as a context key for adding/retrieving http.Header.
 type CtxWithHTTPHeaderKey struct{}
 

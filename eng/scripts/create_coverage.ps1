9af25282d81ae2dd47d5141400d64aba3a77bdba
seankane-msft <seankane@microsoft.com>
2021-08-23 12:35:23 -0400

    changing to powershell tool

--- eng/scripts/create_coverage.ps1
+++ eng/scripts/create_coverage.ps1
@@ -25,4 +25,39 @@ Get-Content ./coverage.json | gocov-html > ./coverage.html
 # Convert this to a simple powershell script for reproducibility
 # use internal tool to fail if coverage is too low
 Pop-Location
-go run ../tools/internal/coverage/main.go  -serviceDirectory $serviceDirectory
+# go run ../tools/internal/coverage/main.go  -serviceDirectory $serviceDirectory
+
+$patternMatches = Get-Content ./coverage.xml | Select-String -Pattern '<coverage line-rate=\"(\d\.\d+)\"'
+
+if ($patternMatches.Length -eq 0) {
+  Write-Host "Coverage.xml file did not contain coverage information"
+  Exit $1
+}
+
+[double] $coverageFloat = $patternMatches.Matches.Groups[1].Captures
+
+Write-Host $coverageFloat
+
+# Read eng/config.json to find appropriate Value
+
+$coverageGoals = Get-Content ./eng/config.json | Out-String | ConvertFrom-Json
+
+Write-Host $coverageGoals
+
+Write-Host $serviceDirectory
+Foreach ($pkg in $coverageGoals.Packages) {
+  Write-Host $pkg
+  if ($pkg.Name -Match $serviceDirectory) {
+    $goalCoverage = [double] $pkg.CoverageGoal
+
+    if ($goalCoverage -le $coverageFloat) {
+      Write-Host "Coverage is lower than the coverage goal"
+      Exit 1
+    } else {
+      Exit 0
+    }
+  }
+}
+
+Write-Host "Could not find coverage goal, specify the coverage goal for your package in eng/config.json"
+Exit $1

764ac5a8a06fccb054cfc57eafb5e2b4515e60cc
seankane-msft <seankane@microsoft.com>
2021-08-23 13:26:32 -0400

    splitting coverage portion and test portion into separate steps

--- eng/scripts/run_tests.ps1
+++ eng/scripts/run_tests.ps1
@@ -15,9 +15,7 @@ Write-Host "Currently in $temp"
 $runTests = $false
 # 0b. Verify there are test files with tests to run in at least one of these directories
 $testDirs | ForEach-Object {
-    Write-Host "[Loop]: Currently in: $_"
     Get-ChildItem -Path $_ -Filter *_test.go | ForEach-Object {
-        Write-Host "[Loop]: $_"
         if (Select-String -path $_ -pattern 'Test' -SimpleMatch) {
             $runTests = $true
         }
@@ -58,57 +56,57 @@ foreach ($td in $testDirs) {
     }
 }
 
-Set-Location $cwd
+# Set-Location $cwd
 
-$coverageFiles = [Collections.Generic.List[String]]@()
-Get-ChildItem -recurse -path . -filter coverage.txt | ForEach-Object {
-    $covFile = $_.FullName
-    Write-Host "Adding $covFile to the list of code coverage files"
-    $coverageFiles.Add($covFile)
-}
+# $coverageFiles = [Collections.Generic.List[String]]@()
+# Get-ChildItem -recurse -path . -filter coverage.txt | ForEach-Object {
+#     $covFile = $_.FullName
+#     Write-Host "Adding $covFile to the list of code coverage files"
+#     $coverageFiles.Add($covFile)
+# }
 
-# merge coverage files
-gocovmerge $coverageFiles > mergedCoverage.txt
-gocov convert ./mergedCoverage.txt > ./coverage.json
+# # merge coverage files
+# gocovmerge $coverageFiles > mergedCoverage.txt
+# gocov convert ./mergedCoverage.txt > ./coverage.json
 
-# gocov converts rely on standard input
-Get-Content ./coverage.json | gocov-xml > ./coverage.xml
-Get-Content ./coverage.json | gocov-html > ./coverage.html
+# # gocov converts rely on standard input
+# Get-Content ./coverage.json | gocov-xml > ./coverage.xml
+# Get-Content ./coverage.json | gocov-html > ./coverage.html
 
 Pop-Location
 
-Set-Location $cwd
-$patternMatches = Get-Content ./coverage.xml | Select-String -Pattern '<coverage line-rate=\"(\d\.\d+)\"'
+# Set-Location $cwd
+# $patternMatches = Get-Content ./coverage.xml | Select-String -Pattern '<coverage line-rate=\"(\d\.\d+)\"'
 
-if ($patternMatches.Length -eq 0) {
-  Write-Host "Coverage.xml file did not contain coverage information"
-  Exit $1
-}
+# if ($patternMatches.Length -eq 0) {
+#   Write-Host "Coverage.xml file did not contain coverage information"
+#   Exit $1
+# }
 
-[double] $coverageFloat = $patternMatches.Matches.Groups[1].Captures
+# [double] $coverageFloat = $patternMatches.Matches.Groups[1].Captures
 
-Write-Host $coverageFloat
+# Write-Host $coverageFloat
 
-# Read eng/config.json to find appropriate Value
+# # Read eng/config.json to find appropriate Value
 
-$coverageGoals = Get-Content ./eng/config.json | Out-String | ConvertFrom-Json
+# $coverageGoals = Get-Content ./eng/config.json | Out-String | ConvertFrom-Json
 
-Write-Host $coverageGoals
+# Write-Host $coverageGoals
 
-Write-Host $serviceDirectory
-Foreach ($pkg in $coverageGoals.Packages) {
-  Write-Host $pkg
-  if ($pkg.Name -Match $serviceDirectory) {
-    $goalCoverage = [double] $pkg.CoverageGoal
+# Write-Host $serviceDirectory
+# Foreach ($pkg in $coverageGoals.Packages) {
+#   Write-Host $pkg
+#   if ($pkg.Name -Match $serviceDirectory) {
+#     $goalCoverage = [double] $pkg.CoverageGoal
 
-    if ($goalCoverage -le $coverageFloat) {
-      Write-Host "Coverage is lower than the coverage goal"
-      Exit 1
-    } else {
-      Exit 0
-    }
-  }
-}
+#     if ($goalCoverage -le $coverageFloat) {
+#       Write-Host "Coverage is lower than the coverage goal"
+#       Exit 1
+#     } else {
+#       Exit 0
+#     }
+#   }
+# }
 
-Write-Host "Could not find coverage goal, specify the coverage goal for your package in eng/config.json"
-Exit $1
+# Write-Host "Could not find coverage goal, specify the coverage goal for your package in eng/config.json"
+# Exit $1

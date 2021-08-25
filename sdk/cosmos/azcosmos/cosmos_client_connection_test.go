0d9b79cd7cd1b2392be5e2ba55ce2e6a47965901
Matias Quaranta <ealsur@users.noreply.github.com>
2021-08-25 12:20:13 -0700

    Cosmos DB - Adding errors on failed responses (#15352)
    * custom error parsing of response
    
    * generate errors on failed responses
    
    * using azcore
    
    * tests
    
    * remove unneeded code in emu tests
    
    * better error parsing
    
    * new tests
    
    * test propagation
    
    * fixing build errors
    
    * missing error body
    
    * implementing interface
    
    * removing wrapping
    
    * as int

---
+++ sdk/cosmos/azcosmos/cosmos_client_connection_test.go
@@ -0,0 +1,69 @@
+// +build !emulator
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azcosmos
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/mock"
+)
+
+func TestEnsureErrorIsGeneratedOnResponse(t *testing.T) {
+	someError := &cosmosErrorResponse{
+		Code: "SomeCode",
+	}
+
+	jsonString, err := json.Marshal(someError)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	srv, close := mock.NewTLSServer()
+	defer close()
+	srv.SetResponse(
+		mock.WithBody(jsonString),
+		mock.WithStatusCode(404))
+
+	pl := azcore.NewPipeline(srv)
+	connection := &cosmosClientConnection{endpoint: srv.URL(), Pipeline: pl}
+	operationContext := cosmosOperationContext{
+		resourceType:    resourceTypeDatabase,
+		resourceAddress: "",
+	}
+	_, err = connection.sendGetRequest("/", context.Background(), operationContext, &CosmosContainerRequestOptions{})
+	if err == nil {
+		t.Fatal("Expected error")
+	}
+
+	asError := err.(*cosmosError)
+	if asError.ErrorCode() != someError.Code {
+		t.Errorf("Expected %v, but got %v", someError.Code, asError.ErrorCode())
+	}
+
+	if err.Error() != asError.Error() {
+		t.Errorf("Expected %v, but got %v", err.Error(), asError.Error())
+	}
+}
+
+func TestEnsureErrorIsNotGeneratedOnResponse(t *testing.T) {
+	srv, close := mock.NewTLSServer()
+	defer close()
+	srv.SetResponse(
+		mock.WithStatusCode(200))
+
+	pl := azcore.NewPipeline(srv)
+	connection := &cosmosClientConnection{endpoint: srv.URL(), Pipeline: pl}
+	operationContext := cosmosOperationContext{
+		resourceType:    resourceTypeDatabase,
+		resourceAddress: "",
+	}
+	_, err := connection.sendGetRequest("/", context.Background(), operationContext, &CosmosContainerRequestOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
---
+++ sdk/cosmos/azcosmos/cosmos_error.go
@@ -0,0 +1,65 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azcosmos
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+)
+
+// cosmosError is used as base error for any error response from the Cosmos service.
+type cosmosError struct {
+	body        string
+	code        string
+	rawResponse *http.Response
+}
+
+func (e *cosmosError) StatusCode() int {
+	if e.rawResponse == nil {
+		return 0
+	}
+	return e.rawResponse.StatusCode
+}
+
+func (e *cosmosError) ErrorCode() string {
+	return e.code
+}
+
+func (e *cosmosError) RawResponse() *http.Response {
+	return e.rawResponse
+}
+
+func newCosmosError(response *azcore.Response) error {
+	bytesRead, err := response.Payload()
+	if err != nil {
+		return err
+	}
+
+	cError := cosmosError{
+		rawResponse: response.Response,
+		body:        string(bytesRead),
+	}
+
+	// Attempt to extract Code from body
+	var cErrorResponse cosmosErrorResponse
+	err = json.Unmarshal(bytesRead, &cErrorResponse)
+	if err == nil {
+		cError.code = cErrorResponse.Code
+	}
+
+	return &cError
+}
+
+func (e *cosmosError) Error() string {
+	if e.body == "" {
+		return "response contained no body"
+	}
+	return e.body
+}
+
+type cosmosErrorResponse struct {
+	Code string `json:"Code"`
+}

f66a8e53f3a8b62118336c5b14f4292575f01a32
Matias Quaranta <ealsur@hotmail.com>
2021-08-25 09:41:03 -0700

    components

--- sdk/cosmos/azcosmos/partition_key_internal.go
+++ sdk/cosmos/azcosmos/partition_key_internal.go
@@ -3,19 +3,80 @@
 
 package azcosmos
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
-type partitionKeyInternal struct{}
+var (
+	nonePartitionKey      = &partitionKeyInternal{components: nil}
+	emptyPartitionKey     = &partitionKeyInternal{components: []interface{}{}}
+	undefinedPartitionKey = &partitionKeyInternal{components: []interface{}{partitionKeyUndefinedComponent{}}}
+)
+
+type partitionKeyInternal struct {
+	components []interface{}
+}
 
 func newPartitionKeyInternal(values []interface{}) (*partitionKeyInternal, error) {
-	components := make([]int, len(values))
-	for _, v := range values {
-		switch v.(type) {
+	components := make([]interface{}, len(values))
+	for i, v := range values {
+		var component interface{}
+		switch val := v.(type) {
 		case nil:
-
-		case bool, string, int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64:
+			component = partitionKeyNullComponent{}
+		case bool:
+			component = partitionKeyBoolComponent{val}
+		case string:
+			component = partitionKeyStringComponent{val}
+		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64:
+			component = partitionKeyNumberComponent{v.(float64)}
 		default:
 			return nil, fmt.Errorf("PartitionKey can only be a string, bool, or a number: '%T'", v)
 		}
+
+		components[i] = component
 	}
+
+	return &partitionKeyInternal{components: components}, nil
+}
+
+func (p *partitionKeyInternal) MarshalJSON() ([]byte, error) {
+	return json.Marshal(p.components)
+}
+
+type partitionKeyNumberComponent struct {
+	value float64
+}
+
+func (p *partitionKeyNumberComponent) MarshalJSON() ([]byte, error) {
+	return json.Marshal(p.value)
+}
+
+type partitionKeyBoolComponent struct {
+	value bool
+}
+
+func (p *partitionKeyBoolComponent) MarshalJSON() ([]byte, error) {
+	return json.Marshal(p.value)
+}
+
+type partitionKeyStringComponent struct {
+	value string
+}
+
+func (p *partitionKeyStringComponent) MarshalJSON() ([]byte, error) {
+	return json.Marshal(p.value)
+}
+
+type partitionKeyNullComponent struct{}
+
+func (p *partitionKeyNullComponent) MarshalJSON() ([]byte, error) {
+	return []byte("null"), nil
+}
+
+type partitionKeyUndefinedComponent struct{}
+
+func (p *partitionKeyUndefinedComponent) MarshalJSON() ([]byte, error) {
+	return []byte("{}"), nil
 }

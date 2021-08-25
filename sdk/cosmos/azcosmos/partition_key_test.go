f07bf141ffc54a014b56f19f1840bf7d4520d552
Matias Quaranta <ealsur@hotmail.com>
2021-08-25 11:42:36 -0700

    equality comparisson

--- sdk/cosmos/azcosmos/partition_key_test.go
+++ sdk/cosmos/azcosmos/partition_key_test.go
@@ -5,6 +5,7 @@
 package azcosmos
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -63,6 +64,39 @@ func TestValidPartitionKeyValues(t *testing.T) {
 
 		if serialization != expectedSerialization {
 			t.Errorf("Expected serialization %v, but got %v", expectedSerialization, serialization)
+		}
+	}
+}
+
+func TestPartitionKeyEquality(t *testing.T) {
+	validTypes := []interface{}{
+		nil,
+		true,
+		false,
+		"some string",
+		int(10),
+		int8(10),
+		int16(10),
+		int32(10),
+		int64(10),
+		uint(10),
+		uint8(10),
+		uint16(10),
+		uint32(10),
+		uint64(10),
+		float32(10.5),
+		float64(10.5),
+	}
+
+	for _, validType := range validTypes {
+		pk, err := NewPartitionKey(validType)
+		if err != nil {
+			t.Errorf("Expected success for partition key type %v and got %v", validType, err)
+		}
+
+		pk2, _ := NewPartitionKey(validType)
+		if !reflect.DeepEqual(pk, pk2) {
+			t.Errorf("Expected %v to equal %v", pk, pk2)
 		}
 	}
 }

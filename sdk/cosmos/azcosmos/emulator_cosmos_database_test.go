6807ac2d162034eb9ce007b02bdcf3549f5fcbb0
Matias Quaranta <ealsur@hotmail.com>
2021-08-27 16:02:03 -0700

    some emulator tests

--- sdk/cosmos/azcosmos/emulator_cosmos_database_test.go
+++ sdk/cosmos/azcosmos/emulator_cosmos_database_test.go
@@ -14,7 +14,38 @@ func TestDatabaseCRUD(t *testing.T) {
 	client := emulatorTests.getClient(t)
 
 	database := CosmosDatabaseProperties{Id: "baseDbTest"}
-	resp, err := client.CreateDatabase(context.TODO(), database, nil)
+
+	resp, err := client.CreateDatabase(context.TODO(), database, nil, nil)
+	if err != nil {
+		t.Fatalf("Failed to create database: %v", err)
+	}
+
+	if resp.DatabaseProperties.Id != database.Id {
+		t.Errorf("Unexpected id match: %v", resp.DatabaseProperties)
+	}
+
+	resp, err = resp.DatabaseProperties.Database.Read(context.TODO(), nil)
+	if err != nil {
+		t.Fatalf("Failed to read database: %v", err)
+	}
+
+	if resp.DatabaseProperties.Id != database.Id {
+		t.Errorf("Unexpected id match: %v", resp.DatabaseProperties)
+	}
+
+	resp, err = resp.DatabaseProperties.Database.Delete(context.TODO(), nil)
+	if err != nil {
+		t.Fatalf("Failed to delete database: %v", err)
+	}
+}
+
+func TestDatabaseWithOfferCRUD(t *testing.T) {
+	emulatorTests := newEmulatorTests()
+	client := emulatorTests.getClient(t)
+
+	database := CosmosDatabaseProperties{Id: "baseDbTest"}
+	tp := NewManualThroughputProperties(400)
+	resp, err := client.CreateDatabase(context.TODO(), database, tp, nil)
 	if err != nil {
 		t.Fatalf("Failed to create database: %v", err)
 	}

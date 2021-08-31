d1f0bb660d9cbcb37e7d1744a2382a390c00f833
seankane-msft <seankane@microsoft.com>
2021-08-31 14:31:19 -0400

    unecessary

--- sdk/azcore/version.go
+++
@@ -1,9 +0,0 @@
-//go:build go1.16
-// +build go1.16
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-package azcore
-
-const version = "v0.20.0"

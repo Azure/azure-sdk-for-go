9819e4d8d48028533b8e9c65a4feedd3a60a3fe5
seankane-msft <seankane@microsoft.com>
2021-08-31 13:24:06 -0400

    adding version

---
+++ sdk/tables/aztable/constants.go
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package aztable
+
+var version = "0.1.0"
+
+const (
+	headerXmsDate       = "x-ms-date"
+	headerAuthorization = "Authorization"
+	etagOData           = "odata.etag"
+)

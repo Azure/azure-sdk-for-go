f2f5e75e7a9627e8ff81c7067ee9798f5d023740
tadelesh <tadelesh.shi@live.cn>
2021-09-02 14:03:36 +0800

    fix: polish according to pr review

--- eng/scripts/Language-Settings.ps1
+++ eng/scripts/Language-Settings.ps1
@@ -2,19 +2,28 @@ $Language = "go"
 $packagePattern = "go.mod"
 # rewrite from ChangeLog-Operations.ps1 used in Get-ChangeLogEntriesFromContent for go uses vx.x.x as version number
 $RELEASE_TITLE_REGEX = "(?<releaseNoteTitle>^\#+\s+(?<version>v$([AzureEngSemanticVersion]::SEMVER_REGEX))(\s+(?<releaseStatus>\(.+\))))"
-# constants for go version fetch
-$GO_VERSION_REGEX = ".+\s*=\s*`".*v?(?<version>$([AzureEngSemanticVersion]::SEMVER_REGEX))`""
-$VERSION_FILE_SUFFIXS = "*constants.go", "*version.go"
 
 # rewrite from artifact-metadata-parsing.ps1 used in RetrievePackages for fetch go single module info
 function Get-go-PackageInfoFromPackageFile ($pkg, $workingDirectory)
 {
     $workFolder = $pkg.Directory
     $releaseNotes = ""
-    $namespaceName = $workFolder.Name
-    $rpName = $workFolder.Parent.Name
 
-    $pkgId = "sdk/$rpName/$namespaceName"
+    if ($workFolder -match "sdk[\/|\\]((?<repopath>(?<service>.*?)[\/|\\])?(?<arm>arm)?(?<pkgname>.*))")
+    {
+        if ($matches["arm"])
+        {
+            $packageName = "arm" + $matches["pkgname"]
+            $rpName = $matches["service"]
+            $pkgId = "sdk/$rpName/$packageName"
+        }
+        else
+        {
+            $packageName = $matches["pkgname"]
+            $pkgId = "sdk/$packageName"
+        }
+    }
+
     $pkgVersion = Get-Version $workFolder
 
     $changeLogLoc = @(Get-ChildItem -Path $workFolder -Recurse -Include "CHANGELOG.md")[0]
@@ -37,30 +46,28 @@ function Get-go-PackageInfoFromPackageFile ($pkg, $workingDirectory)
 # get version from specific files (*constants.go, *version.go)
 function Get-Version ($pkgPath)
 {
-    # find any file with surfix
-    $versionFiles = [Collections.Generic.List[String]]@()
-    foreach ($versionFileSuffix in $VERSION_FILE_SUFFIXS)
+    # find any file with suffix
+    $versionFiles = @()
+    $version_file_suffixs = "*constants.go", "*version.go"
+    foreach ($versionFileSuffix in $version_file_suffixs)
     {
         Get-ChildItem -Recurse -Path $pkgPath -Filter $versionFileSuffix | ForEach-Object {
             Write-Host "Adding $_ to list of version files"
-            $versionFiles.Add($_)
+            $versionFiles += $_
         }
     }
     
     # for each version file, use regex to search go version num
+    $go_version_regex = ".+\s*=\s*`".*v?(?<version>$([AzureEngSemanticVersion]::SEMVER_REGEX))`""
     foreach ($versionFile in $versionFiles)
     {
-        $content = Get-Content $versionFile
-        $content = $content.Split("`n")
         try
         {
-            # walk the document, finding where the version number are
-            foreach ($line in $content)
+            $content = Get-Content $versionFile -Raw
+            # finding where the version number are
+            if ($content -match $go_version_regex)
             {
-                if ($line -match $GO_VERSION_REGEX)
-                {
-                    return $matches["version"]
-                }
+                return $matches["version"]
             }
         }
         catch

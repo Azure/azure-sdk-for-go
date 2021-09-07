abe35f530374c2d0ccff2e14e9ad4f4972632202
JiahuiPeng <46921893+804873052@users.noreply.github.com>
2021-09-07 11:17:59 +0800

    Release armservicebus v0.2.0 (#15445)
    * [Release] sdk/servicebus/armservicebus/0.2.0 generation from spec commit: 51b37b069ecbb9d2fcd300eabd4b10b7911b7d7d
    
    * fix
    
    * fix

--- sdk/servicebus/armservicebus/zz_generated_eventhubs_client.go
+++ sdk/servicebus/armservicebus/zz_generated_eventhubs_client.go
@@ -1,4 +1,5 @@
-// +build go1.13
+//go:build go1.16
+// +build go1.16
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
@@ -11,41 +12,44 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/Azure/azure-sdk-for-go/sdk/armcore"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"net/http"
 	"net/url"
 	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 )
 
 // EventHubsClient contains the methods for the EventHubs group.
 // Don't use this type directly, use NewEventHubsClient() instead.
 type EventHubsClient struct {
-	con            *armcore.Connection
+	ep             string
+	pl             runtime.Pipeline
 	subscriptionID string
 }
 
 // NewEventHubsClient creates a new instance of EventHubsClient with the specified values.
-func NewEventHubsClient(con *armcore.Connection, subscriptionID string) *EventHubsClient {
-	return &EventHubsClient{con: con, subscriptionID: subscriptionID}
+func NewEventHubsClient(con *arm.Connection, subscriptionID string) *EventHubsClient {
+	return &EventHubsClient{ep: con.Endpoint(), pl: con.NewPipeline(module, version), subscriptionID: subscriptionID}
 }
 
 // ListByNamespace - Gets all the Event Hubs in a service bus Namespace.
 // If the operation fails it returns the *ErrorResponse error type.
-func (client *EventHubsClient) ListByNamespace(resourceGroupName string, namespaceName string, options *EventHubsListByNamespaceOptions) EventHubsListByNamespacePager {
-	return &eventHubsListByNamespacePager{
+func (client *EventHubsClient) ListByNamespace(resourceGroupName string, namespaceName string, options *EventHubsListByNamespaceOptions) *EventHubsListByNamespacePager {
+	return &EventHubsListByNamespacePager{
 		client: client,
-		requester: func(ctx context.Context) (*azcore.Request, error) {
+		requester: func(ctx context.Context) (*policy.Request, error) {
 			return client.listByNamespaceCreateRequest(ctx, resourceGroupName, namespaceName, options)
 		},
-		advancer: func(ctx context.Context, resp EventHubsListByNamespaceResponse) (*azcore.Request, error) {
-			return azcore.NewRequest(ctx, http.MethodGet, *resp.EventHubListResult.NextLink)
+		advancer: func(ctx context.Context, resp EventHubsListByNamespaceResponse) (*policy.Request, error) {
+			return runtime.NewRequest(ctx, http.MethodGet, *resp.EventHubListResult.NextLink)
 		},
 	}
 }
 
 // listByNamespaceCreateRequest creates the ListByNamespace request.
-func (client *EventHubsClient) listByNamespaceCreateRequest(ctx context.Context, resourceGroupName string, namespaceName string, options *EventHubsListByNamespaceOptions) (*azcore.Request, error) {
+func (client *EventHubsClient) listByNamespaceCreateRequest(ctx context.Context, resourceGroupName string, namespaceName string, options *EventHubsListByNamespaceOptions) (*policy.Request, error) {
 	urlPath := "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ServiceBus/namespaces/{namespaceName}/eventhubs"
 	if resourceGroupName == "" {
 		return nil, errors.New("parameter resourceGroupName cannot be empty")
@@ -59,36 +63,35 @@ func (client *EventHubsClient) listByNamespaceCreateRequest(ctx context.Context,
 		return nil, errors.New("parameter client.subscriptionID cannot be empty")
 	}
 	urlPath = strings.ReplaceAll(urlPath, "{subscriptionId}", url.PathEscape(client.subscriptionID))
-	req, err := azcore.NewRequest(ctx, http.MethodGet, azcore.JoinPaths(client.con.Endpoint(), urlPath))
+	req, err := runtime.NewRequest(ctx, http.MethodGet, runtime.JoinPaths(client.ep, urlPath))
 	if err != nil {
 		return nil, err
 	}
-	req.Telemetry(telemetryInfo)
-	reqQP := req.URL.Query()
+	reqQP := req.Raw().URL.Query()
 	reqQP.Set("api-version", "2018-01-01-preview")
-	req.URL.RawQuery = reqQP.Encode()
-	req.Header.Set("Accept", "application/json")
+	req.Raw().URL.RawQuery = reqQP.Encode()
+	req.Raw().Header.Set("Accept", "application/json")
 	return req, nil
 }
 
 // listByNamespaceHandleResponse handles the ListByNamespace response.
-func (client *EventHubsClient) listByNamespaceHandleResponse(resp *azcore.Response) (EventHubsListByNamespaceResponse, error) {
-	result := EventHubsListByNamespaceResponse{RawResponse: resp.Response}
-	if err := resp.UnmarshalAsJSON(&result.EventHubListResult); err != nil {
+func (client *EventHubsClient) listByNamespaceHandleResponse(resp *http.Response) (EventHubsListByNamespaceResponse, error) {
+	result := EventHubsListByNamespaceResponse{RawResponse: resp}
+	if err := runtime.UnmarshalAsJSON(resp, &result.EventHubListResult); err != nil {
 		return EventHubsListByNamespaceResponse{}, err
 	}
 	return result, nil
 }
 
 // listByNamespaceHandleError handles the ListByNamespace error response.
-func (client *EventHubsClient) listByNamespaceHandleError(resp *azcore.Response) error {
-	body, err := resp.Payload()
+func (client *EventHubsClient) listByNamespaceHandleError(resp *http.Response) error {
+	body, err := runtime.Payload(resp)
 	if err != nil {
-		return azcore.NewResponseError(err, resp.Response)
+		return runtime.NewResponseError(err, resp)
 	}
 	errType := ErrorResponse{raw: string(body)}
-	if err := resp.UnmarshalAsJSON(&errType); err != nil {
-		return azcore.NewResponseError(fmt.Errorf("%s\n%s", string(body), err), resp.Response)
+	if err := runtime.UnmarshalAsJSON(resp, &errType); err != nil {
+		return runtime.NewResponseError(fmt.Errorf("%s\n%s", string(body), err), resp)
 	}
-	return azcore.NewResponseError(&errType, resp.Response)
+	return runtime.NewResponseError(&errType, resp)
 }

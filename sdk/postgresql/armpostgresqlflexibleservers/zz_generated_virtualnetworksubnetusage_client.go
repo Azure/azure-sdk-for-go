3c69cc52d139afebcc3d8c7b845440c39fb461b2
JiahuiPeng <46921893+804873052@users.noreply.github.com>
2021-09-07 11:32:23 +0800

    Release armpostgresqlflexibleservers v0.2.0  (#15462)
    * [Release] sdk/postgresql/armpostgresqlflexibleservers/0.2.0 generation from spec commit: 851fac2e61b195c3980992de0b837c3dfb8075a1
    
    * fix version date

--- sdk/postgresql/armpostgresqlflexibleservers/zz_generated_virtualnetworksubnetusage_client.go
+++ sdk/postgresql/armpostgresqlflexibleservers/zz_generated_virtualnetworksubnetusage_client.go
@@ -1,4 +1,5 @@
-// +build go1.13
+//go:build go1.16
+// +build go1.16
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
@@ -11,23 +12,26 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/Azure/azure-sdk-for-go/sdk/armcore"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"net/http"
 	"net/url"
 	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 )
 
 // VirtualNetworkSubnetUsageClient contains the methods for the VirtualNetworkSubnetUsage group.
 // Don't use this type directly, use NewVirtualNetworkSubnetUsageClient() instead.
 type VirtualNetworkSubnetUsageClient struct {
-	con            *armcore.Connection
+	ep             string
+	pl             runtime.Pipeline
 	subscriptionID string
 }
 
 // NewVirtualNetworkSubnetUsageClient creates a new instance of VirtualNetworkSubnetUsageClient with the specified values.
-func NewVirtualNetworkSubnetUsageClient(con *armcore.Connection, subscriptionID string) *VirtualNetworkSubnetUsageClient {
-	return &VirtualNetworkSubnetUsageClient{con: con, subscriptionID: subscriptionID}
+func NewVirtualNetworkSubnetUsageClient(con *arm.Connection, subscriptionID string) *VirtualNetworkSubnetUsageClient {
+	return &VirtualNetworkSubnetUsageClient{ep: con.Endpoint(), pl: con.NewPipeline(module, version), subscriptionID: subscriptionID}
 }
 
 // Execute - Get virtual network subnet usage for a given vNet resource id.
@@ -37,18 +41,18 @@ func (client *VirtualNetworkSubnetUsageClient) Execute(ctx context.Context, loca
 	if err != nil {
 		return VirtualNetworkSubnetUsageExecuteResponse{}, err
 	}
-	resp, err := client.con.Pipeline().Do(req)
+	resp, err := client.pl.Do(req)
 	if err != nil {
 		return VirtualNetworkSubnetUsageExecuteResponse{}, err
 	}
-	if !resp.HasStatusCode(http.StatusOK) {
+	if !runtime.HasStatusCode(resp, http.StatusOK) {
 		return VirtualNetworkSubnetUsageExecuteResponse{}, client.executeHandleError(resp)
 	}
 	return client.executeHandleResponse(resp)
 }
 
 // executeCreateRequest creates the Execute request.
-func (client *VirtualNetworkSubnetUsageClient) executeCreateRequest(ctx context.Context, locationName string, parameters VirtualNetworkSubnetUsageParameter, options *VirtualNetworkSubnetUsageExecuteOptions) (*azcore.Request, error) {
+func (client *VirtualNetworkSubnetUsageClient) executeCreateRequest(ctx context.Context, locationName string, parameters VirtualNetworkSubnetUsageParameter, options *VirtualNetworkSubnetUsageExecuteOptions) (*policy.Request, error) {
 	urlPath := "/subscriptions/{subscriptionId}/providers/Microsoft.DBforPostgreSQL/locations/{locationName}/checkVirtualNetworkSubnetUsage"
 	if client.subscriptionID == "" {
 		return nil, errors.New("parameter client.subscriptionID cannot be empty")
@@ -58,36 +62,35 @@ func (client *VirtualNetworkSubnetUsageClient) executeCreateRequest(ctx context.
 		return nil, errors.New("parameter locationName cannot be empty")
 	}
 	urlPath = strings.ReplaceAll(urlPath, "{locationName}", url.PathEscape(locationName))
-	req, err := azcore.NewRequest(ctx, http.MethodPost, azcore.JoinPaths(client.con.Endpoint(), urlPath))
+	req, err := runtime.NewRequest(ctx, http.MethodPost, runtime.JoinPaths(client.ep, urlPath))
 	if err != nil {
 		return nil, err
 	}
-	req.Telemetry(telemetryInfo)
-	reqQP := req.URL.Query()
+	reqQP := req.Raw().URL.Query()
 	reqQP.Set("api-version", "2021-06-01")
-	req.URL.RawQuery = reqQP.Encode()
-	req.Header.Set("Accept", "application/json")
-	return req, req.MarshalAsJSON(parameters)
+	req.Raw().URL.RawQuery = reqQP.Encode()
+	req.Raw().Header.Set("Accept", "application/json")
+	return req, runtime.MarshalAsJSON(req, parameters)
 }
 
 // executeHandleResponse handles the Execute response.
-func (client *VirtualNetworkSubnetUsageClient) executeHandleResponse(resp *azcore.Response) (VirtualNetworkSubnetUsageExecuteResponse, error) {
-	result := VirtualNetworkSubnetUsageExecuteResponse{RawResponse: resp.Response}
-	if err := resp.UnmarshalAsJSON(&result.VirtualNetworkSubnetUsageResult); err != nil {
+func (client *VirtualNetworkSubnetUsageClient) executeHandleResponse(resp *http.Response) (VirtualNetworkSubnetUsageExecuteResponse, error) {
+	result := VirtualNetworkSubnetUsageExecuteResponse{RawResponse: resp}
+	if err := runtime.UnmarshalAsJSON(resp, &result.VirtualNetworkSubnetUsageResult); err != nil {
 		return VirtualNetworkSubnetUsageExecuteResponse{}, err
 	}
 	return result, nil
 }
 
 // executeHandleError handles the Execute error response.
-func (client *VirtualNetworkSubnetUsageClient) executeHandleError(resp *azcore.Response) error {
-	body, err := resp.Payload()
+func (client *VirtualNetworkSubnetUsageClient) executeHandleError(resp *http.Response) error {
+	body, err := runtime.Payload(resp)
 	if err != nil {
-		return azcore.NewResponseError(err, resp.Response)
+		return runtime.NewResponseError(err, resp)
 	}
 	errType := CloudError{raw: string(body)}
-	if err := resp.UnmarshalAsJSON(&errType); err != nil {
-		return azcore.NewResponseError(fmt.Errorf("%s\n%s", string(body), err), resp.Response)
+	if err := runtime.UnmarshalAsJSON(resp, &errType); err != nil {
+		return runtime.NewResponseError(fmt.Errorf("%s\n%s", string(body), err), resp)
 	}
-	return azcore.NewResponseError(&errType, resp.Response)
+	return runtime.NewResponseError(&errType, resp)
 }

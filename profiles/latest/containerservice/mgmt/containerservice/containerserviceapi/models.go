d6522de275ccf846bc4062deadf9d6811c0bf625
804873052 <804873052@qq.com>
2021-09-09 13:29:44 +0800

    Generated from specification/containerservice/resource-manager/readme.md tag package-2021-08 (commit hash: 296412a72eeb85c73377e9c626953bf7513a59d6)

--- profiles/latest/containerservice/mgmt/containerservice/containerserviceapi/models.go
+++ profiles/latest/containerservice/mgmt/containerservice/containerserviceapi/models.go
@@ -9,7 +9,7 @@
 
 package containerserviceapi
 
-import original "github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2021-07-01/containerservice/containerserviceapi"
+import original "github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2021-08-01/containerservice/containerserviceapi"
 
 type AgentPoolsClientAPI = original.AgentPoolsClientAPI
 type MaintenanceConfigurationsClientAPI = original.MaintenanceConfigurationsClientAPI
@@ -18,3 +18,4 @@ type OperationsClientAPI = original.OperationsClientAPI
 type PrivateEndpointConnectionsClientAPI = original.PrivateEndpointConnectionsClientAPI
 type PrivateLinkResourcesClientAPI = original.PrivateLinkResourcesClientAPI
 type ResolvePrivateLinkServiceIDClientAPI = original.ResolvePrivateLinkServiceIDClientAPI
+type SnapshotsClientAPI = original.SnapshotsClientAPI

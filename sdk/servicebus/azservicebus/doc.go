876d86c9ce651a94a39f10a537a5d3cefce59509
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2021-09-16 06:59:05 +0000

    Fixing copyright comments and package doc comment (in doc.go)

--- sdk/servicebus/azservicebus/doc.go
+++ sdk/servicebus/azservicebus/doc.go
@@ -5,8 +5,6 @@
 // Use of this source code is governed by an MIT
 // license that can be found in the LICENSE file.
 
-/*
-Package azservicebus provides clients for sending and receiving messages with Azure ServiceBus
-as well as modifying resources like Queues, Topics and Subscriptions.
-*/
+// Package azservicebus provides clients for sending and receiving messages with Azure ServiceBus
+// as well as modifying resources like Queues, Topics and Subscriptions.
 package azservicebus

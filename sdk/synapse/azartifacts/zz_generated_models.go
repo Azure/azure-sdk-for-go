6c8d09939896d1ed430edd954b945f9a3abf79d2
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2021-09-16 10:10:58 -0400

    Patching Changelogs (#15524)
    * removing v in changelogs for better play with engsys
    
    * fixing date on synapse
    
    * removing line per wes's rec
    
    * temporary comment for docgo situation
    
    * build constraints
    
    * fmt
    
    * disabling doc.go step on arm packages
    
    * using if statement in powershell

--- sdk/synapse/azartifacts/zz_generated_models.go
+++ sdk/synapse/azartifacts/zz_generated_models.go
@@ -1,4 +1,5 @@
-// +build go1.13
+//go:build go1.16
+// +build go1.16
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
@@ -9,9 +10,10 @@ package azartifacts
 
 import (
 	"encoding/json"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"reflect"
 	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 )
 
 // ActivityClassification provides polymorphic access to related types.
--- sdk/synapse/azartifacts/zz_generated_pollers.go
+++ sdk/synapse/azartifacts/zz_generated_pollers.go
@@ -1,4 +1,5 @@
-// +build go1.13
+//go:build go1.16
+// +build go1.16
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
@@ -9,9 +10,10 @@ package azartifacts
 
 import (
 	"context"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"net/http"
 	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 )
 
 // CreateDataFlowDebugSessionResponsePoller provides polling facilities until the operation reaches a terminal state.

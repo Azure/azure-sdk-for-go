39eeb88be23379611ba5b5814201a577f196d473
JiahuiPeng <46921893+804873052@users.noreply.github.com>
2021-10-12 13:42:10 +0800

    Module path change for mgmt. plane SDK (#15668)
    * To better align with the Azure SDK guidelines (https://azure.github.io/azure-sdk/general_introduction.html), we have decided to change the module path to "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/<rp>/<pacakge>". Therefore, we are deprecating the old module path (which is "github.com/Azure/azure-sdk-for-go/sdk/<rp>/<pacakge>") to avoid confusion.
    
    Co-authored-by: tadelesh <tadelesh.shi@live.cn>

---
+++ sdk/resourcemanager/compute/armcompute/example_disks_test.go
@@ -0,0 +1,65 @@
+//go:build go1.16
+// +build go1.16
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package armcompute_test
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute"
+)
+
+func ExampleDisksClient_BeginCreateOrUpdate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armcompute.NewDisksClient(arm.NewDefaultConnection(cred, nil), "<subscription ID>")
+	poller, err := client.BeginCreateOrUpdate(
+		context.Background(),
+		"<resource group name>",
+		"<disk name>",
+		armcompute.Disk{
+			Resource: armcompute.Resource{
+				Name:     to.StringPtr("<disk name>"),
+				Location: to.StringPtr("<Azure location>"),
+			},
+			Properties: &armcompute.DiskProperties{
+				CreationData: &armcompute.CreationData{
+					CreateOption: armcompute.DiskCreateOptionEmpty.ToPtr(),
+				},
+				DiskSizeGB: to.Int32Ptr(64),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("failed to obtain a response: %v", err)
+	}
+	resp, err := poller.PollUntilDone(context.Background(), 30*time.Second)
+	if err != nil {
+		log.Fatalf("failed to obtain a response: %v", err)
+	}
+	log.Printf("disk ID: %v", *resp.Disk.ID)
+}
+
+func ExampleDisksClient_Get() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armcompute.NewDisksClient(arm.NewDefaultConnection(cred, nil), "<subscription ID>")
+	resp, err := client.Get(context.Background(), "<resource group name>", "<disk name>", nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a response: %v", err)
+	}
+	log.Printf("disk ID: %s", *resp.Disk.ID)
+}

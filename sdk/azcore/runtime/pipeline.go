7b667e381fdf4a829c26bf04100c445d02aa9653
Charles Lowell <chlowe@microsoft.com>
2021-10-14 18:30:44 -0700

    delete runtime.NewPipeline

--- sdk/azcore/runtime/pipeline.go
+++ sdk/azcore/runtime/pipeline.go
@@ -11,17 +11,6 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 )
 
-// NewPipeline creates a new Pipeline object from the specified Transport and Policies.
-// If no transport is provided then the default *http.Client transport will be used.
-func NewPipeline(transport pipeline.Transporter, policies ...pipeline.Policy) pipeline.Pipeline {
-	if transport == nil {
-		transport = defaultHTTPClient
-	}
-	// transport policy must always be the last in the slice
-	policies = append(policies, pipeline.PolicyFunc(httpHeaderPolicy), pipeline.PolicyFunc(bodyDownloadPolicy))
-	return pipeline.NewPipeline(transport, policies...)
-}
-
 // NewDefaultPipeline creates a pipeline from connection options, with any additional policies as specified.
 // module, version: used by the telemetry policy, when enabled
 // perCallPolicies: additional policies to invoke once per request
@@ -40,5 +29,10 @@ func NewDefaultPipeline(module, version string, perCallPolicies []policy.Policy,
 	policies = append(policies, options.PerRetryPolicies...)
 	policies = append(policies, perRetryPolicies...)
 	policies = append(policies, NewLogPolicy(&options.Logging))
-	return NewPipeline(options.Transport, policies...)
+	policies = append(policies, pipeline.PolicyFunc(httpHeaderPolicy), pipeline.PolicyFunc(bodyDownloadPolicy))
+	transport := options.Transport
+	if transport == nil {
+		transport = defaultHTTPClient
+	}
+	return pipeline.NewPipeline(transport, policies...)
 }

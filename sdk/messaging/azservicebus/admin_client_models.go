8e130be120164e71505a3949ded80980445b4fb0
richardpark-msft <ripark@microsoft.com>
2021-10-18 13:08:59 -0700

    Documenting some of the values in the admin client models

--- sdk/messaging/azservicebus/admin_client_models.go
+++ sdk/messaging/azservicebus/admin_client_models.go
@@ -102,33 +102,51 @@ type QueueRuntimeProperties struct {
 	TransferMessageCount *int32
 }
 
+// EntityStatus represents the current status of the entity.
 type EntityStatus string
 
 const (
-	EntityStatusActive          EntityStatus = "Active"
-	EntityStatusDisabled        EntityStatus = "Disabled"
-	EntityStatusSendDisabled    EntityStatus = "SendDisabled"
+	// EntityStatusActive indicates an entity can be used for sending and receiving.
+	EntityStatusActive EntityStatus = "Active"
+	// EntityStatusDisabled indicates an entity cannot be used for sending or receiving.
+	EntityStatusDisabled EntityStatus = "Disabled"
+	// EntityStatusSendDisabled indicates that an entity cannot be used for sending.
+	EntityStatusSendDisabled EntityStatus = "SendDisabled"
+	// EntityStatusReceiveDisabled indicates that an entity cannot be used for receiving.
 	EntityStatusReceiveDisabled EntityStatus = "ReceiveDisabled"
 )
 
+// AccessRights represents the rights for an authorization rule.
 type AccessRights int
 
 const (
+	// AccessRightsManage allows management of entities.
 	AccessRightsManage AccessRights = 0
-	AccessRightsSend   AccessRights = 1
+	// AccessRightsSend allows sending to entities.
+	AccessRightsSend AccessRights = 1
+	// AccessRightsListen allows listening to entities.
 	AccessRightsListen AccessRights = 2
 )
 
+// EntityAvailabilityStatus is the availability status of the entity.
 type EntityAvailabilityStatus string
 
 const (
+	// EntityAvailabilityStatusAvailable indicates the entity is available.
 	EntityAvailabilityStatusAvailable EntityAvailabilityStatus = "Available"
-	EntityAvailabilityStatusLimited   EntityAvailabilityStatus = "Limited"
-	EntityAvailabilityStatusRenaming  EntityAvailabilityStatus = "Renaming"
+
+	EntityAvailabilityStatusLimited EntityAvailabilityStatus = "Limited"
+
+	// EntityAvailabilityStatusRenaming indicates the entity is being renamed.
+	EntityAvailabilityStatusRenaming EntityAvailabilityStatus = "Renaming"
+
+	// EntityAvailabilityStatusRestoring indicates the entity is being restored.
 	EntityAvailabilityStatusRestoring EntityAvailabilityStatus = "Restoring"
-	EntityAvailabilityStatusUnknown   EntityAvailabilityStatus = "Unknown"
+
+	EntityAvailabilityStatusUnknown EntityAvailabilityStatus = "Unknown"
 )
 
+// AuthorizationRule encompasses access rights, metadata and a key for authentication.
 type AuthorizationRule struct {
 	ClaimType    string
 	Rights       []AccessRights

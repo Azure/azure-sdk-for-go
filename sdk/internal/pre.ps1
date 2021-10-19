6a594cffd62531e599083fdbd12569ff3de0579d
seankane-msft <seankane@microsoft.com>
2021-10-19 09:41:45 -0400

    undoing unneccessary changes

--- sdk/internal/pre.ps1
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-# Current ask from service team to wait after creating resource
-
-# Start the test server in the background for sanitizers
-
-# go run ./recording/mockserver/.

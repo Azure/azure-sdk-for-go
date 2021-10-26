c3cff98e25d114cf25820cf9011e7f5f0faf6eab
azure-sdk <azuresdk@microsoft.com>
2021-10-26 08:34:35 +0000

    Increment package version after release of resourcemanager/alertsmanagement/armalertsmanagement

--- sdk/resourcemanager/alertsmanagement/armalertsmanagement/zz_generated_constants.go
+++ sdk/resourcemanager/alertsmanagement/armalertsmanagement/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armalertsmanagement
 
 const (
 	module  = "armalertsmanagement"
-	version = "v0.1.0"
+	version = "v0.1.1"
 )
 
 // ActionRuleStatus - Indicates if the given action rule is enabled or disabled

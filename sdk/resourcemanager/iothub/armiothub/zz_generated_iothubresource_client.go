3ba659b9ea1a0664032bff81414dce5151defb89
804873052 <804873052@qq.com>
2021-10-28 10:59:26 +0800

    [Release] sdk/resourcemanager/iothub/armiothub/0.2.0 generation from spec commit: c24ee32fb97dfefbeb34df5f981af8470bdf6593

--- sdk/resourcemanager/iothub/armiothub/zz_generated_iothubresource_client.go
+++ sdk/resourcemanager/iothub/armiothub/zz_generated_iothubresource_client.go
@@ -12,14 +12,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net/http"
-	"net/url"
-	"strings"
-
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	armruntime "github.com/Azure/azure-sdk-for-go/sdk/azcore/arm/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 // IotHubResourceClient contains the methods for the IotHubResource group.
@@ -31,8 +31,15 @@ type IotHubResourceClient struct {
 }
 
 // NewIotHubResourceClient creates a new instance of IotHubResourceClient with the specified values.
-func NewIotHubResourceClient(con *arm.Connection, subscriptionID string) *IotHubResourceClient {
-	return &IotHubResourceClient{ep: con.Endpoint(), pl: con.NewPipeline(module, version), subscriptionID: subscriptionID}
+func NewIotHubResourceClient(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) *IotHubResourceClient {
+	cp := arm.ClientOptions{}
+	if options != nil {
+		cp = *options
+	}
+	if len(cp.Host) == 0 {
+		cp.Host = arm.AzurePublicCloud
+	}
+	return &IotHubResourceClient{subscriptionID: subscriptionID, ep: string(cp.Host), pl: armruntime.NewPipeline(module, version, credential, &cp)}
 }
 
 // CheckNameAvailability - Check if an IoT hub name is available.
--- sdk/resourcemanager/iothub/armiothub/zz_generated_pagers.go
+++ sdk/resourcemanager/iothub/armiothub/zz_generated_pagers.go
@@ -10,11 +10,10 @@ package armiothub
 
 import (
 	"context"
-	"net/http"
-	"reflect"
-
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"net/http"
+	"reflect"
 )
 
 // IotHubResourceGetEndpointHealthPager provides operations for iterating over paged responses.

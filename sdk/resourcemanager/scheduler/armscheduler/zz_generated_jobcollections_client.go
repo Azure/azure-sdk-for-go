a8663de8a5a52027154c3abe33a1909bafdfe536
804873052 <804873052@qq.com>
2021-10-28 10:58:42 +0800

    [Release] sdk/resourcemanager/scheduler/armscheduler/0.2.0 generation from spec commit: c24ee32fb97dfefbeb34df5f981af8470bdf6593

--- sdk/resourcemanager/scheduler/armscheduler/zz_generated_jobcollections_client.go
+++ sdk/resourcemanager/scheduler/armscheduler/zz_generated_jobcollections_client.go
@@ -11,14 +11,14 @@ package armscheduler
 import (
 	"context"
 	"errors"
-	"net/http"
-	"net/url"
-	"strings"
-
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	armruntime "github.com/Azure/azure-sdk-for-go/sdk/azcore/arm/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 // JobCollectionsClient contains the methods for the JobCollections group.
@@ -30,8 +30,15 @@ type JobCollectionsClient struct {
 }
 
 // NewJobCollectionsClient creates a new instance of JobCollectionsClient with the specified values.
-func NewJobCollectionsClient(con *arm.Connection, subscriptionID string) *JobCollectionsClient {
-	return &JobCollectionsClient{ep: con.Endpoint(), pl: con.NewPipeline(module, version), subscriptionID: subscriptionID}
+func NewJobCollectionsClient(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) *JobCollectionsClient {
+	cp := arm.ClientOptions{}
+	if options != nil {
+		cp = *options
+	}
+	if len(cp.Host) == 0 {
+		cp.Host = arm.AzurePublicCloud
+	}
+	return &JobCollectionsClient{subscriptionID: subscriptionID, ep: string(cp.Host), pl: armruntime.NewPipeline(module, version, credential, &cp)}
 }
 
 // CreateOrUpdate - Provisions a new job collection or updates an existing job collection.

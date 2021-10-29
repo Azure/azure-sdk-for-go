373abbac4aff2cf04dc593b6a5d0a0e424a314b7
804873052 <804873052@qq.com>
2021-10-29 14:59:19 +0800

    [Release] sdk/resourcemanager/notificationhubs/armnotificationhubs/0.2.0 generation from spec commit: 69eacf00a36d565d3220d5dd6f4a5293664f1ae9

--- sdk/resourcemanager/notificationhubs/armnotificationhubs/zz_generated_pollers.go
+++ sdk/resourcemanager/notificationhubs/armnotificationhubs/zz_generated_pollers.go
@@ -10,9 +10,8 @@ package armnotificationhubs
 
 import (
 	"context"
-	"net/http"
-
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"net/http"
 )
 
 // NamespacesDeletePoller provides polling facilities until the operation reaches a terminal state.

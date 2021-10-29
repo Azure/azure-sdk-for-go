54704bf4fc9208f5ead3fe150a729b453d75eb17
seankane-msft <seankane@microsoft.com>
2021-10-29 11:23:51 -0400

    code clean up

--- sdk/keyvault/azkeys/internal/auth/challenge_policy.go
+++ sdk/keyvault/azkeys/internal/auth/challenge_policy.go
@@ -7,6 +7,7 @@
 package auth
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/http"
@@ -27,7 +28,7 @@ type KeyVaultChallengePolicy struct {
 func (k *KeyVaultChallengePolicy) Do(req *policy.Request) (*http.Response, error) {
 	if k.scope == nil || k.tenantID == nil {
 		// First request, get both to get the token
-		challengeReq, err := getChallengeRequest(req)
+		challengeReq, err := k.getChallengeRequest(req)
 		if err != nil {
 			return nil, err
 		}
@@ -37,46 +38,33 @@ func (k *KeyVaultChallengePolicy) Do(req *policy.Request) (*http.Response, error
 			return nil, err
 		}
 
-		resource, scope, err := parseAuthChallenge(challengeResp)
+		err = k.findScopeAndTenant(challengeResp)
 		if err != nil {
 			return nil, err
 		}
-		k.tenantID = parseTenant(resource)
-
-		if strings.HasSuffix(scope, "/") {
-			scope += ".default"
-		} else {
-			scope += "/.default"
-		}
-		k.scope = &scope
 	}
 
-	token, err := k.Cred.GetToken(req.Raw().Context(), policy.TokenRequestOptions{
-		Scopes:   []string{*k.scope},
-		TenantID: *k.tenantID,
-	})
+	err := k.getToken(req.Raw().Context())
 	if err != nil {
 		return nil, err
 	}
-	k.cachedToken = token
 	k.decorateRequest(req)
 
+	// try the request
 	resp, err := req.Next()
 	if err != nil {
 		return nil, err
 	}
 
+	// If it fails and has a 401, try it with a new token
 	if resp.StatusCode == 401 {
 		// Check for a new auth policy
-		newTenant, newScope, err := parseAuthChallenge(resp)
+		err := k.findScopeAndTenant(resp)
 
 		// Error parsing challenge, doomed to fail. Return
 		if err != nil {
 			return nil, err
 		}
-
-		k.tenantID = &newTenant
-		k.scope = &newScope
 
 		token, err := k.Cred.GetToken(
 			req.Raw().Context(),
@@ -102,9 +90,23 @@ func (k *KeyVaultChallengePolicy) Do(req *policy.Request) (*http.Response, error
 	return resp, err
 }
 
+func (k *KeyVaultChallengePolicy) getToken(ctx context.Context) error {
+	token, err := k.Cred.GetToken(
+		ctx,
+		policy.TokenRequestOptions{
+			Scopes:   []string{*k.scope},
+			TenantID: *k.tenantID,
+		})
+	if err != nil {
+		return err
+	}
+	k.cachedToken = token
+	return nil
+}
+
 // parses Tenant ID from auth challenge
 // https://login.microsoftonline.com/00000000-0000-0000-0000-000000000000
-func parseTenant(url string) *string {
+func (k KeyVaultChallengePolicy) parseTenant(url string) *string {
 	parts := strings.Split(url, "/")
 	tenant := parts[len(parts)-1]
 	return &tenant
@@ -114,7 +116,53 @@ func (k KeyVaultChallengePolicy) decorateRequest(req *policy.Request) {
 	req.Raw().Header.Set("Authorization", fmt.Sprintf("Bearer %s", k.cachedToken.Token))
 }
 
-func getChallengeRequest(orig *policy.Request) (*http.Request, error) {
+// sets the k.scope and k.tenantID from the WWW-Authenticate header
+func (k *KeyVaultChallengePolicy) findScopeAndTenant(resp *http.Response) error {
+	authHeader := resp.Header.Get("WWW-Authenticate")
+	if authHeader == "" {
+		k.scope = nil
+		k.tenantID = nil
+		return errors.New("response has no WWW-Authenticate header for challenge authentication")
+	}
+
+	// Strip down to auth and resource
+	// Format is "Bearer authorization=\"<site>\" resource=\"<site>\""
+	authHeader = strings.ReplaceAll(authHeader, "Bearer ", "")
+
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 {
+		k.scope = nil
+		k.tenantID = nil
+		return fmt.Errorf("could not understand WWW-Authenticate header: %s", authHeader)
+	}
+
+	vals := make([]string, 0)
+	for _, part := range parts {
+		subParts := strings.Split(part, "=")
+		if len(subParts) != 2 {
+			k.scope = nil
+			k.tenantID = nil
+			return fmt.Errorf("could not understand WWW-Authenticate header: %s", authHeader)
+		}
+		url := subParts[1]
+
+		url = strings.ReplaceAll(url, "\"", "")
+		vals = append(vals, url)
+	}
+
+	if strings.HasSuffix(vals[1], "/") {
+		vals[1] += ".default"
+	} else {
+		vals[1] += "/.default"
+	}
+
+	k.tenantID = k.parseTenant(vals[0])
+	k.scope = &vals[1]
+
+	return nil
+}
+
+func (k KeyVaultChallengePolicy) getChallengeRequest(orig *policy.Request) (*http.Request, error) {
 	req, err := http.NewRequest(orig.Raw().Method, orig.Raw().URL.String(), nil)
 	if err != nil {
 		return nil, err
@@ -124,34 +172,4 @@ func getChallengeRequest(orig *policy.Request) (*http.Request, error) {
 	req.Header.Set("Content-Length", "0")
 
 	return req, err
-}
-
-func parseAuthChallenge(resp *http.Response) (string, string, error) {
-	authHeader := resp.Header.Get("WWW-Authenticate")
-	if authHeader == "" {
-		return "", "", errors.New("response has no WWW-Authenticate header for challenge authentication")
-	}
-
-	// Strip down to auth and resource
-	// Format is "Bearer authorization=\"<site>\" resource=\"<site>\""
-	authHeader = strings.ReplaceAll(authHeader, "Bearer ", "")
-
-	parts := strings.Split(authHeader, " ")
-	if len(parts) != 2 {
-		return "", "", fmt.Errorf("could not understand WWW-Authenticate header: %s", authHeader)
-	}
-
-	vals := make([]string, 0)
-	for _, part := range parts {
-		subParts := strings.Split(part, "=")
-		if len(subParts) != 2 {
-			return "", "", fmt.Errorf("could not understand WWW-Authenticate header: %s", authHeader)
-		}
-		url := subParts[1]
-
-		url = strings.ReplaceAll(url, "\"", "")
-		vals = append(vals, url)
-	}
-
-	return vals[0], vals[1], nil
 }

154e7c094c2f6aeba52d7e911499ac34dbc52189
804873052 <804873052@qq.com>
2021-10-29 15:08:06 +0800

    [Release] sdk/resourcemanager/iothub/armiothub/0.2.0 generation from spec commit: 69eacf00a36d565d3220d5dd6f4a5293664f1ae9

--- sdk/resourcemanager/iothub/armiothub/zz_generated_iothub_client.go
+++ sdk/resourcemanager/iothub/armiothub/zz_generated_iothub_client.go
@@ -12,14 +12,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net/http"
-	"net/url"
-	"strings"
-
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	armruntime "github.com/Azure/azure-sdk-for-go/sdk/azcore/arm/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 // IotHubClient contains the methods for the IotHub group.
@@ -31,8 +31,15 @@ type IotHubClient struct {
 }
 
 // NewIotHubClient creates a new instance of IotHubClient with the specified values.
-func NewIotHubClient(con *arm.Connection, subscriptionID string) *IotHubClient {
-	return &IotHubClient{ep: con.Endpoint(), pl: con.NewPipeline(module, version), subscriptionID: subscriptionID}
+func NewIotHubClient(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) *IotHubClient {
+	cp := arm.ClientOptions{}
+	if options != nil {
+		cp = *options
+	}
+	if len(cp.Host) == 0 {
+		cp.Host = arm.AzurePublicCloud
+	}
+	return &IotHubClient{subscriptionID: subscriptionID, ep: string(cp.Host), pl: armruntime.NewPipeline(module, version, credential, &cp)}
 }
 
 // BeginManualFailover - Manually initiate a failover for the IoT Hub to its secondary region. To learn more, see https://aka.ms/manualfailover

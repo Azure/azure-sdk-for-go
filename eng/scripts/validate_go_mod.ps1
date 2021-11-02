f0361ea0dd433313b8a41bdb0880ba56a545a83b
seankane-msft <seankane@microsoft.com>
2021-11-02 10:31:32 -0400

    Adding step to ensure no replace directives, and adding fake replace in internal

---
+++ eng/scripts/validate_go_mod.ps1
@@ -0,0 +1,22 @@
+Param(
+    [string] $serviceDir
+)
+
+Push-Location sdk/$serviceDir
+
+$goModFiles = Get-ChildItem -Path . -Filter go.mod -Recurse
+
+if ($goModFiles.Length -eq 0) {
+    Write-Host "Could not find a go.mod file in the directory"
+    exit 1
+}
+
+foreach ($goMod in $goModFiles) {
+    $patternMatches = Get-Content $goMod.FullName | Select-String -Pattern "replace "
+    if ($patternMatches.Length -ne 0) {
+        Write-Host "Found a replace directive in go.mod file at $goMod.FullName"
+        exit 1
+    }
+}
+
+Pop-Location
--- sdk/azcore/go.mod
+++ sdk/azcore/go.mod
@@ -1,5 +1,7 @@
 module github.com/Azure/azure-sdk-for-go/sdk/azcore
 
+replace github.com/Azure/azure-sdk-for-go/sdk/internal => ../internal
+
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/internal v0.8.1
 	github.com/stretchr/testify v1.7.0

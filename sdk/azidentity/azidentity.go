0cbbbcc3b1174ba376f43cf6f3d3324f46449c2a
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2021-11-09 09:57:06 -0800

    add pipelineAdapter

--- sdk/azidentity/azidentity.go
+++ sdk/azidentity/azidentity.go
@@ -4,7 +4,11 @@
 package azidentity
 
 import (
+	"bytes"
+	"context"
 	"errors"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
@@ -12,6 +16,10 @@ import (
 	"regexp"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/streaming"
+	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/confidential"
+	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/public"
 )
 
 const azureAuthorityHost = "AZURE_AUTHORITY_HOST"
@@ -91,4 +99,57 @@ func oauthPath(tenantID string) string {
 		return "/oauth2"
 	}
 	return "/oauth2/v2.0"
+}
+
+type pipelineAdapter struct {
+	pl runtime.Pipeline
+}
+
+func (p pipelineAdapter) CloseIdleConnections() {
+	// do nothing
+}
+
+func (p pipelineAdapter) Do(r *http.Request) (*http.Response, error) {
+	req, err := runtime.NewRequest(r.Context(), r.Method, r.URL.String())
+	if err != nil {
+		return nil, err
+	}
+	if r.Body != nil && r.Body != http.NoBody {
+		// create a rewindable body from the existing body as required
+		var body io.ReadSeekCloser
+		if rsc, ok := r.Body.(io.ReadSeekCloser); ok {
+			body = rsc
+		} else {
+			b, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				return nil, err
+			}
+			body = streaming.NopCloser(bytes.NewReader(b))
+		}
+		err = req.SetBody(body, r.Header.Get("Content-Type"))
+		if err != nil {
+			return nil, err
+		}
+	}
+	resp, err := p.pl.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// enables fakes for test scenarios
+type confidentialClient interface {
+	AcquireTokenSilent(ctx context.Context, scopes []string, options ...confidential.AcquireTokenSilentOption) (confidential.AuthResult, error)
+	AcquireTokenByAuthCode(ctx context.Context, code string, redirectURI string, scopes []string, options ...confidential.AcquireTokenByAuthCodeOption) (confidential.AuthResult, error)
+	AcquireTokenByCredential(ctx context.Context, scopes []string) (confidential.AuthResult, error)
+}
+
+// enables fakes for test scenarios
+type publicClient interface {
+	AcquireTokenSilent(ctx context.Context, scopes []string, options ...public.AcquireTokenSilentOption) (public.AuthResult, error)
+	AcquireTokenByUsernamePassword(ctx context.Context, scopes []string, username string, password string) (public.AuthResult, error)
+	AcquireTokenByDeviceCode(ctx context.Context, scopes []string) (public.DeviceCode, error)
+	AcquireTokenByAuthCode(ctx context.Context, code string, redirectURI string, scopes []string, options ...public.AcquireTokenByAuthCodeOption) (public.AuthResult, error)
+	AcquireTokenInteractive(ctx context.Context, scopes []string, options ...public.InteractiveAuthOption) (public.AuthResult, error)
 }

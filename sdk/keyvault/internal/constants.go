86daecd483c25c3053f89f06a764d9ba940e2df8
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2021-11-09 13:28:23 -0500

    Add Pipelines for keyvault/internal and prepare for release (#16113)
    * Release kv/internal
    
    * adding eng/config entry
    
    * adding ci.yml file
    
    * trying to expand coverage
    
    * renaming to ci.keyvault.yml
    
    * configure Name
    
    * fixing issues
    
    * undoing change to internal/coverage
    
    * add license
    
    * adding changelog date
    
    * add a constant for version
    
    * formatting
    
    * Update sdk/keyvault/internal/constants.go
    
    * fixing tests ignoring errors
    
    Co-authored-by: Wes Haggard <weshaggard@users.noreply.github.com>

---
+++ sdk/keyvault/internal/constants.go
@@ -0,0 +1,11 @@
+//go:build go1.16
+// +build go1.16
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package internal
+
+const (
+	version = "v0.1.0" //nolint
+)

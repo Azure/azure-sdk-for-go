132d146a0de41e4fe9e22dbb830dd44264a93d77
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2021-11-22 15:15:41 -0800

    fix lint warning

--- sdk/azidentity/managed_identity_client.go
+++ sdk/azidentity/managed_identity_client.go
@@ -159,7 +159,7 @@ func (c *managedIdentityClient) authenticate(ctx context.Context, id ManagedIDKi
 
 	if c.msiType == msiTypeIMDS && resp.StatusCode == 400 {
 		if id != nil {
-			return nil, newAuthenticationFailedError(errors.New("The requested identity isn't assigned to this resource."), resp)
+			return nil, newAuthenticationFailedError(errors.New("the requested identity isn't assigned to this resource"), resp)
 		}
 		c.unavailableMessage = "No default identity is assigned to this resource."
 		return nil, newCredentialUnavailableError("Managed Identity Credential", c.unavailableMessage)

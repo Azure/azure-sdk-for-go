cb212fec4c0ba91f7515803893d1de640503d592
azure-sdk <azuresdk@microsoft.com>
2021-12-07 02:59:42 +0000

    Increment package version after release of resourcemanager/confidentialledger/armconfidentialledger

--- sdk/resourcemanager/confidentialledger/armconfidentialledger/zz_generated_constants.go
+++ sdk/resourcemanager/confidentialledger/armconfidentialledger/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armconfidentialledger
 
 const (
 	module  = "armconfidentialledger"
-	version = "v0.1.0"
+	version = "v0.1.1"
 )
 
 // CheckNameAvailabilityReason - The reason why the given name is not available.

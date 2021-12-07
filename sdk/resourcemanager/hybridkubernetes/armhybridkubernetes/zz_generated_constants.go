8d660b6d05fe3edeac82015b901683a3dcf97c2f
azure-sdk <azuresdk@microsoft.com>
2021-12-07 09:32:13 +0000

    Increment package version after release of resourcemanager/hybridkubernetes/armhybridkubernetes

--- sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/zz_generated_constants.go
+++ sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armhybridkubernetes
 
 const (
 	module  = "armhybridkubernetes"
-	version = "v0.1.0"
+	version = "v0.1.1"
 )
 
 // AuthenticationMethod - The mode of client authentication.

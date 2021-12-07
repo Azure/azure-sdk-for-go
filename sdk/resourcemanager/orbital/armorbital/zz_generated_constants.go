d32cf78826cf6b000424a29c57ef81e3beb78cd1
azure-sdk <azuresdk@microsoft.com>
2021-12-07 02:49:14 +0000

    Increment package version after release of resourcemanager/orbital/armorbital

--- sdk/resourcemanager/orbital/armorbital/zz_generated_constants.go
+++ sdk/resourcemanager/orbital/armorbital/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armorbital
 
 const (
 	module  = "armorbital"
-	version = "v0.1.0"
+	version = "v0.1.1"
 )
 
 // ActionType - Enum. Indicates the action type. "Internal" refers to actions that are for internal only APIs.

54c5fc1d166391331eb15be7002653197a9b20f4
FAREAST\chunyu <chunyu@microsoft.com>
2021-12-21 11:09:21 +0800

    update automate init script

--- eng/scripts/automation_init.sh
+++ eng/scripts/automation_init.sh
@@ -1,25 +1,7 @@
-set -x
-set -e
-if [ ! "$(go version | awk '{print $3}' | cut -c 3-6)" = "1.16" ]
-then
-  wget https://golang.org/dl/go1.16.9.linux-amd64.tar.gz
-  tar -C $TMPDIR -xzf go1.16.9.linux-amd64.tar.gz
-  export PATH=$TMPDIR/go/bin:$PATH
-fi
-export GOPATH="$(realpath ../../../..)"
-if [ ! -d "$GOPATH/bin" ]
-then
-  mkdir $GOPATH/bin
-fi
+#!/bin/bash
+echo $GOPATH
+echo $GOROOT
 PATH=$PATH:$GOPATH/bin
 export GO111MODULE=on
 cd eng/tools/generator && go build && cp generator $GOPATH/bin && cd ../../..
-ln -s /usr/bin/pwsh $GOPATH/bin/pwsh.exe
-cat > $2 << EOF
-{
-  "envs": {
-    "PATH": "$PATH:$GOPATH",
-    "GOPATH": "$GOPATH"
-  }
-}
-EOF
+# ln -s /usr/bin/pwsh $GOPATH/bin/pwsh.exe

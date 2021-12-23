68e39bacbee8d83dd60f4fc3d1b8ad2ec581e1ed
FAREAST\chunyu <chunyu@microsoft.com>
2021-12-23 13:16:36 +0800

    create symbol link

--- eng/scripts/automation_init.sh
+++ eng/scripts/automation_init.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -x
 echo $GOPATH
 echo $GOROOT
 PATH=$PATH:$GOPATH/bin
+pwd
 export GO111MODULE=on
 cd eng/tools/generator && go build && cp generator $GOPATH/bin && cd ../../..
 # ln -s /usr/bin/pwsh $GOPATH/bin/pwsh.exe
+ls $GOPATH/bin/
+ls $GOPATH/bin/generator.exe
+pwd
+ln -s $GOPATH/bin/generator.exe generator
+ls generator

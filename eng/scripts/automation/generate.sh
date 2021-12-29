5962367091f860e6bda52d7fa04a15388025e5b8
FAREAST\chunyu <chunyu@microsoft.com>
2021-12-29 11:11:42 +0800

    go init script

---
+++ eng/scripts/automation/generate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -x
+eng/tools/generator/generator automation-v2 /tmp/input.json
--- eng/scripts/automation_init.sh
+++ eng/scripts/automation_init.sh
@@ -1,14 +1,50 @@
 #!/bin/bash
 set -x
+echo "GOPATH:$GOPATH"
+echo "GOROOT:$GOROOT"
+ls -l $GOROOT/bin
+
+pwd
+which go
+TMPDIR="/tmp"
+if [ ! "$(go version | awk '{print $3}' | cut -c 3-6)" = "1.16" ]
+then
+  wget https://golang.org/dl/go1.16.9.linux-amd64.tar.gz
+  tar -C $TMPDIR -xzf go1.16.9.linux-amd64.tar.gz
+  export GOROOT=$TMPDIR/go
+  export PATH=$GOROOT/bin:$PATH
+fi
+
+# if [ "$GOPATH" == "" ]; then
+#   DIRECTORY=$(cd `dirname $0` && pwd)
+#   echo $DIRECTORY
+#   export GOPATH=$DIRECTORY/../../../gofolder
+# fi
+DIRECTORY=$(cd `dirname $0` && pwd)
+echo $DIRECTORY
+export GOPATH=$DIRECTORY/../../../gofolder
+# export GOPATH=$TMPDIR/gofolder
+if [ ! -d "$GOPATH/bin" ]; then
+  echo "create gopath folder"
+  mkdir -p $GOPATH/bin
+fi
 echo $GOPATH
-echo $GOROOT
-PATH=$PATH:$GOPATH/bin
-pwd
+
 export GO111MODULE=on
-cd eng/tools/generator && go build && cp generator $GOPATH/bin && cd ../../..
+# cd eng/tools/generator && go build && cp generator $GOPATH/bin && cd ../../..
+cd $DIRECTORY/../tools/generator
+go build
+ls -l
+# go install
+cp generator $GOPATH/bin/
+ls -l
+export PATH=$PATH:$GOPATH/bin
 # ln -s /usr/bin/pwsh $GOPATH/bin/pwsh.exe
-ls $GOPATH/bin/
-ls $GOPATH/bin/generator.exe
 pwd
-ln -s $GOPATH/bin/generator.exe generator
-ls generator
+ls -l $GOPATH
+ls $GOPATH/bin/
+ls $GOPATH/bin/generator
+pwd
+generator
+sudo ln -s $GOPATH/bin/generator /usr/bin/generator
+ls -l /usr/bin/generator

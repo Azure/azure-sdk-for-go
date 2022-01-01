37b38c98542679521a9c0df79503ac1fdb8c84a3
Brandon Kurtz <kurtz.brandon@gmail.com>
2021-12-31 21:22:39 -0800

    uncomment o.Progress(progress) in getDownloadBlobOptions()

--- sdk/storage/azblob/highlevel.go
+++ sdk/storage/azblob/highlevel.go
@@ -7,12 +7,13 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/streaming"
-	"github.com/Azure/azure-sdk-for-go/sdk/internal/uuid"
-	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/internal"
 	"io"
 	"net/http"
 	"sync"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/streaming"
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/uuid"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/internal"
 
 	"bytes"
 	"errors"
@@ -281,7 +282,7 @@ func (b BlobClient) downloadBlobToWriterAt(ctx context.Context, offset int64, co
 						rangeProgress = bytesTransferred
 						progressLock.Lock()
 						progress += diff
-						//o.Progress(progress)
+						o.Progress(progress)
 						progressLock.Unlock()
 					})
 			}

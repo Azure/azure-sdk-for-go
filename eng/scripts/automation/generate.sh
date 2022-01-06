3ff1917029dca17a9901fbcd933da51aa7b87204
Chun Yu <chunyu@microsoft.com>
2022-01-06 21:37:09 +0800

    exit script when generate fail

--- eng/scripts/automation/generate.sh
+++ eng/scripts/automation/generate.sh
@@ -4,41 +4,17 @@ if [ -z $1 ]; then
     echo "Please input inputfile"
     exit 1
 fi
-echo $1
-cat $1
 
 if [ -z $2 ]; then
     echo "Please input outputfile"
     exit 1
 fi
 
-echo $2
-
-env
 generator automation-v2 $1 $2
 
-#mock for test
-# cat > $2 << EOF
-# {
-#   "packages": [
-#     {
-#       "packageName": "armagrifood",
-#       "result": "succeeded",
-#       "path": [
-#         "sdk/resourcemanager/agrifood/armagrifood",
-#         "rush.json"
-#       ],
-#       "packageFolder": "sdk/resourcemanager/agrifood/armagrifood",
-#       "changelog": {
-#         "content": "Feature: something \n Breaking Changes: something\n",
-#         "hasBreakingChange": true
-#       },
-#       "artifacts": [
-#         "sdk/agrifood/azure-arm-agrifood-1.0.0.tgz",
-#       ]
-#     }
-#   ]
-# }
-# EOF
+if [ "$?" != "0" ]; then
+  echo "Failed to generate code."
+  exit 1
+fi
 
 cat $2

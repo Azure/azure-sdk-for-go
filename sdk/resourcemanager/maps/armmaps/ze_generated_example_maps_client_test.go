5fcab4a70ba2372fa46dd41cc67d573130bfe31d
804873052 <804873052@qq.com>
2022-01-13 12:49:16 +0800

    [Release] sdk/resourcemanager/maps/armmaps/0.2.0 generation from spec commit: 8e4746332979ed6ff3577bdc97dd2065a7577598

--- sdk/resourcemanager/maps/armmaps/ze_generated_example_maps_client_test.go
+++ sdk/resourcemanager/maps/armmaps/ze_generated_example_maps_client_test.go
@@ -17,33 +17,47 @@ import (
 )
 
 // x-ms-original-file: specification/maps/resource-manager/Microsoft.Maps/preview/2021-12-01-preview/examples/GetOperations.json
-func ExampleMapsClient_ListOperations() {
+func ExampleClient_ListOperations() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client := armmaps.NewMapsClient("<subscription-id>", cred, nil)
+	client := armmaps.NewClient("<subscription-id>", cred, nil)
 	pager := client.ListOperations(nil)
-	for pager.NextPage(ctx) {
+	for {
+		nextResult := pager.NextPage(ctx)
 		if err := pager.Err(); err != nil {
 			log.Fatalf("failed to advance page: %v", err)
+		}
+		if !nextResult {
+			break
+		}
+		for _, v := range pager.PageResponse().Value {
+			log.Printf("Pager result: %#v\n", v)
 		}
 	}
 }
 
 // x-ms-original-file: specification/maps/resource-manager/Microsoft.Maps/preview/2021-12-01-preview/examples/GetOperationsSubscription.json
-func ExampleMapsClient_ListSubscriptionOperations() {
+func ExampleClient_ListSubscriptionOperations() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client := armmaps.NewMapsClient("<subscription-id>", cred, nil)
+	client := armmaps.NewClient("<subscription-id>", cred, nil)
 	pager := client.ListSubscriptionOperations(nil)
-	for pager.NextPage(ctx) {
+	for {
+		nextResult := pager.NextPage(ctx)
 		if err := pager.Err(); err != nil {
 			log.Fatalf("failed to advance page: %v", err)
+		}
+		if !nextResult {
+			break
+		}
+		for _, v := range pager.PageResponse().Value {
+			log.Printf("Pager result: %#v\n", v)
 		}
 	}
 }

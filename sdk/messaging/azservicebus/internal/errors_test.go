86ce75a21061448fe87ee3314ec4fe72f635f09f
Richard Park <ripark@microsoft.com>
2022-01-13 13:08:20 -0800

    Changing everything over to using the ServiceBusError info (soon to be renamed)

--- sdk/messaging/azservicebus/internal/errors_test.go
+++ sdk/messaging/azservicebus/internal/errors_test.go
@@ -91,82 +91,92 @@ func Test_isPermanentNetError(t *testing.T) {
 	require.True(t, isPermanentNetError(&fakeNetError{}))
 }
 
-func Test_isRetryableAMQPError(t *testing.T) {
-	ctx := context.Background()
+func Test_recoveryKind(t *testing.T) {
+	t.Run("link", func(t *testing.T) {
+		linkErrorCodes := []string{
+			string(amqp.ErrorDetachForced),
+		}
 
-	retryableCodes := []string{
-		string(amqp.ErrorInternalError),
-		string(errorServerBusy),
-		string(errorTimeout),
-		string(errorOperationCancelled),
-		"client.sender:not-enough-link-credit",
-		string(amqp.ErrorUnauthorizedAccess),
-		string(amqp.ErrorDetachForced),
-		string(amqp.ErrorConnectionForced),
-		string(amqp.ErrorTransferLimitExceeded),
-		"amqp: connection closed",
-		"unexpected frame",
-		string(amqp.ErrorNotFound),
+		for _, code := range linkErrorCodes {
+			t.Run(code, func(t *testing.T) {
+				sbe := ToSBE(&amqp.Error{Condition: amqp.ErrorCondition(code)})
+				require.EqualValues(t, RecoveryKindLink, sbe.RecoveryKind, fmt.Sprintf("requires link recovery: %s", code))
+			})
+		}
+
+		t.Run("sentintel errors", func(t *testing.T) {
+			sbe := ToSBE(amqp.ErrLinkClosed)
+			require.EqualValues(t, RecoveryKindLink, sbe.RecoveryKind)
+
+			sbe = ToSBE(amqp.ErrSessionClosed)
+			require.EqualValues(t, RecoveryKindLink, sbe.RecoveryKind)
+		})
+	})
+
+	t.Run("connection", func(t *testing.T) {
+		codes := []string{
+			string(amqp.ErrorConnectionForced),
+		}
+
+		for _, code := range codes {
+			t.Run(code, func(t *testing.T) {
+				sbe := ToSBE(&amqp.Error{Condition: amqp.ErrorCondition(code)})
+				require.EqualValues(t, RecoveryKindConn, sbe.RecoveryKind, fmt.Sprintf("requires connection recovery: %s", code))
+			})
+		}
+
+		t.Run("sentinel errors", func(t *testing.T) {
+			sbe := ToSBE(amqp.ErrConnClosed)
+			require.EqualValues(t, RecoveryKindConn, sbe.RecoveryKind)
+		})
+	})
+
+	t.Run("nonretriable", func(t *testing.T) {
+		codes := []string{
+			string(amqp.ErrorTransferLimitExceeded),
+			string(amqp.ErrorInternalError),
+			string(amqp.ErrorUnauthorizedAccess),
+			string(amqp.ErrorNotFound),
+			string(amqp.ErrorMessageSizeExceeded),
+		}
+
+		for _, code := range codes {
+			t.Run(code, func(t *testing.T) {
+				sbe := ToSBE(&amqp.Error{Condition: amqp.ErrorCondition(code)})
+				require.EqualValues(t, RecoveryKindFatal, sbe.RecoveryKind, fmt.Sprintf("cannot be recovered: %s", code))
+			})
+		}
+	})
+
+	t.Run("none", func(t *testing.T) {
+		codes := []string{
+			string(errorOperationCancelled),
+			string(errorServerBusy),
+			string(errorTimeout),
+		}
+
+		for _, code := range codes {
+			t.Run(code, func(t *testing.T) {
+				sbe := ToSBE(&amqp.Error{Condition: amqp.ErrorCondition(code)})
+				require.EqualValues(t, RecoveryKindNone, sbe.RecoveryKind, fmt.Sprintf("no recovery needed: %s", code))
+			})
+		}
+	})
+}
+
+func Test_IsNonRetriable(t *testing.T) {
+	errs := []error{
+		context.Canceled,
+		context.DeadlineExceeded,
+		ErrNonRetriable{Message: "any message"},
+		fmt.Errorf("wrapped: %w", context.Canceled),
+		fmt.Errorf("wrapped: %w", context.DeadlineExceeded),
+		fmt.Errorf("wrapped: %w", ErrNonRetriable{Message: "any message"}),
 	}
 
-	for _, code := range retryableCodes {
-		require.True(t, isRetryableAMQPError(ctx, &amqp.Error{
-			Condition: amqp.ErrorCondition(code),
-		}))
-
-		// it works equally well if the error is just in the String().
-		// Need to narrow this down some more to see where the errors
-		// might not be getting converted properly.
-		require.True(t, isRetryableAMQPError(ctx, errors.New(code)))
+	for _, err := range errs {
+		require.EqualValues(t, RecoveryKindFatal, ToSBE(err).RecoveryKind)
 	}
-
-	require.False(t, isRetryableAMQPError(ctx, errors.New("some non-amqp related error")))
-}
-
-func Test_shouldRecreateLink(t *testing.T) {
-	require.False(t, shouldRecreateLink(nil))
-
-	require.True(t, shouldRecreateLink(&amqp.DetachError{}))
-
-	// going to treat these as "connection troubles" and throw them into the
-	// connection recovery scenario instead.
-	require.False(t, shouldRecreateLink(amqp.ErrLinkClosed))
-	require.False(t, shouldRecreateLink(amqp.ErrSessionClosed))
-}
-
-func Test_shouldRecreateConnection(t *testing.T) {
-	ctx := context.Background()
-
-	require.False(t, shouldRecreateConnection(ctx, nil))
-	require.True(t, shouldRecreateConnection(ctx, &fakeNetError{}))
-	require.True(t, shouldRecreateConnection(ctx, fmt.Errorf("%w", &fakeNetError{})))
-
-	require.False(t, shouldRecreateLink(amqp.ErrLinkClosed))
-	require.False(t, shouldRecreateLink(fmt.Errorf("wrapped: %w", amqp.ErrLinkClosed)))
-
-	require.False(t, shouldRecreateLink(amqp.ErrSessionClosed))
-	require.False(t, shouldRecreateLink(fmt.Errorf("wrapped: %w", amqp.ErrSessionClosed)))
-}
-
-// TODO: while testing it appeared there were some errors that were getting string-ized
-// We want to eliminate these. 'stress.go' reproduces most of these as you disconnect
-// and reconnect.
-func Test_stringErrorsToEliminate(t *testing.T) {
-	require.True(t, shouldRecreateLink(errors.New("detach frame link detached")))
-	require.True(t, isRetryableAMQPError(context.Background(), errors.New("amqp: connection closed")))
-	require.True(t, IsCancelError(errors.New("context canceled")))
-}
-
-func Test_IsCancelError(t *testing.T) {
-	require.False(t, IsCancelError(nil))
-	require.False(t, IsCancelError(errors.New("not a cancel error")))
-
-	require.True(t, IsCancelError(errors.New("context canceled")))
-
-	require.True(t, IsCancelError(context.Canceled))
-	require.True(t, IsCancelError(context.DeadlineExceeded))
-	require.True(t, IsCancelError(fmt.Errorf("wrapped: %w", context.Canceled)))
-	require.True(t, IsCancelError(fmt.Errorf("wrapped: %w", context.DeadlineExceeded)))
 }
 
 func Test_ServiceBusError_NoRecoveryNeeded(t *testing.T) {
@@ -181,7 +191,7 @@ func Test_ServiceBusError_NoRecoveryNeeded(t *testing.T) {
 	}
 
 	for i, err := range tempErrors {
-		rk := ToSBE(context.Background(), err).RecoveryKind
+		rk := ToSBE(err).RecoveryKind
 		require.EqualValues(t, RecoveryKindNone, rk, fmt.Sprintf("[%d] %v", i, err))
 	}
 }
@@ -194,7 +204,7 @@ func Test_ServiceBusError_ConnectionRecoveryNeeded(t *testing.T) {
 	}
 
 	for i, err := range connErrors {
-		rk := ToSBE(context.Background(), err).RecoveryKind
+		rk := ToSBE(err).RecoveryKind
 		require.EqualValues(t, RecoveryKindConn, rk, fmt.Sprintf("[%d] %v", i, err))
 	}
 }
@@ -208,7 +218,7 @@ func Test_ServiceBusError_LinkRecoveryNeeded(t *testing.T) {
 	}
 
 	for i, err := range linkErrors {
-		rk := ToSBE(context.Background(), err).RecoveryKind
+		rk := ToSBE(err).RecoveryKind
 		require.EqualValues(t, RecoveryKindLink, rk, fmt.Sprintf("[%d] %v", i, err))
 	}
 }
@@ -226,11 +236,11 @@ func Test_ServiceBusError_Fatal(t *testing.T) {
 	}
 
 	for i, cond := range fatalConditions {
-		rk := ToSBE(context.Background(), &amqp.Error{Condition: cond}).RecoveryKind
+		rk := ToSBE(&amqp.Error{Condition: cond}).RecoveryKind
 		require.EqualValues(t, RecoveryKindFatal, rk, fmt.Sprintf("[%d] %s", i, cond))
 	}
 
 	// unknown errors are also considered fatal
-	rk := ToSBE(context.Background(), errors.New("Some unknown error")).RecoveryKind
+	rk := ToSBE(errors.New("Some unknown error")).RecoveryKind
 	require.EqualValues(t, RecoveryKindFatal, rk, "some unknown error")
 }

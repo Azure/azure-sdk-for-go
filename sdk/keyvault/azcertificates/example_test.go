90d45813d15fd8f975954b26f2fba4cf9226aea8
seankane-msft <seankane@microsoft.com>
2022-01-18 16:01:01 -0500

    adding examples and readme

---
+++ sdk/keyvault/azcertificates/example_test.go
@@ -0,0 +1,151 @@
+//go:build go1.16
+// +build go1.16
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package azcertificates_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azcertificates"
+)
+
+func ExampleNewClient() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	vaultURL, ok := os.LookupEnv("AZURE_KEYVAULT_URL")
+	if !ok {
+		log.Fatalf("Could not find 'AZURE_KEYVAULT_URL' in environment variables")
+	}
+
+	client, err := azcertificates.NewClient(vaultURL, cred, nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(client)
+}
+
+func ExampleClient_BeginCreateCertificate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	vaultURL, ok := os.LookupEnv("AZURE_KEYVAULT_URL")
+	if !ok {
+		log.Fatalf("Could not find 'AZURE_KEYVAULT_URL' in environment variables")
+	}
+
+	client, err := azcertificates.NewClient(vaultURL, cred, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.BeginCreateCertificate(context.TODO(), "certificateName", azcertificates.CertificatePolicy{
+		IssuerParameters: &azcertificates.IssuerParameters{
+			Name: to.StringPtr("Self"),
+		},
+		X509CertificateProperties: &azcertificates.X509CertificateProperties{
+			Subject: to.StringPtr("CN=DefaultPolicy"),
+		},
+	}, nil)
+	finalResponse, err := resp.PollUntilDone(context.TODO(), time.Second)
+
+	fmt.Println("Created a certificate with ID: ", *finalResponse.ID)
+}
+
+func ExampleClient_GetCertificate() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	vaultURL, ok := os.LookupEnv("AZURE_KEYVAULT_URL")
+	if !ok {
+		log.Fatalf("Could not find 'AZURE_KEYVAULT_URL' in environment variables")
+	}
+
+	client, err := azcertificates.NewClient(vaultURL, cred, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.GetCertificate(context.TODO(), "myCertName", nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(*resp.ID)
+	fmt.Println(*resp.Policy.IssuerParameters.Name)
+
+	// optionally you can get a specific version
+	resp, err = client.GetCertificate(context.TODO(), "myCertName", &azcertificates.GetCertificateOptions{Version: "myCertVersion"})
+	if err != nil {
+		panic(err)
+	}
+}
+func ExampleClient_UpdateCertificateProperties() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	vaultURL, ok := os.LookupEnv("AZURE_KEYVAULT_URL")
+	if !ok {
+		log.Fatalf("Could not find 'AZURE_KEYVAULT_URL' in environment variables")
+	}
+
+	client, err := azcertificates.NewClient(vaultURL, cred, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.UpdateCertificateProperties(context.TODO(), "myCertName", &azcertificates.UpdateCertificatePropertiesOptions{
+		Version: "myNewVersion",
+		CertificateAttributes: &azcertificates.CertificateAttributes{
+			Attributes: azcertificates.Attributes{Enabled: to.BoolPtr(false)},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(*resp.ID)
+	fmt.Println(*resp.CertificateBundle.Attributes.Enabled)
+}
+
+func ExampleClient_ListCertificates() {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	vaultURL, ok := os.LookupEnv("AZURE_KEYVAULT_URL")
+	if !ok {
+		log.Fatalf("Could not find 'AZURE_KEYVAULT_URL' in environment variables")
+	}
+
+	client, err := azcertificates.NewClient(vaultURL, cred, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	poller := client.ListCertificates(nil)
+	for poller.NextPage(context.TODO()) {
+		for _, cert := range poller.PageResponse().Certificates {
+			fmt.Println(*cert.ID)
+		}
+	}
+	if poller.Err() != nil {
+		panic(err)
+	}
+}

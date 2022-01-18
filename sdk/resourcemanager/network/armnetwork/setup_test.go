50e40adb0f57b38bfd9a14d815db33b978ee9dc7
804873052 <804873052@qq.com>
2022-01-18 17:09:57 +0800

    Add network/armnetwork test case

---
+++ sdk/resourcemanager/network/armnetwork/setup_test.go
@@ -0,0 +1,39 @@
+package armnetwork_test
+
+import (
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/recording"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+var pathToPackage = "sdk/resourcemanager/network/armnetwork/testdata"
+
+func TestMain(m *testing.M) {
+	// Initialize
+	if err := recording.AddGeneralRegexSanitizer(
+		"00000000-0000-0000-0000-000000000000",
+		`/subscriptions/(?<subsId>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})`,
+		&recording.RecordingOptions{
+			UseHTTPS:        true,
+			GroupForReplace: "subsId",
+		},
+	); err != nil {
+		panic(err)
+	}
+
+	// Run
+	exitVal := m.Run()
+
+	// cleanup
+	os.Exit(exitVal)
+}
+
+func startTest(t *testing.T) func() {
+	err := recording.Start(t, pathToPackage, nil)
+	require.NoError(t, err)
+	return func() {
+		err := recording.Stop(t, nil)
+		require.NoError(t, err)
+	}
+}

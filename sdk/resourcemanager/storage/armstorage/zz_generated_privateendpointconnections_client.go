e99580d52cde3efcf36a55ff6a44dacf2da9b9bf
804873052 <804873052@qq.com>
2022-01-25 09:57:39 +0800

    [Release] sdk/resourcemanager/storage/armstorage/0.4.0 generation from spec commit: d830271b241897bad300f9275ff7b27d502aa6c5

--- sdk/resourcemanager/storage/armstorage/zz_generated_privateendpointconnections_client.go
+++ sdk/resourcemanager/storage/armstorage/zz_generated_privateendpointconnections_client.go
@@ -34,17 +34,17 @@ type PrivateEndpointConnectionsClient struct {
 // credential - used to authorize requests. Usually a credential from azidentity.
 // options - pass nil to accept the default values.
 func NewPrivateEndpointConnectionsClient(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) *PrivateEndpointConnectionsClient {
-	cp := arm.ClientOptions{}
-	if options != nil {
-		cp = *options
+	if options == nil {
+		options = &arm.ClientOptions{}
 	}
-	if len(cp.Endpoint) == 0 {
-		cp.Endpoint = arm.AzurePublicCloud
+	ep := options.Endpoint
+	if len(ep) == 0 {
+		ep = arm.AzurePublicCloud
 	}
 	client := &PrivateEndpointConnectionsClient{
 		subscriptionID: subscriptionID,
-		host:           string(cp.Endpoint),
-		pl:             armruntime.NewPipeline(moduleName, moduleVersion, credential, runtime.PipelineOptions{}, &cp),
+		host:           string(ep),
+		pl:             armruntime.NewPipeline(moduleName, moduleVersion, credential, runtime.PipelineOptions{}, options),
 	}
 	return client
 }

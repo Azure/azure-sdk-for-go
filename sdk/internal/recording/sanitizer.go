8cbb5d6cd9e9d835737a6ad1d445a551dbf381a9
scbedd <45376673+scbedd@users.noreply.github.com>
2022-02-16 17:10:31 -0800

    Ensure ResetProxy also has access to a specific testinstance

--- sdk/internal/recording/sanitizer.go
+++ sdk/internal/recording/sanitizer.go
@@ -417,8 +417,18 @@ func ResetProxy(options *RecordingOptions) error {
 	if options == nil {
 		options = defaultOptions()
 	}
+
 	url := fmt.Sprintf("%v/Admin/Reset", options.baseURL())
 	req, err := http.NewRequest("POST", url, nil)
+
+	if options.TestInstance != nil {
+		recordingID := GetRecordingId(options.TestInstance)
+		if recordingID == "" {
+			return fmt.Errorf("did not find a recording ID for test with name '%s'. Did you make sure to call Start?", options.TestInstance.Name())
+		}
+		req.Header.Set("x-recording-id", recordingID)
+	}
+
 	if err != nil {
 		return err
 	}

891b75ea4dc2f6893fb7ae940058876fcf1854a1
Sean Kane <68240067+seankane-msft@users.noreply.github.com>
2022-02-24 13:52:56 -0500

    Update sdk/keyvault/azkeys/client.go
    Co-authored-by: Kim Ying <15070078+kimprice@users.noreply.github.com>

--- sdk/keyvault/azkeys/client.go
+++ sdk/keyvault/azkeys/client.go
@@ -343,7 +343,7 @@ func createRSAKeyResponseFromGenerated(i generated.KeyVaultClientCreateKeyRespon
 
 // CreateRSAKey can be used to create a new RSA key in Azure Key Vault. If the named key already
 // exists, Azure Key Vault creates a new version of the key. RSA key can be created on standard
-// vaults, RSAHSM can be created on Premium SKU Vaults or Managed HSMs.
+// Premium SKU vaults, RSAHSM can be created in Premium SKU vaults or Managed HSMs.
 // It requires the keys/create permission. Pass nil to use the default options.
 func (c *Client) CreateRSAKey(ctx context.Context, name string, options *CreateRSAKeyOptions) (CreateRSAKeyResponse, error) {
 	keyType := RSA

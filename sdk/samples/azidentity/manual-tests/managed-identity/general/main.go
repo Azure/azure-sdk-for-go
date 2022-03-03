fbe3844918413283180119b103c2301cec707ef4
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2022-03-03 15:12:44 -0800

    Update managed identity manual test driver

--- sdk/samples/azidentity/manual-tests/managed-identity/general/main.go
+++ sdk/samples/azidentity/manual-tests/managed-identity/general/main.go
@@ -5,21 +5,40 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 )
 
+var clientID = flag.String("clientID", "", "optional client ID of a user assigned identity. Mutually exclusive with resID.")
+var printToken = flag.Bool("printToken", false, "print the acquired token")
+var resID = flag.String("resID", "", "optional resource ID of a user assigned identity. Mutually exclusive with clientID.")
+var scope = flag.String("scope", "https://management.core.windows.net//.default", "optional scope for access token")
+
 func main() {
-	cred, err := azidentity.NewManagedIdentityCredential("", nil)
+	flag.Parse()
+	opts := &azidentity.ManagedIdentityCredentialOptions{}
+	if *clientID != "" {
+		if *resID != "" {
+			panic(`"clientID" and "resID" are mutually exclusive`)
+		}
+		opts.ID = azidentity.ClientID(*clientID)
+	} else if *resID != "" {
+		opts.ID = azidentity.ResourceID(*resID)
+	}
+	cred, err := azidentity.NewManagedIdentityCredential(opts)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Calling GetToken()...")
-	_, err = cred.GetToken(context.Background(), azcore.TokenRequestOptions{Scopes: []string{"https://vault.azure.net"}})
+	tk, err := cred.GetToken(context.Background(), policy.TokenRequestOptions{Scopes: []string{*scope}})
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Success! Token received.")
+	if *printToken {
+		fmt.Println(tk.Token)
+	}
 }

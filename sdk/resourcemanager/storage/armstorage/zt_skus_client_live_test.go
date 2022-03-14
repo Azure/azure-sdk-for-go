7d0bfcaa95e9ebe40e9ddece4131f8b31c52c055
Jiahui Peng <804873052@qq.com>
2022-03-14 16:51:13 +0800

    live armstorage

---
+++ sdk/resourcemanager/storage/armstorage/zt_skus_client_live_test.go
@@ -0,0 +1,57 @@
+//go:build go1.16
+// +build go1.16
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package armstorage_test
+
+import (
+	"context"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal/testutil"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storage/armstorage"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type SKUsClientTestSuite struct {
+	suite.Suite
+
+	ctx               context.Context
+	cred              azcore.TokenCredential
+	options           *arm.ClientOptions
+	location          string
+	resourceGroupName string
+	subscriptionID    string
+}
+
+func (testsuite *SKUsClientTestSuite) SetupSuite() {
+	testsuite.ctx = context.Background()
+	testsuite.cred, testsuite.options = testutil.GetCredAndClientOptions(testsuite.T())
+	testsuite.location = testutil.GetEnv("LOCATION", "eastus")
+	testsuite.subscriptionID = testutil.GetEnv("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
+	testutil.StartRecording(testsuite.T(), "sdk/resourcemanager/storage/armstorage/testdata")
+	resourceGroup, _, err := testutil.CreateResourceGroup(testsuite.ctx, testsuite.subscriptionID, testsuite.cred, testsuite.options, testsuite.location)
+	testsuite.Require().NoError(err)
+	testsuite.resourceGroupName = *resourceGroup.Name
+
+}
+
+func (testsuite *SKUsClientTestSuite) TearDownSuite() {
+	_, err := testutil.DeleteResourceGroup(testsuite.ctx, testsuite.subscriptionID, testsuite.cred, testsuite.options, testsuite.resourceGroupName)
+	testsuite.Require().NoError(err)
+	testutil.StopRecording(testsuite.T())
+}
+
+func TestSKUsClient(t *testing.T) {
+	suite.Run(t, new(SKUsClientTestSuite))
+}
+
+func (testsuite *SKUsClientTestSuite) TestSKUs() {
+	skusClient := armstorage.NewSKUsClient(testsuite.subscriptionID, testsuite.cred, testsuite.options)
+	resp, err := skusClient.List(testsuite.ctx, nil)
+	testsuite.Require().NoError(err)
+	testsuite.Require().Greater(len(resp.Value), 1)
+}

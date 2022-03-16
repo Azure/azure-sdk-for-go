4cfc42735c3c026b8785788a8ed99e478dde3d5e
seankane-msft <seankane@microsoft.com>
2022-03-16 13:47:22 -0400

    running perf check in the testdata/perf directory

--- eng/scripts/Build_Perf.ps1
+++ eng/scripts/Build_Perf.ps1
@@ -7,11 +7,13 @@ Param(
 Push-Location sdk/$serviceDirectory
 
 if (Test-Path -Path testdata/perf) {
+    Push-Location testdata/perf
     Write-Host "##[command] Building and vetting performance tests in sdk/$serviceDirectory/testdata/perf"
 
     Write-Host "##[command] Executing 'go build .' in sdk/$serviceDirectory/testdata/perf"
     go build .
     if ($LASTEXITCODE) {
+        Pop-Location
         Pop-Location
         exit $LASTEXITCODE
     }
@@ -20,8 +22,10 @@ if (Test-Path -Path testdata/perf) {
     go vet .
     if ($LASTEXITCODE) {
         Pop-Location
+        Pop-Location
         exit $LASTEXITCODE
     }
+    Pop-Location
 } else {
     Write-Host "##[command] Did not find performance tests in sdk/$serviceDirectory/testdata/perf"
 }

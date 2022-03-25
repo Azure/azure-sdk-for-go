3904c2fc6563c59ea4de386efd9bebc85386e7f3
Sean Kane <seankane@microsoft.com>
2022-03-25 14:56:55 -0400

    Adding Possible***Values methods

--- sdk/keyvault/azcertificates/client_test.go
+++ sdk/keyvault/azcertificates/client_test.go
@@ -467,7 +467,7 @@ func TestPolicy(t *testing.T) {
 		},
 		X509CertificateProperties: &X509CertificateProperties{
 			Ekus:             []*string{to.StringPtr("1.3.6.1.5.5.7.3.1"), to.StringPtr("1.3.6.1.5.5.7.3.2")},
-			KeyUsage:         []*CerificateKeyUsage{CertificateKeyUsageDecipherOnly.ToPtr()},
+			KeyUsage:         []*CertificateKeyUsage{CertificateKeyUsageDecipherOnly.ToPtr()},
 			Subject:          to.StringPtr("CN=DefaultPolicy"),
 			ValidityInMonths: to.Int32Ptr(12),
 			SubjectAlternativeNames: &SubjectAlternativeNames{
@@ -531,7 +531,7 @@ func TestCRUDOperations(t *testing.T) {
 		},
 		X509CertificateProperties: &X509CertificateProperties{
 			Ekus:             []*string{to.StringPtr("1.3.6.1.5.5.7.3.1"), to.StringPtr("1.3.6.1.5.5.7.3.2")},
-			KeyUsage:         []*CerificateKeyUsage{CertificateKeyUsageDecipherOnly.ToPtr()},
+			KeyUsage:         []*CertificateKeyUsage{CertificateKeyUsageDecipherOnly.ToPtr()},
 			Subject:          to.StringPtr("CN=DefaultPolicy"),
 			ValidityInMonths: to.Int32Ptr(12),
 			SubjectAlternativeNames: &SubjectAlternativeNames{
--- sdk/keyvault/azcertificates/models.go
+++ sdk/keyvault/azcertificates/models.go
@@ -683,7 +683,7 @@ type X509CertificateProperties struct {
 	Ekus []*string `json:"ekus,omitempty"`
 
 	// List of key usages.
-	KeyUsage []*CerificateKeyUsage `json:"key_usage,omitempty"`
+	KeyUsage []*CertificateKeyUsage `json:"key_usage,omitempty"`
 
 	// The subject name. Should be a valid X509 distinguished Name.
 	Subject *string `json:"subject,omitempty"`
@@ -719,9 +719,9 @@ func x509CertificatePropertiesFromGenerated(g *generated.X509CertificateProperti
 		return nil
 	}
 
-	var ku []*CerificateKeyUsage
+	var ku []*CertificateKeyUsage
 	for _, k := range g.KeyUsage {
-		ku = append(ku, (*CerificateKeyUsage)(k))
+		ku = append(ku, (*CertificateKeyUsage)(k))
 	}
 
 	return &X509CertificateProperties{

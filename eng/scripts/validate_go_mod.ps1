7da63cc9d222ffbd24b20a808bfa0d6535ec0d6f
seankane-msft <seankane@microsoft.com>
2022-04-07 15:11:10 -0400

    using wess func

--- eng/scripts/validate_go_mod.ps1
+++ eng/scripts/validate_go_mod.ps1
@@ -2,6 +2,8 @@ Param(
     [string] $serviceDir
 )
 
+. (Join-Path $PSScriptRoot . Language-Settings.ps1)
+
 Push-Location sdk/$serviceDir
 
 $goModFiles = Get-ChildItem -Path . -Filter go.mod -Recurse
@@ -13,12 +15,17 @@ if ($goModFiles.Length -eq 0) {
 
 $hasError = $false
 foreach ($goMod in $goModFiles) {
+    Write-Host $goMod.Directory
+    $packageProperties = Get-GoModuleProperties $goMod.Directory
+    Write-Host "Props $packageProperties"
     if (-Not ($goMod.FullName -Like "*testdata*")) {
         $name = $goMod.FullName
         $patternMatches = Get-Content $name | Select-String -Pattern "replace "
         if ($patternMatches.Length -ne 0) {
             Write-Host "Found a replace directive in go.mod file at $name"
             $hasError = $true
+        } else {
+            Write-Host "Valid go.mod file at $name"
         }
     }
 }

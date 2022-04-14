5c5fcf60ba94c6089d689eb6f557943c03b5b594
seankane-msft <seankane@microsoft.com>
2022-04-14 12:53:58 -0400

    fixing errors

--- sdk/keyvault/azkeys/testdata/perf/sign.go
+++ sdk/keyvault/azkeys/testdata/perf/sign.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/internal/perf"
 	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azkeys"
@@ -58,7 +59,7 @@ func newSignTest(ctx context.Context, options perf.PerfTestOptions) (perf.Global
 		return nil, err
 	}
 
-	resp, err := client.CreateRSAKey(ctx, d.keyName, &azkeys.CreateRSAKeyOptions{Size: to.Ptr(int32(2048))}
+	resp, err := client.CreateRSAKey(ctx, d.keyName, &azkeys.CreateRSAKeyOptions{Size: to.Ptr(int32(2048))})
 	if err != nil {
 		return nil, err
 	}

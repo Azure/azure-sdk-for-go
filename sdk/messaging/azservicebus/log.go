0b1d5f3e2632cce60658ad7f96dfafb1b2833111
Richard Park <ripark@microsoft.com>
2022-04-14 14:52:14 -0700

    Move any constants/types that get re-exported from `internal` to the public package to the  'azservicebus/internal/exports' sub package to make them easier to keep track of.

--- sdk/messaging/azservicebus/log.go
+++ sdk/messaging/azservicebus/log.go
@@ -3,21 +3,21 @@
 
 package azservicebus
 
-import "github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus/internal"
+import "github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus/internal/exports"
 
 const (
 	// EventConn is used whenever we create a connection or any links (ie: receivers, senders).
-	EventConn = internal.EventConn
+	EventConn = exports.EventConn
 
 	// EventAuth is used when we're doing authentication/claims negotiation.
-	EventAuth = internal.EventAuth
+	EventAuth = exports.EventAuth
 
 	// EventReceiver represents operations that happen on Receivers.
-	EventReceiver = internal.EventReceiver
+	EventReceiver = exports.EventReceiver
 
 	// EventSender represents operations that happen on Senders.
-	EventSender = internal.EventSender
+	EventSender = exports.EventSender
 
 	// EventAdmin is used for operations in the azservicebus/admin.Client
-	EventAdmin = internal.EventAdmin
+	EventAdmin = exports.EventAdmin
 )

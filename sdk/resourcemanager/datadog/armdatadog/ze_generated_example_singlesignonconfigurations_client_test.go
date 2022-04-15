bc745f8ecc8f4117453a9e52905a72d8745d6070
Alancere <804873052@qq.com>
2022-04-15 16:39:04 +0800

    [Release] sdk/resourcemanager/datadog/armdatadog/0.4.0 generation from spec commit: cc26f1d28c939bbcde00a29aa8958b3eebb10cea

--- sdk/resourcemanager/datadog/armdatadog/ze_generated_example_singlesignonconfigurations_client_test.go
+++ sdk/resourcemanager/datadog/armdatadog/ze_generated_example_singlesignonconfigurations_client_test.go
@@ -19,7 +19,7 @@ import (
 )
 
 // Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/datadog/resource-manager/Microsoft.Datadog/stable/2021-03-01/examples/SingleSignOnConfigurations_List.json
-func ExampleSingleSignOnConfigurationsClient_List() {
+func ExampleSingleSignOnConfigurationsClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
@@ -31,7 +31,7 @@ func ExampleSingleSignOnConfigurationsClient_List() {
 		log.Fatalf("failed to create client: %v", err)
 		return
 	}
-	pager := client.List("<resource-group-name>",
+	pager := client.NewListPager("<resource-group-name>",
 		"<monitor-name>",
 		nil)
 	for pager.More() {
--- sdk/resourcemanager/datadog/armdatadog/zz_generated_operations_client.go
+++ sdk/resourcemanager/datadog/armdatadog/zz_generated_operations_client.go
@@ -48,10 +48,10 @@ func NewOperationsClient(credential azcore.TokenCredential, options *arm.ClientO
 	return client, nil
 }
 
-// List - List all operations provided by Microsoft.Datadog for the 2021-03-01 api version.
+// NewListPager - List all operations provided by Microsoft.Datadog for the 2021-03-01 api version.
 // If the operation fails it returns an *azcore.ResponseError type.
 // options - OperationsClientListOptions contains the optional parameters for the OperationsClient.List method.
-func (client *OperationsClient) List(options *OperationsClientListOptions) *runtime.Pager[OperationsClientListResponse] {
+func (client *OperationsClient) NewListPager(options *OperationsClientListOptions) *runtime.Pager[OperationsClientListResponse] {
 	return runtime.NewPager(runtime.PageProcessor[OperationsClientListResponse]{
 		More: func(page OperationsClientListResponse) bool {
 			return page.NextLink != nil && len(*page.NextLink) > 0

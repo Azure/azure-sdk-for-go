b62488c8ad4229b44939f178cf81f0b890b4a34e
SDKAuto <sdkautomation@microsoft.com>
2022-04-21 04:15:25 +0000

    CodeGen from PR 18748 in Azure/azure-rest-api-specs desktopvirtualization: generating the 2022-02 sdk (#18748)
    Co-authored-by: Alex Wilcox <alex.wilcox@cloudsecure.ltd>

--- sdk/resourcemanager/desktopvirtualization/armdesktopvirtualization/zz_generated_constants.go
+++ sdk/resourcemanager/desktopvirtualization/armdesktopvirtualization/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armdesktopvirtualization
 
 const (
 	moduleName    = "armdesktopvirtualization"
-	moduleVersion = "v0.4.0"
+	moduleVersion = "v0.4.1"
 )
 
 // ApplicationGroupType - Resource Type of ApplicationGroup.

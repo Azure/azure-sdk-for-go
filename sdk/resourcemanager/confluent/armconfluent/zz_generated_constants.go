52b6bfe2c2c47911b8d29cc0d684b7677ec9c586
SDKAuto <sdkautomation@microsoft.com>
2022-04-26 09:50:53 +0000

    CodeGen from PR 18781 in Azure/azure-rest-api-specs Fixing linting issue (#18781)

--- sdk/resourcemanager/confluent/armconfluent/zz_generated_constants.go
+++ sdk/resourcemanager/confluent/armconfluent/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armconfluent
 
 const (
 	moduleName    = "armconfluent"
-	moduleVersion = "v0.4.0"
+	moduleVersion = "v0.4.1"
 )
 
 // CreatedByType - The type of identity that created the resource.

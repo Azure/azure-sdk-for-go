e08516c501fd7968d323f49e1dd4a42c6b9832e2
Matias Quaranta <ealsur@hotmail.com>
2022-04-29 09:32:34 -0700

    empty response

---
+++ sdk/data/azcosmos/cosmos_transactional_batch_response.go
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azcosmos
+
+import (
+	"net/http"
+)
+
+// QueryItemsResponse contains response from the query operation.
+type TransactionalBatchResponse struct {
+	Response
+}
+
+func newTransactionalBatchResponse(resp *http.Response) (TransactionalBatchResponse, error) {
+	response := TransactionalBatchResponse{
+		Response: newResponse(resp),
+	}
+	
+
+	return response, nil
+}

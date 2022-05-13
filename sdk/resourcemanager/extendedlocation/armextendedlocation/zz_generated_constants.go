29217679fe073f1e708752b5e20106e4783425bb
SDKAuto <sdkautomation@microsoft.com>
2022-05-13 03:28:38 +0000

    CodeGen from PR 18996 in Azure/azure-rest-api-specs Migrate to net track2 for some resource-manager readmes (#18996)
    * migrate to net track2 for some mgmt RPs
    
    * Update readme.md

--- sdk/resourcemanager/extendedlocation/armextendedlocation/zz_generated_constants.go
+++ sdk/resourcemanager/extendedlocation/armextendedlocation/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armextendedlocation
 
 const (
 	moduleName    = "armextendedlocation"
-	moduleVersion = "v0.4.0"
+	moduleVersion = "v0.4.1"
 )
 
 // CreatedByType - The type of identity that created the resource.

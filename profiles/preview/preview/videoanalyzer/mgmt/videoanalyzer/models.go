264abe17103e7fa15d017a35da1f8c52a81c34de
Alancere <804873052@qq.com>
2022-05-16 09:58:53 +0800

    Generated from specification/datafactory/resource-manager/readme.md tag package-2018-06 (commit hash: cd8f049ce47372fd5955c2dfd11daf4a02050fe3)

--- profiles/preview/preview/videoanalyzer/mgmt/videoanalyzer/models.go
+++ profiles/preview/preview/videoanalyzer/mgmt/videoanalyzer/models.go
@@ -1,8 +1,6 @@
 //go:build go1.9
 // +build go1.9
 
-// Deprecated: We’re retiring the Azure Video Analyzer preview service; you're advised to transition your applications off of Video Analyzer by 01 December 2022. This SDK is no longer maintained and won’t work after the service is retired. To learn how to transition off, please refer to: https://aka.ms/azsdk/videoanalyzer/transitionoffguidance.
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 

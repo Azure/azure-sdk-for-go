fa53752ee68182dbc49915942e45b7ad49dceb69
Jiahui Peng <46921893+Alancere@users.noreply.github.com>
2022-05-18 14:21:00 +0800

    [Release] sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/1.0.0 generation from spec commit: d55b8005f05b040b852c15e74a0f3e36494a15e1 (#18113)

--- sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/ze_generated_example_connectedcluster_client_test.go
+++ sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/ze_generated_example_connectedcluster_client_test.go
@@ -12,8 +12,6 @@ import (
 	"context"
 	"log"
 
-	"time"
-
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridkubernetes/armhybridkubernetes"
@@ -26,28 +24,28 @@ func ExampleConnectedClusterClient_BeginCreate() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhybridkubernetes.NewConnectedClusterClient("<subscription-id>", cred, nil)
+	client, err := armhybridkubernetes.NewConnectedClusterClient("1bfbb5d0-917e-4346-9026-1d3b344417f5", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
 	poller, err := client.BeginCreate(ctx,
-		"<resource-group-name>",
-		"<cluster-name>",
+		"k8sc-rg",
+		"testCluster",
 		armhybridkubernetes.ConnectedCluster{
-			Location: to.Ptr("<location>"),
+			Location: to.Ptr("East US"),
 			Tags:     map[string]*string{},
 			Identity: &armhybridkubernetes.ConnectedClusterIdentity{
 				Type: to.Ptr(armhybridkubernetes.ResourceIdentityTypeSystemAssigned),
 			},
 			Properties: &armhybridkubernetes.ConnectedClusterProperties{
-				AgentPublicKeyCertificate: to.Ptr("<agent-public-key-certificate>"),
+				AgentPublicKeyCertificate: to.Ptr("MIICYzCCAcygAwIBAgIBADANBgkqhkiG9w0BAQUFADAuMQswCQYDVQQGEwJVUzEMMAoGA1UEChMDSUJNMREwDwYDVQQLEwhMb2NhbCBDQTAeFw05OTEyMjIwNTAwMDBaFw0wMDEyMjMwNDU5NTlaMC4xCzAJBgNVBAYTAlVTMQwwCgYDVQQKEwNJQk0xETAPBgNVBAsTCExvY2FsIENBMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD2bZEo7xGaX2/0GHkrNFZvlxBou9v1Jmt/PDiTMPve8r9FeJAQ0QdvFST/0JPQYD20rH0bimdDLgNdNynmyRoS2S/IInfpmf69iyc2G0TPyRvmHIiOZbdCd+YBHQi1adkj17NDcWj6S14tVurFX73zx0sNoMS79q3tuXKrDsxeuwIDAQABo4GQMIGNMEsGCVUdDwGG+EIBDQQ+EzxHZW5lcmF0ZWQgYnkgdGhlIFNlY3VyZVdheSBTZWN1cml0eSBTZXJ2ZXIgZm9yIE9TLzM5MCAoUkFDRikwDgYDVR0PAQH/BAQDAgAGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFJ3+ocRyCTJw067dLSwr/nalx6YMMA0GCSqGSIb3DQEBBQUAA4GBAMaQzt+zaj1GU77yzlr8iiMBXgdQrwsZZWJo5exnAucJAEYQZmOfyLiM D6oYq+ZnfvM0n8G/Y79q8nhwvuxpYOnRSAXFp6xSkrIOeZtJMY1h00LKp/JX3Ng1svZ2agE126JHsQ0bhzN5TKsYfbwfTwfjdWAGy6Vf1nYi/rO+ryMO"),
 			},
 		},
-		&armhybridkubernetes.ConnectedClusterClientBeginCreateOptions{ResumeToken: ""})
+		nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	res, err := poller.PollUntilDone(ctx, 30*time.Second)
+	res, err := poller.PollUntilDone(ctx, nil)
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
@@ -62,13 +60,13 @@ func ExampleConnectedClusterClient_Update() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhybridkubernetes.NewConnectedClusterClient("<subscription-id>", cred, nil)
+	client, err := armhybridkubernetes.NewConnectedClusterClient("1bfbb5d0-917e-4346-9026-1d3b344417f5", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
 	res, err := client.Update(ctx,
-		"<resource-group-name>",
-		"<cluster-name>",
+		"k8sc-rg",
+		"testCluster",
 		armhybridkubernetes.ConnectedClusterPatch{
 			Tags: map[string]*string{
 				"tag1": to.Ptr("value1"),
@@ -90,13 +88,13 @@ func ExampleConnectedClusterClient_Get() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhybridkubernetes.NewConnectedClusterClient("<subscription-id>", cred, nil)
+	client, err := armhybridkubernetes.NewConnectedClusterClient("1bfbb5d0-917e-4346-9026-1d3b344417f5", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
 	res, err := client.Get(ctx,
-		"<resource-group-name>",
-		"<cluster-name>",
+		"k8sc-rg",
+		"testCluster",
 		nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
@@ -112,18 +110,18 @@ func ExampleConnectedClusterClient_BeginDelete() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhybridkubernetes.NewConnectedClusterClient("<subscription-id>", cred, nil)
+	client, err := armhybridkubernetes.NewConnectedClusterClient("1bfbb5d0-917e-4346-9026-1d3b344417f5", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
 	poller, err := client.BeginDelete(ctx,
-		"<resource-group-name>",
-		"<cluster-name>",
-		&armhybridkubernetes.ConnectedClusterClientBeginDeleteOptions{ResumeToken: ""})
+		"k8sc-rg",
+		"testCluster",
+		nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
-	_, err = poller.PollUntilDone(ctx, 30*time.Second)
+	_, err = poller.PollUntilDone(ctx, nil)
 	if err != nil {
 		log.Fatalf("failed to pull the result: %v", err)
 	}
@@ -136,13 +134,13 @@ func ExampleConnectedClusterClient_ListClusterUserCredential() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhybridkubernetes.NewConnectedClusterClient("<subscription-id>", cred, nil)
+	client, err := armhybridkubernetes.NewConnectedClusterClient("1bfbb5d0-917e-4346-9026-1d3b344417f5", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
 	res, err := client.ListClusterUserCredential(ctx,
-		"<resource-group-name>",
-		"<cluster-name>",
+		"k8sc-rg",
+		"testCluster",
 		armhybridkubernetes.ListClusterUserCredentialProperties{
 			AuthenticationMethod: to.Ptr(armhybridkubernetes.AuthenticationMethodAAD),
 			ClientProxy:          to.Ptr(false),
@@ -162,17 +160,16 @@ func ExampleConnectedClusterClient_NewListByResourceGroupPager() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhybridkubernetes.NewConnectedClusterClient("<subscription-id>", cred, nil)
+	client, err := armhybridkubernetes.NewConnectedClusterClient("1bfbb5d0-917e-4346-9026-1d3b344417f5", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListByResourceGroupPager("<resource-group-name>",
+	pager := client.NewListByResourceGroupPager("k8sc-rg",
 		nil)
 	for pager.More() {
 		nextResult, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
-			return
 		}
 		for _, v := range nextResult.Value {
 			// TODO: use page item
@@ -188,7 +185,7 @@ func ExampleConnectedClusterClient_NewListBySubscriptionPager() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armhybridkubernetes.NewConnectedClusterClient("<subscription-id>", cred, nil)
+	client, err := armhybridkubernetes.NewConnectedClusterClient("1bfbb5d0-917e-4346-9026-1d3b344417f5", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
@@ -197,7 +194,6 @@ func ExampleConnectedClusterClient_NewListBySubscriptionPager() {
 		nextResult, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
-			return
 		}
 		for _, v := range nextResult.Value {
 			// TODO: use page item
--- sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/zz_generated_constants.go
+++ sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armhybridkubernetes
 
 const (
 	moduleName    = "armhybridkubernetes"
-	moduleVersion = "v0.4.0"
+	moduleVersion = "v1.0.0"
 )
 
 // AuthenticationMethod - The mode of client authentication.

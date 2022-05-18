9fb2785483ecab8b2503712b2497de30b8d5b5a2
Jiahui Peng <46921893+Alancere@users.noreply.github.com>
2022-05-18 14:23:15 +0800

    [Release] sdk/resourcemanager/hybridconnectivity/armhybridconnectivity/0.5.0 generation from spec commit: d55b8005f05b040b852c15e74a0f3e36494a15e1 (#18116)

--- sdk/resourcemanager/hybridconnectivity/armhybridconnectivity/ze_generated_example_operations_client_test.go
+++ sdk/resourcemanager/hybridconnectivity/armhybridconnectivity/ze_generated_example_operations_client_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridconnectivity/armhybridconnectivity"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/hybridconnectivity/resource-manager/Microsoft.HybridConnectivity/preview/2021-10-06-preview/examples/OperationsList.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/hybridconnectivity/resource-manager/Microsoft.HybridConnectivity/preview/2022-05-01-preview/examples/OperationsList.json
 func ExampleOperationsClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
@@ -32,7 +32,6 @@ func ExampleOperationsClient_NewListPager() {
 		nextResult, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
-			return
 		}
 		for _, v := range nextResult.Value {
 			// TODO: use page item

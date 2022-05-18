d1d4a9a669df3a3a2a8af0213bd0b795f86c264c
Alancere <804873052@qq.com>
2022-05-18 16:17:48 +0800

    [Release] sdk/resourcemanager/relay/armrelay/1.0.0 generation from spec commit: d55b8005f05b040b852c15e74a0f3e36494a15e1

--- sdk/resourcemanager/relay/armrelay/ze_generated_example_operations_client_test.go
+++ sdk/resourcemanager/relay/armrelay/ze_generated_example_operations_client_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/relay/armrelay"
 )
 
-// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/relay/resource-manager/Microsoft.Relay/stable/2017-04-01/examples/RelayOperations_List.json
+// Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/relay/resource-manager/Microsoft.Relay/stable/2021-11-01/examples/RelayOperations_List.json
 func ExampleOperationsClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
@@ -32,7 +32,6 @@ func ExampleOperationsClient_NewListPager() {
 		nextResult, err := pager.NextPage(ctx)
 		if err != nil {
 			log.Fatalf("failed to advance page: %v", err)
-			return
 		}
 		for _, v := range nextResult.Value {
 			// TODO: use page item

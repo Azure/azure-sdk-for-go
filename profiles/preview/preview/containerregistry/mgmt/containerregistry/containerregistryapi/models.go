ec509dba9f929d36b36d8b690aa2d3fd68ef5584
Alancere <804873052@qq.com>
2022-05-20 16:16:44 +0800

    Generated from specification/containerregistry/resource-manager/readme.md tag package-2022-02-preview (commit hash: a8a52b9e6c305f03c3a4c5411d59fc4454b5b372)

--- profiles/preview/preview/containerregistry/mgmt/containerregistry/containerregistryapi/models.go
+++ profiles/preview/preview/containerregistry/mgmt/containerregistry/containerregistryapi/models.go
@@ -9,7 +9,7 @@
 
 package containerregistryapi
 
-import original "github.com/Azure/azure-sdk-for-go/services/preview/containerregistry/mgmt/2021-12-01-preview/containerregistry/containerregistryapi"
+import original "github.com/Azure/azure-sdk-for-go/services/preview/containerregistry/mgmt/2022-02-01-preview/containerregistry/containerregistryapi"
 
 type AgentPoolsClientAPI = original.AgentPoolsClientAPI
 type ConnectedRegistriesClientAPI = original.ConnectedRegistriesClientAPI

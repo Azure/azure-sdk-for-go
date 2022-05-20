893ba2bdae1981c47b663b8d33b147eebd48d21b
Joel Hendrix <jhendrix@microsoft.com>
2022-05-20 09:18:12 -0700

    Update aztemplate with latest azcore and azidentity
    Added version.go per pattern and demonstrated how to create a pipeline.
    Tweaked APIs to be indicative of real-world APIs.

--- sdk/template/aztemplate/client.go
+++ sdk/template/aztemplate/client.go
@@ -1,5 +1,5 @@
-//go:build go1.16
-// +build go1.16
+//go:build go1.18
+// +build go1.18
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
@@ -7,31 +7,47 @@
 package aztemplate
 
 import (
+	"context"
+
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/template/aztemplate/internal"
 )
 
 // ClientOptions contains optional parameters for NewClient
-type ClientOptions struct{}
+type ClientOptions struct {
+	azcore.ClientOptions
+}
 
-// Client is the client to interact with
+// Client is the client to interact with.
+// Don't use this type directly, use NewClient() instead.
 type Client struct {
 	client *internal.TemplateClient
 }
 
 // NewClient returns a pointer to a Client
 func NewClient(cred azcore.TokenCredential, options *ClientOptions) (*Client, error) {
+	if options == nil {
+		options = &ClientOptions{}
+	}
 
-	//if options == nil {
-	//options = &ClientOptions{}
-	//}
-	tc := internal.NewTemplateClient()
+	pl := runtime.NewPipeline(moduleName, moduleVersion, runtime.PipelineOptions{
+		PerRetry: []policy.Policy{
+			runtime.NewBearerTokenPolicy(cred, []string{"service_scope"}, nil),
+		},
+	}, &options.ClientOptions)
 
-	return &Client{client: tc}, nil
+	return &Client{client: internal.NewTemplateClient(pl)}, nil
 
 }
 
+// SomeServiceActionOptions contains the optional values for the Client.SomeServiceAction method.
+type SomeServiceActionOptions struct {
+	// placeholder for future options
+}
+
 // SomeServiceAction does some service action
-func (c *Client) SomeServiceAction() {
-	c.client.PrintInfo()
+func (c *Client) SomeServiceAction(ctx context.Context, options *SomeServiceActionOptions) {
+	c.client.SomeAPI(ctx, nil)
 }

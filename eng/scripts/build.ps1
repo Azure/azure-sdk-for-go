107b24b9dba74d3ca46abf283b3fd22a69af1c48
tadelesh <tadelesh.shi@live.cn>
2022-06-10 14:45:05 +0800

    tool: default generate code file without filename prefix

--- eng/scripts/build.ps1
+++ eng/scripts/build.ps1
@@ -11,7 +11,7 @@ param(
     [string]$config = "autorest.md",
     [string]$autorestVersion = "3.8.2",
     [string]$goExtension = "@autorest/go@4.0.0-preview.41",
-    [string]$filePrefix = "zz_generated_",
+    [string]$filePrefix,
     [string]$outputFolder
 )
 

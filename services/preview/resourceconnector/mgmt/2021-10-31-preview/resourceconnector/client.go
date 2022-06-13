9eb7f00e5bdf990826439d0ce0141f855723ade7
Alancere <804873052@qq.com>
2022-06-13 17:50:52 +0800

    fix deprecated message

--- services/preview/resourceconnector/mgmt/2021-10-31-preview/resourceconnector/client.go
+++ services/preview/resourceconnector/mgmt/2021-10-31-preview/resourceconnector/client.go
@@ -1,4 +1,4 @@
-// Deprecated: Please note, this package has been deprecated. A replacement package is available github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resourceconnector/armresourceconnector. We strongly encourage you to upgrade to continue receiving updates. See Migration Guide for guidance on upgrading. Refer to our deprecation policy for more details.
+// Deprecated: Please note, this package has been deprecated. A replacement package is available [github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resourceconnector/armresourceconnector](https://pkg.go.dev/github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resourceconnector/armresourceconnector). We strongly encourage you to upgrade to continue receiving updates. See [Migration Guide](https://aka.ms/azsdk/golang/t2/migration) for guidance on upgrading. Refer to our [deprecation policy](https://azure.github.io/azure-sdk/policies_support.html) for more details.
 //
 // Package resourceconnector implements the Azure ARM Resourceconnector service API version 2021-10-31-preview.
 //

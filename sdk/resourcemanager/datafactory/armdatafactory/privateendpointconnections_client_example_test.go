92236d8963478566f36c7d2098bb2f8b6af34ad2
804873052 <804873052@qq.com>
2022-07-01 11:08:19 +0800

    [Release] sdk/resourcemanager/datafactory/armdatafactory/2.0.0 generation from spec commit: 5b336209b5083a44bde12fbde57c460b2104986e

--- sdk/resourcemanager/datafactory/armdatafactory/privateendpointconnections_client_example_test.go
+++ sdk/resourcemanager/datafactory/armdatafactory/privateendpointconnections_client_example_test.go
@@ -13,7 +13,7 @@ import (
 	"log"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/datafactory/armdatafactory"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/datafactory/armdatafactory/v2"
 )
 
 // Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/datafactory/resource-manager/Microsoft.DataFactory/stable/2018-06-01/examples/PrivateEndPointConnections_ListByFactory.json

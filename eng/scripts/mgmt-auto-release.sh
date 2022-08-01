fdd4e2b393fbc9a324e112d9ab58e1bdc0da836d
804873052 <804873052@qq.com>
2022-08-01 13:48:51 +0800

    add args

--- eng/scripts/mgmt-auto-release.sh
+++ eng/scripts/mgmt-auto-release.sh
@@ -10,12 +10,12 @@ git clone https://github.com/Azure/azure-sdk-for-go.git
 git clone https://github.com/Azure/azure-rest-api-specs.git
 
 cd azure-sdk-for-go
-git remote add fork https://Azure:$(azuresdk-github-pat)@github.com/Azure/azure-sdk-for-go.git
+git remote add fork https://Azure:"$1"@github.com/Azure/azure-sdk-for-go.git
 cd ../
 
 go install github.com/Azure/azure-sdk-for-go/eng/tools/generator@latest
 
-generator issue -t $(azuresdk-github-pat) > sdk-release.json
+generator issue -t $1 > sdk-release.json
 cat sdk-release.json
 
 file_size=`du -b ./sdk-release.json |awk '{print $1}'`
@@ -24,5 +24,5 @@ if [ ${file_size} -le 70 ]; then
   echo "There are no services that need to be released"
 else
   echo "run generator release-v2..."
-  generator release-v2 ./azure-sdk-for-go ./azure-rest-api-specs ./sdk-release.json -t $(azuresdk-github-pat)
+  generator release-v2 ./azure-sdk-for-go ./azure-rest-api-specs ./sdk-release.json -t $1
 fi

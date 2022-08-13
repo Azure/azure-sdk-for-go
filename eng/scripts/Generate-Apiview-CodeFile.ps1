4f4b94a3ab4a032392a5ee3bf0992224470f447a
praveenkuttappan <prmarott@microsoft.com>
2022-08-12 22:18:29 -0400

    Use existin function to find packages in service directory

--- eng/scripts/Generate-Apiview-CodeFile.ps1
+++ eng/scripts/Generate-Apiview-CodeFile.ps1
@@ -9,14 +9,11 @@ Set-StrictMode -Version 3
 . (Join-Path $PSScriptRoot .. common scripts common.ps1)
 
 $sdks = Get-AllPackageInfoFromRepo $ServiceDirectory
-Write-Host $sdks
 if ($sdks)
 {
     foreach ($sdk in $sdks)
     {
         $pkgRoot = $sdk.DirectoryPath
-        Write-Host "Processing SDK $($pkgRoot)"
-        $pkgRoot = Split-Path -Path $module
         Write-Host "Generating API review file for package $($pkgRoot), review file: $($CodeFileOutDirectory)"
         &$ParserPath $pkgRoot $CodeFileOutDirectory
     }

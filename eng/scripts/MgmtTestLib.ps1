a60b56e115c808e0d76d305ec6405d83e0c4dd2b
804873052 <804873052@qq.com>
2022-08-19 14:43:44 +0800

    upgrade to @autorest/gotest@4.2.2

--- eng/scripts/MgmtTestLib.ps1
+++ eng/scripts/MgmtTestLib.ps1
@@ -21,7 +21,7 @@ function Invoke-MgmtTestgen ()
         [string]$config = "autorest.md",
         [string]$autorestVersion = "3.8.2",
         [string]$goExtension = "@autorest/go@4.0.0-preview.43",
-        [string]$testExtension = "@autorest/gotest@4.2.1",
+        [string]$testExtension = "@autorest/gotest@4.2.2",
         [string]$outputFolder
     )
     if ($clean)

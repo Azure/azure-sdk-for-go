de6bd39d120d8f8bbab81f8295f3a60d62734392
Joel Hendrix <jhendrix@microsoft.com>
2022-08-23 14:59:19 -0700

    Move more tests to their respective sub-packages
    Enabled various tests that were commented out (there's more to do).

--- sdk/storage/azblob/service/client_test.go
+++ sdk/storage/azblob/service/client_test.go
@@ -528,3 +528,73 @@ func (s *ServiceUnrecordedTestsSuite) TestSASServiceClient() {
 	_, err = svcClient.DeleteContainer(context.Background(), containerName+"002", nil)
 	_require.Nil(err)
 }
+
+func (s *ServiceUnrecordedTestsSuite) TestSASContainerClient() {
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	accountName := os.Getenv("AZURE_STORAGE_ACCOUNT_NAME")
+	accountKey := os.Getenv("AZURE_STORAGE_ACCOUNT_KEY")
+	cred, err := azblob.NewSharedKeyCredential(accountName, accountKey)
+	_require.Nil(err)
+
+	serviceClient, err := service.NewClientWithSharedKeyCredential(fmt.Sprintf("https://%s.blob.core.windows.net/", accountName), cred, nil)
+	_require.Nil(err)
+
+	containerName := testcommon.GenerateContainerName(testName)
+	containerClient := serviceClient.NewContainerClient(containerName)
+
+	permissions := container.SASPermissions{
+		Read: true,
+		Add:  true,
+	}
+	start := time.Now().Add(-5 * time.Minute).UTC()
+	expiry := time.Now().Add(time.Hour)
+
+	sasUrl, err := containerClient.GetSASURL(permissions, start, expiry)
+	_require.Nil(err)
+
+	containerClient2, err := container.NewClientWithNoCredential(sasUrl, nil)
+	_require.Nil(err)
+
+	_, err = containerClient2.Create(context.Background(), &container.CreateOptions{Metadata: testcommon.BasicMetadata})
+	_require.NotNil(err)
+	testcommon.ValidateBlobErrorCode(_require, err, bloberror.AuthorizationFailure)
+}
+
+func (s *ServiceUnrecordedTestsSuite) TestSASContainerClient2() {
+	_require := require.New(s.T())
+	testName := s.T().Name()
+	accountName := os.Getenv("AZURE_STORAGE_ACCOUNT_NAME")
+	accountKey := os.Getenv("AZURE_STORAGE_ACCOUNT_KEY")
+	cred, err := azblob.NewSharedKeyCredential(accountName, accountKey)
+	_require.Nil(err)
+
+	serviceClient, err := service.NewClientWithSharedKeyCredential(fmt.Sprintf("https://%s.blob.core.windows.net/", accountName), cred, nil)
+	_require.Nil(err)
+
+	containerName := testcommon.GenerateContainerName(testName)
+	containerClient := serviceClient.NewContainerClient(containerName)
+
+	sasUrlReadAdd, err := containerClient.GetSASURL(container.SASPermissions{Read: true, Add: true},
+		time.Now().Add(-5*time.Minute).UTC(), time.Now().Add(time.Hour))
+	_require.Nil(err)
+	_, err = containerClient.Create(context.Background(), &container.CreateOptions{Metadata: testcommon.BasicMetadata})
+	_require.Nil(err)
+
+	containerClient1, err := container.NewClientWithNoCredential(sasUrlReadAdd, nil)
+	_require.Nil(err)
+
+	_, err = containerClient1.GetProperties(context.Background(), nil)
+	_require.Nil(err)
+	//validateBlobErrorCode(_require, err, bloberror.AuthorizationFailure)
+	//
+	//sasUrlRCWL, err := containerClient.GetSASURL(container.SASPermissions{Add: true, Create: true, Delete: true, List: true},
+	//	time.Now().Add(-5*time.Minute).UTC(), time.Now().Add(time.Hour))
+	//_require.Nil(err)
+	//
+	//containerClient2, err := container.NewClientWithNoCredential(sasUrlRCWL, nil)
+	//_require.Nil(err)
+	//
+	//_, err = containerClient2.Create(ctx, nil)
+	//_require.Nil(err)
+}

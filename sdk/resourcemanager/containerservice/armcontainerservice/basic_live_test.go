c96f7b9957dffd349ff1792e7bbea350ada138a3
tadelesh <tadelesh.shi@live.cn>
2022-08-26 16:59:31 +0800

    fix secret and env variable

--- sdk/resourcemanager/containerservice/armcontainerservice/basic_live_test.go
+++ sdk/resourcemanager/containerservice/armcontainerservice/basic_live_test.go
@@ -44,11 +44,11 @@ func (testsuite *BasicTestSuite) SetupSuite() {
 	testsuite.agentPoolName = "agentpoolna"
 	testsuite.configName = "configna"
 	testsuite.resourceName = "containerservicena"
-	testsuite.azureClientId = testutil.GetEnv("AZURE_CLIENT_ID", "")
-	testsuite.azureClientSecret = testutil.GetEnv("AZURE_CLIENT_SECRET", "")
+	testsuite.azureClientId = testutil.GetEnv("AZURE_CLIENT_ID", "00000000-0000-0000-0000-000000000000")
+	testsuite.azureClientSecret = testutil.GetEnv("AZURE_CLIENT_SECRET", "secret")
 	testsuite.location = testutil.GetEnv("LOCATION", "eastus")
 	testsuite.resourceGroupName = testutil.GetEnv("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
-	testsuite.subscriptionId = testutil.GetEnv("AZURE_SUBSCRIPTION_ID", "")
+	testsuite.subscriptionId = testutil.GetEnv("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
 
 	testutil.StartRecording(testsuite.T(), "sdk/resourcemanager/containerservice/armcontainerservice/testdata")
 	resourceGroup, _, err := testutil.CreateResourceGroup(testsuite.ctx, testsuite.subscriptionId, testsuite.cred, testsuite.options, testsuite.location)

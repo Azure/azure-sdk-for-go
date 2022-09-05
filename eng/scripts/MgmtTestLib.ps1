506482587ce9732edea24691a369ee80d959d685
tadelesh <tadelesh.shi@live.cn>
2022-09-05 11:33:43 +0800

    pump autorest version

--- eng/scripts/MgmtTestLib.ps1
+++ eng/scripts/MgmtTestLib.ps1
@@ -21,8 +21,8 @@ function Invoke-MgmtTestgen ()
         [string]$autorestPath = "",
         [string]$config = "autorest.md",
         [string]$autorestVersion = "3.8.2",
-        [string]$goExtension = "@autorest/go@4.0.0-preview.43",
-        [string]$testExtension = "@autorest/gotest@4.2.2",
+        [string]$goExtension = "@autorest/go@4.0.0-preview.44",
+        [string]$testExtension = "@autorest/gotest@4.3.0",
         [string]$outputFolder
     )
     if ($clean)

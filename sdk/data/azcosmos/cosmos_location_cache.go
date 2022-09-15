cc3ac459b00983d6841ef09c0d97561ff8fe7caa
Iman Malik <imanmalik@microsoft.com>
2022-09-15 02:57:03 -0400

    define initial types and structs

---
+++ sdk/data/azcosmos/cosmos_location_cache.go
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azcosmos
+
+import (
+	"net/url"
+	"sync"
+	"time"
+)
+
+type opType int
+
+const (
+	none opType = iota
+	read
+	write
+)
+
+type locationUnavailabilityInfo struct {
+	lastCheckTime time.Time
+	unavailableOp opType
+}
+
+type locationCache struct {
+	prefLocations             []string
+	defaultEndpt              url.URL
+	enableEndptDiscovery      bool
+	useMultipleWriteLocations bool
+	connLimit                 int
+	mu sync.Mutex // possibly try RWMutex
+	locationUnavailabilityInfoMap map[url.URL]locationUnavailabilityInfo 
+	lastUpdateTime time.Time
+	enableMultipleWriteLocations bool
+	unavailableLocationExpirationTime time.Duration
+}

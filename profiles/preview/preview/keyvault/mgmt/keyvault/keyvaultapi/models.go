c3514ba9dc3e257c7af6e4cd325ad65aec54c433
Alancere <804873052@qq.com>
2022-09-21 15:17:38 +0800

    Generated from specification/keyvault/resource-manager/readme.md tag package-preview-2021-11 (commit hash: 1c8d7850afbec9ede6de6f2d14bcc30896a74ed6)

--- profiles/preview/preview/keyvault/mgmt/keyvault/keyvaultapi/models.go
+++ profiles/preview/preview/keyvault/mgmt/keyvault/keyvaultapi/models.go
@@ -9,10 +9,14 @@
 
 package keyvaultapi
 
-import original "github.com/Azure/azure-sdk-for-go/services/preview/keyvault/mgmt/2020-04-01-preview/keyvault/keyvaultapi"
+import original "github.com/Azure/azure-sdk-for-go/services/preview/keyvault/mgmt/2021-11-01-preview/keyvault/keyvaultapi"
 
+type KeysClientAPI = original.KeysClientAPI
+type MHSMPrivateEndpointConnectionsClientAPI = original.MHSMPrivateEndpointConnectionsClientAPI
+type MHSMPrivateLinkResourcesClientAPI = original.MHSMPrivateLinkResourcesClientAPI
 type ManagedHsmsClientAPI = original.ManagedHsmsClientAPI
 type OperationsClientAPI = original.OperationsClientAPI
 type PrivateEndpointConnectionsClientAPI = original.PrivateEndpointConnectionsClientAPI
 type PrivateLinkResourcesClientAPI = original.PrivateLinkResourcesClientAPI
+type SecretsClientAPI = original.SecretsClientAPI
 type VaultsClientAPI = original.VaultsClientAPI

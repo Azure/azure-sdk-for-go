933df5603c4b06a06d6bad3529142c45bfcc6f55
siminsavani-msft <77068571+siminsavani-msft@users.noreply.github.com>
2022-10-06 12:49:49 -0400

    Adding to commented test

--- sdk/storage/azblob/service/client_test.go
+++ sdk/storage/azblob/service/client_test.go
@@ -600,6 +600,7 @@ func (s *ServiceUnrecordedTestsSuite) TestSASContainerClient2() {
 	//_require.Nil(err)
 }
 
+// Note: Test is commented out because it needs a valid ManagedIdentityCredential to run.
 /*func (s *ServiceRecordedTestsSuite) TestUserDelegationSAS() {
 	_require := require.New(s.T())
 	testName := s.T().Name()
@@ -609,7 +610,7 @@ func (s *ServiceUnrecordedTestsSuite) TestSASContainerClient2() {
 	cred, err := azidentity.NewManagedIdentityCredential(&optsClientID)
 	_require.Nil(err)
 
-	svcClient, err := azblob.NewClient(fmt.Sprintf("https://%s.blob.core.windows.net/", accountName), cred, &azblob.ClientOptions{})
+	svcClient, err := service.NewClient(fmt.Sprintf("https://%s.blob.core.windows.net/", accountName), cred, &service.ClientOptions{})
 	_require.Nil(err)
 
 	// Set current and past time, create KeyInfo

7eff4ffb22eb1bd6c37b578feae84361e986296f
Alancere <804873052@qq.com>
2022-10-11 15:50:37 +0800

    fix

--- sdk/resourcemanager/privatedns/armprivatedns/privatedns_live_test.go
+++ sdk/resourcemanager/privatedns/armprivatedns/privatedns_live_test.go
@@ -41,7 +41,7 @@ func (testsuite *PrivatednsTestSuite) SetupSuite() {
 	testsuite.privateZoneName = "scenario_privatezone.com"
 	testsuite.location = testutil.GetEnv("LOCATION", "eastus")
 	testsuite.resourceGroupName = testutil.GetEnv("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
-	testsuite.subscriptionId = testutil.GetEnv("AZURE_SUBSCRIPTION_ID", "")
+	testsuite.subscriptionId = testutil.GetEnv("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
 
 	testutil.StartRecording(testsuite.T(), "sdk/resourcemanager/privatedns/armprivatedns/testdata")
 	resourceGroup, _, err := testutil.CreateResourceGroup(testsuite.ctx, testsuite.subscriptionId, testsuite.cred, testsuite.options, testsuite.location)

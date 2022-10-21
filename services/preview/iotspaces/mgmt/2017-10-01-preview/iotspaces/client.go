7ff7f08bea2df7127d8c406b5daa552921b366c3
Peng Jiahui <46921893+Alancere@users.noreply.github.com>
2022-10-21 16:33:25 +0800

    Release v67.0.1 (#19370)
    * Release V67.1.0
    
    * replace tree/legacy to blob/v67.0.0
    
    * v67.0.1
    
    * update deprecated message

--- services/preview/iotspaces/mgmt/2017-10-01-preview/iotspaces/client.go
+++ services/preview/iotspaces/mgmt/2017-10-01-preview/iotspaces/client.go
@@ -1,3 +1,5 @@
+// Deprecated: This library is deprecated and no longer supported. The Azure IoTSpaces service that this library supported has been retired as of the end of 2020. Please consider using the Azure Digital Twins service ([What is Azure Digital Twins? - Azure Digital Twins | Microsoft Learn](https://learn.microsoft.com/en-us/azure/digital-twins/overview))
+//
 // Package iotspaces implements the Azure ARM Iotspaces service API version 2017-10-01-preview.
 //
 // Use this API to manage the IoTSpaces service instances in your Azure subscription.

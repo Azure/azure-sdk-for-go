e97e2cadee0bbb01506e08676fa70fcb9d1a08a6
Alancere <804873052@qq.com>
2022-10-26 14:02:55 +0800

    v2.2.0

--- sdk/resourcemanager/containerservice/armcontainerservice/autorest.md
+++ sdk/resourcemanager/containerservice/armcontainerservice/autorest.md
@@ -8,5 +8,5 @@ require:
 - https://github.com/Azure/azure-rest-api-specs/blob/82ea406b73d671269217053d7ef336450d860345/specification/containerservice/resource-manager/readme.md
 - https://github.com/Azure/azure-rest-api-specs/blob/82ea406b73d671269217053d7ef336450d860345/specification/containerservice/resource-manager/readme.go.md
 license-header: MICROSOFT_MIT_NO_VERSION
-module-version: 3.0.0
+module-version: 2.2.0
 ```
--- sdk/resourcemanager/containerservice/armcontainerservice/privatelinkresources_client_example_test.go
+++ sdk/resourcemanager/containerservice/armcontainerservice/privatelinkresources_client_example_test.go
@@ -13,7 +13,7 @@ import (
 	"log"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v3"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v2"
 )
 
 // Generated from example definition: https://github.com/Azure/azure-rest-api-specs/tree/main/specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2022-09-01/examples/PrivateLinkResourcesList.json

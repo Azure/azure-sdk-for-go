7d65f014ef16eb22f6799036d93364b74a2d1f6a
Alancere <804873052@qq.com>
2022-10-31 10:20:14 +0800

    fix

--- sdk/resourcemanager/trafficmanager/armtrafficmanager/constants.go
+++ sdk/resourcemanager/trafficmanager/armtrafficmanager/constants.go
@@ -11,7 +11,7 @@ package armtrafficmanager
 
 const (
 	moduleName    = "armtrafficmanager"
-	moduleVersion = "1.1.0-beta.1"
+	moduleVersion = "v1.1.0-beta.1"
 )
 
 // AllowedEndpointRecordType - The allowed type DNS record types for this profile.

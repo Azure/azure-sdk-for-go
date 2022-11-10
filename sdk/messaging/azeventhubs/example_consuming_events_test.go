4b43a797b23dfc57ad8c317b880ae163dea95827
ripark <ripark@microsoft.com>
2022-11-09 17:44:59 -0800

    Updating documentation for the PartitionClient.ReceiveEvents, with some troubleshooting docs.
    Updated the example_consuming_events.go to be more of a full fledged example, demonstrating how the client can be used with multiple partition clients and also ensuring that people are more aware of partitions in general.

--- sdk/messaging/azeventhubs/example_consuming_events_test.go
+++ sdk/messaging/azeventhubs/example_consuming_events_test.go
@@ -5,8 +5,10 @@ package azeventhubs_test
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
@@ -14,10 +16,17 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azeventhubs"
 )
 
+// Example_consumingEventsUsingConsumerClient shows how to start consuming events in partitions
+// in an Event Hub.
+//
+// If you have an Azure Storage account you can use the [Processor] type instead, which will handle
+// distributing partitions between multiple consumers. See example_processor_test.go for usage of
+// that type.
 func Example_consumingEventsUsingConsumerClient() {
 	eventHubNamespace := os.Getenv("EVENTHUB_NAMESPACE") // <ex: myeventhubnamespace.servicebus.windows.net>
 	eventHubName := os.Getenv("EVENTHUB_NAME")
-	eventHubPartitionID := os.Getenv("EVENTHUB_PARTITION")
+
+	fmt.Printf("Event Hub Namespace: %s, hubname: %s\n", eventHubNamespace, eventHubName)
 
 	defaultAzureCred, err := azidentity.NewDefaultAzureCredential(nil)
 
@@ -27,9 +36,9 @@ func Example_consumingEventsUsingConsumerClient() {
 
 	// Can also use a connection string:
 	//
-	// consumerClient, err = azeventhubs.NewConsumerClientFromConnectionString(connectionString, "", "partition id", consumerGroup, nil)
+	// consumerClient, err = azeventhubs.NewConsumerClientFromConnectionString(connectionString, eventHubName, azeventhubs.DefaultConsumerGroup, nil)
 	//
-	consumerClient, err = azeventhubs.NewConsumerClient(eventHubNamespace, eventHubName, azeventhubs.DefaultConsumerGroup, defaultAzureCred, nil)
+	consumerClient, err := azeventhubs.NewConsumerClient(eventHubNamespace, eventHubName, azeventhubs.DefaultConsumerGroup, defaultAzureCred, nil)
 
 	if err != nil {
 		panic(err)
@@ -37,33 +46,87 @@ func Example_consumingEventsUsingConsumerClient() {
 
 	defer consumerClient.Close(context.TODO())
 
-	partitionClient, err := consumerClient.NewPartitionClient(eventHubPartitionID, nil)
+	// Consuming events from Azure Event Hubs requires an Event Hub AND a partition to receive from.
+	// This differs from sending events where a partition ID is not required, although events are
+	// assigned a partition when stored on the server side.
+	//
+	// For this example, we'll assume we don't know which partitions will have events, so we'll just
+	// receive from all of them.
+	eventHubProperties, err := consumerClient.GetEventHubProperties(context.Background(), nil)
 
 	if err != nil {
 		panic(err)
 	}
 
-	defer partitionClient.Close(context.TODO())
+	overallReceiveTime := 5 * time.Minute
+	appCtx, cancelApp := context.WithTimeout(context.Background(), overallReceiveTime)
+	defer cancelApp()
 
-	for {
-		// ReceiveEvents will wait until it either receives the # of events requested (100, in this call)
-		// or if the context is cancelled, in which case it'll return any messages it has received.
-		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	fmt.Printf("Starting PartitionClients, will receive for %s...\n", overallReceiveTime)
 
-		events, err := partitionClient.ReceiveEvents(ctx, 100, nil)
-		cancel()
+	wg := sync.WaitGroup{}
 
-		if err != nil {
-			panic(err)
-		}
+	for _, tmpPartitionID := range eventHubProperties.PartitionIDs {
+		wg.Add(1)
 
-		processConsumedEvents(events)
+		go func(partitionID string) {
+			defer wg.Done()
+
+			// NOTE: If you're not changing any options for this function you can pass `nil` for the options
+			// parameter.
+			partitionClient, err := consumerClient.NewPartitionClient(partitionID, &azeventhubs.PartitionClientOptions{
+				// The default start position is Latest, which means this partition client will only start receiving
+				// events stored AFTER the receiver has initialized.
+				//
+				// If you need to read events earlier than that or require a more deterministic start, you can use
+				// the other fields in StartPosition, which allow you to choose a starting sequence number, offset
+				// or even a timestamp.
+				StartPosition: azeventhubs.StartPosition{
+					Latest: to.Ptr(true),
+				},
+			})
+
+			if err != nil {
+				panic(err)
+			}
+
+			defer partitionClient.Close(context.TODO())
+
+			fmt.Printf("[Partition: %s] Starting receive loop for partition\n", partitionID)
+
+			for {
+				ctx, cancel := context.WithTimeout(appCtx, time.Minute)
+				events, err := partitionClient.ReceiveEvents(ctx, 100, nil)
+				cancel()
+
+				if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
+					if appCtx.Err() != nil {
+						// Gracefully exit. the 'defer partitionClient.Close(context.Background())' above
+						// will take care of closing the PartitionClient.
+						fmt.Printf("[Partition: %s] Application is stopping, stopping receive for partition\n", partitionID)
+						break
+					}
+
+					// We didn't get any events before our context cancelled. Let's loop again.
+					fmt.Printf("[Partition: %s] No events arrived in 1m, trying to receive again\n", partitionID)
+				} else if err != nil {
+					panic(err)
+				}
+
+				processConsumedEvents(partitionID, events)
+			}
+		}(tmpPartitionID)
 	}
+
+	fmt.Printf("Waiting for %s, for events to arrive on any partition", overallReceiveTime)
+	wg.Wait()
+	fmt.Printf("Done receiving events\n")
 }
 
-func processConsumedEvents(events []*azeventhubs.ReceivedEventData) {
+func processConsumedEvents(partitionID string, events []*azeventhubs.ReceivedEventData) {
 	for _, event := range events {
-		// process the event in some way
-		fmt.Printf("Event received with body %v\n", event.Body)
+		// We're assuming the Body is a byte-encoded string. EventData.Body supports any payload
+		// that can be encoded to []byte.
+		fmt.Printf("  [Partition: %s] Event received with body '%s'\n", partitionID, string(event.Body))
 	}
 }

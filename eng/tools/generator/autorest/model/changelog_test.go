dd275b326b08535c105e67214c660991ff19f576
Alancere <804873052@qq.com>
2022-11-21 10:36:59 +0800

    gofmt

--- eng/tools/generator/autorest/model/changelog_test.go
+++ eng/tools/generator/autorest/model/changelog_test.go
@@ -9,27 +9,27 @@ import (
 
 func TestSortChangeItem(t *testing.T) {
 	s := map[string]delta.Signature{
-		"*X":{},
-		"*NewD":{},
-		"C":{},
-		"NewA":{},
-		"B":{},
-		"*NewH":{},
-		"D.Get":{},
-		"*D.Create":{},
+		"*X":        {},
+		"*NewD":     {},
+		"C":         {},
+		"NewA":      {},
+		"B":         {},
+		"*NewH":     {},
+		"D.Get":     {},
+		"*D.Create": {},
 	}
 
 	sortResult := sortChangeItem(s)
 
 	expcted := []string{
-		"NewA", 
-		"B", 
-		"C", 
-		"*NewD", 
-		"*D.Create", 
-		"D.Get", 
-		"*NewH", 
+		"NewA",
+		"B",
+		"C",
+		"*NewD",
+		"*D.Create",
+		"D.Get",
+		"*NewH",
 		"*X",
 	}
-	assert.Equal(t,expcted,sortResult)
+	assert.Equal(t, expcted, sortResult)
 }

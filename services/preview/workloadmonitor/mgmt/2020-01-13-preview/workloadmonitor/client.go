9a75a0f751ffddb7fbcf2fd028712cfbc1ea2f01
Alancere <804873052@qq.com>
2022-12-13 15:54:57 +0800

    deprecated services/preview/workloadmonitor/mgmt

--- services/preview/workloadmonitor/mgmt/2020-01-13-preview/workloadmonitor/client.go
+++ services/preview/workloadmonitor/mgmt/2020-01-13-preview/workloadmonitor/client.go
@@ -1,4 +1,5 @@
-// Deprecated: Please note, this package has been deprecated. A replacement package is available [github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/workloadmonitor/armworkloadmonitor](https://pkg.go.dev/github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/workloadmonitor/armworkloadmonitor). We strongly encourage you to upgrade to continue receiving updates. See [Migration Guide](https://aka.ms/azsdk/golang/t2/migration) for guidance on upgrading. Refer to our [deprecation policy](https://azure.github.io/azure-sdk/policies_support.html) for more details.
+// Deprecated: The service backing this library is retired on November 30th, 2022. At that point, this library will no longer work. Please migrate to Azure Monitor Log Alerts API in module github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/monitor/armmonitor.
+// For more details on the Azure VM Insights Guest Health retirement, please visit: https://azure.microsoft.com/updates/transition-to-azure-monitor-log-alerts-for-vm-guest-health-by-30-november-2022
 //
 // Package workloadmonitor implements the Azure ARM Workloadmonitor service API version 2020-01-13-preview.
 //

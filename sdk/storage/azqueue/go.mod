406f3ab4682fff4397301ae988d8b512155efc69
Tamer Sherif <tasherif@microsoft.com>
2023-01-04 20:14:23 -0800

    added sas perms and basic tests

--- sdk/storage/azqueue/go.mod
+++ sdk/storage/azqueue/go.mod
@@ -5,9 +5,15 @@ go 1.19
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.2.0
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0
+	github.com/stretchr/testify v1.7.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/dnaeon/go-vcr v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ sdk/storage/azqueue/service/client_test.go
@@ -0,0 +1,88 @@
+package service_test
+
+import (
+	"context"
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/recording"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azqueue"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azqueue/internal/shared"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azqueue/internal/testcommon"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azqueue/service"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	recordMode := recording.GetRecordMode()
+	t.Logf("Running service Tests in %s mode\n", recordMode)
+	if recordMode == recording.LiveMode {
+		suite.Run(t, &ServiceRecordedTestsSuite{})
+		suite.Run(t, &ServiceUnrecordedTestsSuite{})
+	} else if recordMode == recording.PlaybackMode {
+		suite.Run(t, &ServiceRecordedTestsSuite{})
+	} else if recordMode == recording.RecordingMode {
+		suite.Run(t, &ServiceRecordedTestsSuite{})
+	}
+}
+
+func (s *ServiceRecordedTestsSuite) BeforeTest(suite string, test string) {
+	testcommon.BeforeTest(s.T(), suite, test)
+}
+
+func (s *ServiceRecordedTestsSuite) AfterTest(suite string, test string) {
+	testcommon.AfterTest(s.T(), suite, test)
+}
+
+func (s *ServiceUnrecordedTestsSuite) BeforeTest(suite string, test string) {
+
+}
+
+func (s *ServiceUnrecordedTestsSuite) AfterTest(suite string, test string) {
+
+}
+
+type ServiceRecordedTestsSuite struct {
+	suite.Suite
+}
+
+type ServiceUnrecordedTestsSuite struct {
+	suite.Suite
+}
+
+//TODO: TestListQueues
+//TODO: TestCreateQueue
+//TODO: TestDeleteQueue
+
+func (s *ServiceUnrecordedTestsSuite) TestServiceClientFromConnectionString() {
+	_require := require.New(s.T())
+	//testName := s.T().Name()
+
+	accountName, _ := testcommon.GetAccountInfo(testcommon.TestAccountDefault)
+	connectionString := testcommon.GetConnectionString(testcommon.TestAccountDefault)
+
+	parsedConnStr, err := shared.ParseConnectionString(connectionString)
+	_require.Nil(err)
+	_require.Equal(parsedConnStr.ServiceURL, "https://"+accountName+".queue.core.windows.net/")
+
+	sharedKeyCred, err := azqueue.NewSharedKeyCredential(parsedConnStr.AccountName, parsedConnStr.AccountKey)
+	_require.Nil(err)
+
+	svcClient, err := service.NewClientWithSharedKeyCredential(parsedConnStr.ServiceURL, sharedKeyCred, nil)
+	_require.Nil(err)
+	//containerClient := testcommon.CreateNewContainer(context.Background(), _require, testcommon.GenerateContainerName(testName), svcClient)
+	//defer testcommon.DeleteContainer(context.Background(), _require, containerClient)
+	sProps, err := svcClient.GetProperties(context.Background(), nil)
+	_require.Nil(err)
+	_require.NotZero(sProps)
+}
+
+func (s *ServiceRecordedTestsSuite) TestGetProperties() {
+	_require := require.New(s.T())
+	svcClient, err := testcommon.GetServiceClient(s.T(), testcommon.TestAccountDefault, nil)
+	_require.NoError(err)
+
+	// Ensure the call succeeded. Don't test for specific account properties because we can't/don't want to set account properties.
+	sProps, err := svcClient.GetProperties(context.Background(), nil)
+	_require.Nil(err)
+	_require.NotZero(sProps)
+}

0860071ed796b1a3cdc49dd06ba09ca2fba8a369
tadelesh <tadelesh.shi@live.cn>
2023-01-17 13:59:36 +0800

    fix test failure in CI of mgmt. packages

--- eng/scripts/MgmtTestLib.ps1
+++ eng/scripts/MgmtTestLib.ps1
@@ -22,7 +22,7 @@ function Invoke-MgmtTestgen ()
         [string]$config = "autorest.md",
         [string]$autorestVersion = "3.8.2",
         [string]$goExtension = "@autorest/go@4.0.0-preview.45",
-        [string]$testExtension = "@autorest/gotest@4.5.0",
+        [string]$testExtension = "@autorest/gotest@4.5.1",
         [string]$outputFolder
     )
     if ($clean)

4b0d1c3ef849a09777a2b8483d369dd26c9d1e78
Chenjie Shi <tadelesh.shi@live.cn>
2023-02-08 09:00:17 +0800

    upgrade core lib version for ACR preview (#19950)
    * upgrade core lib version
    
    * add basic config for live test
    
    * update test and recording to support live test
    
    * use mgmt. package to import images for test
    
    * fix live test env problem
    
    * fix env bug and run go mod tidy
    
    * add new line

--- sdk/containers/azcontainerregistry/authentication_client_test.go
+++ sdk/containers/azcontainerregistry/authentication_client_test.go
@@ -8,24 +8,30 @@ package azcontainerregistry
 
 import (
 	"context"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/stretchr/testify/require"
+	"reflect"
+	"strings"
 	"testing"
 )
 
 func Test_authenticationClient_ExchangeAADAccessTokenForACRRefreshToken(t *testing.T) {
 	startRecording(t)
-	cred, options := getCredAndClientOptions(t)
-	client := newAuthenticationClient("https://azacrlivetest.azurecr.io", &authenticationClientOptions{ClientOptions: options})
+	endpoint, cred, options := getEndpointCredAndClientOptions(t)
+	client := newAuthenticationClient(endpoint, &authenticationClientOptions{ClientOptions: options})
 	ctx := context.Background()
+	if reflect.ValueOf(options.Cloud).IsZero() {
+		options.Cloud = cloud.AzurePublic
+	}
 	accessToken, err := cred.GetToken(
 		ctx,
 		policy.TokenRequestOptions{
-			Scopes: []string{"https://management.core.windows.net/.default"},
+			Scopes: []string{options.Cloud.Services[ServiceName].Audience + "/.default"},
 		})
 	require.NoError(t, err)
-	resp, err := client.ExchangeAADAccessTokenForACRRefreshToken(ctx, postContentSchemaGrantTypeAccessToken, "azacrlivetest.azurecr.io", &authenticationClientExchangeAADAccessTokenForACRRefreshTokenOptions{
+	resp, err := client.ExchangeAADAccessTokenForACRRefreshToken(ctx, postContentSchemaGrantTypeAccessToken, strings.TrimPrefix(endpoint, "https://"), &authenticationClientExchangeAADAccessTokenForACRRefreshTokenOptions{
 		AccessToken: &accessToken.Token,
 	})
 	require.NoError(t, err)
@@ -34,21 +40,24 @@ func Test_authenticationClient_ExchangeAADAccessTokenForACRRefreshToken(t *testi
 
 func Test_authenticationClient_ExchangeACRRefreshTokenForACRAccessToken(t *testing.T) {
 	startRecording(t)
-	cred, options := getCredAndClientOptions(t)
-	client := newAuthenticationClient("https://azacrlivetest.azurecr.io", &authenticationClientOptions{ClientOptions: options})
+	endpoint, cred, options := getEndpointCredAndClientOptions(t)
+	client := newAuthenticationClient(endpoint, &authenticationClientOptions{ClientOptions: options})
 	ctx := context.Background()
+	if reflect.ValueOf(options.Cloud).IsZero() {
+		options.Cloud = cloud.AzurePublic
+	}
 	accessToken, err := cred.GetToken(
 		ctx,
 		policy.TokenRequestOptions{
-			Scopes: []string{"https://management.core.windows.net/.default"},
+			Scopes: []string{options.Cloud.Services[ServiceName].Audience + "/.default"},
 		})
 	require.NoError(t, err)
-	refreshResp, err := client.ExchangeAADAccessTokenForACRRefreshToken(ctx, postContentSchemaGrantTypeAccessToken, "azacrlivetest.azurecr.io", &authenticationClientExchangeAADAccessTokenForACRRefreshTokenOptions{
+	refreshResp, err := client.ExchangeAADAccessTokenForACRRefreshToken(ctx, postContentSchemaGrantTypeAccessToken, strings.TrimPrefix(endpoint, "https://"), &authenticationClientExchangeAADAccessTokenForACRRefreshTokenOptions{
 		AccessToken: &accessToken.Token,
 	})
 	require.NoError(t, err)
 	require.NotEmpty(t, *refreshResp.acrRefreshToken.RefreshToken)
-	accessResp, err := client.ExchangeACRRefreshTokenForACRAccessToken(ctx, "azacrlivetest.azurecr.io", "registry:catalog:*", *refreshResp.acrRefreshToken.RefreshToken, &authenticationClientExchangeACRRefreshTokenForACRAccessTokenOptions{GrantType: to.Ptr(tokenGrantTypeRefreshToken)})
+	accessResp, err := client.ExchangeACRRefreshTokenForACRAccessToken(ctx, strings.TrimPrefix(endpoint, "https://"), "registry:catalog:*", *refreshResp.acrRefreshToken.RefreshToken, &authenticationClientExchangeACRRefreshTokenForACRAccessTokenOptions{GrantType: to.Ptr(tokenGrantTypeRefreshToken)})
 	require.NoError(t, err)
 	require.NotEmpty(t, *accessResp.acrAccessToken.AccessToken)
 }

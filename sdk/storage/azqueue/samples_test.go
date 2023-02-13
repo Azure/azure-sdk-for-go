8e941eb434dc2798a44e8edb7f323ca2a9754562
Tamer Sherif <tasherif@microsoft.com>
2023-02-13 14:51:58 -0800

    sample todos

---
+++ sdk/storage/azqueue/samples_test.go
@@ -0,0 +1,7 @@
+package azqueue_test
+
+// TODO: Create queues, list them, delete them
+// TODO: Create queue, enqueue, dequeue, delete
+// TODO: Create queue, enqueue, peek, dequeue, delete
+// TODO: Create queue, enqueue, peek messages, dequeue messages, clear messages, delete
+// TODO: Create queue, enqueue, update, dequeue, delete

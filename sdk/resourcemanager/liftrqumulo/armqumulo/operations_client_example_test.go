6ef3de2e05440aa1613ba2f1619d6f4ee72e7cde
Alancere <804873052@qq.com>
2023-03-28 15:47:15 +0800

    [Release] sdk/resourcemanager/liftrqumulo/armqumulo/0.2.0 generation from spec commit: 2ac8dec9cbc4ad4db7537de603339f069d482078

--- sdk/resourcemanager/liftrqumulo/armqumulo/operations_client_example_test.go
+++ sdk/resourcemanager/liftrqumulo/armqumulo/operations_client_example_test.go
@@ -24,11 +24,11 @@ func ExampleOperationsClient_NewListPager_operationsListMaximumSetGen() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armqumulo.NewOperationsClient(cred, nil)
+	clientFactory, err := armqumulo.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListPager(nil)
+	pager := clientFactory.NewOperationsClient().NewListPager(nil)
 	for pager.More() {
 		page, err := pager.NextPage(ctx)
 		if err != nil {
@@ -64,11 +64,11 @@ func ExampleOperationsClient_NewListPager_operationsListMinimumSetGen() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	client, err := armqumulo.NewOperationsClient(cred, nil)
+	clientFactory, err := armqumulo.NewClientFactory("<subscription-id>", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := client.NewListPager(nil)
+	pager := clientFactory.NewOperationsClient().NewListPager(nil)
 	for pager.More() {
 		page, err := pager.NextPage(ctx)
 		if err != nil {

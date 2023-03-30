ec80bb99dc9a0a9982f6e63af4ed51fd1e086a54
Alancere <804873052@qq.com>
2023-03-30 17:51:15 +0800

    update test

--- eng/tools/generator/cmd/v2/common/fileProcessor_test.go
+++ eng/tools/generator/cmd/v2/common/fileProcessor_test.go
@@ -20,91 +20,106 @@ func TestCalculateNewVersion(t *testing.T) {
 
 	// previous 0.x.x
 	// fix with stable
-	newVersion, err := CalculateNewVersion(fixChange, "0.5.0", false)
+	newVersion, prl, err := CalculateNewVersion(fixChange, "0.5.0", false)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "1.0.0")
+	assert.Equal(t, FirstStableLabel, prl)
 
 	// fix with beat
-	newVersion, err = CalculateNewVersion(fixChange, "0.5.0", true)
+	newVersion, prl, err = CalculateNewVersion(fixChange, "0.5.0", true)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "0.5.1")
+	assert.Equal(t, BetaLabel, prl)
 
 	// breaking with stable
-	newVersion, err = CalculateNewVersion(breakingChange, "0.5.0", false)
+	newVersion, prl, err = CalculateNewVersion(breakingChange, "0.5.0", false)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "1.0.0")
+	assert.Equal(t, FirstStableLabel, prl)
 
 	// breaking with beta
-	newVersion, err = CalculateNewVersion(breakingChange, "0.5.0", true)
+	newVersion, prl, err = CalculateNewVersion(breakingChange, "0.5.0", true)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "0.6.0")
+	assert.Equal(t, BetaLabel, prl)
 
 	// additive with stable
-	newVersion, err = CalculateNewVersion(additiveChange, "0.5.0", false)
+	newVersion, prl, err = CalculateNewVersion(additiveChange, "0.5.0", false)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "1.0.0")
+	assert.Equal(t, FirstStableLabel, prl)
 
 	// additive with beta
-	newVersion, err = CalculateNewVersion(additiveChange, "0.5.0", true)
+	newVersion, prl, err = CalculateNewVersion(additiveChange, "0.5.0", true)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "0.6.0")
+	assert.Equal(t, BetaLabel, prl)
 
 	// previous 1.2.0
 	// fix with stable
-	newVersion, err = CalculateNewVersion(fixChange, "1.2.0", false)
+	newVersion, prl, err = CalculateNewVersion(fixChange, "1.2.0", false)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "1.2.1")
+	assert.Equal(t, StableLabel, prl)
 
 	// fix with beat
-	newVersion, err = CalculateNewVersion(fixChange, "1.2.0", true)
+	newVersion, prl, err = CalculateNewVersion(fixChange, "1.2.0", true)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "1.2.1-beta.1")
+	assert.Equal(t, BetaLabel, prl)
 
 	// breaking with stable
-	newVersion, err = CalculateNewVersion(breakingChange, "1.2.0", false)
+	newVersion, prl, err = CalculateNewVersion(breakingChange, "1.2.0", false)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "2.0.0")
+	assert.Equal(t, StableBreakingChangeLabel, prl)
 
 	// breaking with beta
-	newVersion, err = CalculateNewVersion(breakingChange, "1.2.0", true)
+	newVersion, prl, err = CalculateNewVersion(breakingChange, "1.2.0", true)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "2.0.0-beta.1")
+	assert.Equal(t, BetaBreakingChangeLabel, prl)
 
 	// additive with stable
-	newVersion, err = CalculateNewVersion(additiveChange, "1.2.0", false)
+	newVersion, prl, err = CalculateNewVersion(additiveChange, "1.2.0", false)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "1.3.0")
+	assert.Equal(t, StableLabel, prl)
 
 	// additive with beta
-	newVersion, err = CalculateNewVersion(additiveChange, "1.2.0", true)
+	newVersion, prl, err = CalculateNewVersion(additiveChange, "1.2.0", true)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "1.3.0-beta.1")
+	assert.Equal(t, BetaLabel, prl)
 
 	// previous 1.2.0-beta.1
 	// fix with stable
-	newVersion, err = CalculateNewVersion(fixChange, "1.2.0-beta.1", false)
+	newVersion, _, err = CalculateNewVersion(fixChange, "1.2.0-beta.1", false)
 	assert.NotEmpty(t, err)
 
 	// fix with beat
-	newVersion, err = CalculateNewVersion(fixChange, "1.2.0-beta.1", true)
+	newVersion, prl, err = CalculateNewVersion(fixChange, "1.2.0-beta.1", true)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "1.2.0-beta.2")
+	assert.Equal(t, BetaLabel, prl)
 
 	// breaking with stable
-	newVersion, err = CalculateNewVersion(breakingChange, "1.2.0-beta.1", false)
+	newVersion, _, err = CalculateNewVersion(breakingChange, "1.2.0-beta.1", false)
 	assert.NotEmpty(t, err)
 
 	// breaking with beta
-	newVersion, err = CalculateNewVersion(breakingChange, "1.2.0-beta.1", true)
+	newVersion, prl, err = CalculateNewVersion(breakingChange, "1.2.0-beta.1", true)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "1.2.0-beta.2")
+	assert.Equal(t, BetaLabel, prl)
 
 	// additive with stable
-	newVersion, err = CalculateNewVersion(additiveChange, "1.2.0-beta.1", false)
+	newVersion, _, err = CalculateNewVersion(additiveChange, "1.2.0-beta.1", false)
 	assert.NotEmpty(t, err)
 
 	// additive with beta
-	newVersion, err = CalculateNewVersion(additiveChange, "1.2.0-beta.1", true)
+	newVersion, prl, err = CalculateNewVersion(additiveChange, "1.2.0-beta.1", true)
 	assert.NoError(t, err)
 	assert.Equal(t, newVersion.String(), "1.2.0-beta.2")
+	assert.Equal(t, BetaLabel, prl)
 }

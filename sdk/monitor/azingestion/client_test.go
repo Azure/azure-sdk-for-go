7b34b65f312aa44f13cc366dc5311689760d3f04
gracewilcox <graceawilcox@gmail.com>
2023-03-30 17:06:46 -0700

    add custom client

---
+++ sdk/monitor/azingestion/client_test.go
@@ -0,0 +1,30 @@
+//go:build go1.18
+// +build go1.18
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package azingestion_test
+
+import (
+	"testing"
+)
+
+func TestUpload(t *testing.T) {
+	// endpoint := os.Getenv("LOGS_INGESTION_ENDPOINT")
+
+	// credential, err := azidentity.NewDefaultAzureCredential(nil)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// client, err := azingestion.NewClient(endpoint, credential, nil)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// _, err = client.Upload(context.Background(), "ruleID", "stream", "", nil)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+}

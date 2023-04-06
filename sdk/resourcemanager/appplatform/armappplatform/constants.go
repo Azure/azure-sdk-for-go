c7afd96128aa642fdbfed629d9061e2f6fccfeac
Alancere <804873052@qq.com>
2023-04-06 14:14:31 +0800

    fix

--- sdk/resourcemanager/appplatform/armappplatform/constants.go
+++ sdk/resourcemanager/appplatform/armappplatform/constants.go
@@ -11,7 +11,7 @@ package armappplatform
 
 const (
 	moduleName    = "armappplatform"
-	moduleVersion = "v2.0.0"
+	moduleVersion = "v1.1.0"
 )
 
 // ActionType - Enum. Indicates the action type. "Internal" refers to actions that are for internal only APIs.

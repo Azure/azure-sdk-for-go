686845899bbe301b14251c90b727132d97a13b6b
Alancere <804873052@qq.com>
2023-04-13 14:09:14 +0800

    [Release] sdk/resourcemanager/maintenance/armmaintenance/1.1.1 generation from spec commit: 0a5ed101ca3f969cf50d420b5e6717cbdf1edb3c

--- sdk/resourcemanager/maintenance/armmaintenance/models_serde.go
+++ sdk/resourcemanager/maintenance/armmaintenance/models_serde.go
@@ -439,7 +439,7 @@ func (o Operation) MarshalJSON() ([]byte, error) {
 	populate(objectMap, "isDataAction", o.IsDataAction)
 	populate(objectMap, "name", o.Name)
 	populate(objectMap, "origin", o.Origin)
-	populate(objectMap, "properties", &o.Properties)
+	populateAny(objectMap, "properties", o.Properties)
 	return json.Marshal(objectMap)
 }
 
@@ -750,6 +750,16 @@ func populate(m map[string]any, k string, v any) {
 	} else if azcore.IsNullValue(v) {
 		m[k] = nil
 	} else if !reflect.ValueOf(v).IsNil() {
+		m[k] = v
+	}
+}
+
+func populateAny(m map[string]any, k string, v any) {
+	if v == nil {
+		return
+	} else if azcore.IsNullValue(v) {
+		m[k] = nil
+	} else {
 		m[k] = v
 	}
 }

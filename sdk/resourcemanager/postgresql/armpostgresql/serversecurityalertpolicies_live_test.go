8afe106b2fc70919acd263a5db4e0226258ca789
Alancere <804873052@qq.com>
2023-04-23 14:10:08 +0800

    set subscriptionId default value

--- sdk/resourcemanager/postgresql/armpostgresql/serversecurityalertpolicies_live_test.go
+++ sdk/resourcemanager/postgresql/armpostgresql/serversecurityalertpolicies_live_test.go
@@ -42,7 +42,7 @@ func (testsuite *ServerSecurityAlertPoliciesTestSuite) SetupSuite() {
 	testsuite.adminPassword = testutil.GetEnv("ADMIN_PASSWORD", "")
 	testsuite.location = testutil.GetEnv("LOCATION", "westus")
 	testsuite.resourceGroupName = testutil.GetEnv("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
-	testsuite.subscriptionId = testutil.GetEnv("AZURE_SUBSCRIPTION_ID", "00000000-00000000-00000000-00000000")
+	testsuite.subscriptionId = testutil.GetEnv("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
 	resourceGroup, _, err := testutil.CreateResourceGroup(testsuite.ctx, testsuite.subscriptionId, testsuite.cred, testsuite.options, testsuite.location)
 	testsuite.Require().NoError(err)
 	testsuite.resourceGroupName = *resourceGroup.Name

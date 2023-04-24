4c1783e1697edaaf1cc0dcbaeb44b0a9e3f304df
gracewilcox <graceawilcox@gmail.com>
2023-04-24 11:07:27 -0700

    test resources

--- sdk/monitor/azingestion/client_test.go
+++ sdk/monitor/azingestion/client_test.go
@@ -7,24 +7,31 @@
 package azingestion_test
 
 import (
+	"context"
+	"os"
 	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/monitor/azingestion"
 )
 
 func TestUpload(t *testing.T) {
-	// endpoint := os.Getenv("LOGS_INGESTION_ENDPOINT")
+	endpoint := os.Getenv("LOGS_INGESTION_ENDPOINT")
+	ruleID := os.Getenv("DATA_COLLECTION_RULE_ID")
+	var streamName = "<STREAM_NAME>"
 
-	// credential, err := azidentity.NewDefaultAzureCredential(nil)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// client, err := azingestion.NewClient(endpoint, credential, nil)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// _, err = client.Upload(context.Background(), "ruleID", "stream", "", nil)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	credential, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		panic(err)
+	}
+	client, err := azingestion.NewClient(endpoint, credential, nil)
+	if err != nil {
+		panic(err)
+	}
+	letters := []any{"a", "b", "c", "d"}
+	_, err = client.Upload(context.Background(), ruleID, streamName, letters, nil)
+	if err != nil {
+		panic(err)
+	}
 
 }
--- sdk/monitor/azingestion/go.mod
+++ sdk/monitor/azingestion/go.mod
@@ -2,14 +2,13 @@ module github.com/Azure/azure-sdk-for-go/sdk/monitor/azingestion
 
 go 1.18
 
-require (
-	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.4.0
-	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.2.2
-)
+require github.com/Azure/azure-sdk-for-go/sdk/azcore v1.4.0
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.2.2 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.2.0 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v0.9.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
@@ -18,4 +17,5 @@ require (
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )

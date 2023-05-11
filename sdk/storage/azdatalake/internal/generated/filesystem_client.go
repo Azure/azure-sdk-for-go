3efbd78906c9c517ac147a54c2acc7899cea8a5f
Tamer Sherif <tasherif@microsoft.com>
2023-05-11 16:01:58 -0700

    added client files

---
+++ sdk/storage/azdatalake/internal/generated/filesystem_client.go
@@ -0,0 +1,23 @@
+//go:build go1.18
+// +build go1.18
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package generated
+
+import (
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"time"
+)
+
+func (client *FileSystemClient) Endpoint() string {
+	return client.endpoint
+}
+
+func (client *FileSystemClient) Pipeline() runtime.Pipeline {
+	return client.pl
+}
+
+// used to convert times from UTC to GMT before sending across the wire
+var gmt = time.FixedZone("GMT", 0)

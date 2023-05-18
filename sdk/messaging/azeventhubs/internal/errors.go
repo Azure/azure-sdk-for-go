374102217eab220ace2d8528a05f182c9843f1e1
Richard Park <ripark@microsoft.com>
2023-05-17 22:23:56 -0700

    Code coverage dipped too much, noticed a bunch of dead code.

--- sdk/messaging/azeventhubs/internal/errors.go
+++ sdk/messaging/azeventhubs/internal/errors.go
@@ -6,11 +6,9 @@ package internal
 import (
 	"context"
 	"errors"
-	"fmt"
 	"io"
 	"net"
 	"net/http"
-	"reflect"
 	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azeventhubs/internal/amqpwrap"
@@ -209,12 +207,6 @@ func GetRecoveryKind(err error) RecoveryKind {
 		return RecoveryKindConn
 	}
 
-	if IsDrainingError(err) {
-		// temporary, operation should just be retryable since drain will
-		// eventually complete.
-		return RecoveryKindNone
-	}
-
 	// then it's _probably_ an actual *amqp.Error, in which case we bucket it by
 	// the 'condition'.
 	var amqpError *amqp.Error
@@ -257,92 +249,11 @@ func GetRecoveryKind(err error) RecoveryKind {
 	return RecoveryKindConn
 }
 
-type (
-	// ErrMissingField indicates that an expected property was missing from an AMQP message. This should only be
-	// encountered when there is an error with this library, or the server has altered its behavior unexpectedly.
-	ErrMissingField string
-
-	// ErrMalformedMessage indicates that a message was expected in the form of []byte was not a []byte. This is likely
-	// a bug and should be reported.
-	ErrMalformedMessage string
-
-	// ErrIncorrectType indicates that type assertion failed. This should only be encountered when there is an error
-	// with this library, or the server has altered its behavior unexpectedly.
-	ErrIncorrectType struct {
-		Key          string
-		ExpectedType reflect.Type
-		ActualValue  any
-	}
-
-	// ErrAMQP indicates that the server communicated an AMQP error with a particular
-	ErrAMQP amqpwrap.RPCResponse
-
-	// ErrNoMessages is returned when an operation returned no messages. It is not indicative that there will not be
-	// more messages in the future.
-	ErrNoMessages struct{}
-
-	// ErrNotFound is returned when an entity is not found (404)
-	ErrNotFound struct {
-		EntityPath string
-	}
-
-	// ErrConnectionClosed indicates that the connection has been closed.
-	ErrConnectionClosed string
-)
-
-func (e ErrMissingField) Error() string {
-	return fmt.Sprintf("missing value %q", string(e))
-}
-
-func (e ErrMalformedMessage) Error() string {
-	return "message was expected in the form of []byte was not a []byte"
-}
-
-// NewErrIncorrectType lets you skip using the `reflect` package. Just provide a variable of the desired type as
-// 'expected'.
-func NewErrIncorrectType(key string, expected, actual any) ErrIncorrectType {
-	return ErrIncorrectType{
-		Key:          key,
-		ExpectedType: reflect.TypeOf(expected),
-		ActualValue:  actual,
-	}
-}
-
-func (e ErrIncorrectType) Error() string {
-	return fmt.Sprintf(
-		"value at %q was expected to be of type %q but was actually of type %q",
-		e.Key,
-		e.ExpectedType,
-		reflect.TypeOf(e.ActualValue))
-}
-
-func (e ErrAMQP) Error() string {
-	return fmt.Sprintf("server says (%d) %s", e.Code, e.Description)
-}
-
-func (e ErrNoMessages) Error() string {
-	return "no messages available"
-}
-
-func (e ErrNotFound) Error() string {
-	return fmt.Sprintf("entity at %s not found", e.EntityPath)
-}
-
-// IsErrNotFound returns true if the error argument is an ErrNotFound type
-func IsErrNotFound(err error) bool {
-	_, ok := err.(ErrNotFound)
-	return ok
-}
-
 func IsNotAllowedError(err error) bool {
 	var e *amqp.Error
 
 	return errors.As(err, &e) &&
 		e.Condition == amqp.ErrCondNotAllowed
-}
-
-func (e ErrConnectionClosed) Error() string {
-	return fmt.Sprintf("the connection has been closed: %s", string(e))
 }
 
 func IsOwnershipLostError(err error) bool {

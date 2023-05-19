5a603df7d3614de4d330902a0a2f9c76749ac557
Richard Park <ripark@microsoft.com>
2023-05-18 18:29:17 -0700

    Adding in some required files.

---
+++ sdk/samples/azeventhubs/ehpartition/constants.go
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package main
+
+// Version is the semantic version number
+const Version = "v0.0.1"

91345db83bd5ede418560d734a2950c2d79bc22d
SDKAuto <sdkautomation@microsoft.com>
2023-05-31 06:31:04 +0000

    CodeGen from PR 24171 in Azure/azure-rest-api-specs Fix Python misspelling (#24171)

--- sdk/resourcemanager/mixedreality/armmixedreality/CHANGELOG.md
+++ sdk/resourcemanager/mixedreality/armmixedreality/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release History
 
+## 0.6.1 (2023-05-31)
+### Other Changes
+
+
 ## 0.6.0 (2023-03-31)
 ### Features Added
 
--- sdk/resourcemanager/mixedreality/armmixedreality/constants.go
+++ sdk/resourcemanager/mixedreality/armmixedreality/constants.go
@@ -11,7 +11,7 @@ package armmixedreality
 
 const (
 	moduleName    = "armmixedreality"
-	moduleVersion = "v0.6.0"
+	moduleVersion = "v0.6.1"
 )
 
 // CreatedByType - The type of identity that created the resource.

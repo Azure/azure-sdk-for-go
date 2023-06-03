a0711250ab442cf57c50e8c5842ff7b83fc3d8b7
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2023-06-02 18:34:42 -0700

    prefer mockSTS to mock.NewServer

--- sdk/azidentity/azidentity_test.go
+++ sdk/azidentity/azidentity_test.go
@@ -35,10 +35,8 @@ const (
 )
 
 var (
-	accessTokenRespSuccess    = []byte(fmt.Sprintf(`{"access_token": "%s", "expires_in": %d}`, tokenValue, tokenExpiresIn))
-	instanceDiscoveryResponse = getInstanceDiscoveryResponse(fakeTenantID)
-	tenantDiscoveryResponse   = getTenantDiscoveryResponse(fakeTenantID)
-	testTRO                   = policy.TokenRequestOptions{Scopes: []string{liveTestScope}}
+	accessTokenRespSuccess = []byte(fmt.Sprintf(`{"access_token": "%s", "expires_in": %d}`, tokenValue, tokenExpiresIn))
+	testTRO                = policy.TokenRequestOptions{Scopes: []string{liveTestScope}}
 )
 
 // constants for this file
@@ -46,97 +44,8 @@ const (
 	testHost = "https://localhost"
 )
 
-func getInstanceDiscoveryResponse(tenant string) []byte {
-	return []byte(strings.ReplaceAll(`{
-		"tenant_discovery_endpoint": "https://login.microsoftonline.com/{tenant}/v2.0/.well-known/openid-configuration",
-		"api-version": "1.1",
-		"metadata": [
-			{
-				"preferred_network": "login.microsoftonline.com",
-				"preferred_cache": "login.windows.net",
-				"aliases": [
-					"login.microsoftonline.com",
-					"login.windows.net",
-					"login.microsoft.com",
-					"sts.windows.net"
-				]
-			}
-		]
-	}`, "{tenant}", tenant))
-}
-
-func getTenantDiscoveryResponse(tenant string) []byte {
-	return []byte(strings.ReplaceAll(`{
-		"token_endpoint": "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/token",
-		"token_endpoint_auth_methods_supported": [
-			"client_secret_post",
-			"private_key_jwt",
-			"client_secret_basic"
-		],
-		"jwks_uri": "https://login.microsoftonline.com/{tenant}/discovery/v2.0/keys",
-		"response_modes_supported": [
-			"query",
-			"fragment",
-			"form_post"
-		],
-		"subject_types_supported": [
-			"pairwise"
-		],
-		"id_token_signing_alg_values_supported": [
-			"RS256"
-		],
-		"response_types_supported": [
-			"code",
-			"id_token",
-			"code id_token",
-			"id_token token"
-		],
-		"scopes_supported": [
-			"openid",
-			"profile",
-			"email",
-			"offline_access"
-		],
-		"issuer": "https://login.microsoftonline.com/{tenant}/v2.0",
-		"request_uri_parameter_supported": false,
-		"userinfo_endpoint": "https://graph.microsoft.com/oidc/userinfo",
-		"authorization_endpoint": "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize",
-		"device_authorization_endpoint": "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/devicecode",
-		"http_logout_supported": true,
-		"frontchannel_logout_supported": true,
-		"end_session_endpoint": "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/logout",
-		"claims_supported": [
-			"sub",
-			"iss",
-			"cloud_instance_name",
-			"cloud_instance_host_name",
-			"cloud_graph_host_name",
-			"msgraph_host",
-			"aud",
-			"exp",
-			"iat",
-			"auth_time",
-			"acr",
-			"nonce",
-			"preferred_username",
-			"name",
-			"tid",
-			"ver",
-			"at_hash",
-			"c_hash",
-			"email"
-		],
-		"kerberos_endpoint": "https://login.microsoftonline.com/{tenant}/kerberos",
-		"tenant_region_scope": "NA",
-		"cloud_instance_name": "microsoftonline.com",
-		"cloud_graph_host_name": "graph.windows.net",
-		"msgraph_host": "graph.microsoft.com",
-		"rbac_url": "https://pas.windows.net"
-	}`, "{tenant}", tenant))
-}
-
-func validateX5C(t *testing.T, certs []*x509.Certificate) mock.ResponsePredicate {
-	return func(req *http.Request) bool {
+func validateX5C(t *testing.T, certs []*x509.Certificate) func(*http.Request) *http.Response {
+	return func(req *http.Request) *http.Response {
 		err := req.ParseForm()
 		if err != nil {
 			t.Fatal("expected a form body")
@@ -157,7 +66,7 @@ func validateX5C(t *testing.T, certs []*x509.Certificate) mock.ResponsePredicate
 		} else if actual := len(v); actual != len(certs) {
 			t.Fatalf("expected %d certs, got %d", len(certs), actual)
 		}
-		return true
+		return nil
 	}
 }
 

6355ac8fbdd386d54bb799973a5f2ebff18c7ee5
Alancere <804873052@qq.com>
2023-07-20 14:16:19 +0800

    upgrage @autorest/go version

--- eng/scripts/build.ps1
+++ eng/scripts/build.ps1
@@ -11,7 +11,7 @@ param(
     [switch]$alwaysSetBodyParamRequired,
     [switch]$removeUnreferencedTypes,
     [string]$config = "autorest.md",
-    [string]$goExtension = "@autorest/go@4.0.0-preview.48",
+    [string]$goExtension = "@autorest/go@4.0.0-preview.51",
     [string]$filePrefix,
     [string]$outputFolder
 )

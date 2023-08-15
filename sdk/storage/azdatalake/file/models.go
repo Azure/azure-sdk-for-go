00c663b29bb4fee007c12d994ead0789f97552ce
Joel Hendrix <jhendrix@microsoft.com>
2023-08-15 15:51:21 -0700

    Refactor handling of setting file expiry policy
    Switch to using a struct containing explicit fields instead of an
    interface to improve discovery and self-documentation.

--- sdk/storage/azdatalake/file/models.go
+++ sdk/storage/azdatalake/file/models.go
@@ -8,16 +8,14 @@ package file
 
 import (
 	"errors"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"io"
+	"reflect"
+
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blob"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azdatalake/internal/exported"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azdatalake/internal/generated"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azdatalake/internal/path"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azdatalake/internal/shared"
-	"io"
-	"net/http"
-	"strconv"
-	"time"
 )
 
 const (
@@ -42,7 +40,7 @@ type CreateOptions struct {
 	// HTTPHeaders contains the HTTP headers for path operations.
 	HTTPHeaders *HTTPHeaders
 	// Expiry specifies the type and time of expiry for the file.
-	Expiry CreationExpiryType
+	Expiry CreateExpiryValues
 	// LeaseDuration specifies the duration of the lease, in seconds, or negative one
 	// (-1) for a lease that never expires. A non-infinite lease can be
 	// between 15 and 60 seconds.
@@ -61,6 +59,18 @@ type CreateOptions struct {
 	ACL *string
 }
 
+// CreateExpiryValues describes when a newly created file should expire.
+// A zero-value indicates the file has no expiration date.
+type CreateExpiryValues struct {
+	// ExpiresOn contains the time the file should expire.
+	// The value will either be an absolute UTC time in RFC1123 format or an integer expressing a number of milliseconds.
+	// NOTE: when ExpiryType is CreateExpiryTypeNeverExpire, this value is ignored.
+	ExpiresOn string
+
+	// ExpiryType indicates how the value of ExpiresOn should be interpreted (absolute, relative to now, etc).
+	ExpiryType CreateExpiryType
+}
+
 func (o *CreateOptions) format() (*generated.LeaseAccessConditions, *generated.ModifiedAccessConditions, *generated.PathHTTPHeaders, *generated.PathClientCreateOptions, *generated.CPKInfo) {
 	resource := generated.PathResourceTypeFile
 	createOpts := &generated.PathClientCreateOptions{
@@ -70,13 +80,11 @@ func (o *CreateOptions) format() (*generated.LeaseAccessConditions, *generated.M
 		return nil, nil, nil, createOpts, nil
 	}
 	leaseAccessConditions, modifiedAccessConditions := exported.FormatPathAccessConditions(o.AccessConditions)
-	if o.Expiry == nil {
-		createOpts.ExpiryOptions = nil
-		createOpts.ExpiresOn = nil
-	} else {
-		expOpts, expiresOn := o.Expiry.Format()
-		createOpts.ExpiryOptions = (*generated.PathExpiryOptions)(&expOpts)
-		createOpts.ExpiresOn = expiresOn
+	if !reflect.ValueOf(o.Expiry).IsZero() {
+		createOpts.ExpiryOptions = &o.Expiry.ExpiryType
+		if o.Expiry.ExpiryType != CreateExpiryTypeNeverExpire {
+			createOpts.ExpiresOn = &o.Expiry.ExpiresOn
+		}
 	}
 	createOpts.ACL = o.ACL
 	createOpts.Group = o.Group
@@ -491,41 +499,17 @@ func (o *DownloadFileOptions) format() *blob.DownloadFileOptions {
 	return downloadFileOptions
 }
 
-// CreationExpiryType defines values for Create() ExpiryType
-type CreationExpiryType interface {
-	Format() (generated.ExpiryOptions, *string)
-	notPubliclyImplementable()
+// SetExpiryValues describes when a file should expire.
+// A zero-value indicates the file has no expiration date.
+type SetExpiryValues struct {
+	// ExpiresOn contains the time the file should expire.
+	// The value will either be an absolute UTC time in RFC1123 format or an integer expressing a number of milliseconds.
+	// NOTE: when ExpiryType is SetExpiryTypeNeverExpire, this value is ignored.
+	ExpiresOn string
+
+	// ExpiryType indicates how the value of ExpiresOn should be interpreted (absolute, relative to now, etc).
+	ExpiryType SetExpiryType
 }
-
-// CreationExpiryTypeAbsolute defines the absolute time for the blob expiry
-type CreationExpiryTypeAbsolute time.Time
-
-// CreationExpiryTypeRelativeToNow defines the duration relative to now for the blob expiry
-type CreationExpiryTypeRelativeToNow time.Duration
-
-// CreationExpiryTypeNever defines that the blob will be set to never expire
-type CreationExpiryTypeNever struct {
-	// empty struct since NeverExpire expiry type does not require expiry time
-}
-
-func (e CreationExpiryTypeAbsolute) Format() (generated.ExpiryOptions, *string) {
-	return generated.ExpiryOptionsAbsolute, to.Ptr(time.Time(e).UTC().Format(http.TimeFormat))
-
-}
-
-func (e CreationExpiryTypeAbsolute) notPubliclyImplementable() {}
-
-func (e CreationExpiryTypeRelativeToNow) Format() (generated.ExpiryOptions, *string) {
-	return generated.ExpiryOptionsRelativeToNow, to.Ptr(strconv.FormatInt(time.Duration(e).Milliseconds(), 10))
-}
-
-func (e CreationExpiryTypeRelativeToNow) notPubliclyImplementable() {}
-
-func (e CreationExpiryTypeNever) Format() (generated.ExpiryOptions, *string) {
-	return generated.ExpiryOptionsNeverExpire, nil
-}
-
-func (e CreationExpiryTypeNever) notPubliclyImplementable() {}
 
 // ACLFailedEntry contains the failed ACL entry (response model).
 type ACLFailedEntry = path.ACLFailedEntry
@@ -533,23 +517,10 @@ type ACLFailedEntry = path.ACLFailedEntry
 // SetAccessControlRecursiveResponse contains part of the response data returned by the []OP_AccessControl operations.
 type SetAccessControlRecursiveResponse = generated.SetAccessControlRecursiveResponse
 
-// SetExpiryType defines values for ExpiryType.
-type SetExpiryType = exported.SetExpiryType
-
-// SetExpiryTypeAbsolute defines the absolute time for the expiry.
-type SetExpiryTypeAbsolute = exported.SetExpiryTypeAbsolute
-
-// SetExpiryTypeRelativeToNow defines the duration relative to now for the expiry.
-type SetExpiryTypeRelativeToNow = exported.SetExpiryTypeRelativeToNow
-
-// SetExpiryTypeRelativeToCreation defines the duration relative to creation for the expiry.
-type SetExpiryTypeRelativeToCreation = exported.SetExpiryTypeRelativeToCreation
-
-// SetExpiryTypeNever defines that will be set to never expire.
-type SetExpiryTypeNever = exported.SetExpiryTypeNever
-
 // SetExpiryOptions contains the optional parameters for the Client.SetExpiry method.
-type SetExpiryOptions = exported.SetExpiryOptions
+type SetExpiryOptions struct {
+	// placeholder for future options
+}
 
 type HTTPRange = exported.HTTPRange
 

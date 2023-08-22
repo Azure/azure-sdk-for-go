5b7dd699be099641dae3653a5edab4a8cfb88664
Alancere <804873052@qq.com>
2023-08-22 11:45:35 +0800

    fix fake

--- sdk/resourcemanager/containerservice/armcontainerservice/fake/managedclustersnapshots_server.go
+++ sdk/resourcemanager/containerservice/armcontainerservice/fake/managedclustersnapshots_server.go
@@ -16,7 +16,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/fake/server"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice/v4"
 	"net/http"
 	"net/url"
 	"regexp"

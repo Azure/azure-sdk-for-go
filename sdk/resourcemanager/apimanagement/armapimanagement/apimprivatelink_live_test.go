8eeea659ed1360f87cd3d8e6bee9fd83314f5612
Alancere <804873052@qq.com>
2023-08-24 10:40:28 +0800

    update armapimanagement live test

--- sdk/resourcemanager/apimanagement/armapimanagement/apimprivatelink_live_test.go
+++ sdk/resourcemanager/apimanagement/armapimanagement/apimprivatelink_live_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/apimanagement/armapimanagement"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/apimanagement/armapimanagement/v2"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal/testutil"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	"github.com/stretchr/testify/suite"

3b06a713d03ff788ab1f5d39f093837fd3ceab11
magodo <wztdyl@sina.com>
2023-08-28 13:27:32 +0800

    license

--- sdk/azcore/runtime/transport_default_dialer_other.go
+++ sdk/azcore/runtime/transport_default_dialer_other.go
@@ -1,5 +1,8 @@
 //go:build !wasm
 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package runtime
 
 import (
--- sdk/azcore/runtime/transport_default_dialer_wasm.go
+++ sdk/azcore/runtime/transport_default_dialer_wasm.go
@@ -1,5 +1,8 @@
 //go:build (js && wasm) || wasip1
 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package runtime
 
 import (

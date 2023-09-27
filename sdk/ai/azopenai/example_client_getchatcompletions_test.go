41fec1f4ce4b7fb4b656b078318388c9e8f831d9
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2023-09-27 23:46:11 +0000

    The contract is still under design, but the gist is that you should be careful to check if value are initialized because content can sometimes carry only content filtering annotations, etc...

--- sdk/ai/azopenai/example_client_getchatcompletions_test.go
+++ sdk/ai/azopenai/example_client_getchatcompletions_test.go
@@ -79,8 +79,26 @@ func ExampleClient_GetChatCompletions() {
 	for _, choice := range resp.Choices {
 		gotReply = true
 
+		if choice.ContentFilterResults != nil {
+			fmt.Fprintf(os.Stderr, "Content filter results\n")
+
+			if choice.ContentFilterResults.Error != nil {
+				fmt.Fprintf(os.Stderr, "  Error:%v\n", choice.ContentFilterResults.Error)
+			}
+
+			fmt.Fprintf(os.Stderr, "  Hate: sev: %v, filtered: %v\n", *choice.ContentFilterResults.Hate.Severity, *choice.ContentFilterResults.Hate.Filtered)
+			fmt.Fprintf(os.Stderr, "  SelfHarm: sev: %v, filtered: %v\n", *choice.ContentFilterResults.SelfHarm.Severity, *choice.ContentFilterResults.SelfHarm.Filtered)
+			fmt.Fprintf(os.Stderr, "  Sexual: sev: %v, filtered: %v\n", *choice.ContentFilterResults.Sexual.Severity, *choice.ContentFilterResults.Sexual.Filtered)
+			fmt.Fprintf(os.Stderr, "  Violence: sev: %v, filtered: %v\n", *choice.ContentFilterResults.Violence.Severity, *choice.ContentFilterResults.Violence.Filtered)
+		}
+
 		if choice.Message != nil && choice.Message.Content != nil {
 			fmt.Fprintf(os.Stderr, "Content[%d]: %s\n", *choice.Index, *choice.Message.Content)
+		}
+
+		if choice.FinishReason != nil {
+			// this choice's conversation is complete.
+			fmt.Fprintf(os.Stderr, "Finish reason[%d]: %s\n", *choice.Index, *choice.FinishReason)
 		}
 	}
 

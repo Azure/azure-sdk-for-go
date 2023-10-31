3c893779c81d2d787aa9c9f2bd1f76f366ee928a
Matthew Solomon <89044647+MBSolomon@users.noreply.github.com>
2023-10-31 13:16:13 -0700

    Add Non Retriable Error Wrapper for all SDK's (#21845)
    * add non retriable error logic to sdk/internal folder path so it can be consumed by all sdk resources
    
    * move to errorinfo
    
    * rm shared files
    
    * modify errorinfo_tests
    
    * doc comments and tests
    
    * NonRetriable switch

---
+++ sdk/internal/errorinfo/errorinfo_test.go
@@ -0,0 +1,29 @@
+//go:build go1.18
+// +build go1.18
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package errorinfo
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestNonRetriableError(t *testing.T) {
+	const dnr string = "Do Not Retry"
+
+	// Create sample error.
+	err := NonRetriableError(errors.New(dnr))
+
+	// Check error message is correct
+	if err.Error() != dnr {
+		t.Fatalf("Expected error message to be '%q' but got '%q'.", dnr, err.Error())
+	}
+
+	var e NonRetriable
+	if !errors.As(err, &e) {
+		t.Fatalf("Expected error to be of type NonRetriable")
+	}
+}

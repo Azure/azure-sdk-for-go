d22fe8ad93534e57f5a915c49364672cd5de8fd0
Peng Jiahui <46921893+Alancere@users.noreply.github.com>
2023-11-13 16:58:06 +0800

    fix Replace fake module (#21961)

--- eng/tools/generator/cmd/v2/common/generation.go
+++ eng/tools/generator/cmd/v2/common/generation.go
@@ -311,7 +311,7 @@ func (ctx *GenerateContext) GenerateForSingleRPNamespace(generateParam *Generate
 			return nil, err
 		}
 
-		if changelog.HasBreakingChanges() && isGenerateFake(packagePath) {
+		if _, err := os.Stat(filepath.Join(packagePath, "fake")); !os.IsNotExist(err) && changelog.HasBreakingChanges() {
 			log.Printf("Replace fake module v2+...")
 			if err = replaceModuleImport(packagePath, generateParam.RPName, generateParam.NamespaceName, previousVersion, version.String(),
 				"fake", "_server.go"); err != nil {

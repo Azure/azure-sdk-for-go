fc81a2707390931c7907c179d84f6b5c583d827f
Alancere <804873052@qq.com>
2023-11-14 15:38:50 +0800

    regenerate

--- sdk/resourcemanager/paloaltonetworksngfw/armpanngfw/fake/operations_server.go
+++ sdk/resourcemanager/paloaltonetworksngfw/armpanngfw/fake/operations_server.go
@@ -15,7 +15,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/fake/server"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/paloaltonetworksngfw/armpanngfw"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/paloaltonetworksngfw/armpanngfw/v2"
 	"net/http"
 )
 

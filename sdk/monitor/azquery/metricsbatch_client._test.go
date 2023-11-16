da17d0322e93008c701c02238bf59ae78df898c8
gracewilcox <graceawilcox@gmail.com>
2023-11-16 09:22:48 -0800

    add constructor

---
+++ sdk/monitor/azquery/metricsbatch_client._test.go
@@ -0,0 +1,14 @@
+//go:build go1.18
+// +build go1.18
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package azquery_test
+
+import (
+	"testing"
+)
+
+func TestQueryBatch_Metrics(t *testing.T) {
+}

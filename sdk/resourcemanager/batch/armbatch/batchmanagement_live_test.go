94eaae47da45f4cb5ce044fd05ce79269f6e0492
Alancere <804873052@qq.com>
2023-11-21 16:13:44 +0800

    fix

--- sdk/resourcemanager/batch/armbatch/batchmanagement_live_test.go
+++ sdk/resourcemanager/batch/armbatch/batchmanagement_live_test.go
@@ -49,9 +49,9 @@ func (testsuite *BatchManagementTestSuite) SetupSuite() {
 	testsuite.poolName, _ = recording.GenerateAlphaNumericID(testsuite.T(), "poolname", 14, false)
 	testsuite.storageAccountName, _ = recording.GenerateAlphaNumericID(testsuite.T(), "storagea", 14, true)
 	testsuite.versionName, _ = recording.GenerateAlphaNumericID(testsuite.T(), "versionn", 14, false)
-	testsuite.location = testutil.GetEnv("LOCATION", "westus")
-	testsuite.resourceGroupName = testutil.GetEnv("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
-	testsuite.subscriptionId = testutil.GetEnv("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
+	testsuite.location = recording.GetEnvVariable("LOCATION", "westus")
+	testsuite.resourceGroupName = recording.GetEnvVariable("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
+	testsuite.subscriptionId = recording.GetEnvVariable("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
 	resourceGroup, _, err := testutil.CreateResourceGroup(testsuite.ctx, testsuite.subscriptionId, testsuite.cred, testsuite.options, testsuite.location)
 	testsuite.Require().NoError(err)
 	testsuite.resourceGroupName = *resourceGroup.Name

95a1fce371990240f25c0709a6b4aa0cf0452469
Alancere <804873052@qq.com>
2023-11-22 15:11:15 +0800

    fix

--- sdk/resourcemanager/mediaservices/armmediaservices/streamingservice_live_test.go
+++ sdk/resourcemanager/mediaservices/armmediaservices/streamingservice_live_test.go
@@ -52,9 +52,9 @@ func (testsuite *StreamingserviceTestSuite) SetupSuite() {
 	testsuite.liveOutputName, _ = recording.GenerateAlphaNumericID(testsuite.T(), "liveoutp", 14, false)
 	testsuite.storageAccountName, _ = recording.GenerateAlphaNumericID(testsuite.T(), "storageaccount", 20, true)
 	testsuite.streamingEndpointName, _ = recording.GenerateAlphaNumericID(testsuite.T(), "streamin", 14, false)
-	testsuite.location = testutil.GetEnv("LOCATION", "westus")
-	testsuite.resourceGroupName = testutil.GetEnv("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
-	testsuite.subscriptionId = testutil.GetEnv("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
+	testsuite.location = recording.GetEnvVariable("LOCATION", "westus")
+	testsuite.resourceGroupName = recording.GetEnvVariable("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
+	testsuite.subscriptionId = recording.GetEnvVariable("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
 	resourceGroup, _, err := testutil.CreateResourceGroup(testsuite.ctx, testsuite.subscriptionId, testsuite.cred, testsuite.options, testsuite.location)
 	testsuite.Require().NoError(err)
 	testsuite.resourceGroupName = *resourceGroup.Name

822f0d0a88b9d4b15976bf4223ab0e490ab1c287
Alancere <804873052@qq.com>
2023-11-22 15:51:40 +0800

    [Release] sdk/resourcemanager/storagesync/armstoragesync/1.2.0 generation from spec commit: 164375e67a1bffb207bcf603772c289dbe42d7b5

--- sdk/resourcemanager/storagesync/armstoragesync/microsoftstoragesync_client_example_test.go
+++ sdk/resourcemanager/storagesync/armstoragesync/microsoftstoragesync_client_example_test.go
@@ -37,9 +37,9 @@ func ExampleMicrosoftStorageSyncClient_LocationOperationStatus() {
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
 	// res.LocationOperationStatus = armstoragesync.LocationOperationStatus{
 	// 	Name: to.Ptr("eyJwYXJ0aXRpb25JZCI6ImE1ZDNiMDU4LTYwN2MtNDI0Ny05Y2FmLWJlZmU4NGQ0ZDU0NyIsIndvcmtmbG93SWQiOiJjYzg1MTY2YS0xMjI2LTQ4MGYtYWM5ZC1jMmRhNTVmY2M2ODYiLCJ3b3JrZmxvd09wZXJhdGlvbklkIjoiOTdmODU5ZTAtOGY1MC00ZTg4LWJkZDEtNWZlYzgwYTVlYzM0tui="),
-	// 	EndTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-09-14T19:59:30.8055279Z"); return t}()),
+	// 	EndTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-09-14T19:59:30.805Z"); return t}()),
 	// 	ID: to.Ptr("/subscriptions/52b8da2f-61e0-4a1f-8dde-336911f367fb/providers/microsoft.storagesync/locations/westus/operations/eyJwYXJ0aXRpb25JZCI6ImE1ZDNiMDU4LTYwN2MtNDI0Ny05Y2FmLWJlZmU4NGQ0ZDU0NyIsIndvcmtmbG93SWQiOiJjYzg1MTY2YS0xMjI2LTQ4MGYtYWM5ZC1jMmRhNTVmY2M2ODYiLCJ3b3JrZmxvd09wZXJhdGlvbklkIjoiOTdmODU5ZTAtOGY1MC00ZTg4LWJkZDEtNWZlYzgwYTVlYzM0tui="),
-	// 	StartTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-09-14T19:59:24.6247688Z"); return t}()),
+	// 	StartTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2021-09-14T19:59:24.624Z"); return t}()),
 	// 	Status: to.Ptr("Succeeded"),
 	// }
 }

0aaa669f1df7bd23411cef6c377531851491827e
Alancere <804873052@qq.com>
2023-11-27 14:18:39 +0800

    armappplatform assets

--- sdk/resourcemanager/appplatform/armappplatform/configservices_live_test.go
+++ sdk/resourcemanager/appplatform/armappplatform/configservices_live_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/internal/recording"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appplatform/armappplatform"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appplatform/armappplatform/v2"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal/v2/testutil"
 	"github.com/stretchr/testify/suite"
 )

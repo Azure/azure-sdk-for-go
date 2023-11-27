7c9f16e18f87993487b65fe24b59f84ddd722f16
Alancere <804873052@qq.com>
2023-11-27 13:58:30 +0800

    [Release] sdk/resourcemanager/mysql/armmysql/1.3.0 generation from spec commit: 739f4f5d1a66adc8c556e17e2ab4360aeaf546e7

--- sdk/resourcemanager/mysql/armmysql/constants.go
+++ sdk/resourcemanager/mysql/armmysql/constants.go
@@ -10,7 +10,7 @@ package armmysql
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mysql/armmysql"
-	moduleVersion = "v1.2.0"
+	moduleVersion = "v1.3.0"
 )
 
 // CreateMode - The mode to create a new server.

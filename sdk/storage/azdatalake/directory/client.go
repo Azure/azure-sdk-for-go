bfc3f622cd9adbcfcb443bfeb1346ad8de2892c6
tanyasethi-msft <124860586+tanyasethi-msft@users.noreply.github.com>
2023-12-28 15:18:17 +0530

    create new sub directory (#22177)
    * Implement NewSubdirectoryClient
    
    * Lowercase d in subDirectoryName
    
    Co-authored-by: Gauri Prasad <51212198+gapra-msft@users.noreply.github.com>
    
    * Make test recorded and add API calls
    
    * Extend testing
    
    * Add recorded tests
    
    * Update recorded test
    
    * doc
    
    * Update assets.json
    
    * create subdirectory client
    
    * changelog
    
    * revert to unrecorded test
    
    ---------
    
    Co-authored-by: Adele Reed <adreed@microsoft.com>
    Co-authored-by: adreed-msft <49764384+adreed-msft@users.noreply.github.com>
    Co-authored-by: Gauri Prasad <51212198+gapra-msft@users.noreply.github.com>

--- sdk/storage/azdatalake/directory/client.go
+++ sdk/storage/azdatalake/directory/client.go
@@ -228,6 +228,29 @@ func (d *Client) NewFileClient(fileName string) (*file.Client, error) {
 	return (*file.Client)(base.NewPathClient(fileURL, newBlobURL, newBlobClient, d.generatedDirClientWithDFS().InternalClient().WithClientName(shared.FileClient), d.sharedKey(), d.identityCredential(), d.getClientOptions())), nil
 }
 
+// NewSubdirectoryClient creates a new directory.Client object by concatenating subdirectoryName to the end of this Client's URL.
+// The new directory.Client uses the same request policy pipeline as the Client.
+func (d *Client) NewSubdirectoryClient(subdirectoryName string) (*Client, error) {
+	subdirectoryName = url.PathEscape(strings.TrimRight(subdirectoryName, "/"))
+	subDirectoryURL := runtime.JoinPaths(d.DFSURL(), subdirectoryName)
+	newBlobURL, subDirectoryURL := shared.GetURLs(subDirectoryURL)
+	var newBlobClient *blockblob.Client
+	clientOptions := &blockblob.ClientOptions{ClientOptions: d.getClientOptions().ClientOptions}
+	var err error
+	if d.identityCredential() != nil {
+		newBlobClient, err = blockblob.NewClient(newBlobURL, *d.identityCredential(), clientOptions)
+	} else if d.sharedKey() != nil {
+		blobSharedKey, _ := exported.ConvertToBlobSharedKey(d.sharedKey())
+		newBlobClient, err = blockblob.NewClientWithSharedKeyCredential(newBlobURL, blobSharedKey, clientOptions)
+	} else {
+		newBlobClient, err = blockblob.NewClientWithNoCredential(newBlobURL, clientOptions)
+	}
+	if err != nil {
+		return nil, exported.ConvertToDFSError(err)
+	}
+	return (*Client)(base.NewPathClient(subDirectoryURL, newBlobURL, newBlobClient, d.generatedDirClientWithDFS().InternalClient().WithClientName(shared.DirectoryClient), d.sharedKey(), d.identityCredential(), d.getClientOptions())), nil
+}
+
 // Create creates a new directory.
 func (d *Client) Create(ctx context.Context, options *CreateOptions) (CreateResponse, error) {
 	lac, mac, httpHeaders, createOpts, cpkOpts := options.format()

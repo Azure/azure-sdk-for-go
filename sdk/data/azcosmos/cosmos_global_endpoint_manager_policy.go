7b6cd4f24e2b58d25351439ccb15c8620d2e12fe
simorenoh <simonmorenohe@gmail.com>
2024-01-09 16:01:59 -0500

    refactor go routine for CI pipeline lint

--- sdk/data/azcosmos/cosmos_global_endpoint_manager_policy.go
+++ sdk/data/azcosmos/cosmos_global_endpoint_manager_policy.go
@@ -5,7 +5,6 @@ package azcosmos
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
@@ -18,9 +17,9 @@ type globalEndpointManagerPolicy struct {
 func (p *globalEndpointManagerPolicy) Do(req *policy.Request) (*http.Response, error) {
 	shouldRefresh := p.gem.ShouldRefresh()
 	if shouldRefresh {
-		fmt.Println("should refresh true go routine")
-		go p.gem.Update(context.Background())
+		go func() {
+			_ = p.gem.Update(context.Background())
+		}()
 	}
-	fmt.Println("policy done")
 	return req.Next()
 }

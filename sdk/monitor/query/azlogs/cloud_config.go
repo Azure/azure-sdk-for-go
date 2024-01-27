d488ec8ba317191e4e217bdedca837517a6d1423
gracewilcox <graceawilcox@gmail.com>
2024-01-26 16:27:28 -0800

    move code

---
+++ sdk/monitor/query/azlogs/cloud_config.go
@@ -0,0 +1,29 @@
+//go:build go1.18
+// +build go1.18
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package azlogs
+
+import "github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
+
+// Cloud Service Names for Monitor Query Logs and Metrics, used to identify the respective cloud.ServiceConfiguration
+const (
+	ServiceNameLogs cloud.ServiceName = "query/azlogs"
+)
+
+func init() {
+	cloud.AzureChina.Services[ServiceNameLogs] = cloud.ServiceConfiguration{
+		Audience: "https://api.loganalytics.azure.cn",
+		Endpoint: "https://api.loganalytics.azure.cn/v1",
+	}
+	cloud.AzureGovernment.Services[ServiceNameLogs] = cloud.ServiceConfiguration{
+		Audience: "https://api.loganalytics.us",
+		Endpoint: "https://api.loganalytics.us/v1",
+	}
+	cloud.AzurePublic.Services[ServiceNameLogs] = cloud.ServiceConfiguration{
+		Audience: "https://api.loganalytics.io",
+		Endpoint: "https://api.loganalytics.io/v1",
+	}
+}

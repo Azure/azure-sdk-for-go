49f15b4efd3e6b09c673bccf421d118bab071199
Peng Jiahui <804873052@qq.com>
2024-02-05 16:36:10 +0800

    update

--- eng/scripts/Ignore-Mgmt-REAMDE-Link.ps1
+++ eng/scripts/Ignore-Mgmt-REAMDE-Link.ps1
@@ -1,10 +1,12 @@
 param (
+    [ValidateNotNullOrEmpty()]
     [string]$serviePath,
+    [ValidateNotNullOrEmpty()]
     [string]$outputFile
 )
 
 if ($serviePath -notlike "*sdk/resourcemanager*" -or $serviePath -like "*sdk/resourcemanager/internal*") {
-    return
+    exit
 }
 
 if (-not (Test-Path $serviePath/README.md)) {
@@ -17,13 +19,19 @@ if (-not (Test-Path $outputFile)) {
     exit
 }
 
-$lines = Get-Content -Path $serviePath/README.md
+try {
+    $lines = Get-Content -Path $serviePath/README.md
 
-$pattern = "(https://pkg\.go\.dev/github\.com/Azure/azure-sdk-for-go/$serviePath[a-zA-Z0-9_.\-/]*)"
-$matchPkg = $lines | Where-Object { $_ -match $pattern }
-
-foreach ($m in $matchPkg) {
-    $link = [regex]::Match($m, $pattern).Groups[1].Value
-    Write-Host "Ignore Mgmt README.md Link: $link"
-    $link | Out-File -FilePath $outputFile -Append
+    $pattern = "(https://pkg\.go\.dev/github\.com/Azure/azure-sdk-for-go/$serviePath[a-zA-Z0-9_.\-/]*)"
+    $matchPkg = $lines | Where-Object { $_ -match $pattern }
+    
+    foreach ($m in $matchPkg) {
+        $link = [regex]::Match($m, $pattern).Groups[1].Value
+        Write-Host "Ignore Mgmt README.md Link: $link"
+        $link | Out-File -FilePath $outputFile -Append
+    }
+}
+catch {
+    Write-Host "Ignore-Mgmt-README-Link failed with exception:`n$_"
+    exit 1
 }

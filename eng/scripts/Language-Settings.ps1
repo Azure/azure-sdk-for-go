cda1b1b713b8dd46044fa05660cff2a24c0bc930
Praveen Kuttappan <prmarott@microsoft.com>
2024-03-18 14:11:27 -0400

    Ensure APIView artifact filter returns array of artifacts

--- eng/scripts/Language-Settings.ps1
+++ eng/scripts/Language-Settings.ps1
@@ -134,7 +134,7 @@ function SetPackageVersion ($PackageName, $Version, $ReleaseDate, $PackageProper
 
 function Find-Go-Artifacts-For-Apireview($ArtifactPath, $PackageName)
 {
-  $artifact = Get-ChildItem -Path (Join-Path $ArtifactPath $PackageName) -Filter "*.gosource"
+  $artifact = @(Get-ChildItem -Path (Join-Path $ArtifactPath $PackageName) -Filter "*.gosource")
   if ($artifact)
   {
     $packages = @{

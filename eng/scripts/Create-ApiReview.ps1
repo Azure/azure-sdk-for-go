521481ca095419c28f016c2655168f7c434cd163
Praveen Kuttappan <prmarott@microsoft.com>
2024-03-18 16:05:32 -0400

    Updates to use common script to generate API review

--- eng/scripts/Create-ApiReview.ps1
+++ eng/scripts/Create-ApiReview.ps1
@@ -4,17 +4,16 @@ Param(
     [Parameter(Mandatory=$True)]
     [string] $OutPath,
     [Parameter(Mandatory=$True)]
-    [string] $ApiviewUri,
-    [Parameter(Mandatory=$True)]
     [string] $ApiKey,
-    [Parameter(Mandatory=$True)]
-    [string] $ApiLabel,
     [Parameter(Mandatory=$True)]
     [string] $SourceBranch,
     [Parameter(Mandatory=$True)]
     [string] $DefaultBranch,
     [Parameter(Mandatory=$True)]
-    [string] $ConfigFileDir
+    [string] $ConfigFileDir,
+    [string] $RepoName,
+    [string] $BuildId,
+    [bool] $MarkPackageAsShipped = $false
 )
 
 
@@ -22,6 +21,7 @@ Write-Host "$PSScriptRoot"
 . (Join-Path $PSScriptRoot .. common scripts common.ps1)
 $createReviewScript = (Join-Path $PSScriptRoot .. common scripts Create-APIReview.ps1)
 
+$artifactList = @()
 foreach ($sdk in (Get-AllPackageInfoFromRepo $ServiceDirectory))
 {
     Write-Host "Creating API review artifact for $($sdk.Name)"
@@ -30,6 +30,10 @@ foreach ($sdk in (Get-AllPackageInfoFromRepo $ServiceDirectory))
     Compress-Archive -Path $sdk.DirectoryPath -DestinationPath $outPath/$($sdk.Name)/$fileName -force
     Rename-Item $outPath/$($sdk.Name)/$fileName.zip -NewName "$fileName.gosource"
 
-    Write-Host "Send request to APIView to create review for $($sdk.Name)"
-    &($createReviewScript) -ArtifactPath $outPath -APIViewUri $ApiviewUri -APIKey $ApiKey -APILabel $ApiLabel -PackageName $sdk.Name -SourceBranch $SourceBranch -DefaultBranch $DefaultBranch -ConfigFileDir $ConfigFileDir
+    $artifactList += [PSCustomObject]@{
+        name = $sdk.Name
+    }
 }
+
+Write-Host "Create Go APIView using generated artifacts"
+&($createReviewScript) -ArtifactList $artifactList -ArtifactPath $outPath -APIKey $ApiKey -SourceBranch $SourceBranch -DefaultBranch $DefaultBranch -ConfigFileDir $ConfigFileDir -RepoName $RepoName -BuildId $BuildId -MarkPackageAsShipped $MarkPackageAsShipped

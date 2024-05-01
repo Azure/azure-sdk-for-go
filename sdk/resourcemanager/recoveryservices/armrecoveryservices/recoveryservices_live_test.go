b5e744e860ab875b361f523a1362f30d93da6019
ReleaseHelper <ReleaseHelper>
2024-05-01 01:10:52 +0000

    [Release] sdk/resourcemanager/recoveryservices/armrecoveryservices/2.0.0 generation from spec commit: 33c4457b1d13f83965f4fe3367dca4a6df898100

--- sdk/resourcemanager/recoveryservices/armrecoveryservices/recoveryservices_live_test.go
+++ sdk/resourcemanager/recoveryservices/armrecoveryservices/recoveryservices_live_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/internal/recording"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal/v2/testutil"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/recoveryservices/armrecoveryservices"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/recoveryservices/armrecoveryservices/v2"
 	"github.com/stretchr/testify/suite"
 )
 

82dbbb9f1c9b7a60875961c349bf557107f070cd
Joel Hendrix <jhendrix@microsoft.com>
2024-05-07 13:45:14 -0700

    refine

--- sdk/tracing/azotel/examples_test.go
+++ sdk/tracing/azotel/examples_test.go
@@ -21,24 +21,25 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
-func Example_jaegerExporter() {
+func Example_otlpExporter() {
 	// end-to-end example creating an OTel TracerProvider that exports to Jaeger
-	// then uses the azotel adapter to connect it to an Azure SDK client.
+	// via OTLP then uses the azotel adapter to connect it to an Azure SDK client.
 
 	ctx := context.Background()
 
-	// create the Jaeger exporter
-	exp, err := otlptracehttp.New(ctx)
+	// create the OTLP exporter, using the local Jaeger OTLP endpoint
+	// NOTE: this assumes you have a local instance of Jaeger running on the same machine
+	exp, err := otlptracehttp.New(ctx, otlptracehttp.WithEndpointURL("http://localhost:4318/v1/traces"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// create an OTel TracerProvider that uses the Jaeger exporter
+	// create an OTel TracerProvider that uses the OTLP exporter
 	otelTP := otelsdk.NewTracerProvider(
 		otelsdk.WithBatcher(exp),
 		otelsdk.WithResource(resource.NewWithAttributes(
 			semconv.SchemaURL,
-			semconv.ServiceNameKey.String("Example_jaegerExporter"),
+			semconv.ServiceNameKey.String("Example_otlpExporter"),
 		)),
 	)
 
@@ -64,7 +65,7 @@ func Example_jaegerExporter() {
 		log.Fatal(err)
 	}
 
-	// shut down the tracing provider to flush all spans to Jaeger
+	// shut down the tracing provider to flush all spans to OTLP
 	if err = otelTP.Shutdown(context.TODO()); err != nil {
 		log.Fatal(err)
 	}

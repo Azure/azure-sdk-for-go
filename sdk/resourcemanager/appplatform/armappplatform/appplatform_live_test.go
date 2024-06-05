4d7422f57e0e612a16160b2ca0ac075fc62fcfa8
Alancere <804873052@qq.com>
2024-06-05 15:57:26 +0800

    remove t.Skip

--- sdk/resourcemanager/appplatform/armappplatform/appplatform_live_test.go
+++ sdk/resourcemanager/appplatform/armappplatform/appplatform_live_test.go
@@ -61,9 +61,6 @@ func (testsuite *AppplatformTestSuite) TearDownSuite() {
 }
 
 func TestAppplatformTestSuite(t *testing.T) {
-	// if recording.GetRecordMode() == recording.PlaybackMode {
-	// 	t.Skip("https://github.com/Azure/azure-sdk-for-go/issues/22869")
-	// }
 	suite.Run(t, new(AppplatformTestSuite))
 }
 

eaf2ffc55a006cb8aaa449c20d4e5f53c16bff0d
Alancere <804873052@qq.com>
2024-06-05 17:00:00 +0800

    sdk/resourcemanager/internal/v3

--- sdk/resourcemanager/elasticsan/armelasticsan/elasticsan_live_test.go
+++ sdk/resourcemanager/elasticsan/armelasticsan/elasticsan_live_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/internal/recording"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/elasticsan/armelasticsan"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal/v2/testutil"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal/v3/testutil"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -40,7 +40,7 @@ type ElasticsanTestSuite struct {
 }
 
 func (testsuite *ElasticsanTestSuite) SetupSuite() {
-	testutil.StartRecording(testsuite.T(), "sdk/resourcemanager/elasticsan/armelasticsan/testdata")
+	testutil.StartRecording(testsuite.T(), pathToPackage)
 
 	testsuite.ctx = context.Background()
 	testsuite.cred, testsuite.options = testutil.GetCredAndClientOptions(testsuite.T())

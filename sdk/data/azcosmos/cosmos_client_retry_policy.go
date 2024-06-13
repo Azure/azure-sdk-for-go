5301c964805df6e74a315d1074471d690e40e567
Matias Quaranta <ealsur@hotmail.com>
2024-06-13 08:36:37 -0700

    Using it in policy

--- sdk/data/azcosmos/cosmos_client_retry_policy.go
+++ sdk/data/azcosmos/cosmos_client_retry_policy.go
@@ -16,30 +16,25 @@ import (
 )
 
 type clientRetryPolicy struct {
-	gem                    *globalEndpointManager
-	useWriteEndpoint       bool
-	retryCount             int
-	sessionRetryCount      int
-	preferredLocationIndex int
+	gem *globalEndpointManager
 }
 
 const maxRetryCount = 120
 const defaultBackoff = 1
 
 func (p *clientRetryPolicy) Do(req *policy.Request) (*http.Response, error) {
-	p.resetPolicyCounters()
 	o := pipelineRequestOptions{}
 	if !req.OperationValue(&o) {
 		return nil, fmt.Errorf("failed to obtain request options, please check request being sent: %s", req.Body())
 	}
 	for {
-		resolvedEndpoint := p.gem.ResolveServiceEndpoint(p.retryCount, o.isWriteOperation, p.useWriteEndpoint)
+		resolvedEndpoint := p.gem.ResolveServiceEndpoint(o.retryContext.retryCount, o.isWriteOperation, o.retryContext.useWriteEndpoint)
 		req.Raw().Host = resolvedEndpoint.Host
 		req.Raw().URL.Host = resolvedEndpoint.Host
 		response, err := req.Next() // err can happen in weird scenarios (connectivity, etc)
 		if err != nil {
 			if p.isNetworkConnectionError(err) {
-				shouldRetry, errRetry := p.attemptRetryOnNetworkError(req)
+				shouldRetry, errRetry := p.attemptRetryOnNetworkError(req, &o.retryContext)
 				if errRetry != nil {
 					return nil, errRetry
 				}
@@ -50,16 +45,16 @@ func (p *clientRetryPolicy) Do(req *policy.Request) (*http.Response, error) {
 				if err != nil {
 					return nil, err
 				}
-				p.retryCount += 1
+				o.retryContext.retryCount += 1
 				continue
 			}
 			return nil, err
 		}
 		subStatus := response.Header.Get(cosmosHeaderSubstatus)
 		if p.shouldRetryStatus(response.StatusCode, subStatus) {
-			p.useWriteEndpoint = false
+			o.retryContext.useWriteEndpoint = false
 			if response.StatusCode == http.StatusForbidden {
-				shouldRetry, err := p.attemptRetryOnEndpointFailure(req, o.isWriteOperation)
+				shouldRetry, err := p.attemptRetryOnEndpointFailure(req, o.isWriteOperation, &o.retryContext)
 				if err != nil {
 					return nil, err
 				}
@@ -67,11 +62,11 @@ func (p *clientRetryPolicy) Do(req *policy.Request) (*http.Response, error) {
 					return nil, errorinfo.NonRetriableError(azruntime.NewResponseErrorWithErrorCode(response, response.Status))
 				}
 			} else if response.StatusCode == http.StatusNotFound {
-				if !p.attemptRetryOnSessionUnavailable(req, o.isWriteOperation) {
+				if !p.attemptRetryOnSessionUnavailable(req, o.isWriteOperation, &o.retryContext) {
 					return nil, errorinfo.NonRetriableError(azruntime.NewResponseErrorWithErrorCode(response, response.Status))
 				}
 			} else if response.StatusCode == http.StatusServiceUnavailable {
-				if !p.attemptRetryOnServiceUnavailable(req, o.isWriteOperation) {
+				if !p.attemptRetryOnServiceUnavailable(req, o.isWriteOperation, &o.retryContext) {
 					return nil, errorinfo.NonRetriableError(azruntime.NewResponseErrorWithErrorCode(response, response.Status))
 				}
 			}
@@ -79,7 +74,7 @@ func (p *clientRetryPolicy) Do(req *policy.Request) (*http.Response, error) {
 			if err != nil {
 				return response, err
 			}
-			p.retryCount += 1
+			o.retryContext.retryCount += 1
 			continue
 		}
 
@@ -97,8 +92,8 @@ func (p *clientRetryPolicy) shouldRetryStatus(status int, subStatus string) (sho
 	return false
 }
 
-func (p *clientRetryPolicy) attemptRetryOnNetworkError(req *policy.Request) (bool, error) {
-	if (p.retryCount > maxRetryCount) || !p.gem.locationCache.enableCrossRegionRetries {
+func (p *clientRetryPolicy) attemptRetryOnNetworkError(req *policy.Request, retryContext *retryContext) (bool, error) {
+	if (retryContext.retryCount > maxRetryCount) || !p.gem.locationCache.enableCrossRegionRetries {
 		return false, nil
 	}
 
@@ -119,8 +114,8 @@ func (p *clientRetryPolicy) attemptRetryOnNetworkError(req *policy.Request) (boo
 	return true, nil
 }
 
-func (p *clientRetryPolicy) attemptRetryOnEndpointFailure(req *policy.Request, isWriteOperation bool) (bool, error) {
-	if (p.retryCount > maxRetryCount) || !p.gem.locationCache.enableCrossRegionRetries {
+func (p *clientRetryPolicy) attemptRetryOnEndpointFailure(req *policy.Request, isWriteOperation bool, retryContext *retryContext) (bool, error) {
+	if (retryContext.retryCount > maxRetryCount) || !p.gem.locationCache.enableCrossRegionRetries {
 		return false, nil
 	}
 	if isWriteOperation {
@@ -144,40 +139,34 @@ func (p *clientRetryPolicy) attemptRetryOnEndpointFailure(req *policy.Request, i
 	return true, nil
 }
 
-func (p *clientRetryPolicy) attemptRetryOnSessionUnavailable(req *policy.Request, isWriteOperation bool) bool {
+func (p *clientRetryPolicy) attemptRetryOnSessionUnavailable(req *policy.Request, isWriteOperation bool, retryContext *retryContext) bool {
 	if p.gem.CanUseMultipleWriteLocations() {
 		endpoints := p.gem.locationCache.locationInfo.availReadLocations
 		if isWriteOperation {
 			endpoints = p.gem.locationCache.locationInfo.availWriteLocations
 		}
-		if p.sessionRetryCount >= len(endpoints) {
+		if retryContext.sessionRetryCount >= len(endpoints) {
 			return false
 		}
 	} else {
-		if p.sessionRetryCount > 0 {
+		if retryContext.sessionRetryCount > 0 {
 			return false
 		}
-		p.useWriteEndpoint = true
+		retryContext.useWriteEndpoint = true
 	}
-	p.sessionRetryCount += 1
+	retryContext.sessionRetryCount += 1
 	return true
 }
 
-func (p *clientRetryPolicy) attemptRetryOnServiceUnavailable(req *policy.Request, isWriteOperation bool) bool {
-	if !p.gem.locationCache.enableCrossRegionRetries || p.preferredLocationIndex >= len(p.gem.preferredLocations) {
+func (p *clientRetryPolicy) attemptRetryOnServiceUnavailable(req *policy.Request, isWriteOperation bool, retryContext *retryContext) bool {
+	if !p.gem.locationCache.enableCrossRegionRetries || retryContext.preferredLocationIndex >= len(p.gem.preferredLocations) {
 		return false
 	}
 	if isWriteOperation && !p.gem.CanUseMultipleWriteLocations() {
 		return false
 	}
-	p.preferredLocationIndex += 1
+	retryContext.preferredLocationIndex += 1
 	return true
-}
-
-func (p *clientRetryPolicy) resetPolicyCounters() {
-	p.retryCount = 0
-	p.sessionRetryCount = 0
-	p.preferredLocationIndex = 0
 }
 
 // isNetworkConnectionError checks if the error is related to failure to connect / resolve DNS

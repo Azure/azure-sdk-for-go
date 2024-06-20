b883e4ad8ca64e0eb9317c64ab9555e4ad1697d2
Alancere <804873052@qq.com>
2024-06-20 17:59:41 +0800

    sync GoEmitterOptions

--- eng/tools/generator/typespec/typespecgo/option.go
+++ eng/tools/generator/typespec/typespecgo/option.go
@@ -5,14 +5,17 @@ https://github.com/Azure/autorest.go/blob/main/packages/typespec-go/src/lib.ts#G
 @azure-tools/typespec-go option
 */
 type GoEmitterOptions struct {
-	AzcoreVersion         string `yaml:"azcore-version"`
-	DisallowUnknownFields bool   `yaml:"disallow-unknown-fields"`
-	FilePrefix            string `yaml:"file-prefix"`
-	GenerateFake          bool   `yaml:"generate-fakes"`
-	InjectSpanc           bool   `yaml:"inject-spans"`
-	Module                string `yaml:"module"`
-	ModuleVersion         string `yaml:"module-version"`
-	RawJsonAsBytes        bool   `yaml:"rawjson-as-bytes"`
-	SliceElementsByVal    bool   `yaml:"slice-elements-byval"`
-	SingleClient          bool   `yaml:"single-client"`
+	AzcoreVersion           string `yaml:"azcore-version"`
+	DisallowUnknownFields   bool   `yaml:"disallow-unknown-fields"`
+	FilePrefix              string `yaml:"file-prefix"`
+	GenerateFake            bool   `yaml:"generate-fakes"`
+	InjectSpanc             bool   `yaml:"inject-spans"`
+	Module                  string `yaml:"module"`
+	ModuleVersion           string `yaml:"module-version"`
+	RawJsonAsBytes          bool   `yaml:"rawjson-as-bytes"`
+	SliceElementsByVal      bool   `yaml:"slice-elements-byval"`
+	SingleClient            bool   `yaml:"single-client"`
+	Stutter                 string `yaml:"stutter"`
+	FixConstStuttering      bool   `yaml:"fix-const-stuttering"`
+	RemoveUnreferencedTypes bool   `yaml:"remove-unreferenced-types"`
 }

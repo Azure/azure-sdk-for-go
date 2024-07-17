f4fea3ec1a4be59caf232814ce58473093fadc91
Alancere <804873052@qq.com>
2024-07-17 14:45:21 +0800

    fix

--- eng/scripts/automation_init.sh
+++ eng/scripts/automation_init.sh
@@ -18,7 +18,7 @@ echo "output json file: $outputFile"
 TMPDIR="/tmp"
 if [ ! "$(go version | awk '{print $3}' | cut -c 3-6)" = "1.22" ]
 then
-  wget https://golang.org/dl/go1.22.0.linux-amd64.tar.gz
+  wget -q https://golang.org/dl/go1.22.0.linux-amd64.tar.gz
   tar -C $TMPDIR -xzf go1.22.0.linux-amd64.tar.gz
   export GOROOT=$TMPDIR/go
   export PATH=$GOROOT/bin:$PATH

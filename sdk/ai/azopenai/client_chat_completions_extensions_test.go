07eaf8f0da68726eb5d9a5872f64f1ddf0423755
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2024-09-25 13:15:32 -0700

    [azopenai] Fixing issue where content filtering being non-deterministic is making tests fail. (#23490)
    * Fixing issue where content filtering being non-deterministic is making our tests flaky.
    * Allowing throttling in another oversubscribed test.

--- sdk/ai/azopenai/client_chat_completions_extensions_test.go
+++ sdk/ai/azopenai/client_chat_completions_extensions_test.go
@@ -30,7 +30,7 @@ func TestChatCompletions_extensions_bringYourOwnData(t *testing.T) {
 		},
 		DeploymentName: &azureOpenAI.ChatCompletionsOYD.Model,
 	}, nil)
-	require.NoError(t, err)
+	customRequireNoError(t, err, true)
 	require.NotEmpty(t, resp)
 
 	msgContext := resp.Choices[0].Message.Context
@@ -54,7 +54,7 @@ func TestChatExtensionsStreaming_extensions_bringYourOwnData(t *testing.T) {
 		DeploymentName: &azureOpenAI.ChatCompletionsOYD.Model,
 	}, nil)
 
-	require.NoError(t, err)
+	customRequireNoError(t, err, true)
 	defer streamResp.ChatCompletionsStream.Close()
 
 	text := ""

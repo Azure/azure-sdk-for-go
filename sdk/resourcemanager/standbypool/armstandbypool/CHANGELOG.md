97b9f93ff52e27ec73a720d6d95027dbeaa14e23
Chenjie Shi <tadelesh.shi@live.cn>
2024-09-27 10:21:42 +0800

    [Release] sdk/resourcemanager/standbypool/armstandbypool/1.0.0 (#23508)
    * [Release] sdk/resourcemanager/standbypool/armstandbypool/1.0.0 generation from spec commit: 3ca7edabc2edf1117b7d912ba34f2694721e3ff3
    
    * update example
    
    * format

--- sdk/resourcemanager/standbypool/armstandbypool/CHANGELOG.md
+++ sdk/resourcemanager/standbypool/armstandbypool/CHANGELOG.md
@@ -1,5 +1,38 @@
 # Release History
 
+## 1.0.0 (2024-09-26)
+### Breaking Changes
+
+- Type of `StandbyContainerGroupPoolResourceUpdateProperties.ContainerGroupProperties` has been changed from `*ContainerGroupPropertiesUpdate` to `*ContainerGroupProperties`
+- Type of `StandbyContainerGroupPoolResourceUpdateProperties.ElasticityProfile` has been changed from `*StandbyContainerGroupPoolElasticityProfileUpdate` to `*StandbyContainerGroupPoolElasticityProfile`
+- Type of `StandbyVirtualMachinePoolResourceUpdateProperties.ElasticityProfile` has been changed from `*StandbyVirtualMachinePoolElasticityProfileUpdate` to `*StandbyVirtualMachinePoolElasticityProfile`
+- Struct `ContainerGroupProfileUpdate` has been removed
+- Struct `ContainerGroupPropertiesUpdate` has been removed
+- Struct `StandbyContainerGroupPoolElasticityProfileUpdate` has been removed
+- Struct `StandbyVirtualMachinePoolElasticityProfileUpdate` has been removed
+
+### Features Added
+
+- New function `*ClientFactory.NewStandbyContainerGroupPoolRuntimeViewsClient() *StandbyContainerGroupPoolRuntimeViewsClient`
+- New function `*ClientFactory.NewStandbyVirtualMachinePoolRuntimeViewsClient() *StandbyVirtualMachinePoolRuntimeViewsClient`
+- New function `NewStandbyContainerGroupPoolRuntimeViewsClient(string, azcore.TokenCredential, *arm.ClientOptions) (*StandbyContainerGroupPoolRuntimeViewsClient, error)`
+- New function `*StandbyContainerGroupPoolRuntimeViewsClient.Get(context.Context, string, string, string, *StandbyContainerGroupPoolRuntimeViewsClientGetOptions) (StandbyContainerGroupPoolRuntimeViewsClientGetResponse, error)`
+- New function `*StandbyContainerGroupPoolRuntimeViewsClient.NewListByStandbyPoolPager(string, string, *StandbyContainerGroupPoolRuntimeViewsClientListByStandbyPoolOptions) *runtime.Pager[StandbyContainerGroupPoolRuntimeViewsClientListByStandbyPoolResponse]`
+- New function `NewStandbyVirtualMachinePoolRuntimeViewsClient(string, azcore.TokenCredential, *arm.ClientOptions) (*StandbyVirtualMachinePoolRuntimeViewsClient, error)`
+- New function `*StandbyVirtualMachinePoolRuntimeViewsClient.Get(context.Context, string, string, string, *StandbyVirtualMachinePoolRuntimeViewsClientGetOptions) (StandbyVirtualMachinePoolRuntimeViewsClientGetResponse, error)`
+- New function `*StandbyVirtualMachinePoolRuntimeViewsClient.NewListByStandbyPoolPager(string, string, *StandbyVirtualMachinePoolRuntimeViewsClientListByStandbyPoolOptions) *runtime.Pager[StandbyVirtualMachinePoolRuntimeViewsClientListByStandbyPoolResponse]`
+- New struct `ContainerGroupInstanceCountSummary`
+- New struct `PoolResourceStateCount`
+- New struct `StandbyContainerGroupPoolRuntimeViewResource`
+- New struct `StandbyContainerGroupPoolRuntimeViewResourceListResult`
+- New struct `StandbyContainerGroupPoolRuntimeViewResourceProperties`
+- New struct `StandbyVirtualMachinePoolRuntimeViewResource`
+- New struct `StandbyVirtualMachinePoolRuntimeViewResourceListResult`
+- New struct `StandbyVirtualMachinePoolRuntimeViewResourceProperties`
+- New struct `VirtualMachineInstanceCountSummary`
+- New field `MinReadyCapacity` in struct `StandbyVirtualMachinePoolElasticityProfile`
+
+
 ## 0.1.0 (2024-04-26)
 
 The package of `github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/standbypool/armstandbypool` is using our [next generation design principles](https://azure.github.io/azure-sdk/general_introduction.html).
--- sdk/resourcemanager/standbypool/armstandbypool/standbycontainergrouppoolruntimeviews_client_example_test.go
+++ sdk/resourcemanager/standbypool/armstandbypool/standbycontainergrouppoolruntimeviews_client_example_test.go
@@ -11,8 +11,8 @@ import (
 	"log"
 )
 
-// Generated from example definition: 2024-03-01/StandbyVirtualMachines_Get.json
-func ExampleStandbyVirtualMachinesClient_Get() {
+// Generated from example definition: 2024-03-01/StandbyContainerGroupPoolRuntimeViews_Get.json
+func ExampleStandbyContainerGroupPoolRuntimeViewsClient_Get() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
@@ -22,22 +22,39 @@ func ExampleStandbyVirtualMachinesClient_Get() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := clientFactory.NewStandbyVirtualMachinesClient().Get(ctx, "rgstandbypool", "pool", "virtualMachine", nil)
+	res, err := clientFactory.NewStandbyContainerGroupPoolRuntimeViewsClient().Get(ctx, "rgstandbypool", "pool", "latest", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
 	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
-	// res = armstandbypool.StandbyVirtualMachinesClientGetResponse{
-	// 	StandbyVirtualMachineResource: &armstandbypool.StandbyVirtualMachineResource{
-	// 		Properties: &armstandbypool.StandbyVirtualMachineResourceProperties{
-	// 			VirtualMachineResourceID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000009/resourceGroups/rgstandbypool/provider/Microsoft.Compute/virtualMachines/virtualMachine"),
+	// res = armstandbypool.StandbyContainerGroupPoolRuntimeViewsClientGetResponse{
+	// 	StandbyContainerGroupPoolRuntimeViewResource: &armstandbypool.StandbyContainerGroupPoolRuntimeViewResource{
+	// 		Properties: &armstandbypool.StandbyContainerGroupPoolRuntimeViewResourceProperties{
+	// 			InstanceCountSummary: []*armstandbypool.ContainerGroupInstanceCountSummary{
+	// 				{
+	// 					InstanceCountsByState: []*armstandbypool.PoolResourceStateCount{
+	// 						{
+	// 							State: to.Ptr("creating"),
+	// 							Count: to.Ptr[int64](100),
+	// 						},
+	// 						{
+	// 							State: to.Ptr("running"),
+	// 							Count: to.Ptr[int64](500),
+	// 						},
+	// 						{
+	// 							State: to.Ptr("deleting"),
+	// 							Count: to.Ptr[int64](20),
+	// 						},
+	// 					},
+	// 				},
+	// 			},
 	// 			ProvisioningState: to.Ptr(armstandbypool.ProvisioningStateSucceeded),
 	// 		},
-	// 		ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000009/resourceGroups/rgstandbypool/providers/Microsoft.StandbyPool/standbyVirtualMachinePools/pool/standbyVirtualMachines/virtualMachine"),
+	// 		ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000009/resourceGroups/rgstandbypool/providers/Microsoft.StandbyPool/standbyContainerGroupPools/pool/runtimeViews/latest"),
 	// 		Name: to.Ptr("pool"),
-	// 		Type: to.Ptr("Microsoft.StandbyPool/standbyVirtualMachinePools/virtualMachines"),
+	// 		Type: to.Ptr("Microsoft.StandbyPool/standbyContainerGroupPools/runtimeViews"),
 	// 		SystemData: &armstandbypool.SystemData{
 	// 			CreatedBy: to.Ptr("pooluser@microsoft.com"),
 	// 			CreatedByType: to.Ptr(armstandbypool.CreatedByTypeUser),
@@ -50,8 +67,8 @@ func ExampleStandbyVirtualMachinesClient_Get() {
 	// }
 }
 
-// Generated from example definition: 2024-03-01/StandbyVirtualMachines_ListByStandbyVirtualMachinePoolResource.json
-func ExampleStandbyVirtualMachinesClient_NewListByStandbyVirtualMachinePoolResourcePager() {
+// Generated from example definition: 2024-03-01/StandbyContainerGroupPoolRuntimeViews_ListByStandbyPool.json
+func ExampleStandbyContainerGroupPoolRuntimeViewsClient_NewListByStandbyPoolPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
@@ -61,7 +78,7 @@ func ExampleStandbyVirtualMachinesClient_NewListByStandbyVirtualMachinePoolResou
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	pager := clientFactory.NewStandbyVirtualMachinesClient().NewListByStandbyVirtualMachinePoolResourcePager("rgstandbypool", "pool", nil)
+	pager := clientFactory.NewStandbyContainerGroupPoolRuntimeViewsClient().NewListByStandbyPoolPager("rgstandbypool", "pool", nil)
 	for pager.More() {
 		page, err := pager.NextPage(ctx)
 		if err != nil {
@@ -72,17 +89,34 @@ func ExampleStandbyVirtualMachinesClient_NewListByStandbyVirtualMachinePoolResou
 			_ = v
 		}
 		// If the HTTP response code is 200 as defined in example definition, your page structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
-		// page = armstandbypool.StandbyVirtualMachinesClientListByStandbyVirtualMachinePoolResourceResponse{
-		// 	StandbyVirtualMachineResourceListResult: armstandbypool.StandbyVirtualMachineResourceListResult{
-		// 		Value: []*armstandbypool.StandbyVirtualMachineResource{
+		// page = armstandbypool.StandbyContainerGroupPoolRuntimeViewsClientListByStandbyPoolResponse{
+		// 	StandbyContainerGroupPoolRuntimeViewResourceListResult: armstandbypool.StandbyContainerGroupPoolRuntimeViewResourceListResult{
+		// 		Value: []*armstandbypool.StandbyContainerGroupPoolRuntimeViewResource{
 		// 			{
-		// 				Properties: &armstandbypool.StandbyVirtualMachineResourceProperties{
-		// 					VirtualMachineResourceID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000009/resourceGroups/rgstandbypool/provider/Microsoft.Compute/virtualMachines/virtualMachine"),
+		// 				Properties: &armstandbypool.StandbyContainerGroupPoolRuntimeViewResourceProperties{
+		// 					InstanceCountSummary: []*armstandbypool.ContainerGroupInstanceCountSummary{
+		// 						{
+		// 							InstanceCountsByState: []*armstandbypool.PoolResourceStateCount{
+		// 								{
+		// 									State: to.Ptr("creating"),
+		// 									Count: to.Ptr[int64](100),
+		// 								},
+		// 								{
+		// 									State: to.Ptr("running"),
+		// 									Count: to.Ptr[int64](500),
+		// 								},
+		// 								{
+		// 									State: to.Ptr("deleting"),
+		// 									Count: to.Ptr[int64](20),
+		// 								},
+		// 							},
+		// 						},
+		// 					},
 		// 					ProvisioningState: to.Ptr(armstandbypool.ProvisioningStateSucceeded),
 		// 				},
-		// 				ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000009/resourceGroups/rgstandbypool/providers/Microsoft.StandbyPool/standbyVirtualMachinePools/pool/standbyVirtualMachines/virtualMachine"),
+		// 				ID: to.Ptr("/subscriptions/00000000-0000-0000-0000-000000000009/resourceGroups/rgstandbypool/providers/Microsoft.StandbyPool/standbyContainerGroupPools/pool/runtimeViews/latest"),
 		// 				Name: to.Ptr("pool"),
-		// 				Type: to.Ptr("Microsoft.StandbyPool/standbyVirtualMachinePools/virtualMachines"),
+		// 				Type: to.Ptr("Microsoft.StandbyPool/standbyContainerGroupPools/runtimeViews"),
 		// 				SystemData: &armstandbypool.SystemData{
 		// 					CreatedBy: to.Ptr("pooluser@microsoft.com"),
 		// 					CreatedByType: to.Ptr(armstandbypool.CreatedByTypeUser),

0c60dc8411aa69b127122f96967d726996001649
Chenjie Shi <tadelesh.shi@live.cn>
2024-09-27 10:20:10 +0800

    change filtering words (#23507)

--- eng/tools/generator/cmd/v2/common/cmdProcessor.go
+++ eng/tools/generator/cmd/v2/common/cmdProcessor.go
@@ -243,7 +243,7 @@ func ExecuteTspClient(path string, args ...string) error {
 
 			// filter diagnostic errors
 			if len(newErrMsgs) >= 1 &&
-				newErrMsgs[0] == "Diagnostics were reported during compilation. Use the `--debug` flag to see the diagnostic output." {
+				newErrMsgs[0] == "Diagnostics were reported during compilation. Use the `--debug` flag to see if there is warning diagnostic output." {
 				newErrMsgs = newErrMsgs[1:]
 
 				errDiags := getErrorDiagnostics(strings.Split(stdoutBuffer.String(), "\n"))

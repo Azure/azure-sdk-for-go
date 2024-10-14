7fabfc1e8dd118a531dd74e4db25d2fdb616cf05
gunjansingh-msft <gunjansingh@microsoft.com>
2024-10-14 20:49:59 +0530

    fixing azfile live tests

--- sdk/storage/azfile/directory/client_test.go
+++ sdk/storage/azfile/directory/client_test.go
@@ -2365,5 +2365,5 @@ func (d *DirectoryRecordedTestsSuite) TestDirectoryAudienceNegative() {
 
 	_, err = dirClientAudience.Create(context.Background(), nil)
 	_require.Error(err)
-	testcommon.ValidateFileErrorCode(_require, err, fileerror.AuthenticationFailed)
+	testcommon.ValidateFileErrorCode(_require, err, fileerror.InvalidAuthenticationInfo)
 }

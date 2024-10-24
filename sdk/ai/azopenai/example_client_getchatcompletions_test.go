5e2c0fbbbb5c806bbadf6fc9caea5be5c712e150
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2024-10-24 10:59:07 -0700

    [azopenai] Adding in a structured output example, using functions and response (#23653)

--- sdk/ai/azopenai/example_client_getchatcompletions_test.go
+++ sdk/ai/azopenai/example_client_getchatcompletions_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/ai/azopenai"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 )
 
 func ExampleClient_GetChatCompletions() {
@@ -398,4 +399,268 @@ func ExampleClient_GetChatCompletionsStream() {
 	}
 
 	// Output:
+}
+
+func ExampleClient_GetChatCompletions_structuredOutputs() {
+	// This example is a Go implementation of the example from this article:
+	// https://openai.com/index/introducing-structured-outputs-in-the-api/
+	modelDeploymentID := os.Getenv("AOAI_CHAT_COMPLETIONS_STRUCTURED_OUTPUTS_MODEL")
+
+	// Ex: "https://<your-azure-openai-host>.openai.azure.com"
+	azureOpenAIEndpoint := os.Getenv("AOAI_CHAT_COMPLETIONS_STRUCTURED_OUTPUTS_ENDPOINT")
+
+	if modelDeploymentID == "" || azureOpenAIEndpoint == "" {
+		fmt.Fprintf(os.Stderr, "Skipping example, environment variables missing\n")
+		return
+	}
+
+	tokenCredential, err := azidentity.NewDefaultAzureCredential(nil)
+
+	if err != nil {
+		//  TODO: Update the following line with your application specific error handling logic
+		log.Printf("ERROR: %s", err)
+		return
+	}
+
+	client, err := azopenai.NewClient(azureOpenAIEndpoint, tokenCredential, nil)
+
+	if err != nil {
+		// TODO: Update the following line with your application specific error handling logic
+		log.Printf("ERROR: %s", err)
+		return
+	}
+
+	structuredJSONToolDefParams := []byte(`{
+		"type": "object",
+		"properties": {
+			"table_name": {
+				"type": "string",
+				"enum": [
+					"orders"
+				]
+			},
+			"columns": {
+				"type": "array",
+				"items": {
+					"type": "string",
+					"enum": [
+						"id",
+						"status",
+						"expected_delivery_date",
+						"delivered_at",
+						"shipped_at",
+						"ordered_at",
+						"canceled_at"
+					]
+				}
+			},
+			"conditions": {
+				"type": "array",
+				"items": {
+					"type": "object",
+					"properties": {
+						"column": {
+							"type": "string"
+						},
+						"operator": {
+							"type": "string",
+							"enum": [
+								"=",
+								">",
+								"<",
+								">=",
+								"<=",
+								"!="
+							]
+						},
+						"value": {
+							"anyOf": [
+								{
+									"type": "string"
+								},
+								{
+									"type": "number"
+								},
+								{
+									"type": "object",
+									"properties": {
+										"column_name": {
+											"type": "string"
+										}
+									},
+									"required": [
+										"column_name"
+									],
+									"additionalProperties": false
+								}
+							]
+						}
+					},
+					"required": [
+						"column",
+						"operator",
+						"value"
+					],
+					"additionalProperties": false
+				}
+			},
+			"order_by": {
+				"type": "string",
+				"enum": [
+					"asc",
+					"desc"
+				]
+			}
+		},
+		"required": [
+			"table_name",
+			"columns",
+			"conditions",
+			"order_by"
+		],
+		"additionalProperties": false
+	}`)
+
+	resp, err := client.GetChatCompletions(context.TODO(), azopenai.ChatCompletionsOptions{
+		DeploymentName: &modelDeploymentID,
+		Messages: []azopenai.ChatRequestMessageClassification{
+			&azopenai.ChatRequestAssistantMessage{
+				Content: azopenai.NewChatRequestAssistantMessageContent("You are a helpful assistant. The current date is August 6, 2024. You help users query for the data they are looking for by calling the query function."),
+			},
+			&azopenai.ChatRequestUserMessage{
+				Content: azopenai.NewChatRequestUserMessageContent("look up all my orders in may of last year that were fulfilled but not delivered on time"),
+			},
+		},
+		Tools: []azopenai.ChatCompletionsToolDefinitionClassification{
+			&azopenai.ChatCompletionsFunctionToolDefinition{
+				Function: &azopenai.ChatCompletionsFunctionToolDefinitionFunction{
+					Strict:     to.Ptr(true),
+					Name:       to.Ptr("query"),
+					Parameters: structuredJSONToolDefParams,
+				},
+			},
+		},
+	}, nil)
+
+	if err != nil {
+		// TODO: Update the following line with your application specific error handling logic
+		log.Printf("ERROR: %s", err)
+		return
+	}
+
+	// and now the results should contain proper JSON for the arguments, every time.
+	fn := resp.Choices[0].Message.ToolCalls[0].(*azopenai.ChatCompletionsFunctionToolCall).Function
+
+	argumentsObj := map[string]any{}
+
+	err = json.Unmarshal([]byte(*fn.Arguments), &argumentsObj)
+
+	if err != nil {
+		//  TODO: Update the following line with your application specific error handling logic
+		log.Printf("ERROR: %s", err)
+		return
+	}
+
+	fmt.Printf("%#v\n", argumentsObj)
+}
+
+func ExampleClient_GetChatCompletions_structuredOutputs_responseFormat() {
+	// This example is a Go implementation of the example from this article:
+	// https://openai.com/index/introducing-structured-outputs-in-the-api/
+	modelDeploymentID := os.Getenv("AOAI_CHAT_COMPLETIONS_STRUCTURED_OUTPUTS_MODEL")
+
+	// Ex: "https://<your-azure-openai-host>.openai.azure.com"
+	azureOpenAIEndpoint := os.Getenv("AOAI_CHAT_COMPLETIONS_STRUCTURED_OUTPUTS_ENDPOINT")
+
+	if modelDeploymentID == "" || azureOpenAIEndpoint == "" {
+		fmt.Fprintf(os.Stderr, "Skipping example, environment variables missing\n")
+		return
+	}
+
+	// In Azure OpenAI you must deploy a model before you can use it in your client. For more information
+	// see here: https://learn.microsoft.com/azure/cognitive-services/openai/how-to/create-resource
+	tokenCredential, err := azidentity.NewDefaultAzureCredential(nil)
+
+	if err != nil {
+		//  TODO: Update the following line with your application specific error handling logic
+		log.Printf("ERROR: %s", err)
+		return
+	}
+
+	client, err := azopenai.NewClient(azureOpenAIEndpoint, tokenCredential, nil)
+
+	if err != nil {
+		// TODO: Update the following line with your application specific error handling logic
+		log.Printf("ERROR: %s", err)
+		return
+	}
+
+	structuredOutputJSONSchema := []byte(`{
+	"type": "object",
+	"properties": {
+		"steps": {
+			"type": "array",
+			"items": {
+				"type": "object",
+				"properties": {
+					"explanation": {
+						"type": "string"
+					},
+					"output": {
+						"type": "string"
+					}
+				},
+				"required": [
+					"explanation",
+					"output"
+				],
+				"additionalProperties": false
+			}
+		},
+		"final_answer": {
+			"type": "string"
+		}
+	},
+	"required": [
+		"steps",
+		"final_answer"
+	],
+	"additionalProperties": false
+}`)
+
+	resp, err := client.GetChatCompletions(context.TODO(), azopenai.ChatCompletionsOptions{
+		DeploymentName: &modelDeploymentID,
+		Messages: []azopenai.ChatRequestMessageClassification{
+			&azopenai.ChatRequestAssistantMessage{
+				Content: azopenai.NewChatRequestAssistantMessageContent("You are a helpful math tutor."),
+			},
+			&azopenai.ChatRequestUserMessage{
+				Content: azopenai.NewChatRequestUserMessageContent("solve 8x + 31 = 2"),
+			},
+		},
+		ResponseFormat: &azopenai.ChatCompletionsJSONSchemaResponseFormat{
+			JSONSchema: &azopenai.ChatCompletionsJSONSchemaResponseFormatJSONSchema{
+				Name:   to.Ptr("math_response"),
+				Strict: to.Ptr(true),
+				Schema: structuredOutputJSONSchema,
+			},
+		},
+	}, nil)
+
+	if err != nil {
+		// TODO: Update the following line with your application specific error handling logic
+		log.Printf("ERROR: %s", err)
+		return
+	}
+
+	responseObj := map[string]any{}
+	err = json.Unmarshal([]byte(*resp.Choices[0].Message.Content), &responseObj)
+
+	if err != nil {
+		//  TODO: Update the following line with your application specific error handling logic
+		log.Printf("ERROR: %s", err)
+		return
+	}
+
+	fmt.Printf("%#v", responseObj)
 }

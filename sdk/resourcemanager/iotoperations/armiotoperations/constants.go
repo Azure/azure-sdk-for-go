a74d5134ff63a9b54e5838c61111611db331b33e
tadelesh <tadelesh.shi@live.cn>
2024-10-24 22:06:36 +0800

    change version

--- sdk/resourcemanager/iotoperations/armiotoperations/constants.go
+++ sdk/resourcemanager/iotoperations/armiotoperations/constants.go
@@ -6,7 +6,7 @@ package armiotoperations
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/iotoperations/armiotoperations"
-	moduleVersion = "v1.0.0"
+	moduleVersion = "v0.1.0"
 )
 
 // ActionType - Extensible enum. Indicates the action type. "Internal" refers to actions that are for internal only APIs.

6c9f6572204f9c48495e2c2fd436cc9020daec66
Piotr Michalak <pimi@boyum-it.com>
2024-10-28 10:24:47 +0100

    Remove deserialization tests

--- sdk/data/azcosmos/partition_key_definition_test.go
+++ sdk/data/azcosmos/partition_key_definition_test.go
@@ -4,7 +4,6 @@
 package azcosmos
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -53,93 +52,5 @@ func TestPartitionKeyDefinitionSerialization(t *testing.T) {
 	expected = `{"kind":"MultiHash","paths":["somePath"],"version":2}`
 	if string(jsonString) != expected {
 		t.Errorf("Expected serialization %v, but got %v", expected, string(jsonString))
-	}
-}
-
-func TestPartitionKeyDefinitionDeserialization(t *testing.T) {
-	def := PartitionKeyDefinition{
-		Paths:   []string{"somePath"},
-		Version: 2,
-	}
-
-	jsonString, err := def.MarshalJSON()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var otherDef PartitionKeyDefinition
-	err = otherDef.UnmarshalJSON(jsonString)
-
-	if err != nil {
-		t.Fatal(err, string(jsonString))
-	}
-
-	// Kind is inferred based on the number of paths
-	if otherDef.Kind != PartitionKeyKindHash {
-		t.Errorf("Expected Kind to be %v, but got %v", def.Kind, otherDef.Kind)
-	}
-
-	if !reflect.DeepEqual(def.Paths, otherDef.Paths) {
-		t.Errorf("Expected Paths to be %v, but got %v", def.Paths, otherDef.Paths)
-	}
-
-	if def.Version != otherDef.Version {
-		t.Errorf("Expected Version to be %v, but got %v", def.Version, otherDef.Version)
-	}
-
-	def = PartitionKeyDefinition{
-		Paths:   []string{"somePath", "someOtherPath"},
-		Version: 2,
-	}
-
-	jsonString, err = def.MarshalJSON()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = otherDef.UnmarshalJSON(jsonString)
-	if err != nil {
-		t.Fatal(err, string(jsonString))
-	}
-
-	// Kind is inferred based on the number of paths
-	if otherDef.Kind != PartitionKeyKindMultiHash {
-		t.Errorf("Expected Kind to be %v, but got %v", PartitionKeyKindMultiHash, otherDef.Kind)
-	}
-
-	if !reflect.DeepEqual(def.Paths, otherDef.Paths) {
-		t.Errorf("Expected Paths to be %v, but got %v", def.Paths, otherDef.Paths)
-	}
-
-	if def.Version != otherDef.Version {
-		t.Errorf("Expected Version to be %v, but got %v", def.Version, otherDef.Version)
-	}
-
-	def = PartitionKeyDefinition{
-		Kind:    PartitionKeyKindMultiHash,
-		Paths:   []string{"somePath"},
-		Version: 2,
-	}
-
-	jsonString, err = def.MarshalJSON()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = otherDef.UnmarshalJSON(jsonString)
-	if err != nil {
-		t.Fatal(err, string(jsonString))
-	}
-
-	if def.Kind != otherDef.Kind {
-		t.Errorf("Expected Kind to be %v, but got %v", def.Kind, otherDef.Kind)
-	}
-
-	if !reflect.DeepEqual(def.Paths, otherDef.Paths) {
-		t.Errorf("Expected Paths to be %v, but got %v", def.Paths, otherDef.Paths)
-	}
-
-	if def.Version != otherDef.Version {
-		t.Errorf("Expected Version to be %v, but got %v", def.Version, otherDef.Version)
 	}
 }

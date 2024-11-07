4e87b657f95977380ae9e54e24eebbe83073f3cd
Ben Broderick Phillips <bebroder@microsoft.com>
2024-11-07 17:56:46 -0500

    Refresh OIDC token in azadmin test resources post script

--- sdk/security/keyvault/azadmin/test-resources-post.ps1
+++ sdk/security/keyvault/azadmin/test-resources-post.ps1
@@ -15,6 +15,22 @@ param (
     [Parameter()]
     [hashtable] $DeploymentOutputs,
 
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $SubscriptionId,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $TenantId,
+
+    [Parameter(Mandatory = $true)]
+    [ValidatePattern('^[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$')]
+    [string] $TestApplicationId,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Environment,
+
     # Captures any arguments from eng/New-TestResources.ps1 not declared here (no parameter errors).
     [Parameter(ValueFromRemainingArguments = $true)]
     $RemainingArguments
@@ -86,6 +102,10 @@ $wrappingFiles = foreach ($i in 0..2) {
 
     Resolve-Path "$baseName.cer"
 }
+
+Log "Refreshing OIDC token"
+Connect-AzAccount -ServicePrincipal -Tenant $TenantId -ApplicationId $TestApplicationId -FederatedToken $env:ARM_OIDC_TOKEN -Environment $Environment
+Set-AzContext -Subscription $SubscriptionId
 
 Log "Downloading security domain from '$hsmUrl'"
 

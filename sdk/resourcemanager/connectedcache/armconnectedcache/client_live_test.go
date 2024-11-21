be4411c5af074e755f664c6a1b600b6c3b81dabf
REDMOND\v-liujudy <1002946066@qq.com>
2024-11-21 14:51:22 +0800

    add live test

--- sdk/resourcemanager/connectedcache/armconnectedcache/client_live_test.go
+++ sdk/resourcemanager/connectedcache/armconnectedcache/client_live_test.go
@@ -8,157 +8,97 @@ import (
 	"testing"
 	"time"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/recording"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/connectedcache/armconnectedcache"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal/testutil"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 // C:\Users\v-liujudy\go\pkg\mod\github.com\!azure\azure-sdk-for-go\sdk\resourcemanager
 const (
-	SubscriptionID    = "faa080af-c1d8-40ad-9cce-e1a450ca5b57"
-	ResourceGroupName = "judytest04"
-	ResourceLocation  = "eastus2"
-	ResourceName      = "judy-test001"
-	PathToPackage     = "sdk/resourcemanager/sql/armsql/testdata"
+	SubscriptionID   = "faa080af-c1d8-40ad-9cce-e1a450ca5b57"
+	ResourceLocation = "eastus2"
 )
 
-func TestCreateOrUpdateGroupOfSubscriptionId(t *testing.T) {
-	subsriptionId := os.Getenv("AZURE_SUBSCRIPTION_ID")
-	assert.NotEmpty(t, subsriptionId)
-	// get default credential
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
-	assert.Nil(t, err)
-	assert.NotNil(t, cred)
-	fmt.Println("get default credential")
-
-	// new client factory
-	clientFactory, err := armresources.NewClientFactory(subsriptionId, cred, nil)
-	assert.Nil(t, err)
-	assert.NotNil(t, clientFactory)
-	client := clientFactory.NewResourceGroupsClient()
-	assert.NotNil(t, client)
-	ctx := context.Background()
-
-	createOrUpdateGroupResponse, err := client.CreateOrUpdate(ctx, ResourceGroupName, armresources.ResourceGroup{
-		Location: to.Ptr(ResourceLocation),
-	}, nil)
-	assert.Nil(t, err)
-	assert.NotNil(t, createOrUpdateGroupResponse)
-	fmt.Println("create resource group client")
-
-	// check whether create new group successfully
-	checkGroupExistResponse, err := client.CheckExistence(ctx, ResourceGroupName, nil)
-	assert.Nil(t, err)
-	assert.True(t, checkGroupExistResponse.Success)
-	fmt.Println("create new resource group ", ResourceGroupName, " of ", subsriptionId, "successfully")
+type AppConnectedCacheTestSuite struct {
+	suite.Suite
+	ctx               context.Context
+	cred              azcore.TokenCredential
+	options           *arm.ClientOptions
+	armEndpoint       string
+	location          string
+	resourceGroupName string
+	subscriptionId    string
 }
 
-func TestCreateOrUpdateIspCustomersClient(t *testing.T) {
+func (testsuite *AppConnectedCacheTestSuite) SetupSuite() {
+	testutil.StartRecording(testsuite.T(), pathToPackage)
+	testsuite.ctx = context.Background()
+	testsuite.cred, testsuite.options = testutil.GetCredAndClientOptions(testsuite.T())
+	testsuite.armEndpoint = "https://management.azure.com"
+	testsuite.location = recording.GetEnvVariable("LOCATION", "westus")
+	testsuite.resourceGroupName = recording.GetEnvVariable("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
+	testsuite.subscriptionId = recording.GetEnvVariable("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
+	testsuite.Prepare()
+}
+
+func TestConnectedCacheTestSuite(t *testing.T) {
+	suite.Run(t, new(AppConnectedCacheTestSuite))
+}
+
+func (testsuite *AppConnectedCacheTestSuite) TestClear() {
+	clientFactory, err := armresources.NewClientFactory(testsuite.subscriptionId, testsuite.cred, nil)
+	testsuite.Require().NoError(err)
+	resourceGroupsClientDeleteResponse, err := clientFactory.NewResourceGroupsClient().BeginDelete(testsuite.ctx, testsuite.resourceGroupName, nil)
+	testsuite.Require().NoError(err)
+	time.Sleep(time.Second * 2)
+	_, err = resourceGroupsClientDeleteResponse.Poll(testsuite.ctx)
+	testsuite.Require().NoError(err)
+	fmt.Println("delete resource group successfully")
+}
+
+func (testsuite *AppConnectedCacheTestSuite) TestGetIspCustomersClient() {
 	// make sure that the group has been created
-	TestCreateOrUpdateGroupOfSubscriptionId(t)
-
-	// cred := GetAzureCredentail()
-	// assert.NotNil(t, cred)
-	// // create new msql resource under group
-	// connectedCacheClientFactory, err := armconnectedcache.NewClientFactory(SubscriptionID, cred, nil)
-	// assert.Nil(t, err)
-	// assert.NotNil(t, connectedCacheClientFactory)
-
-	// serverclient, err := connectedCacheClientFactory.NewCacheNodesOperationsClient().Get()
-	// assert.NotNil(t, serverclient)
-
-	subsriptionId := os.Getenv("AZURE_SUBSCRIPTION_ID")
+	ResourceName := recording.GetEnvVariable("RESOURCE_NAME", "scenarioTestTempClient")
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	clientFactory, err := armconnectedcache.NewClientFactory(subsriptionId, cred, nil)
-	if err != nil {
-		log.Fatalf("failed to create client: %v", err)
-	}
-	poller, err := clientFactory.NewIspCustomersClient().BeginCreateOrUpdate(ctx, ResourceGroupName, ResourceName, armconnectedcache.IspCustomerResource{
-		Location: to.Ptr("westus"),
-		Properties: &armconnectedcache.CustomerProperty{
-			Customer: &armconnectedcache.CustomerEntity{
-				FullyQualifiedResourceID: to.Ptr("uqsbtgae"),
-				CustomerName:             to.Ptr("mkpzynfqihnjfdbaqbqwyhd"),
-				ContactEmail:             to.Ptr("xquos"),
-				ContactPhone:             to.Ptr("vue"),
-				ContactName:              to.Ptr("wxyqjoyoscmvimgwhpitxky"),
-				IsEntitled:               to.Ptr(true),
-				ReleaseVersion:           to.Ptr[int32](20),
-				ClientTenantID:           to.Ptr("fproidkpgvpdnac"),
-				IsEnterpriseManaged:      to.Ptr(true),
-				ShouldMigrate:            to.Ptr(true),
-				ResendSignupCode:         to.Ptr(true),
-				VerifySignupCode:         to.Ptr(true),
-				VerifySignupPhrase:       to.Ptr("tprjvttkgmrqlsyicnidhm"),
-			},
-			AdditionalCustomerProperties: &armconnectedcache.AdditionalCustomerProperties{
-				CustomerEmail:                 to.Ptr("zdjgibsidydyzm"),
-				CustomerTransitAsn:            to.Ptr("habgklnxqzmozqpazoyejwiphezpi"),
-				CustomerAsn:                   to.Ptr("hgrelgnrtdkleisnepfolu"),
-				CustomerEntitlementSKUID:      to.Ptr("b"),
-				CustomerEntitlementSKUGUID:    to.Ptr("rvzmdpxyflgqetvpwupnfaxsweiiz"),
-				CustomerEntitlementSKUName:    to.Ptr("waaqfijr"),
-				CustomerEntitlementExpiration: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, "2024-01-30T00:54:04.773Z"); return t }()),
-				OptionalProperty1:             to.Ptr("qhmwxza"),
-				OptionalProperty2:             to.Ptr("l"),
-				OptionalProperty3:             to.Ptr("mblwwvbie"),
-				OptionalProperty4:             to.Ptr("vzuek"),
-				OptionalProperty5:             to.Ptr("fzjodscdfcdr"),
-			},
-			Error: &armconnectedcache.ErrorDetail{},
-		},
-		Tags: map[string]*string{
-			"key1878": to.Ptr("warz"),
-		},
-	}, nil)
-	if err != nil {
-		log.Fatalf("failed to finish the request: %v", err)
-	}
-	res, err := poller.PollUntilDone(ctx, nil)
-	if err != nil {
-		log.Fatalf("failed to pull the result: %v", err)
-	}
-	fmt.Println("created resourceId:", res.ID)
+	clientFactory, err := armconnectedcache.NewClientFactory(testsuite.subscriptionId, cred, nil)
 
-	res1, err1 := clientFactory.NewIspCustomersClient().Get(ctx, ResourceGroupName, ResourceName, nil)
+	_, err1 := clientFactory.NewIspCustomersClient().Get(ctx, testsuite.resourceGroupName, ResourceName, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err1)
 	}
-	fmt.Println("resourceId:", res1.ID)
+	fmt.Println("TestGetIspCustomersClient over")
 }
 
-func GetAzureCredentail() (cred *azidentity.DefaultAzureCredential) {
+func (testsuite *AppConnectedCacheTestSuite) Prepare() {
+	resoureGroupName := os.Getenv("RESOURCE_GROUP_NAME")
+	subsriptionId := os.Getenv("AZURE_SUBSCRIPTION_ID")
 	// get default credential
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
-	if err != nil {
-		fmt.Println("get default credentail fail, error:", err.Error())
-		return
-	}
-	fmt.Println("get default credential")
-	return cred
-}
-
-func CreateAzureResourceGroup(groupName string) (group *armresources.ResourceGroup, err error) {
-	cred := GetAzureCredentail()
-	clientFactory, err := armresources.NewClientFactory(SubscriptionID, cred, nil)
-	if err != nil {
-		fmt.Println("create resource client factory fail, error:", err.Error())
-		return
-	}
+	testsuite.Require().NoError(err)
+	// new client factory
+	clientFactory, err := armresources.NewClientFactory(subsriptionId, cred, nil)
+	testsuite.Require().NoError(err)
 	client := clientFactory.NewResourceGroupsClient()
-	createOrUpdateGroupResponse, err := client.CreateOrUpdate(context.TODO(), groupName, armresources.ResourceGroup{
+	ctx := context.Background()
+
+	_, err = client.CreateOrUpdate(ctx, resoureGroupName, armresources.ResourceGroup{
 		Location: to.Ptr(ResourceLocation),
 	}, nil)
-	if err != nil {
-		fmt.Println("create resource group fail, error:", err.Error())
-		return
-	}
-	group = &createOrUpdateGroupResponse.ResourceGroup
-	return
+	testsuite.Require().NoError(err)
+
+	// check whether create new group successfully
+	_, err = client.CheckExistence(ctx, resoureGroupName, nil)
+	testsuite.Require().NoError(err)
+
+	fmt.Println("create new resource group ", resoureGroupName, " of ", subsriptionId, "successfully")
 }

37251c6e021a51f1f336d198982b28c0c5d93ed0
Karen Chen <karenchen@microsoft.com>
2025-02-06 20:44:50 -0800

    add propagator and link support to azcore/tracing

--- sdk/azcore/tracing/tracing.go
+++ sdk/azcore/tracing/tracing.go
@@ -19,16 +19,18 @@ type ProviderOptions struct {
 // NewProvider creates a new Provider with the specified values.
 //   - newTracerFn is the underlying implementation for creating Tracer instances
 //   - options contains optional values; pass nil to accept the default value
-func NewProvider(newTracerFn func(name, version string) Tracer, options *ProviderOptions) Provider {
+func NewProvider(newTracerFn func(name, version string) Tracer, newPropagatorFn func() Propagator, options *ProviderOptions) Provider {
 	return Provider{
-		newTracerFn: newTracerFn,
+		newTracerFn:     newTracerFn,
+		newPropagatorFn: newPropagatorFn,
 	}
 }
 
-// Provider is the factory that creates Tracer instances.
+// Provider is the factory that creates Tracer and Propagator instances.
 // It defaults to a no-op provider.
 type Provider struct {
-	newTracerFn func(name, version string) Tracer
+	newTracerFn     func(name, version string) Tracer
+	newPropagatorFn func() Propagator
 }
 
 // NewTracer creates a new Tracer for the specified module name and version.
@@ -41,12 +43,22 @@ func (p Provider) NewTracer(module, version string) (tracer Tracer) {
 	return
 }
 
+// NewPropagator creates a new Propagator.
+func (p Provider) NewPropagator() Propagator {
+	if p.newPropagatorFn != nil {
+		return p.newPropagatorFn()
+	}
+	return Propagator{}
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // TracerOptions contains the optional values when creating a Tracer.
 type TracerOptions struct {
 	// SpanFromContext contains the implementation for the Tracer.SpanFromContext method.
 	SpanFromContext func(context.Context) Span
+	// LinkFromContext contains the implementation for the Tracer.LinkFromContext method.
+	LinkFromContext func(context.Context, ...Attribute) Link
 }
 
 // NewTracer creates a Tracer with the specified values.
@@ -59,14 +71,17 @@ func NewTracer(newSpanFn func(ctx context.Context, spanName string, options *Spa
 	return Tracer{
 		newSpanFn:         newSpanFn,
 		spanFromContextFn: options.SpanFromContext,
+		linkFromContextFn: options.LinkFromContext,
 	}
 }
 
 // Tracer is the factory that creates Span instances.
 type Tracer struct {
 	attrs             []Attribute
+	links             []Link
 	newSpanFn         func(ctx context.Context, spanName string, options *SpanOptions) (context.Context, Span)
 	spanFromContextFn func(ctx context.Context) Span
+	linkFromContextFn func(ctx context.Context, attrs ...Attribute) Link
 }
 
 // Start creates a new span and a context.Context that contains it.
@@ -106,6 +121,15 @@ func (t Tracer) SpanFromContext(ctx context.Context) Span {
 	return Span{}
 }
 
+// LinkFromContext returns a link encapsulating the SpanContext of the current context.
+// If the provided context has no Span, an empty Link is returned.
+func (t Tracer) LinkFromContext(ctx context.Context, attrs ...Attribute) Link {
+	if t.linkFromContextFn != nil {
+		return t.linkFromContextFn(ctx, attrs...)
+	}
+	return Link{}
+}
+
 // SpanOptions contains optional settings for creating a span.
 type SpanOptions struct {
 	// Kind indicates the kind of Span.
@@ -113,6 +137,9 @@ type SpanOptions struct {
 
 	// Attributes contains key-value pairs of attributes for the span.
 	Attributes []Attribute
+
+	// Links contains the links to other spans.
+	Links []Link
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -129,6 +156,13 @@ type SpanImpl struct {
 
 	// AddEvent contains the implementation for the Span.AddEvent method.
 	AddEvent func(string, ...Attribute)
+
+	// AddLink contains the implementation for the Span.AddLink method.
+	AddLink func(Link)
+
+	// SpanContext returns the SpanContext of the Span. The returned SpanContext
+	// is usable even after the End method has been called for the Span.
+	SpanContext func() SpanContext
 
 	// SetStatus contains the implementation for the Span.SetStatus method.
 	SetStatus func(SpanStatus, string)
@@ -170,6 +204,21 @@ func (s Span) AddEvent(name string, attrs ...Attribute) {
 	}
 }
 
+// AddLink adds a link to the span.
+func (s Span) AddLink(link Link) {
+	if s.impl.AddLink != nil {
+		s.impl.AddLink(link)
+	}
+}
+
+// SpanContext returns the SpanContext of the Span.
+func (s Span) SpanContext() SpanContext {
+	if s.impl.SpanContext != nil {
+		return s.impl.SpanContext()
+	}
+	return SpanContext{}
+}
+
 // SetStatus sets the status on the span along with a description.
 func (s Span) SetStatus(code SpanStatus, desc string) {
 	if s.impl.SetStatus != nil {
@@ -188,4 +237,128 @@ type Attribute struct {
 	// Types that are natively supported include int64, float64, int, bool, string.
 	// Any other type will be formatted per rules of fmt.Sprintf("%v").
 	Value any
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type Link struct {
+	// SpanContext of the linked Span.
+	SpanContext SpanContext
+
+	// Attributes describe the aspects of the link.
+	Attributes []Attribute
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// SpanID is a unique identity of a span in a trace.
+type SpanID [8]byte
+
+// TraceID is a unique identity of a trace.
+type TraceID [16]byte
+
+// TraceFlags contains flags that can be set on a SpanContext.
+type TraceFlags byte
+
+type TraceState interface {
+	String() string
+}
+
+type SpanContext struct {
+	spanID     SpanID
+	traceID    TraceID
+	traceFlags TraceFlags
+	traceState TraceState
+	remote     bool
+}
+
+func (sc SpanContext) SpanID() SpanID {
+	return sc.spanID
+}
+
+func (sc SpanContext) TraceID() TraceID {
+	return sc.traceID
+}
+
+func (sc SpanContext) TraceFlags() TraceFlags {
+	return sc.traceFlags
+}
+
+func (sc SpanContext) TraceState() TraceState {
+	return sc.traceState
+}
+
+func (sc SpanContext) IsRemote() bool {
+	return sc.remote
+}
+
+// SpanContextConfig contains mutable fields usable for constructing
+// an immutable SpanContext.
+type SpanContextConfig struct {
+	TraceID    TraceID
+	SpanID     SpanID
+	TraceFlags TraceFlags
+	TraceState TraceState
+	Remote     bool
+}
+
+// NewSpanContext constructs a SpanContext using values from the provided
+// SpanContextConfig.
+func NewSpanContext(config SpanContextConfig) SpanContext {
+	return SpanContext{
+		traceID:    config.TraceID,
+		spanID:     config.SpanID,
+		traceFlags: config.TraceFlags,
+		traceState: config.TraceState,
+		remote:     config.Remote,
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type Propagator struct {
+	impl PropagatorImpl
+}
+
+// Inject injects the span context into the carrier.
+func (p Propagator) Inject(ctx context.Context, carrier Carrier) {
+	if p.impl.Inject != nil {
+		p.impl.Inject(ctx, carrier)
+	}
+}
+
+// Extract extracts the span context from the carrier.
+func (p Propagator) Extract(ctx context.Context, carrier Carrier) context.Context {
+	if p.impl.Extract != nil {
+		return p.impl.Extract(ctx, carrier)
+	}
+	return ctx
+}
+
+// Fields returns the fields that the propagator can inject and extract.
+func (p Propagator) Fields() []string {
+	if p.impl.Fields != nil {
+		return p.impl.Fields()
+	}
+	return nil
+}
+
+type PropagatorImpl struct {
+	// Inject contains the implementation for the Propagator.Inject method.
+	Inject func(ctx context.Context, carrier Carrier)
+	// Extract contains the implementation for the Propagator.Extract method.
+	Extract func(ctx context.Context, carrier Carrier) context.Context
+	// Fields contains the implementation for the Propagator.Fields method.
+	Fields func() []string
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type Carrier interface {
+	// Get returns the value associated with the passed key.
+	Get(key string) string
+	// Set stores the key-value pair.
+	Set(key string, value string)
+	// Keys lists the keys stored in this carrier.
+	Keys() []string
 }
--- sdk/azcore/tracing/tracing_test.go
+++ sdk/azcore/tracing/tracing_test.go
@@ -32,15 +32,28 @@ func TestProviderZeroValues(t *testing.T) {
 
 func TestProvider(t *testing.T) {
 	var addEventCalled bool
+	var addLinkCalled bool
+	var spanContextCalled bool
 	var endCalled bool
 	var setAttributesCalled bool
 	var setStatusCalled bool
 	var spanFromContextCalled bool
+	var linkFromContextCalled bool
+	var injectCalled bool
+	var extractCalled bool
+	var fieldsCalled bool
 
 	pr := NewProvider(func(name, version string) Tracer {
 		return NewTracer(func(context.Context, string, *SpanOptions) (context.Context, Span) {
 			return nil, NewSpan(SpanImpl{
-				AddEvent:      func(string, ...Attribute) { addEventCalled = true },
+				AddEvent: func(string, ...Attribute) { addEventCalled = true },
+				AddLink:  func(link Link) { addLinkCalled = true },
+				SpanContext: func() SpanContext {
+					spanContextCalled = true
+					return NewSpanContext(SpanContextConfig{
+						Remote: true,
+					})
+				},
 				End:           func() { endCalled = true },
 				SetAttributes: func(...Attribute) { setAttributesCalled = true },
 				SetStatus:     func(SpanStatus, string) { setStatusCalled = true },
@@ -50,13 +63,33 @@ func TestProvider(t *testing.T) {
 				spanFromContextCalled = true
 				return Span{}
 			},
+			LinkFromContext: func(ctx context.Context, attribute ...Attribute) Link {
+				linkFromContextCalled = true
+				return Link{}
+			},
 		})
+	}, func() Propagator {
+		return Propagator{
+			PropagatorImpl{
+				Inject: func(context.Context, Carrier) { injectCalled = true },
+				Extract: func(context.Context, Carrier) context.Context {
+					extractCalled = true
+					return context.Background()
+				},
+				Fields: func() []string {
+					fieldsCalled = true
+					return nil
+				},
+			},
+		}
 	}, nil)
 	tr := pr.NewTracer("name", "version")
 	require.NotZero(t, tr)
 	require.True(t, tr.Enabled())
 	sp := tr.SpanFromContext(context.Background())
 	require.Zero(t, sp)
+	lk := tr.LinkFromContext(context.Background())
+	require.Zero(t, lk)
 	tr.SetAttributes(Attribute{Key: "some", Value: "attribute"})
 	require.Len(t, tr.attrs, 1)
 	require.EqualValues(t, tr.attrs[0].Key, "some")
@@ -67,12 +100,33 @@ func TestProvider(t *testing.T) {
 	require.NotZero(t, sp)
 
 	sp.AddEvent("event")
+
+	sp.AddLink(Link{})
+	sc := sp.SpanContext()
+	require.NotNil(t, sc)
+	require.Zero(t, sc.TraceID())
+	require.Zero(t, sc.SpanID())
+	require.Zero(t, sc.TraceFlags())
+	require.Nil(t, sc.TraceState())
+	require.True(t, sc.IsRemote())
+
 	sp.End()
 	sp.SetAttributes()
 	sp.SetStatus(SpanStatusError, "desc")
 	require.True(t, addEventCalled)
+	require.True(t, addLinkCalled)
+	require.True(t, spanContextCalled)
 	require.True(t, endCalled)
 	require.True(t, setAttributesCalled)
 	require.True(t, setStatusCalled)
 	require.True(t, spanFromContextCalled)
+	require.True(t, linkFromContextCalled)
+
+	pp := pr.NewPropagator()
+	pp.Inject(context.Background(), nil)
+	pp.Extract(context.Background(), nil)
+	pp.Fields()
+	require.True(t, injectCalled)
+	require.True(t, extractCalled)
+	require.True(t, fieldsCalled)
 }

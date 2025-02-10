3b25f5cff89ed3e4b2aa399ea1e55bc84dab8788
gracewilcox <graceawilcox@gmail.com>
2025-02-10 13:57:25 -0800

    add test

--- sdk/security/keyvault/internal/fake_challenge_test.go
+++ sdk/security/keyvault/internal/fake_challenge_test.go
@@ -4,9 +4,29 @@
 package internal
 
 import (
+	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestFakeChallenge(t *testing.T) {
+	interceptor := &FakeChallenge{}
+	req, err := http.NewRequest(http.MethodGet, "https://42.vault.azure.net", nil)
+	require.NoError(t, err)
 
+	resp, err, intercepted := interceptor.Do(req)
+	require.NoError(t, err)
+	require.True(t, intercepted)
+	require.NotNil(t, resp)
+	require.Equal(t, http.StatusUnauthorized, resp.StatusCode)
+	headers := resp.Header
+	_, ok := headers["Www-Authenticate"]
+	require.True(t, ok)
+
+	req.Header.Set("Authorization", "fakeauthorization")
+	resp, err, intercepted = interceptor.Do(resp.Request)
+	require.NoError(t, err)
+	require.False(t, intercepted)
+	require.Nil(t, resp)
 }

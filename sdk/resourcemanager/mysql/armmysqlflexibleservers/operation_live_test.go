e85c8ac2b928d5588cc1cbb206c25cbf69a610c7
jliusan <v-liujudy@microsoft.com>
2025-02-26 14:46:32 +0800

    add live test

---
+++ sdk/resourcemanager/mysql/armmysqlflexibleservers/operation_live_test.go
@@ -0,0 +1,64 @@
+//go:build go1.18
+// +build go1.18
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package armmysqlflexibleservers_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/recording"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal/v3/testutil"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mysql/armmysqlflexibleservers/v2"
+	"github.com/stretchr/testify/suite"
+)
+
+type MysqlflexibleserverTestSuite struct {
+	suite.Suite
+
+	ctx               context.Context
+	cred              azcore.TokenCredential
+	options           *arm.ClientOptions
+	location          string
+	resourceGroupName string
+	subscriptionId    string
+}
+
+func (testsuite *MysqlflexibleserverTestSuite) SetupSuite() {
+	testutil.StartRecording(testsuite.T(), pathToPackage)
+
+	testsuite.ctx = context.Background()
+	testsuite.cred, testsuite.options = testutil.GetCredAndClientOptions(testsuite.T())
+	testsuite.location = recording.GetEnvVariable("LOCATION", "westus")
+	testsuite.resourceGroupName = recording.GetEnvVariable("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
+	testsuite.subscriptionId = recording.GetEnvVariable("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
+	resourceGroup, _, err := testutil.CreateResourceGroup(testsuite.ctx, testsuite.subscriptionId, testsuite.cred, testsuite.options, testsuite.location)
+	testsuite.Require().NoError(err)
+	testsuite.resourceGroupName = *resourceGroup.Name
+}
+
+func (testsuite *MysqlflexibleserverTestSuite) TearDownSuite() {
+	_, err := testutil.DeleteResourceGroup(testsuite.ctx, testsuite.subscriptionId, testsuite.cred, testsuite.options, testsuite.resourceGroupName)
+	testsuite.Require().NoError(err)
+	testutil.StopRecording(testsuite.T())
+}
+
+func TestMysqlflexibleserverTestSuite(t *testing.T) {
+	suite.Run(t, new(MysqlflexibleserverTestSuite))
+}
+
+func (testsuite *MysqlflexibleserverTestSuite) TestServiceGet() {
+	ctx := context.Background()
+	clientFactory, err := armmysqlflexibleservers.NewClientFactory(testsuite.subscriptionId, testsuite.cred, nil)
+	testsuite.Require().NoError(err)
+	pager := clientFactory.NewOperationsClient().NewListPager(nil)
+	for pager.More() {
+		_, err := pager.NextPage(ctx)
+		testsuite.Require().NoError(err)
+	}
+}

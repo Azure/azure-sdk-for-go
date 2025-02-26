81e78cd8ea71e3a38629ee477329ee92c0a7cb1b
jliusan <v-liujudy@microsoft.com>
2025-02-26 13:26:42 +0800

    update test

--- sdk/resourcemanager/hybridconnectivity/armhybridconnectivity/endpoints_client_example_test.go
+++ sdk/resourcemanager/hybridconnectivity/armhybridconnectivity/endpoints_client_example_test.go
@@ -23,7 +23,7 @@ func ExampleEndpointsClient_CreateOrUpdate_hybridConnectivityEndpointsPutCustom(
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := clientFactory.NewEndpointsClient().CreateOrUpdate(ctx, "subscriptions/f5bcc1d9-23af-4ae9-aca1-041d0f593a63/resourceGroups/hybridRG/providers/Microsoft.HybridCompute/machines/testMachine", "custom",armhybridconnectivity.EndpointResource{}, nil)
+	res, err := clientFactory.NewEndpointsClient().CreateOrUpdate(ctx, "subscriptions/f5bcc1d9-23af-4ae9-aca1-041d0f593a63/resourceGroups/hybridRG/providers/Microsoft.HybridCompute/machines/testMachine", "custom", armhybridconnectivity.EndpointResource{}, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

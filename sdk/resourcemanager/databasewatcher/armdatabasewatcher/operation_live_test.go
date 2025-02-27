b214a65afc9e27f7dc2843d575100a8acd4d6f0f
jliusan <v-liujudy@microsoft.com>
2025-02-27 15:30:34 +0800

    update test

--- sdk/resourcemanager/databasewatcher/armdatabasewatcher/operation_live_test.go
+++ sdk/resourcemanager/databasewatcher/armdatabasewatcher/operation_live_test.go
@@ -7,17 +7,13 @@ package armdatabasewatcher_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
-	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/internal/recording"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/databasewatcher/armdatabasewatcher"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal/v3/testutil"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -31,7 +27,6 @@ type DatabaseWatcherTestSuite struct {
 	cred              azcore.TokenCredential
 	options           *arm.ClientOptions
 	location          string
-	resourceGroupName string
 	subscriptionId    string
 }
 
@@ -41,15 +36,8 @@ func (testsuite *DatabaseWatcherTestSuite) SetupSuite() {
 	testsuite.ctx = context.Background()
 	testsuite.cred, testsuite.options = testutil.GetCredAndClientOptions(testsuite.T())
 	testsuite.location = recording.GetEnvVariable("LOCATION", ResourceLocation)
-	testsuite.resourceGroupName = recording.GetEnvVariable("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
+	// testsuite.resourceGroupName = recording.GetEnvVariable("RESOURCE_GROUP_NAME", "scenarioTestTempGroup")
 	testsuite.subscriptionId = recording.GetEnvVariable("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
-
-	resourceGroup, _, err := testutil.CreateResourceGroup(testsuite.ctx, testsuite.subscriptionId, testsuite.cred, testsuite.options, testsuite.location)
-	testsuite.Require().NoError(err)
-	testsuite.resourceGroupName = *resourceGroup.Name
-
-	fmt.Println("create new resource group ", testsuite.resourceGroupName, " of ", testsuite.subscriptionId, "successfully")
-	testsuite.Prepare()
 }
 
 func TestDatabaseWatcherTestSuite(t *testing.T) {
@@ -57,8 +45,6 @@ func TestDatabaseWatcherTestSuite(t *testing.T) {
 }
 
 func (testsuite *DatabaseWatcherTestSuite) TearDownSuite() {
-	_, err := testutil.DeleteResourceGroup(testsuite.ctx, testsuite.subscriptionId, testsuite.cred, testsuite.options, testsuite.resourceGroupName)
-	testsuite.Require().NoError(err)
 	testutil.StopRecording(testsuite.T())
 }
 
@@ -70,29 +56,4 @@ func (testsuite *DatabaseWatcherTestSuite) TestOperationGet() {
 		_, err := pager.NextPage(testsuite.ctx)
 		testsuite.Require().NoError(err)
 	}
-}
-
-func (testsuite *DatabaseWatcherTestSuite) Prepare() {
-	// get default credential
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
-	testsuite.Require().NoError(err)
-	// new client factory
-
-	fmt.Println("subscriptionId", testsuite.subscriptionId, "groupName", testsuite.resourceGroupName, "location", testsuite.location)
-	clientFactory, err := armresources.NewClientFactory(testsuite.subscriptionId, cred, testsuite.options)
-	testsuite.Require().NoError(err)
-	client := clientFactory.NewResourceGroupsClient()
-
-	testsuite.Require().NoError(err)
-	// check whether create new group successfully
-	res, err := client.CheckExistence(testsuite.ctx, testsuite.resourceGroupName, nil)
-	testsuite.Require().NoError(err)
-	if !res.Success {
-		_, err = client.CreateOrUpdate(testsuite.ctx, testsuite.resourceGroupName, armresources.ResourceGroup{
-			Location: to.Ptr(testsuite.location),
-		}, nil)
-		testsuite.Require().NoError(err)
-	}
-
-	fmt.Println("create new resource group ", testsuite.resourceGroupName, " of ", testsuite.subscriptionId, "successfully")
 }

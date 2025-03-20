fb20510ecf4a3c227650d907ccaca7549264609b
Scott Beddall <scbedd@microsoft.com>
2025-03-20 15:30:59 -0700

    most of build LOOKs right now

--- eng/scripts/Build_Perf.ps1
+++ eng/scripts/Build_Perf.ps1
@@ -1,58 +1,83 @@
 #Requires -Version 7.0
 
 Param(
-    [string] $serviceDirectory,
+    [string] $ServiceDirectories,
     [bool] $useAzcoreFromMain
 )
 
-Push-Location sdk/$serviceDirectory
+function Build-Perf {
+    param(
+        [string] $ServiceDirectory,
+        [bool] $useAzcoreFromMain
+    )
+    Push-Location sdk/$ServiceDirectory
 
-# Find all 'testdata' directories
-$perfDirectories = Get-ChildItem -Path . -Filter testdata -Recurse
+    # Find all 'testdata' directories
+    $perfDirectories = Get-ChildItem -Path . -Filter testdata -Recurse
 
-if ($perfDirectories.Length -eq 0) {
-    Write-Host "##[command] Did not find any performance tests in the directory $(Get-Location)"
-    exit 0
-}
+    if ($perfDirectories.Length -eq 0) {
+        Write-Host "##[command] Did not find any performance tests in the directory $(Get-Location)"
+        return $true
+    }
 
-$failed = $false
+    $failed = $false
 
-foreach ($perfDir in $perfDirectories) {
-    Push-Location $perfDir
+    foreach ($perfDir in $perfDirectories) {
+        Push-Location $perfDir
 
-    if (Test-Path -Path perf) {
-        Push-Location perf
-        Write-Host "##[command] Building and vetting performance tests in $perfDir/perf"
+        if (Test-Path -Path perf) {
+            Push-Location perf
+            Write-Host "##[command] Building and vetting performance tests in $perfDir/perf"
 
-        if ($useAzcoreFromMain) {
-            # using a live azcore might be dragging in updated dependencies
-            Write-Host "##[command] Executing 'go mod tidy' in $perfDir/perf"
-            go mod tidy
+            if ($useAzcoreFromMain) {
+                # using a live azcore might be dragging in updated dependencies
+                Write-Host "##[command] Executing 'go mod tidy' in $perfDir/perf"
+                go mod tidy
+                if ($LASTEXITCODE) {
+                    $failed = $true
+                }
+            }
+
+            Write-Host "##[command] Executing 'go build .' in $perfDir/perf"
+            go build .
             if ($LASTEXITCODE) {
                 $failed = $true
             }
+
+            Write-Host "##[command] Executing 'go vet .' in $perfDir/perf"
+            go vet .
+            if ($LASTEXITCODE) {
+                $failed = $true
+            }
+            Pop-Location
         }
 
-        Write-Host "##[command] Executing 'go build .' in $perfDir/perf"
-        go build .
-        if ($LASTEXITCODE) {
-            $failed = $true
-        }
-
-        Write-Host "##[command] Executing 'go vet .' in $perfDir/perf"
-        go vet .
-        if ($LASTEXITCODE) {
-            $failed = $true
-        }
         Pop-Location
     }
 
     Pop-Location
+
+    if ($failed) {
+        Write-Host "##[command] a failure occurred vetting/building one or more performance tests in $ServiceDirectory"
+        return $false
+    }
+
+    return $true
 }
 
-Pop-Location
+$services = $ServiceDirectories -split ","
+
+$failed = $false
+
+foreach($serviceDirectory in $services) {
+    $result = Build-Perf $serviceDirectory $useAzcoreFromMain
+
+    if (!$result) {
+        $failed = $true
+    }
+}
 
 if ($failed) {
-    Write-Host "##[command] a failure occurred vetting/building one or more performance tests"
+    Write-Host "##[error] a failure occurred vetting/building one or more performance tests in $ServiceDirectories"
     exit 1
 }
--- eng/scripts/run_tests.ps1
+++ eng/scripts/run_tests.ps1
@@ -8,51 +8,79 @@ Param(
 )
 
 $ErrorActionPreference = 'Stop'
-Push-Location sdk/$serviceDirectory
 
-if ($enableRaceDetector) {
-    Write-Host "##[command] Executing 'go test -timeout $testTimeout -race -v -coverprofile coverage.txt ./...' in sdk/$serviceDirectory"
-    go test -timeout $testTimeout -race -v -coverprofile coverage.txt ./... | Tee-Object -FilePath outfile.txt
-} else {
-    Write-Host "##[command] Executing 'go test -timeout $testTimeout -v -coverprofile coverage.txt ./...' in sdk/$serviceDirectory"
-    go test -timeout $testTimeout -v -coverprofile coverage.txt ./... | Tee-Object -FilePath outfile.txt
+function Invoke-Test {
+    param (
+        [string] $serviceDirectory,
+        [string] $testTimeout,
+        [bool] $enableRaceDetector
+    )
+    Push-Location sdk/$serviceDirectory
+
+    if ($enableRaceDetector) {
+        Write-Host "##[command] Executing 'go test -timeout $testTimeout -race -v -coverprofile coverage.txt ./...' in sdk/$serviceDirectory"
+        go test -timeout $testTimeout -race -v -coverprofile coverage.txt ./... | Tee-Object -FilePath outfile.txt
+    } else {
+        Write-Host "##[command] Executing 'go test -timeout $testTimeout -v -coverprofile coverage.txt ./...' in sdk/$serviceDirectory"
+        go test -timeout $testTimeout -v -coverprofile coverage.txt ./... | Tee-Object -FilePath outfile.txt
+    }
+
+    # go test will return a non-zero exit code on test failures so don't skip generating the report in this case
+    $GOTESTEXITCODE = $LASTEXITCODE
+
+    Get-Content -Raw outfile.txt | go-junit-report > report.xml
+
+    # if no tests were actually run (e.g. examples) delete the coverage file so it's omitted from the coverage report
+    if (Select-String -path ./report.xml -pattern '<testsuites></testsuites>' -simplematch -quiet) {
+        Write-Host "##[command]Deleting empty coverage file"
+        Remove-Item coverage.txt
+        Remove-Item outfile.txt
+        Remove-Item report.xml
+
+        Pop-Location
+    } else {
+        # Tests were actually run create a coverage report
+        $repoRoot = Resolve-Path "$PSScriptRoot/../../"
+
+        gocov convert ./coverage.txt > ./coverage.json
+
+        # gocov converts rely on standard input
+        Get-Content ./coverage.json | gocov-xml > ./coverage.xml
+        Get-Content ./coverage.json | gocov-html > ./coverage.html
+
+        Move-Item -Force ./coverage.xml $repoRoot
+        Move-Item -Force ./coverage.html $repoRoot
+
+        # use internal tool to fail if coverage is too low
+        Pop-Location
+
+        go run $repoRoot/eng/tools/internal/coverage/coverage.go `
+            -config $repoRoot/eng/config.json `
+            -serviceDirectory $serviceDirectory `
+            -searchDirectory $repoRoot
+    }
+
+    if ($GOTESTEXITCODE) {
+        return $GOTESTEXITCODE
+    }
+    return 0
 }
 
-# go test will return a non-zero exit code on test failures so don't skip generating the report in this case
-$GOTESTEXITCODE = $LASTEXITCODE
 
-Get-Content -Raw outfile.txt | go-junit-report > report.xml
+$services = $ServiceDirectories -split ","
 
-# if no tests were actually run (e.g. examples) delete the coverage file so it's omitted from the coverage report
-if (Select-String -path ./report.xml -pattern '<testsuites></testsuites>' -simplematch -quiet) {
-    Write-Host "##[command]Deleting empty coverage file"
-    Remove-Item coverage.txt
-    Remove-Item outfile.txt
-    Remove-Item report.xml
+$failed = $false
 
-    Pop-Location
-} else {
-    # Tests were actually run create a coverage report
-    $repoRoot = Resolve-Path "$PSScriptRoot/../../"
+foreach($serviceDirectory in $services) {
+    $result = Invoke-Test $serviceDirectory $testTimeout $enableRaceDetector
 
-    gocov convert ./coverage.txt > ./coverage.json
-
-    # gocov converts rely on standard input
-    Get-Content ./coverage.json | gocov-xml > ./coverage.xml
-    Get-Content ./coverage.json | gocov-html > ./coverage.html
-
-    Move-Item -Force ./coverage.xml $repoRoot
-    Move-Item -Force ./coverage.html $repoRoot
-
-    # use internal tool to fail if coverage is too low
-    Pop-Location
-
-    go run $repoRoot/eng/tools/internal/coverage/coverage.go `
-        -config $repoRoot/eng/config.json `
-        -serviceDirectory $serviceDirectory `
-        -searchDirectory $repoRoot
+    if ($result -gt 0) {
+        $failed = $true
+    }
 }
 
-if ($GOTESTEXITCODE) {
-    exit $GOTESTEXITCODE
+if ($failed) {
+    Write-Host "##[error] a failure occurred testing the directories: $ServiceDirectories. Check above details for more information."
+    exit 1
 }
+

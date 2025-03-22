e45ff4ccd2a85d612892f9ef41ed9ccc5a02ecff
Scott Beddall <scbedd@microsoft.com>
2025-03-21 19:35:54 -0700

    I was accidentally forcing failure

--- eng/scripts/Build_Perf.ps1
+++ eng/scripts/Build_Perf.ps1
@@ -10,11 +10,8 @@ $services = $ServiceDirectories -split ","
 $failed = $false
 
 foreach($serviceDirectory in $services) {
-    $result = &$PSScriptRoot/perf.ps1 $serviceDirectory $useAzcoreFromMain
+    &$PSScriptRoot/perf.ps1 $serviceDirectory $useAzcoreFromMain
 
-    if (!$result) {
-        $failed = $true
-    }
     if ($LASTEXITCODE) {
         Write-Host "##[error] a failure occurred vetting/building one or more performance tests in $serviceDirectory"
         $failed = $true

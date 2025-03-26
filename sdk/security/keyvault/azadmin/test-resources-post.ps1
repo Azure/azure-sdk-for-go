4f0e11de80f015e314bac87f83180886c71dbccc
Ben Broderick Phillips <bebroder@microsoft.com>
2025-03-26 16:25:47 -0400

    Use self contained post script for azkeys live tests

--- sdk/security/keyvault/azadmin/test-resources-post.ps1
+++ sdk/security/keyvault/azadmin/test-resources-post.ps1
@@ -32,6 +32,9 @@ param (
     [ValidateNotNullOrEmpty()]
     [string] $Environment,
 
+    [Parameter()]
+    [switch] $CI = ($null -ne $env:SYSTEM_TEAMPROJECTID),
+
     # Captures any arguments from eng/New-TestResources.ps1 not declared here (no parameter errors).
     [Parameter(ValueFromRemainingArguments = $true)]
     $RemainingArguments
@@ -90,12 +93,14 @@ if (!$DeploymentOutputs['AZURE_MANAGEDHSM_URL']) {
     exit
 }
 
-Log "Refreshing OIDC token"
-az cloud set -n $Environment
-az login --federated-token $env:ARM_OIDC_TOKEN --service-principal -t $TenantId -u $TestApplicationId
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
-az account set --subscription $SubscriptionId
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
+if ($CI) {
+    Log "Refreshing login"
+    az cloud set -n $Environment
+    az login --federated-token $env:ARM_OIDC_TOKEN --service-principal -t $TenantId -u $TestApplicationId
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+    az account set --subscription $SubscriptionId
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
 
 [Uri] $hsmUrl = $DeploymentOutputs['AZURE_MANAGEDHSM_URL']
 $hsmName = $hsmUrl.Host.Substring(0, $hsmUrl.Host.IndexOf('.'))

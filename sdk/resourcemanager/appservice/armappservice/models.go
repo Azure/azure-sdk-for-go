308d26bcc709587ed2715df1c07380fcff8db169
JiaqiZhang-Dev <jiaqzhang@microsoft.com>
2025-04-03 17:26:02 +0800

    fix broken links in autogen docs

--- sdk/resourcemanager/appservice/armappservice/models.go
+++ sdk/resourcemanager/appservice/armappservice/models.go
@@ -727,7 +727,7 @@ type AzureActiveDirectoryRegistration struct {
 
 	// The OpenID Connect Issuer URI that represents the entity which issues access tokens for this application. When using Azure
 	// Active Directory, this value is the URI of the directory tenant, e.g.
-	// https://login.microsoftonline.com/v2.0/{tenant-guid}/. This URI is a case-sensitive identifier for the token issuer. More
+	// `https://login.microsoftonline.com/v2.0/{tenant-guid}/`. This URI is a case-sensitive identifier for the token issuer. More
 	// information on OpenID Connect Discovery:
 	// http://openid.net/specs/openid-connect-discovery-1_0.html
 	OpenIDIssuer *string
@@ -7254,7 +7254,7 @@ type SiteAuthSettingsProperties struct {
 
 	// The OpenID Connect Issuer URI that represents the entity which issues access tokens for this application. When using Azure
 	// Active Directory, this value is the URI of the directory tenant, e.g.
-	// https://sts.windows.net/{tenant-guid}/. This URI is a case-sensitive identifier for the token issuer. More information
+	// `https://sts.windows.net/{tenant-guid}/`. This URI is a case-sensitive identifier for the token issuer. More information
 	// on OpenID Connect Discovery:
 	// http://openid.net/specs/openid-connect-discovery-1_0.html
 	Issuer *string

4b22a18de6d878469e41cf939015526dcda036c4
jliusan <v-liujudy@microsoft.com>
2025-04-07 13:50:57 +0800

    [Release] sdk/resourcemanager/standbypool/armstandbypool/2.0.0 generation from spec commit: 3db6867b8e524ea6d1bc7a3bbb989fe50dd2f184

--- sdk/resourcemanager/standbypool/armstandbypool/operations_client_example_test.go
+++ sdk/resourcemanager/standbypool/armstandbypool/operations_client_example_test.go
@@ -7,11 +7,11 @@ package armstandbypool_test
 import (
 	"context"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/standbypool/armstandbypool"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/standbypool/armstandbypool/v2"
 	"log"
 )
 
-// Generated from example definition: 2024-03-01/Operations_List.json
+// Generated from example definition: 2025-03-01/Operations_List.json
 func ExampleOperationsClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {

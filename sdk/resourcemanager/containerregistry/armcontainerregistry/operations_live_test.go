8f8b0f52fa9435903857c3b7358b914d4edc4df3
jliusan <v-liujudy@microsoft.com>
2025-04-21 16:36:07 +0800

    update test

---
+++ sdk/resourcemanager/containerregistry/armcontainerregistry/operations_live_test.go
@@ -0,0 +1,53 @@
+//go:build go1.18
+// +build go1.18
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package armcontainerregistry_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/internal/recording"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerregistry/armcontainerregistry"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/internal/v3/testutil"
+	"github.com/stretchr/testify/suite"
+)
+
+type ContainerregistryOperationsTestSuite struct {
+	suite.Suite
+
+	ctx            context.Context
+	cred           azcore.TokenCredential
+	options        *arm.ClientOptions
+	subscriptionId string
+}
+
+func (testsuite *ContainerregistryOperationsTestSuite) SetupSuite() {
+	testutil.StartRecording(testsuite.T(), pathToPackage)
+	testsuite.ctx = context.Background()
+	testsuite.cred, testsuite.options = testutil.GetCredAndClientOptions(testsuite.T())
+	testsuite.subscriptionId = recording.GetEnvVariable("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
+}
+
+func (testsuite *ContainerregistryOperationsTestSuite) TearDownSuite() {
+	testutil.StopRecording(testsuite.T())
+}
+
+func TestContainerregistryOperationTestSuite(t *testing.T) {
+	suite.Run(t, new(ContainerregistryOperationsTestSuite))
+}
+
+func (testsuite *ContainerregistryOperationsTestSuite) TestContainerregister() {
+	clientFactory, err := armcontainerregistry.NewClientFactory(testsuite.subscriptionId, testsuite.cred, testsuite.options)
+	testsuite.Require().NoError(err)
+	pager := clientFactory.NewOperationsClient().NewListPager(nil)
+	for pager.More() {
+		_, err = pager.NextPage(testsuite.ctx)
+		testsuite.Require().NoError(err)
+	}
+}

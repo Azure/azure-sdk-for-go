7e118ce7225433d397946e2c06908503eabbcc2e
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-04-23 12:21:18 -0700

    [Cosmos] some tidy up to Cosmos query engine API (#24485)
    * some tidy up to Cosmos query engine API
    
    * pass completed state through PipelineResult

--- sdk/data/azcosmos/unstable/queryengine/cosmos_query_engine.go
+++ sdk/data/azcosmos/unstable/queryengine/cosmos_query_engine.go
@@ -56,9 +56,8 @@ type QueryPipeline interface {
 	Query() string
 	// IsComplete gets a boolean indicating if the pipeline has concluded
 	IsComplete() bool
-	// NextBatch gets the next batch of items, which will be empty if there are no more items in the buffer, and the next set of QueryRequests which must be fulfilled, which will be empty if there are no more requests.
-	// If both the items and requests are empty, the pipeline has concluded.
-	NextBatch() ([][]byte, []QueryRequest, error)
+	// Run executes a single turn of the pipeline, yielding a PipelineResult containing the items and requests for more data.
+	Run() (*PipelineResult, error)
 	// ProvideData provides more data for a given partition key range ID, using data retrieved from the server in response to making a DataRequest.
 	ProvideData(data QueryResult) error
 	// Close frees the resources associated with the pipeline.

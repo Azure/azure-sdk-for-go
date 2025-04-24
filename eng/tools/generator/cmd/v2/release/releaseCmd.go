e2111ee8a08d95b355ee4ab2b049d61e45828cb8
JiaqiZhang-Dev <jiaqzhang@microsoft.com>
2025-04-24 11:19:10 +0800

    remove `force-stable-version` flag

--- eng/tools/generator/cmd/v2/release/releaseCmd.go
+++ eng/tools/generator/cmd/v2/release/releaseCmd.go
@@ -93,7 +93,6 @@ func BindFlags(flagSet *pflag.FlagSet) {
 	flagSet.String("package-config", "", "Additional config for package")
 	flagSet.String("go-version", "1.18", "Go version")
 	flagSet.StringP("token", "t", "", "Specify the personal access token of Github")
-	flagSet.Bool("force-stable-version", false, "Even if input-files contains preview files, they are forced to be generated as stable versions. At the same time, the tag must not contain preview.")
 	flagSet.String("tsp-config", "", "The path of the typespec tspconfig.yaml")
 	flagSet.String("tsp-option", "", "Emit typespec-go options, only valid when tsp-config is configured. e: option1=value1;option2=value2")
 	flagSet.StringSlice("tsp-client-option", nil, "The tsp-client(@azure-tools/typespec-client-generator-cli) init options. e: --save-inputs,--debug")
@@ -113,7 +112,6 @@ func ParseFlags(flagSet *pflag.FlagSet) Flags {
 		GoVersion:           flags.GetString(flagSet, "go-version"),
 		Token:               flags.GetString(flagSet, "token"),
 		UpdateSpecVersion:   flags.GetBool(flagSet, "update-spec-version"),
-		ForceStableVersion:  flags.GetBool(flagSet, "force-stable-version"),
 		TypeSpecConfig:      flags.GetString(flagSet, "tsp-config"),
 		TypeSpecGoOption:    flags.GetString(flagSet, "tsp-option"),
 		TspClientOption:     flags.GetStringSlice(flagSet, "tsp-client-option"),
@@ -200,7 +198,6 @@ func (c *commandContext) generate(sdkRepo repo.SDKRepository, specCommitHash str
 			ReleaseDate:          c.flags.ReleaseDate,
 			SkipGenerateExample:  c.flags.SkipGenerateExample,
 			GoVersion:            c.flags.GoVersion,
-			ForceStableVersion:   c.flags.ForceStableVersion,
 		})
 		if len(errs) > 0 {
 			err = errs[0]

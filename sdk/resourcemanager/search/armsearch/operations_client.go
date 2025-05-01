ac7550cba28fdcddb72a6a5e160f5d2f7b603e28
ReleaseHelper <ReleaseHelper>
2025-05-01 01:08:51 +0000

    [Release] sdk/resourcemanager/search/armsearch/1.4.0 generation from spec commit: fb3217991ff57b5760525aeba1a0670bfe0880fa

--- sdk/resourcemanager/search/armsearch/operations_client.go
+++ sdk/resourcemanager/search/armsearch/operations_client.go
@@ -36,7 +36,7 @@ func NewOperationsClient(credential azcore.TokenCredential, options *arm.ClientO
 
 // NewListPager - Lists all of the available REST API operations of the Microsoft.Search provider.
 //
-// Generated from API version 2025-02-01-preview
+// Generated from API version 2025-05-01
 //   - options - OperationsClientListOptions contains the optional parameters for the OperationsClient.NewListPager method.
 func (client *OperationsClient) NewListPager(options *OperationsClientListOptions) *runtime.Pager[OperationsClientListResponse] {
 	return runtime.NewPager(runtime.PagingHandler[OperationsClientListResponse]{
@@ -70,7 +70,7 @@ func (client *OperationsClient) listCreateRequest(ctx context.Context, _ *Operat
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2025-02-01-preview")
+	reqQP.Set("api-version", "2025-05-01")
 	req.Raw().URL.RawQuery = reqQP.Encode()
 	req.Raw().Header["Accept"] = []string{"application/json"}
 	return req, nil

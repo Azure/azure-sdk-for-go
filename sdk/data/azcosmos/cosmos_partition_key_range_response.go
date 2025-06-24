52f343e3f6d446f55a0246eb86d9af52b8762625
Adrian Clement <136120860+adrianwclement@users.noreply.github.com>
2025-06-24 18:44:37 +0000

    Refactored PartitionKeyRange to PartitionKeyRangeProperties

--- sdk/data/azcosmos/cosmos_partition_key_range_response.go
+++ sdk/data/azcosmos/cosmos_partition_key_range_response.go
@@ -6,57 +6,38 @@ package azcosmos
 import (
 	"encoding/json"
 	"net/http"
-	"time"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	azruntime "github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 )
-
-// PartitionKeyRange represents a single partition key range from GET /pkranges endpoint
-// Includes all the needed fields to represent a partition key range
-type PartitionKeyRange struct {
-	ID                      string       `json:"id"`
-	ResourceID              string       `json:"_rid"`
-	ETag                    *azcore.ETag `json:"_etag"`
-	MinInclusive            string       `json:"minInclusive"`
-	MaxExclusive            string       `json:"maxExclusive"`
-	ResourceIDPrefix        int          `json:"ridPrefix"`
-	SelfLink                string       `json:"_self"`
-	ThroughputFraction      float64      `json:"throughputFraction"`
-	Status                  string       `json:"status"`
-	Parents                 []string     `json:"parents"`
-	OwnedArchivalPKRangeIds []string     `json:"ownedArchivalPKRangeIds"`
-	LastModified            time.Time    `json:"_ts"`
-	LSN                     int64        `json:"lsn"`
-}
 
 // PartitionKeyRangeResponse represents the response from GET /pkranges endpoint
 // Contains the list of partition key ranges
 // Rid is for the high level resource id
 // count is for the number of partition key ranges returned
 type PartitionKeyRangeResponse struct {
+	// ResourceID is the resource id of the partition key ranges
+	ResourceID string `json:"_rid"`
+	// PartitionKeyRanges contains the list of partition key ranges
+	PartitionKeyRanges []PartitionKeyRangeProperties `json:"PartitionKeyRanges"`
+	// Count is the number of partition key ranges returned in the response
+	Count int `json:"_count"`
 	Response
-	ResourceID         string              `json:"_rid"`
-	PartitionKeyRanges []PartitionKeyRange `json:"PartitionKeyRanges"`
-	Count              int                 `json:"_count"`
 }
 
 // newPartitionKeyRangeResponse creates a new PartitionKeyRangeResponse from an HTTP response
-// It will parse the HTTP response and return a list of PartitionKeyRange objects
+// It will parse the HTTP response and return a list of PartitionKeyRangeProperty objects
 func newPartitionKeyRangeResponse(resp *http.Response) (PartitionKeyRangeResponse, error) {
 	response := PartitionKeyRangeResponse{
 		Response: newResponse(resp),
 	}
 
-	defer resp.Body.Close() // Standard practice to close the response body
+	defer resp.Body.Close()
 
-	// Read the response body and slice with azruntime.Payload
 	body, err := azruntime.Payload(resp)
 	if err != nil {
 		return response, err
 	}
-	// Unmarshal the JSON response into the PartitionKeysResponse struct
-	// Acts like a deserializer that will map the JSON body to the struct createed
+
 	if err := json.Unmarshal(body, &response); err != nil {
 		return response, err
 	}

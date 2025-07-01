117240383c8252eb73e34c923547369eef87a1d5
Adrian Clement <136120860+adrianwclement@users.noreply.github.com>
2025-07-01 23:42:17 +0000

    Created CompositeContinuationToken, allowing change feed processing to be resumed at different points

---
+++ sdk/data/azcosmos/cosmos_change_feed_composite_continuation_token.go
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azcosmos
+
+type CompositeContinuationToken struct {
+	// ResourceID is the ID of the resource for which the continuation token is valid.
+	ResourceID string `json:"resourceId"`
+	// Continuation is the list of Epk Ranges part of the continuation token
+	Continuation []ChangeFeedRange `json:"continuation"`
+}
+
+// newCompositeContinuationToken creates a new CompositeContinuationToken with the specified resource ID and continuation ranges.
+// This function is used to create a continuation token for the Cosmos DB change feed.
+// It is designed for internal use only and should not be used directly by clients.
+func newCompositeContinuationToken(resourceID string, continuation []ChangeFeedRange) CompositeContinuationToken {
+	return CompositeContinuationToken{
+		ResourceID:   resourceID,
+		Continuation: continuation,
+	}
+}

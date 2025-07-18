073547cf41b1a804c0fe86c448b7372fe71f74a2
ReleaseHelper <ReleaseHelper>
2025-07-18 14:39:18 +0800

    update test

--- sdk/resourcemanager/dashboard/armdashboard/manageddashboards_client_example_test.go
+++ sdk/resourcemanager/dashboard/armdashboard/manageddashboards_client_example_test.go
@@ -8,7 +8,7 @@ import (
 	"context"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/dashboard/armdashboard"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/dashboard/armdashboard/v2"
 	"log"
 )
 

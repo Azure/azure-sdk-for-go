5af35862002ddf1c2b900a68ac7f44745e950854
ReleaseHelper <ReleaseHelper>
2025-07-22 16:23:15 +0800

    update version

--- sdk/resourcemanager/carbonoptimization/armcarbonoptimization/version.go
+++ sdk/resourcemanager/carbonoptimization/armcarbonoptimization/version.go
@@ -6,5 +6,5 @@ package armcarbonoptimization
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/carbonoptimization/armcarbonoptimization"
-	moduleVersion = "v0.1.0"
+	moduleVersion = "v1.0.0"
 )

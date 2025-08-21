9a21512f29fe15a6cf596bf67113c64a06acf5a5
ReleaseHelper <ReleaseHelper>
2025-08-21 01:10:34 +0000

    [Release] sdk/resourcemanager/containerservice/armcontainerservice/7.3.0 generation from spec commit: 551674fef42784a6d62d92149d637a5d450f5128

--- sdk/resourcemanager/containerservice/armcontainerservice/fake/privatelinkresources_server.go
+++ sdk/resourcemanager/containerservice/armcontainerservice/fake/privatelinkresources_server.go
@@ -89,7 +89,7 @@ func (p *PrivateLinkResourcesServerTransport) dispatchList(req *http.Request) (*
 	const regexStr = `/subscriptions/(?P<subscriptionId>[!#&$-;=?-\[\]_a-zA-Z0-9~%@]+)/resourceGroups/(?P<resourceGroupName>[!#&$-;=?-\[\]_a-zA-Z0-9~%@]+)/providers/Microsoft\.ContainerService/managedClusters/(?P<resourceName>[!#&$-;=?-\[\]_a-zA-Z0-9~%@]+)/privateLinkResources`
 	regex := regexp.MustCompile(regexStr)
 	matches := regex.FindStringSubmatch(req.URL.EscapedPath())
-	if matches == nil || len(matches) < 3 {
+	if len(matches) < 4 {
 		return nil, fmt.Errorf("failed to parse path %s", req.URL.Path)
 	}
 	resourceGroupNameParam, err := url.PathUnescape(matches[regex.SubexpIndex("resourceGroupName")])

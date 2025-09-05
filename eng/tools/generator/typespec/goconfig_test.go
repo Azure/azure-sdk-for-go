0e8d8e63b54b77241de95b9840fada22a03c0371
Chenjie Shi <tadelesh.shi@live.cn>
2025-09-05 13:55:56 +0800

    Adopt new tsp-client change (#25165)

--- eng/tools/generator/typespec/goconfig_test.go
+++ eng/tools/generator/typespec/goconfig_test.go
@@ -19,49 +19,51 @@ func TestGoEmitterOptionsAreValid(t *testing.T) {
 	for _, module := range modules {
 		t.Run("Check "+module, func(t *testing.T) {
 			// module format is correct
-			goOption := map[string]any{
-				"module": module,
+			goEmitOptions := typespec.GoEmitterOptions{
+				Module: module,
 			}
-			goEmitOptions, err := typespec.NewGoEmitterOptions(goOption)
-			assert.NoError(t, err)
-			err = goEmitOptions.Validate()
+			err := goEmitOptions.Validate()
 			assert.NoError(t, err)
 		})
 	}
 }
 
 func TestGoEmitterOptionsValidate(t *testing.T) {
-	goOption := map[string]any{
-		"module": "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/xxx/armxxx",
+	goEmitOptions := typespec.GoEmitterOptions{
+		Module: "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/xxx/armxxx",
 	}
-	goEmitOptions, err := typespec.NewGoEmitterOptions(goOption)
-	assert.NoError(t, err)
-	err = goEmitOptions.Validate()
+	err := goEmitOptions.Validate()
 	assert.NoError(t, err)
 
 	// module format is wrong
-	goOption = map[string]any{
-		"module": "github.com/Azure/azure-sdk-for-go/sdk/xxx/armxxx",
+	goEmitOptions = typespec.GoEmitterOptions{
+		Module: "github.com/Azure/azure-sdk-for-go/sdk/xxx/armxxx",
 	}
-	goEmitOptions, err = typespec.NewGoEmitterOptions(goOption)
-	assert.NoError(t, err)
 	err = goEmitOptions.Validate()
 	assert.EqualError(t, err, typespec.ErrModuleFormat.Error())
 
-	goOption = map[string]any{
-		"module": "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/xxx/xxx",
+	goEmitOptions = typespec.GoEmitterOptions{
+		Module: "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/xxx/xxx",
 	}
-	goEmitOptions, err = typespec.NewGoEmitterOptions(goOption)
-	assert.NoError(t, err)
 	err = goEmitOptions.Validate()
 	assert.EqualError(t, err, typespec.ErrModuleFormat.Error())
 
 	// module is empty
-	goOption = map[string]any{
-		"module": "",
+	goEmitOptions = typespec.GoEmitterOptions{
+		Module: "",
 	}
-	goEmitOptions, err = typespec.NewGoEmitterOptions(goOption)
-	assert.NoError(t, err)
 	err = goEmitOptions.Validate()
-	assert.NoError(t, err)
+	assert.EqualError(t, err, typespec.ErrModuleEmpty.Error())
+
+	// containing-module is empty
+	goEmitOptions = typespec.GoEmitterOptions{
+		ContainingModule: "",
+	}
+	err = goEmitOptions.Validate()
+	assert.EqualError(t, err, typespec.ErrModuleEmpty.Error())
+
+	// empty config
+	goEmitOptions = typespec.GoEmitterOptions{}
+	err = goEmitOptions.Validate()
+	assert.EqualError(t, err, typespec.ErrModuleEmpty.Error())
 }

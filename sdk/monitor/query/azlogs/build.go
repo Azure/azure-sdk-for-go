3a50cd1979ae33e9f61c53f658ef624b48d669c7
Grace Wilcox <43627800+gracewilcox@users.noreply.github.com>
2025-09-16 14:42:38 -0700

    [query/azlogs] remove `goimports` (#25252)

--- sdk/monitor/query/azlogs/build.go
+++ sdk/monitor/query/azlogs/build.go
@@ -1,5 +1,4 @@
 //go:generate go run ./testdata/generate/transforms.go
-//go:generate goimports -w .
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.

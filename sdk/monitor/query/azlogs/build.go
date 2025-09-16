d51390927f8e05504938b258bedcff3e07858aaf
gracewilcox <graceawilcox@gmail.com>
2025-09-16 13:42:00 -0700

    gofmt

--- sdk/monitor/query/azlogs/build.go
+++ sdk/monitor/query/azlogs/build.go
@@ -1,5 +1,5 @@
 //go:generate go run ./testdata/generate/transforms.go
-//go:generate goimports -w .
+//go:generate gofmt -w .
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.

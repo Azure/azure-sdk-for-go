c16b69f740f9ab526e5e3220a65a4960d5a355c8
Judy Liu <v-liujudy@microsoft.com>
2025-09-16 11:17:43 +0800

    revert code

--- sdk/resourcemanager/containerservice/armdeploymentsafeguards/version.go
+++ sdk/resourcemanager/containerservice/armdeploymentsafeguards/version.go
@@ -6,5 +6,5 @@ package armdeploymentsafeguards
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armdeploymentsafeguards"
-	moduleVersion = "v0.2.0"
+	moduleVersion = "v0.1.0"
 )

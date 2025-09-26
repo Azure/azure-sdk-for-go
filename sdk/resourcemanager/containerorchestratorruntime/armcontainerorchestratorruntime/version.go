e72363fbb356997c4a1073bad94ffa2f9748e3d8
judy.liu <v-liujudy@microsoft.com>
2025-09-26 10:13:02 +0800

    revert version

--- sdk/resourcemanager/containerorchestratorruntime/armcontainerorchestratorruntime/version.go
+++ sdk/resourcemanager/containerorchestratorruntime/armcontainerorchestratorruntime/version.go
@@ -6,5 +6,5 @@ package armcontainerorchestratorruntime
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerorchestratorruntime/armcontainerorchestratorruntime"
-	moduleVersion = "v1.0.0"
+	moduleVersion = "v0.1.0"
 )

9f0afc6f7eb360e7d03378d89ec917daa247a2c8
Judy Liu <v-liujudy@microsoft.com>
2025-09-29 16:59:35 +0800

    [Release] sdk/resourcemanager/cognitiveservices/armcognitiveservices/2.0.0 generation from spec commit: 1004eed4202d64b48157c084fe2830760f8190f4 (#25238)

--- sdk/resourcemanager/cognitiveservices/armcognitiveservices/fake/operations_server.go
+++ sdk/resourcemanager/cognitiveservices/armcognitiveservices/fake/operations_server.go
@@ -12,7 +12,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/fake/server"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cognitiveservices/armcognitiveservices"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cognitiveservices/armcognitiveservices/v2"
 	"net/http"
 )
 

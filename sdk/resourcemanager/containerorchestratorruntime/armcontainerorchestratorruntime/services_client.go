40d7bf2db2d5a7e2b9439ba3646a3d6de4795be6
Judy Liu <v-liujudy@microsoft.com>
2025-09-29 10:14:59 +0800

    regen service (#25336)

--- sdk/resourcemanager/containerorchestratorruntime/armcontainerorchestratorruntime/services_client.go
+++ sdk/resourcemanager/containerorchestratorruntime/armcontainerorchestratorruntime/services_client.go
@@ -147,7 +147,6 @@ func (client *ServicesClient) deleteCreateRequest(ctx context.Context, resourceU
 	reqQP := req.Raw().URL.Query()
 	reqQP.Set("api-version", "2024-03-01")
 	req.Raw().URL.RawQuery = reqQP.Encode()
-	req.Raw().Header["Accept"] = []string{"application/json"}
 	return req, nil
 }
 

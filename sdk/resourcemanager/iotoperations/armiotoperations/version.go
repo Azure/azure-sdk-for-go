61ce1a41969510005186bdafbba98f124d291f9a
Judy Liu <v-liujudy@microsoft.com>
2025-10-17 13:55:17 +0800

    update version

--- sdk/resourcemanager/iotoperations/armiotoperations/version.go
+++ sdk/resourcemanager/iotoperations/armiotoperations/version.go
@@ -6,5 +6,5 @@ package armiotoperations
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/iotoperations/armiotoperations"
-	moduleVersion = "v0.1.0"
+	moduleVersion = "v1.0.0"
 )
--- sdk/resourcemanager/workloadssapvirtualinstance/armworkloadssapvirtualinstance/version.go
+++ sdk/resourcemanager/workloadssapvirtualinstance/armworkloadssapvirtualinstance/version.go
@@ -6,5 +6,5 @@ package armworkloadssapvirtualinstance
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/workloadssapvirtualinstance/armworkloadssapvirtualinstance"
-	moduleVersion = "v0.1.0"
+	moduleVersion = "v1.0.0"
 )

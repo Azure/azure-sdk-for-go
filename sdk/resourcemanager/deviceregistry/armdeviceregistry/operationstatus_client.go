1657c800fa7e2ccf0dd63536ed0231d84b27369c
azure-sdk <azuresdk@microsoft.com>
2025-10-23 17:18:59 +0000

    Configurations:  'specification/deviceregistry/DeviceRegistry.Management/tspconfig.yaml', API Version: 2025-10-01, SDK Release Type: stable, and CommitSHA: 'b562b95b6429f78f6c8629d5751bfeef1be7cacd' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5491389 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/resourcemanager/deviceregistry/armdeviceregistry/operationstatus_client.go
+++ sdk/resourcemanager/deviceregistry/armdeviceregistry/operationstatus_client.go
@@ -42,7 +42,7 @@ func NewOperationStatusClient(subscriptionID string, credential azcore.TokenCred
 // Get - Returns the current status of an async operation.
 // If the operation fails it returns an *azcore.ResponseError type.
 //
-// Generated from API version 2024-11-01
+// Generated from API version 2025-10-01
 //   - location - The location name.
 //   - operationID - The ID of an ongoing async operation.
 //   - options - OperationStatusClientGetOptions contains the optional parameters for the OperationStatusClient.Get method.
@@ -88,7 +88,7 @@ func (client *OperationStatusClient) getCreateRequest(ctx context.Context, locat
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2024-11-01")
+	reqQP.Set("api-version", "2025-10-01")
 	req.Raw().URL.RawQuery = reqQP.Encode()
 	req.Raw().Header["Accept"] = []string{"application/json"}
 	return req, nil

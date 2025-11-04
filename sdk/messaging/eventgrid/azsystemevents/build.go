30eccad476a6b5a0d35566421ef1b3b04c41728b
ripark <ripark@microsoft.com>
2025-11-04 14:23:58 -0800

    - Remove the potentially awesome recursive calls to tsp-client (now you should just _call_ tsp-client update and it'll orchestrate the entire thing) - Update to use gofmt instead of goimports, so it's the only one guaranteed to b

--- sdk/messaging/eventgrid/azsystemevents/build.go
+++ sdk/messaging/eventgrid/azsystemevents/build.go
@@ -4,9 +4,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-//go:generate tsp-client update
-//go:generate goimports -w .
+//go:generate gofmt -w .
 //go:generate go run ./internal/generate
-//go:generate goimports -w ./..
+//go:generate gofmt -w ./..
 
 package azsystemevents

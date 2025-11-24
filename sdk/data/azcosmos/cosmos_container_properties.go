88f491221fa05feb34b4819b72e8f2fdfb5312d8
tvaron3 <tomas.varon1802@gmail.com>
2025-11-24 12:45:17 -0500

    fix lint errors

--- sdk/data/azcosmos/cosmos_container_properties.go
+++ sdk/data/azcosmos/cosmos_container_properties.go
@@ -64,7 +64,7 @@ func (tp ContainerProperties) MarshalJSON() ([]byte, error) {
 	}
 
 	if tp.ETag != nil {
-		buffer.WriteString(",\"_etag\":")
+		fmt.Fprint(buffer, ",\"_etag\":")
 		etag, err := json.Marshal(tp.ETag)
 		if err != nil {
 			return nil, err
@@ -80,7 +80,7 @@ func (tp ContainerProperties) MarshalJSON() ([]byte, error) {
 		fmt.Fprintf(buffer, ",\"_ts\":%v", strconv.FormatInt(tp.LastModified.Unix(), 10))
 	}
 
-	buffer.WriteString(",\"partitionKey\":")
+	fmt.Fprint(buffer, ",\"partitionKey\":")
 	buffer.Write(pkDefinition)
 
 	if tp.DefaultTimeToLive != nil {
@@ -88,7 +88,7 @@ func (tp ContainerProperties) MarshalJSON() ([]byte, error) {
 	}
 
 	if tp.AnalyticalStoreTimeToLiveInSeconds != nil {
-		buffer.WriteString(fmt.Sprintf(",\"analyticalStorageTtl\":%v", *tp.AnalyticalStoreTimeToLiveInSeconds))
+		fmt.Fprintf(buffer, ",\"analyticalStorageTtl\":%v", *tp.AnalyticalStoreTimeToLiveInSeconds)
 	}
 
 	if tp.IndexingPolicy != nil {
@@ -96,7 +96,7 @@ func (tp ContainerProperties) MarshalJSON() ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
-		buffer.WriteString(",\"indexingPolicy\":")
+		fmt.Fprint(buffer, ",\"indexingPolicy\":")
 		buffer.Write(indexingPolicy)
 	}
 
@@ -105,7 +105,7 @@ func (tp ContainerProperties) MarshalJSON() ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
-		buffer.WriteString(",\"uniqueKeyPolicy\":")
+		fmt.Fprint(buffer, ",\"uniqueKeyPolicy\":")
 		buffer.Write(uniquePolicy)
 	}
 
@@ -114,7 +114,7 @@ func (tp ContainerProperties) MarshalJSON() ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
-		buffer.WriteString(",\"conflictResolutionPolicy\":")
+		fmt.Fprint(buffer, ",\"conflictResolutionPolicy\":")
 		buffer.Write(conflictPolicy)
 	}
 
@@ -123,7 +123,7 @@ func (tp ContainerProperties) MarshalJSON() ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
-		buffer.WriteString(",\"vectorEmbeddingPolicy\":")
+		fmt.Fprint(buffer, ",\"vectorEmbeddingPolicy\":")
 		buffer.Write(vectorPolicy)
 	}
 
@@ -132,11 +132,11 @@ func (tp ContainerProperties) MarshalJSON() ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
-		buffer.WriteString(",\"fullTextPolicy\":")
+		fmt.Fprint(buffer, ",\"fullTextPolicy\":")
 		buffer.Write(fullTextPolicy)
 	}
 
-	buffer.WriteString("}")
+	fmt.Fprint(buffer, "}")
 	return buffer.Bytes(), nil
 }
 

98a3fbe4cf08c22acb02093a662bad0cbed7ae4a
Judy Liu <v-liujudy@microsoft.com>
2025-12-02 11:47:02 +0800

    revert devopsinfrastructure file

--- sdk/resourcemanager/devopsinfrastructure/armdevopsinfrastructure/operations_client_example_test.go
+++ sdk/resourcemanager/devopsinfrastructure/armdevopsinfrastructure/operations_client_example_test.go
@@ -11,7 +11,7 @@ import (
 	"log"
 )
 
-// Generated from example definition: 2025-09-20/ListOperations.json
+// Generated from example definition: 2024-10-19/ListOperations.json
 func ExampleOperationsClient_NewListPager() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {

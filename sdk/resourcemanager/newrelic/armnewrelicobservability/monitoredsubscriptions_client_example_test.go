363f874a2c4b38b1ad8014a7a24caa323d317efd
azure-sdk <azuresdk@microsoft.com>
2025-12-08 02:22:10 +0000

    Regenerate SDK based on typespec-go branch main

--- sdk/resourcemanager/newrelic/armnewrelicobservability/monitoredsubscriptions_client_example_test.go
+++ sdk/resourcemanager/newrelic/armnewrelicobservability/monitoredsubscriptions_client_example_test.go
@@ -6,10 +6,9 @@ package armnewrelicobservability_test
 
 import (
 	"context"
-	"log"
-
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/newrelic/armnewrelicobservability/v2"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/newrelic/armnewrelicobservability"
+	"log"
 )
 
 // Generated from example definition: 2025-05-01-preview/MonitoredSubscriptions_CreateOrUpdate.json
@@ -23,7 +22,7 @@ func ExampleMonitoredSubscriptionsClient_BeginCreateorUpdate() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := clientFactory.NewMonitoredSubscriptionsClient().BeginCreateorUpdate(ctx, "myResourceGroup", "myMonitor", armnewrelicobservability.ConfigurationNameDefault, armnewrelicobservability.MonitoredSubscriptionProperties{}, nil)
+	poller, err := clientFactory.NewMonitoredSubscriptionsClient().BeginCreateorUpdate(ctx, "myResourceGroup", "myMonitor", armnewrelicobservability.ConfigurationNameDefault, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}
@@ -324,7 +323,7 @@ func ExampleMonitoredSubscriptionsClient_BeginUpdate() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	poller, err := clientFactory.NewMonitoredSubscriptionsClient().BeginUpdate(ctx, "myResourceGroup", "myMonitor", armnewrelicobservability.ConfigurationNameDefault, armnewrelicobservability.MonitoredSubscriptionProperties{}, nil)
+	poller, err := clientFactory.NewMonitoredSubscriptionsClient().BeginUpdate(ctx, "myResourceGroup", "myMonitor", armnewrelicobservability.ConfigurationNameDefault, nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
 	}

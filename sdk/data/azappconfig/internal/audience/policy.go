cc80d74c89f0d46f989fc94893fb745065d56983
Lingling Ye (from Dev Box) <linglingye@microsoft.com>
2025-12-17 15:06:21 +0800

    add audience error hanlding policy

---
+++ sdk/data/azappconfig/internal/audience/policy.go
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package audience
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+)
+
+const AadAudienceErrorCode = "AADSTS500011"
+
+type AudienceErrorHandlingPolicy struct {
+	AudienceConfigured bool
+}
+
+func NewAudienceErrorHandlingPolicy(audienceConfigured bool) *AudienceErrorHandlingPolicy {
+	return &AudienceErrorHandlingPolicy{
+		AudienceConfigured: audienceConfigured,
+	}
+}
+
+func (p *AudienceErrorHandlingPolicy) Do(req *policy.Request) (*http.Response, error) {
+    resp, err := req.Next()
+    if err != nil {
+        if strings.Contains(err.Error(), AadAudienceErrorCode) {
+            if p.AudienceConfigured {
+                return nil, errors.New("unable to authenticate to Azure App Configuration. An incorrect token audience was provided. Please set AppConfigurationClientOptions.audience to the appropriate audience for the target cloud. For details on how to configure the authentication token audience visit https://aka.ms/appconfig/client-token-audience")
+            } else {
+                return nil, errors.New("unable to authenticate to Azure App Configuration. No authentication token audience was provided. Please set AppConfigurationClientOptions.audience to the appropriate audience for the target cloud. For details on how to configure the authentication token audience visit https://aka.ms/appconfig/client-token-audience")
+            }
+        }
+        return nil, err
+    }
+    
+    return resp, nil
+}

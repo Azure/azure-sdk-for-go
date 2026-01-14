d983fa145793c31d43a6116d52d9fb17a2ddd1aa
Lingling Ye (from Dev Box) <linglingye@microsoft.com>
2026-01-14 11:17:29 +0800

    update

--- sdk/data/azappconfig/internal/audience/policy.go
+++ sdk/data/azappconfig/internal/audience/policy.go
@@ -4,7 +4,7 @@
 package audience
 
 import (
-	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -28,9 +28,9 @@ func (p *AudienceErrorHandlingPolicy) Do(req *policy.Request) (*http.Response, e
 	if err != nil {
 		if strings.Contains(err.Error(), aadAudienceErrorCode) {
 			if p.AudienceConfigured {
-				return nil, errors.New("unable to authenticate to Azure App Configuration. An incorrect token audience was provided. Please set ClientOptions.Cloud for the target cloud . For details on how to configure the authentication token audience visit https://aka.ms/appconfig/client-token-audience and examples in https://github.com/Azure/azure-sdk-for-go/blob/main/sdk/data/azappconfig/cloud_config.go")
+				return nil, fmt.Errorf("unable to authenticate to Azure App Configuration. An incorrect token audience was provided. Please set ClientOptions.Cloud for the target cloud . For details on how to configure the authentication token audience visit https://aka.ms/appconfig/client-token-audience. %w", err)
 			} else {
-				return nil, errors.New("unable to authenticate to Azure App Configuration. No authentication token audience was provided. Please set ClientOptions.Cloud for the target cloud. For details on how to configure the authentication token audience visit https://aka.ms/appconfig/client-token-audience and examples in https://github.com/Azure/azure-sdk-for-go/blob/main/sdk/data/azappconfig/cloud_config.go")
+				return nil, fmt.Errorf("unable to authenticate to Azure App Configuration. No authentication token audience was provided. Please set ClientOptions.Cloud for the target cloud. For details on how to configure the authentication token audience visit https://aka.ms/appconfig/client-token-audience. %w", err)
 			}
 		}
 		return nil, err

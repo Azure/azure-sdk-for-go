c994006c13a50bfe1ba8c962180201127135a37b
judy.liu <v-liujudy@microsoft.com>
2026-01-23 10:54:28 +0800

    fix ci failure

--- sdk/resourcemanager/redhatopenshift/armredhatopenshift/go.mod
+++ sdk/resourcemanager/redhatopenshift/armredhatopenshift/go.mod
@@ -5,7 +5,6 @@ go 1.24.0
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.21.0
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.10.1
-	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/redhatopenshift/armredhatopenshift v1.6.0
 )
 
 require (
--- sdk/resourcemanager/redhatopenshift/armredhatopenshift/openshiftversions_client_example_test.go
+++ sdk/resourcemanager/redhatopenshift/armredhatopenshift/openshiftversions_client_example_test.go
@@ -11,7 +11,7 @@ import (
 	"log"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/redhatopenshift/armredhatopenshift"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/redhatopenshift/armredhatopenshift/v2"
 )
 
 // Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/6e34caed36815fc876c8e8c0371db76f809e52e8/specification/redhatopenshift/resource-manager/Microsoft.RedHatOpenShift/OpenShiftClusters/stable/2025-07-25/examples/OpenShiftVersions_List.json

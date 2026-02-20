5957551333ae9e2ede6e6deb2c20e4dc00c03291
REDMOND\wiboris <wiboris@microsoft.com>
2026-02-20 15:55:16 -0800

    updating tests

--- sdk/resourcemanager/batch/armbatch/batchmanagement_live_test.go
+++ sdk/resourcemanager/batch/armbatch/batchmanagement_live_test.go
@@ -166,14 +166,14 @@ func (testsuite *BatchManagementTestSuite) Prepare() {
 	fmt.Println("Call operation: Application_Create")
 	applicationClient, err := armbatch.NewApplicationClient(testsuite.subscriptionId, testsuite.cred, testsuite.options)
 	testsuite.Require().NoError(err)
-	_, err = applicationClient.Create(testsuite.ctx, testsuite.resourceGroupName, testsuite.accountName, testsuite.applicationName, &armbatch.ApplicationClientCreateOptions{
-		Parameters: &armbatch.Application{
-			Properties: &armbatch.ApplicationProperties{
-				AllowUpdates: to.Ptr(false),
-				DisplayName:  to.Ptr("myAppName"),
-			},
+	_, err = applicationClient.Create(testsuite.ctx, testsuite.resourceGroupName, testsuite.accountName, testsuite.applicationName, armbatch.Application{
+		Properties: &armbatch.ApplicationProperties{
+			AllowUpdates: to.Ptr(false),
+			DisplayName:  to.Ptr("myAppName"),
 		},
-	})
+	},
+		&armbatch.ApplicationClientCreateOptions{},
+	)
 	testsuite.Require().NoError(err)
 }
 
@@ -398,7 +398,7 @@ func (testsuite *BatchManagementTestSuite) TestApplicationPackage() {
 	fmt.Println("Call operation: ApplicationPackage_Create")
 	applicationPackageClient, err := armbatch.NewApplicationPackageClient(testsuite.subscriptionId, testsuite.cred, testsuite.options)
 	testsuite.Require().NoError(err)
-	_, err = applicationPackageClient.Create(testsuite.ctx, testsuite.resourceGroupName, testsuite.accountName, testsuite.applicationName, testsuite.versionName, &armbatch.ApplicationPackageClientCreateOptions{})
+	_, err = applicationPackageClient.Create(testsuite.ctx, testsuite.resourceGroupName, testsuite.accountName, testsuite.applicationName, testsuite.versionName, armbatch.ApplicationPackage{}, &armbatch.ApplicationPackageClientCreateOptions{})
 	testsuite.Require().NoError(err)
 
 	// From step ApplicationPackage_List

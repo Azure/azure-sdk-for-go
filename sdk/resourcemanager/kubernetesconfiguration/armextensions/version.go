ae5dc881fd0abba565d41eefae9da91b7297958a
Chenjie Shi <tadelesh.shi@live.cn>
2026-03-11 17:15:31 +0800

    Update version.go

--- sdk/resourcemanager/kubernetesconfiguration/armextensions/version.go
+++ sdk/resourcemanager/kubernetesconfiguration/armextensions/version.go
@@ -6,5 +6,5 @@ package armextensions
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/kubernetesconfiguration/armextensions"
-	moduleVersion = "v1.0.0"
+	moduleVersion = "v0.1.0"
 )

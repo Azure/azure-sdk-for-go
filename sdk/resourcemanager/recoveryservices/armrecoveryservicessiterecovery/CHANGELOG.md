2b34a0a03ff2f5cec035cda6489e8c9f531ce168
judy <1002946066@qq.com>
2026-03-11 13:50:06 +0800

    update version

--- sdk/resourcemanager/recoveryservices/armrecoveryservicessiterecovery/CHANGELOG.md
+++ sdk/resourcemanager/recoveryservices/armrecoveryservicessiterecovery/CHANGELOG.md
@@ -1,6 +1,6 @@
 # Release History
 
-## 3.0.0 (2026-03-09)
+## 3.0.0-beta.1 (2026-03-09)
 ### Breaking Changes
 
 - Struct `ApplianceQueryParameter` has been removed
--- sdk/resourcemanager/recoveryservices/armrecoveryservicessiterecovery/version.go
+++ sdk/resourcemanager/recoveryservices/armrecoveryservicessiterecovery/version.go
@@ -6,5 +6,5 @@ package armrecoveryservicessiterecovery
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/recoveryservices/armrecoveryservicessiterecovery"
-	moduleVersion = "v3.0.0"
+	moduleVersion = "v3.0.0-beta.1"
 )

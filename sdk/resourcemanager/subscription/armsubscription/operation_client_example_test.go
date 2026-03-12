b1daaee67df4909fd3ccba6633ab58a090af6cd7
judy <1002946066@qq.com>
2026-03-12 14:05:24 +0800

    add code

--- sdk/resourcemanager/subscription/armsubscription/operation_client_example_test.go
+++ sdk/resourcemanager/subscription/armsubscription/operation_client_example_test.go
@@ -11,8 +11,8 @@ import (
 	"log"
 )
 
-// Generated from example definition: 2025-11-01-preview/getBillingAccountPolicy.json
-func ExampleBillingAccountClient_GetPolicy() {
+// Generated from example definition: 2025-11-01-preview/getSubscriptionOperation.json
+func ExampleOperationClient_BeginGet() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
@@ -22,27 +22,17 @@ func ExampleBillingAccountClient_GetPolicy() {
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := clientFactory.NewBillingAccountClient().GetPolicy(ctx, "testBillingAccountId", nil)
+	poller, err := clientFactory.NewOperationClient().BeginGet(ctx, "e4b8d068-f574-462a-a76f-6fa0afc613c9", nil)
 	if err != nil {
 		log.Fatalf("failed to finish the request: %v", err)
+	}
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
 	}
 	// You could use response here. We use blank identifier for just demo purposes.
 	_ = res
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
-	// res = armsubscription.BillingAccountClientGetPolicyResponse{
-	// 	BillingAccountPoliciesResponse: &armsubscription.BillingAccountPoliciesResponse{
-	// 		Name: to.Ptr("testBillingAccountId"),
-	// 		Type: to.Ptr("Microsoft.Subscription/policies"),
-	// 		ID: to.Ptr("/providers/Microsoft.Subscription/Policies/policyForBillingAccount"),
-	// 		Properties: &armsubscription.BillingAccountPoliciesResponseProperties{
-	// 			AllowTransfers: to.Ptr(true),
-	// 			ServiceTenants: []*armsubscription.ServiceTenantResponse{
-	// 				{
-	// 					TenantID: to.Ptr("b8ed2088-c458-4e77-bd61-9e048d96a1c0"),
-	// 					TenantName: to.Ptr("testServiceTenant"),
-	// 				},
-	// 			},
-	// 		},
-	// 	},
+	// res = armsubscription.OperationClientGetResponse{
 	// }
 }

2d4d25cc65e330f06391a547e140ab67524dfea7
azure-sdk <azuresdk@microsoft.com>
2026-03-19 10:02:21 +0000

    Configurations:  'specification/cdn/resource-manager/Microsoft.Cdn/Cdn/tspconfig.yaml', API Version: 6/1/2025, SDK Release Type: beta, and CommitSHA: 'aa822c9c01b9e83fbc8ad8ec4c308203a3c29287' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6034386 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/resourcemanager/cdn/armcdn/management_client_example_test.go
+++ sdk/resourcemanager/cdn/armcdn/management_client_example_test.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cdn/armcdn/v2"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cdn/armcdn/v3"
 )
 
 // Generated from example definition: https://github.com/Azure/azure-rest-api-specs/blob/92de53a5f1e0e03c94b40475d2135d97148ed014/specification/cdn/resource-manager/Microsoft.Cdn/stable/2024-02-01/examples/CheckEndpointNameAvailability.json

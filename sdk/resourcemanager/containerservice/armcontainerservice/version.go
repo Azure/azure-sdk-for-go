902ea8b16a61b5d5dd616134f582a5d4ad9eb169
azure-sdk <azuresdk@microsoft.com>
2026-03-23 05:20:07 +0000

    Configurations:  'specification/containerservice/resource-manager/Microsoft.ContainerService/aks/tspconfig.yaml', API Version: 2026-01-01, SDK Release Type: stable, and CommitSHA: '947878feb7a528ff4344f8fe67ab85522afcbb79' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6046261 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/resourcemanager/containerservice/armcontainerservice/version.go
+++ sdk/resourcemanager/containerservice/armcontainerservice/version.go
@@ -6,5 +6,5 @@ package armcontainerservice
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice"
-	moduleVersion = "v9.0.0"
+	moduleVersion = "v9.0.1"
 )

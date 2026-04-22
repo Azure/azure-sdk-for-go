3108f18912baa6d2fad19551a5c07c4a5df2dea2
azure-sdk <azuresdk@microsoft.com>
2026-04-22 05:55:23 +0000

    Configurations:  'specification/computelimit/resource-manager/Microsoft.ComputeLimit/ComputeLimit/tspconfig.yaml', API Version: 2026-04-30, SDK Release Type: stable, and CommitSHA: '4850dea187a72ae7ceaf0bd61b2e4a4c4b4a3b3e' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6190740 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/resourcemanager/computelimit/armcomputelimit/version.go
+++ sdk/resourcemanager/computelimit/armcomputelimit/version.go
@@ -6,5 +6,5 @@ package armcomputelimit
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/computelimit/armcomputelimit"
-	moduleVersion = "v1.0.0"
+	moduleVersion = "v1.1.0"
 )

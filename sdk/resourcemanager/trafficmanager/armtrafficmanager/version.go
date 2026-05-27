80e4c687ef68c7871f97abf2f4747baad125d37c
Chenjie Shi <tadelesh.shi@live.cn>
2026-05-27 09:59:27 +0800

    Update version.go

--- sdk/resourcemanager/trafficmanager/armtrafficmanager/version.go
+++ sdk/resourcemanager/trafficmanager/armtrafficmanager/version.go
@@ -6,5 +6,5 @@ package armtrafficmanager
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/trafficmanager/armtrafficmanager"
-	moduleVersion = "v2.0.0-beta.2"
+	moduleVersion = "v2.0.0-beta.1"
 )

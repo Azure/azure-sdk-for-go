1be317078fbc0e9f6c1810b1d15bf1dddf008bb8
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2026-08-24 15:05:06 +0800

    Update version.go

--- sdk/resourcemanager/hybridconnectivity/armhybridconnectivity/version.go
+++ sdk/resourcemanager/hybridconnectivity/armhybridconnectivity/version.go
@@ -6,5 +6,5 @@ package armhybridconnectivity
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridconnectivity/armhybridconnectivity"
-	moduleVersion = "v2.0.0"
+	moduleVersion = "v1.2.0"
 )

85be73bf7a2fbbc40bcfc64a408ee707a01c867d
Johnathan W <j7n.w4r@gmail.com>
2026-08-24 13:40:38 -0400

    fix(azservicebus): restore retry delay behavior

--- sdk/messaging/azservicebus/internal/utils/retrier.go
+++ sdk/messaging/azservicebus/internal/utils/retrier.go
@@ -125,7 +125,7 @@ func calcDelay(o exported.RetryOptions, try int32) time.Duration { // try is >=1
 	}
 
 	delay := factor * o.RetryDelay
-	if o.RetryDelay > 0 && delay/o.RetryDelay != factor {
+	if delay < factor {
 		// overflow has happened so set to max value
 		delay = time.Duration(math.MaxInt64)
 	}

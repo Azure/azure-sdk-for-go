a0641b890b30217e7ff3849723476f609eb9abdf
azure-sdk <azuresdk@microsoft.com>
2026-09-14 06:25:03 +0000

    Configurations:  'specification/resources/resource-manager/Microsoft.Authorization/policy/tspconfig.yaml', API Version: 2026-07-01, SDK Release Type: stable, and CommitSHA: '530ebfb31d6cf1b28f2c475013b0657d1a01c2a6' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6830986 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/resourcemanager/resources/armpolicy/fake/tokens_server.go
+++ sdk/resourcemanager/resources/armpolicy/fake/tokens_server.go
@@ -155,7 +155,7 @@ func (t *TokensServerTransport) dispatchAcquireAtResourceGroup(req *http.Request
 	if t.srv.AcquireAtResourceGroup == nil {
 		return nil, &nonRetriableError{errors.New("fake for method AcquireAtResourceGroup not implemented")}
 	}
-	const regexStr = `/subscriptions/(?P<subscriptionId>[!#&$-;=?-\[\]_a-zA-Z0-9~%@]+)/resourceGroups/(?P<resourceGroupName>[!#&$-;=?-\[\]_a-zA-Z0-9~%@]+)/providers/Microsoft\.Authorization/acquirePolicyToken`
+	const regexStr = `/subscriptions/(?P<subscriptionId>[a-zA-Z0-9._~%!$&'()*+,;=:@-]+)/resourceGroups/(?P<resourceGroupName>[a-zA-Z0-9._~%!$&'()*+,;=:@-]+)/providers/Microsoft\.Authorization/acquirePolicyToken`
 	regex := regexp.MustCompile(regexStr)
 	matches := regex.FindStringSubmatch(req.URL.EscapedPath())
 	if len(matches) < 3 {
--- sdk/resourcemanager/resources/armpolicy/tokens_client.go
+++ sdk/resourcemanager/resources/armpolicy/tokens_client.go
@@ -189,7 +189,7 @@ func (client *TokensClient) AcquireAtResourceGroup(ctx context.Context, resource
 func (client *TokensClient) acquireAtResourceGroupCreateRequest(ctx context.Context, resourceGroupName string, parameters TokenRequest, _ *TokensClientAcquireAtResourceGroupOptions) (*policy.Request, error) {
 	urlPath := "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Authorization/acquirePolicyToken"
 	if client.subscriptionID == "" {
-		return nil, errors.New("parameter client.subscriptionID cannot be empty")
+		return nil, errors.New("parameter subscriptionID cannot be empty")
 	}
 	urlPath = strings.ReplaceAll(urlPath, "{subscriptionId}", url.PathEscape(client.subscriptionID))
 	if resourceGroupName == "" {

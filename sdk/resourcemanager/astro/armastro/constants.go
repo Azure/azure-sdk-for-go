c270d7f58a31a4de341e84f6ef9eb007e770df91
Sweety <switi.mhaiske@espressif.com>
2026-09-23 00:35:45 +0530

    Deprecate sdk/resourcemanager/astro/armastro (Astronomer.Astro retirement) (#27566)

--- sdk/resourcemanager/astro/armastro/constants.go
+++ sdk/resourcemanager/astro/armastro/constants.go
@@ -7,7 +7,7 @@ package armastro
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/astro/armastro"
-	moduleVersion = "v0.1.0"
+	moduleVersion = "v0.1.1"
 )
 
 // ActionType - Enum. Indicates the action type. "Internal" refers to actions that are for internal only APIs.
